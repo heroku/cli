45e8fa8e12d7cbff5c10ddda3e09fa7085398818
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2014-12-20 07:07:44 -0800

    use heroku-cli for version

--- version.go
+++ version.go
@@ -12,7 +12,7 @@ var version = &Topic{
 		{
 			ShortHelp: "print the version",
 			Run: func(ctx *Context) {
-				fmt.Printf("heroku-toolbelt/%s (%s-%s) %s\n", Version, runtime.GOARCH, runtime.GOOS, runtime.Version())
+				fmt.Printf("heroku-cli/%s (%s-%s) %s\n", Version, runtime.GOARCH, runtime.GOOS, runtime.Version())
 				if Channel != "master" {
 					fmt.Printf("Channel: %s\n", Channel)
 				}
