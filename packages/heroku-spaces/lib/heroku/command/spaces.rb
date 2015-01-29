d696c1e13d82cac21cf577b113e614f43ea68aa2
Ryan Brainard <brainard@heroku.com>
2015-01-28 20:38:10 -0800

    Add confirmation to space destroy

--- packages/heroku-spaces/lib/heroku/command/spaces.rb
+++ packages/heroku-spaces/lib/heroku/command/spaces.rb
@@ -80,6 +80,7 @@ class Heroku::Command::Spaces < Heroku::Command::Base
     require_argument! :space
     validate_arguments!
 
+    confirm
     action("Destroying space #{options[:space]}") do
       @space = api.delete_space(options[:space]).body
     end
