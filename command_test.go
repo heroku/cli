685263fd6c89e4dbbba221c653af3b6982dbea22
dickeyxxx <jeff@dickeyxxx.com>
2015-01-29 15:25:56 -0800

    fixed command tests

--- command_test.go
+++ command_test.go
@@ -1,29 +1,26 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
 	"testing"
 
 	c "github.com/smartystreets/goconvey/convey"
 )
 
 func TestCommand(t *testing.T) {
-	c.Convey("Usage()", t, func() {
+	c.Convey("commandUsage()", t, func() {
 		c.Convey("basic", func() {
 			cmd := &Command{
 				Topic:   "apps",
 				Command: "info",
 			}
-			c.So(cmd.Usage(), c.ShouldEqual, "apps:info")
+			c.So(commandUsage(cmd), c.ShouldEqual, "apps:info")
 		})
 		c.Convey("topic root command", func() {
 			cmd := &Command{
 				Topic:   "apps",
 				Command: "",
 			}
-			c.So(cmd.Usage(), c.ShouldEqual, "apps")
+			c.So(commandUsage(cmd), c.ShouldEqual, "apps")
 		})
 		c.Convey("with app", func() {
 			cmd := &Command{
@@ -31,7 +28,7 @@ func TestCommand(t *testing.T) {
 				Command:  "info",
 				NeedsApp: true,
 			}
-			c.So(cmd.Usage(), c.ShouldEqual, "apps:info --app APP")
+			c.So(commandUsage(cmd), c.ShouldEqual, "apps:info --app APP")
 		})
 		c.Convey("with required argument", func() {
 			cmd := &Command{
@@ -39,7 +36,7 @@ func TestCommand(t *testing.T) {
 				Command: "info",
 				Args:    []Arg{{Name: "foo"}},
 			}
-			c.So(cmd.Usage(), c.ShouldEqual, "apps:info FOO")
+			c.So(commandUsage(cmd), c.ShouldEqual, "apps:info FOO")
 		})
 		c.Convey("with optional argument", func() {
 			cmd := &Command{
@@ -47,7 +44,7 @@ func TestCommand(t *testing.T) {
 				Command: "info",
 				Args:    []Arg{{Name: "foo", Optional: true}},
 			}
-			c.So(cmd.Usage(), c.ShouldEqual, "apps:info [FOO]")
+			c.So(commandUsage(cmd), c.ShouldEqual, "apps:info [FOO]")
 		})
 		c.Convey("with multiple arguments", func() {
 			cmd := &Command{
@@ -55,7 +52,7 @@ func TestCommand(t *testing.T) {
 				Command: "info",
 				Args:    []Arg{{Name: "foo"}, {Name: "bar"}},
 			}
-			c.So(cmd.Usage(), c.ShouldEqual, "apps:info FOO BAR")
+			c.So(commandUsage(cmd), c.ShouldEqual, "apps:info FOO BAR")
 		})
 		c.Convey("with a flag argument", func() {
 			cmd := &Command{
@@ -63,17 +60,7 @@ func TestCommand(t *testing.T) {
 				Command: "info",
 				Flags:   []Flag{{Name: "foo"}},
 			}
-			c.So(cmd.Usage(), c.ShouldEqual, "apps:info [--foo]")
+			c.So(commandUsage(cmd), c.ShouldEqual, "apps:info [--foo]")
 		})
-	})
-
-	c.Convey("commands", t, func() {
-		stdout := &bytes.Buffer{}
-		Stdout = stdout
-		commandsListCmd.Run(&Context{})
-		var result map[string]interface{}
-		fmt.Println(stdout.String())
-		json.Unmarshal(stdout.Bytes(), &result)
-		c.So(result["commands"], c.ShouldEqual, `{"name":"commads",`)
 	})
 }
