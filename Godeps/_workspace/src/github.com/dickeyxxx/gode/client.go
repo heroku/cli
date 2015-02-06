482016dbde7e6acad1dd8f76ef3a1125d199bfab
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2015-02-06 15:42:01 -0800

    gode 2.0.1

--- Godeps/_workspace/src/github.com/dickeyxxx/gode/client.go
+++ Godeps/_workspace/src/github.com/dickeyxxx/gode/client.go
@@ -13,12 +13,6 @@ const DefaultNodeVersion = "1.1.0"
 // Override this by setting client.Version.
 const DefaultNpmVersion = "2.4.1"
 
-// This is a Cloudfront CDN that mirrors http://iojs.org/dist
-// It is much faster.
-const NodeCDN = "http://d1gnwkq6ucr1xr.cloudfront.net"
-
-// const NodeCDN = "http://d1nhjzpj45o0rc.cloudfront.net" for http://nodejs.org/dist
-
 // Client is the interface between Node and Go.
 // It also setups up the Node environment if needed.
 type Client struct {
@@ -52,13 +46,13 @@ func (c *Client) nodeBase() string {
 func (c *Client) nodeURL() string {
 	switch {
 	case runtime.GOOS == "windows" && runtime.GOARCH == "386":
-		return NodeCDN + "/v" + c.NodeVersion + "/iojs.exe"
+		return "https://iojs.org/dist/v" + c.NodeVersion + "/iojs.exe"
 	case runtime.GOOS == "windows" && runtime.GOARCH == "amd64":
-		return NodeCDN + "/v" + c.NodeVersion + "/x64/iojs.exe"
+		return "https://iojs.org/dist/v" + c.NodeVersion + "/x64/iojs.exe"
 	case runtime.GOARCH == "386":
-		return NodeCDN + "/v" + c.NodeVersion + "/" + c.nodeBase() + ".tar.gz"
+		return "https://iojs.org/dist/v" + c.NodeVersion + "/" + c.nodeBase() + ".tar.gz"
 	default:
-		return NodeCDN + "/v" + c.NodeVersion + "/" + c.nodeBase() + ".tar.gz"
+		return "https://iojs.org/dist/v" + c.NodeVersion + "/" + c.nodeBase() + ".tar.gz"
 	}
 }
 
