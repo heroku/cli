0ffe31ae10951c3183e9d20f1deb9b6d0b5757c0
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2015-02-15 20:36:34 -0800

    added herokuDir

--- context.go
+++ context.go
@@ -4,12 +4,13 @@ package main
 // It contains information about the user's command arguments
 // as well as Heroku information like the auth token and app name (if requested).
 type Context struct {
-	Topic   *Topic            `json:"topic"`
-	Command *Command          `json:"command"`
-	App     string            `json:"app"`
-	Args    map[string]string `json:"args"`
-	Cwd     string            `json:"cwd"`
-	Auth    struct {
+	Topic     *Topic            `json:"topic"`
+	Command   *Command          `json:"command"`
+	App       string            `json:"app"`
+	Args      map[string]string `json:"args"`
+	Cwd       string            `json:"cwd"`
+	HerokuDir string            `json:"herokuDir"`
+	Auth      struct {
 		Username string `json:"username"`
 		Password string `json:"password"`
 	} `json:"auth"`
