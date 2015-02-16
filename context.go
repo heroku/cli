090e11782b53f8284a7799db9a167f8549bcfea8
dickeyxxx <jeff@dickeyxxx.com>
2015-02-15 20:14:47 -0800

    add cwd to js context

--- context.go
+++ context.go
@@ -8,6 +8,7 @@ type Context struct {
 	Command *Command          `json:"command"`
 	App     string            `json:"app"`
 	Args    map[string]string `json:"args"`
+	Cwd     string            `json:"cwd"`
 	Auth    struct {
 		Username string `json:"username"`
 		Password string `json:"password"`
