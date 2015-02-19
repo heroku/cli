09cdfc2a3cfa24cd911744ac02e06628c38f4a19
Raul Barroso <ra.barroso@gmail.com>
2015-02-19 11:51:05 -0800

    Show updates for realz

--- packages/heroku-status/lib/commands/status.js
+++ packages/heroku-status/lib/commands/status.js
@@ -9,22 +9,23 @@ var api = require('request').defaults({
 });
 
 var colors = require('colors');
+var timeago = require('timeago');
+var dateFormat = require('dateformat');
 
-function printStatus(status, incidents) {
+function printStatus(status) {
   var message;
   var color = colors.green;
   switch(status) {
     case 'green':
-      color = colors.green;
       message =  'No known issues at this time.';
       break;
     case 'red':
       color = colors.red;
-      // TODO: Grab message from incidents, and do something with it.
+      message = 'Red';
       break;
     case 'yellow': // ?
       color = color.yellow;
-      // TODO: Grab message from incidents, and do something with it.
+      message = 'Yellow';
       break;
   }
   return (color(message));
@@ -44,8 +45,17 @@ module.exports = {
     }, function (err, _, response) {
       if (err) { throw err; }
       console.log('=== Heroku Status');
-      console.log('Development: %s', printStatus(response.status.Development, response.issues));
-      console.log('Production: %s', printStatus(response.status.Production, response.issues));
+      console.log('Development: %s', printStatus(response.status.Development));
+      console.log('Production: %s', printStatus(response.status.Production));
+
+      response.issues.forEach(function(incident) {
+        console.log('\n=== %s %s', incident.title, dateFormat(incident.created_at, "UTC:h:MM:ss TT Z"));
+
+        incident.updates.forEach(function(update) {
+          var updateType = update.update_type.substr(0, 1).toUpperCase() + update.update_type.substr(1); // Capitalize first letter
+          console.log('%s (%s) [%s] %s\n', dateFormat(incident.created_at, "UTC:h:MM:ss TT Z"), timeago(update.updated_at), updateType, update.contents)
+        });
+      });
     });
   }
 }
