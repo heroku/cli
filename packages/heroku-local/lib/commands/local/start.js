dcb5fbaa3a6d3527cd4ebd3d2802c5bd62d2c38b
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2015-02-26 17:42:38 -0800

    finished start command

--- packages/heroku-local/lib/commands/local/start.js
+++ packages/heroku-local/lib/commands/local/start.js
@@ -1,101 +1,29 @@
 'use strict';
-let fs      = require('fs');
-let path    = require('path');
-let request = require('request');
-let spawn   = require('child_process').spawn;
-
-const foregoVersion = '0.16.1';
-let foregoFilename = `forego-${foregoVersion}`;
-
-if (process.platform === 'windows') {
-  foregoFilename = `forego-${foregoVersion}.exe`;
-}
-
-function foregoURL() {
-  let arch, platform;
-  let filename = 'forego';
-  switch (process.arch) {
-    case 'x64':
-      arch = 'amd64';
-    break;
-    case 'ia32':
-      arch = '386';
-    break;
-    default:
-      throw new Error(`Unsupported architecture: ${process.arch}`);
-  }
-  switch (process.platform) {
-    case 'darwin':
-      platform = 'darwin';
-    break;
-    case 'linux':
-      platform = 'linux';
-    break;
-    case 'win32':
-      platform = 'windows';
-      filename = 'forego.exe';
-    break;
-    default:
-      throw new Error(`Unsupported architecture: ${process.arch}`);
-  }
-  return `https://godist.herokuapp.com/projects/ddollar/forego/releases/${foregoVersion}/${platform}-${arch}/${filename}`;
-}
+let Forego  = require('../../forego');
 
 function handleErr (err) {
   console.error(err.stack);
   process.exit(1);
 }
 
-function downloadForego (path, cb) {
-  process.stderr.write(`Downloading ${foregoFilename} to ${path}... `);
-  request(foregoURL(), function (err) {
-    if (err) { handleErr(err); }
-    console.error('done');
-    // for some reason this seems necessary
-    setTimeout(cb, 500);
-  })
-  .pipe(fs.createWriteStream(path, {mode: 0o0755}));
-}
-
-function ensureSetup (path, cb) {
-  fs.open(path, 'r', function (err) {
-    if (err) {
-      downloadForego(path, cb);
-    } else {
-      cb();
-    }
-  });
-}
-
-function start (opts) {
-  let args = ['start'];
-  if (opts.args.processname) {
-    args.push(opts.args.processname);
-  }
-  console.log(opts.args);
-  spawn(opts.path, args, {
-    cwd: opts.cwd,
-    stdio: [0, 1, 2]
-  });
-}
-
 module.exports = {
   topic: 'local',
   command: 'start',
   description: 'run heroku app locally',
-  help: 'TODO',
+  help: 'Start the application specified by a Procfile (defaults to ./Procfile)',
   args: [{name: 'processname', optional: true}],
   flags: [
     {name: 'procfile', char: 'f', hasValue: true},
     {name: 'env', char: 'e', hasValue: true},
     {name: 'concurrency', char: 'c', hasValue: true},
     {name: 'port', char: 'p', hasValue: true},
-    {name: 'r', char: 'r', hasValue: true}
+    {name: 'r', char: 'r', hasValue: false}
   ],
   run: function (ctx) {
-    var foregoPath = path.join(ctx.herokuDir, foregoFilename);
-    ensureSetup(foregoPath, function () {
-      start({path: foregoPath, cwd: ctx.cwd, args: ctx.args});
+    let forego = new Forego(ctx.herokuDir);
+    forego.ensureSetup(function (err) {
+      if (err) { handleErr(err); }
+      forego.start({cwd: ctx.cwd, args: ctx.args});
     });
   }
 };
---
+++ packages/heroku-local/lib/forego.js
@@ -0,0 +1,100 @@
+'use strict';
+let fs      = require('fs');
+let path    = require('path');
+let request = require('request');
+let spawn   = require('child_process').spawn;
+
+const foregoVersion = '0.16.1';
+
+function Forego(dir) {
+  this.dir = dir;
+  this.filename = `forego-${foregoVersion}`;
+
+  if (process.platform === 'windows') {
+    this.filename = `forego-${foregoVersion}.exe`;
+  }
+
+  this.path = path.join(this.dir, this.filename);
+
+}
+
+Forego.prototype = {
+  start: function (opts) {
+    let args = ['start'];
+    if (opts.args.processname) {
+      args.push(opts.args.processname);
+    }
+    if (opts.args.procfile) {
+      args.push('-f', opts.args.procfile);
+    }
+    if (opts.args.env) {
+      args.push('-e', opts.args.env);
+    }
+    if (opts.args.concurrency) {
+      args.push('-c', opts.args.concurrency);
+    }
+    if (opts.args.port) {
+      args.push('-p', opts.args.port);
+    }
+    if (opts.args.r) {
+      args.push('-r');
+    }
+    spawn(this.path, args, {
+      cwd: opts.cwd,
+      stdio: [0, 1, 2]
+    });
+  },
+
+  ensureSetup: function (cb) {
+    fs.open(this.path, 'r', function (err) {
+      if (err) {
+        this.download(cb);
+      } else {
+        cb();
+      }
+    });
+  },
+
+  download: function (cb) {
+    process.stderr.write(`Downloading ${this.filename} ${this.dir}... `);
+    request(this.foregoURL(), function (err) {
+      if (err) { cb(err); }
+      console.error('done');
+      // for some reason this seems necessary
+      setTimeout(cb, 500);
+    })
+    .pipe(fs.createWriteStream(this.path, {mode: 0o0755}));
+  },
+
+  url: function() {
+    let arch, platform;
+    let filename = 'forego';
+    switch (process.arch) {
+      case 'x64':
+        arch = 'amd64';
+      break;
+      case 'ia32':
+        arch = '386';
+      break;
+      default:
+        throw new Error(`Unsupported architecture: ${process.arch}`);
+    }
+    switch (process.platform) {
+      case 'darwin':
+        platform = 'darwin';
+      break;
+      case 'linux':
+        platform = 'linux';
+      break;
+      case 'win32':
+        platform = 'windows';
+        filename = 'forego.exe';
+      break;
+      default:
+        throw new Error(`Unsupported architecture: ${process.arch}`);
+    }
+    return `https://godist.herokuapp.com/projects/ddollar/forego/releases/${foregoVersion}/${platform}-${arch}/${filename}`;
+  }
+};
+
+module.exports = Forego;
