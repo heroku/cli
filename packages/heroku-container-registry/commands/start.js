884612fbd7f4ff7b68e029b74a9c3ee89ee7783f
Hunter Loftis <hunter@hunterloftis.com>
2015-04-28 16:15:32 -0700

    Merge pull request #34 from heroku/output-url-on-start
    log web process URL on docker:start

--- packages/heroku-container-registry/commands/start.js
+++ packages/heroku-container-registry/commands/start.js
@@ -1,5 +1,7 @@
 var path = require('path');
 var child = require('child_process');
+var url = require('url');
+var colors = require('colors');
 var docker = require('../lib/docker');
 var state = require('../lib/state');
 var envutil = require('../lib/env-util');
@@ -21,9 +23,15 @@ module.exports = function(topic) {
 
 function startImage(imageId, cwd) {
   if (!imageId) return;
-  console.log('starting image...');
+  console.log('\nstarting image...');
+  console.log('web process will be available at', colors.yellow.underline(getURL()));
   var envArgComponent = envutil.getFormattedEnvArgComponent(cwd);
   child.execSync(`docker run ${envArgComponent} -p 3000:3000 --rm -it ${imageId} || true`, {
     stdio: [0, 1, 2]
   });
+}
+
+function getURL() {
+  var host = url.parse(process.env.DOCKER_HOST).hostname;
+  return `http://${host}:3000/`;
 }
