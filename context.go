3b00e6eb16752110d5e8ad5ca6d55637bf3d189a
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2015-04-29 15:09:45 -0700

    added flags to context and refactored arg parsing

--- context.go
+++ context.go
@@ -4,13 +4,14 @@ package main
 // It contains information about the user's command arguments
 // as well as Heroku information like the auth token and app name (if requested).
 type Context struct {
-	Topic     *Topic      `json:"topic"`
-	Command   *Command    `json:"command"`
-	App       string      `json:"app"`
-	Args      interface{} `json:"args"`
-	Cwd       string      `json:"cwd"`
-	HerokuDir string      `json:"herokuDir"`
-	Debug     bool        `json:"debug"`
+	Topic     *Topic            `json:"topic"`
+	Command   *Command          `json:"command"`
+	App       string            `json:"app"`
+	Args      interface{}       `json:"args"`
+	Flags     map[string]string `json:"flags"`
+	Cwd       string            `json:"cwd"`
+	HerokuDir string            `json:"herokuDir"`
+	Debug     bool              `json:"debug"`
 	Auth      struct {
 		Username string `json:"username"`
 		Password string `json:"password"`
