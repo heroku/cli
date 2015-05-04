95bdab276a3e21bd7e3521b1a7a0b2077b768c79
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2015-05-04 16:38:38 -0700

    updated dependencies

--- Godeps/_workspace/src/github.com/franela/goreq/goreq.go
+++ Godeps/_workspace/src/github.com/franela/goreq/goreq.go
@@ -3,7 +3,6 @@ package goreq
 import (
 	"bufio"
 	"bytes"
-	"compress/flate"
 	"compress/gzip"
 	"compress/zlib"
 	"crypto/tls"
@@ -52,11 +51,9 @@ type compression struct {
 }
 
 type Response struct {
-	Uri           string
-	StatusCode    int
-	ContentLength int64
-	Body          *Body
-	Header        http.Header
+	*http.Response
+	Uri  string
+	Body *Body
 }
 
 type headerTuple struct {
@@ -121,22 +118,16 @@ func Gzip() *compression {
 
 func Deflate() *compression {
 	reader := func(buffer io.Reader) (io.ReadCloser, error) {
-		return flate.NewReader(buffer), nil
-	}
-	writer := func(buffer io.Writer) (io.WriteCloser, error) {
-		return flate.NewWriter(buffer, -1)
-	}
-	return &compression{writer: writer, reader: reader, ContentEncoding: "deflate"}
-}
-
-func Zlib() *compression {
-	reader := func(buffer io.Reader) (io.ReadCloser, error) {
 		return zlib.NewReader(buffer)
 	}
 	writer := func(buffer io.Writer) (io.WriteCloser, error) {
 		return zlib.NewWriter(buffer), nil
 	}
 	return &compression{writer: writer, reader: reader, ContentEncoding: "deflate"}
+}
+
+func Zlib() *compression {
+	return Deflate()
 }
 
 func paramParse(query interface{}) (string, error) {
@@ -250,8 +241,6 @@ func (r Request) WithCookie(c *http.Cookie) Request {
 }
 
 func (r Request) Do() (*Response, error) {
-	var req *http.Request
-	var er error
 	var transport = defaultTransport
 	var client = defaultClient
 	var resUri string
@@ -311,65 +300,11 @@ func (r Request) Do() (*Response, error) {
 		transport.TLSClientConfig.InsecureSkipVerify = false
 	}
 
-	b, e := prepareRequestBody(r.Body)
-	if e != nil {
-		// there was a problem marshaling the body
-		return nil, &Error{Err: e}
-	}
+	req, err := r.NewRequest()
 
-	if r.QueryString != nil {
-		param, e := paramParse(r.QueryString)
-		if e != nil {
-			return nil, &Error{Err: e}
-		}
-		r.Uri = r.Uri + "?" + param
-	}
-
-	var bodyReader io.Reader
-	if b != nil && r.Compression != nil {
-		buffer := bytes.NewBuffer([]byte{})
-		readBuffer := bufio.NewReader(b)
-		writer, err := r.Compression.writer(buffer)
-		if err != nil {
-			return nil, &Error{Err: err}
-		}
-		_, e = readBuffer.WriteTo(writer)
-		writer.Close()
-		if e != nil {
-			return nil, &Error{Err: e}
-		}
-		bodyReader = buffer
-	} else {
-		bodyReader = b
-	}
-	req, er = http.NewRequest(r.Method, r.Uri, bodyReader)
-
-	if er != nil {
+	if err != nil {
 		// we couldn't parse the URL.
-		return nil, &Error{Err: er}
-	}
-
-	// add headers to the request
-	req.Host = r.Host
-
-	r.addHeaders(req.Header)
-	if r.Compression != nil {
-		req.Header.Add("Content-Encoding", r.Compression.ContentEncoding)
-		req.Header.Add("Accept-Encoding", r.Compression.ContentEncoding)
-	}
-	if r.headers != nil {
-		for _, header := range r.headers {
-			req.Header.Add(header.name, header.value)
-		}
-	}
-
-	//use basic auth if required
-	if r.BasicAuthUsername != "" {
-		req.SetBasicAuth(r.BasicAuthUsername, r.BasicAuthPassword)
-	}
-
-	for _, c := range r.cookies {
-		req.AddCookie(c)
+		return nil, &Error{Err: err}
 	}
 
 	timeout := false
@@ -409,7 +344,7 @@ func (r Request) Do() (*Response, error) {
 		var response *Response
 		//If redirect fails we still want to return response data
 		if redirectFailed {
-			response = &Response{Uri: resUri, StatusCode: res.StatusCode, ContentLength: res.ContentLength, Header: res.Header, Body: &Body{reader: res.Body}}
+			response = &Response{res, resUri, &Body{reader: res.Body}}
 		}
 
 		//If redirect fails and we haven't set a redirect count we shouldn't return an error
@@ -425,9 +360,9 @@ func (r Request) Do() (*Response, error) {
 		if err != nil {
 			return nil, &Error{Err: err}
 		}
-		return &Response{Uri: resUri, StatusCode: res.StatusCode, ContentLength: res.ContentLength, Header: res.Header, Body: &Body{reader: res.Body, compressedReader: compressedReader}}, nil
+		return &Response{res, resUri, &Body{reader: res.Body, compressedReader: compressedReader}}, nil
 	} else {
-		return &Response{Uri: resUri, StatusCode: res.StatusCode, ContentLength: res.ContentLength, Header: res.Header, Body: &Body{reader: res.Body}}, nil
+		return &Response{res, resUri, &Body{reader: res.Body}}, nil
 	}
 }
 
@@ -437,6 +372,69 @@ func (r Request) addHeaders(headersMap http.Header) {
 	}
 	headersMap.Add("Accept", r.Accept)
 	headersMap.Add("Content-Type", r.ContentType)
+}
+
+func (r Request) NewRequest() (*http.Request, error) {
+
+	b, e := prepareRequestBody(r.Body)
+	if e != nil {
+		// there was a problem marshaling the body
+		return nil, &Error{Err: e}
+	}
+
+	if r.QueryString != nil {
+		param, e := paramParse(r.QueryString)
+		if e != nil {
+			return nil, &Error{Err: e}
+		}
+		r.Uri = r.Uri + "?" + param
+	}
+
+	var bodyReader io.Reader
+	if b != nil && r.Compression != nil {
+		buffer := bytes.NewBuffer([]byte{})
+		readBuffer := bufio.NewReader(b)
+		writer, err := r.Compression.writer(buffer)
+		if err != nil {
+			return nil, &Error{Err: err}
+		}
+		_, e = readBuffer.WriteTo(writer)
+		writer.Close()
+		if e != nil {
+			return nil, &Error{Err: e}
+		}
+		bodyReader = buffer
+	} else {
+		bodyReader = b
+	}
+
+	req, err := http.NewRequest(r.Method, r.Uri, bodyReader)
+	if err != nil {
+		return nil, err
+	}
+	// add headers to the request
+	req.Host = r.Host
+
+	r.addHeaders(req.Header)
+	if r.Compression != nil {
+		req.Header.Add("Content-Encoding", r.Compression.ContentEncoding)
+		req.Header.Add("Accept-Encoding", r.Compression.ContentEncoding)
+	}
+	if r.headers != nil {
+		for _, header := range r.headers {
+			req.Header.Add(header.name, header.value)
+		}
+	}
+
+	//use basic auth if required
+	if r.BasicAuthUsername != "" {
+		req.SetBasicAuth(r.BasicAuthUsername, r.BasicAuthPassword)
+	}
+
+	for _, c := range r.cookies {
+		req.AddCookie(c)
+	}
+	return req, nil
 }
 
 // Return value if nonempty, def otherwise.
