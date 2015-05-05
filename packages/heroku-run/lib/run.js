d38e68f078289782a0aef65479e724f22f0456a1
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2015-05-05 00:01:54 -0700

    stdin support

--- packages/heroku-run/lib/run.js
+++ packages/heroku-run/lib/run.js
@@ -8,30 +8,35 @@ let errors = require('./errors');
 let term = require('./term');
 let heroku;
 
-function buildCommandWithExitStatus(command) {
-  return `${command}; echo heroku-command-exit-status $?`;
-}
-
-function* startDyno(app, command) {
-  let env = {};
-  if (tty.isatty(0)) {
-    let size = yield term.getTermSize();
-    env.COLUMNS = size.COLUMNS;
-    env.LINES = size.LINES;
-  }
+function startDyno(app, command) {
   return heroku.apps(app).dynos().create({
     command: command,
     attach: true,
-    env: env
+    env: tty.isatty(1) ? term.getTermSize() : {}
   });
 }
 
 function attachToRendezvous(uri) {
   let c = tls.connect(uri.port, uri.hostname);
   c.setEncoding('utf8');
+  process.stdin.setEncoding('utf8');
   c.on('connect', function () {
     c.write(uri.path.substr(1) + '\r\n');
+    if (!tty.isatty(0)) {
+      setTimeout(function () {
+        process.stdin.on('readable', function () {
+          c.write(String(process.stdin.read()));
+        });
+        process.stdin.on('end', function () {
+          c.end();
+        });
+      }, 10000);
+    }
   });
+  if (tty.isatty(0)) {
+    process.stdin.setRawMode(true);
+    process.stdin.pipe(c);
+  }
   let firstLine = true;
   c.on('data', function (data) {
     // discard first line
@@ -57,9 +62,7 @@ module.exports = function run (context) {
     heroku = new Heroku({token: context.auth.password});
     let command = context.args.join(' ');
     process.stdout.write(`Running \`${command}\` attached to terminal... `);
-    if (context.flags['exit-code']) {
-      command = buildCommandWithExitStatus(command);
-    }
+    command = `${command}; echo heroku-command-exit-status $?`;
     let dyno = yield startDyno(context.app, command);
     console.log(`up, ${dyno.name}`);
     attachToRendezvous(url.parse(dyno.attach_url));
