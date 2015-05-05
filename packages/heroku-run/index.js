27c02dfa5671c0cfee5c463ca05eae09ff70bd3f
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2015-05-05 12:40:57 -0700

    added run command to move away from _run

--- packages/heroku-run/index.js
+++ packages/heroku-run/index.js
@@ -2,13 +2,24 @@
 let run = require('./lib/run');
 
 exports.topics = [{
-  name: '_run',
+  name: 'run',
   description: 'run a one-off process inside a Heroku dyno'
 }];
 
 exports.commands = [
   {
     topic: '_run',
+    help: `run a one-off process inside a Heroku dyno`,
+    variableArgs: true,
+    needsAuth: true,
+    needsApp: true,
+    flags: [
+      {name: 'exit-code', description: 'raise same exit code from dyno process'},
+    ],
+    run: run
+  },
+  {
+    topic: 'run',
     help: `run a one-off process inside a Heroku dyno`,
     variableArgs: true,
     needsAuth: true,
