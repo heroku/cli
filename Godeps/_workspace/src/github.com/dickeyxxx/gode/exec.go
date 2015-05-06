68e3d1a8b4e07912c2d38bd3e64c876352e438e3
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2015-05-06 16:34:00 -0700

    updated golock and gode

--- Godeps/_workspace/src/github.com/dickeyxxx/gode/exec.go
+++ Godeps/_workspace/src/github.com/dickeyxxx/gode/exec.go
@@ -1,6 +1,8 @@
 package gode
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os/exec"
 	"path/filepath"
 )
@@ -13,6 +15,23 @@ func (c *Client) RunScript(script string) *exec.Cmd {
 		panic(err)
 	}
 	cmd := exec.Command(nodePath, "-e", script)
+	cmd.Dir = c.RootPath
+	return cmd
+}
+
+// DebugScript is the same as RunScript except it launches with node-inspector
+func (c *Client) DebugScript(script string) *exec.Cmd {
+	scriptPath := filepath.Join(c.RootPath, "debug.js")
+	if err := ioutil.WriteFile(scriptPath, []byte(script), 0644); err != nil {
+		panic(err)
+	}
+	path := filepath.Join(c.RootPath, "node_modules", ".bin", "node-debug")
+	if _, err := exec.LookPath(path); err != nil {
+		fmt.Print("Installing node-inspector... ")
+		c.InstallPackage("node-inspector")
+		fmt.Println("done")
+	}
+	cmd := exec.Command(path, "debug.js")
 	cmd.Dir = c.RootPath
 	return cmd
 }
