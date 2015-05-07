a830a9c4338e2f44a01a687e3a3646fb09709393
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2015-05-07 10:39:46 -0700

    added version user agent to context

--- version.go
+++ version.go
@@ -21,10 +21,14 @@ Example:
   $ heroku version
   heroku-toolbelt/1.2.3 (x86_64-darwin11.2.0) ruby/1.9.3`,
 	Run: func(ctx *Context) {
-		fmt.Printf("heroku-cli/%s (%s-%s) %s", Version, runtime.GOARCH, runtime.GOOS, runtime.Version())
+		fmt.Printf(version())
 		if Channel != "master" {
 			fmt.Printf(" %s", Channel)
 		}
 		fmt.Println()
 	},
+}
+
+func version() string {
+	return fmt.Sprintf("heroku-cli/%s (%s-%s) %s", Version, runtime.GOARCH, runtime.GOOS, runtime.Version())
 }
