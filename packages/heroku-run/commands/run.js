9811bdd06cda08dcd63032e3a80d5e32720fb206
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2015-05-19 18:45:08 -0700

    fix stdin piping

--- packages/heroku-run/commands/run.js
+++ packages/heroku-run/commands/run.js
@@ -1,8 +1,9 @@
 'use strict';
-let tls   = require('tls');
-let url   = require('url');
-let tty   = require('tty');
-let cli   = require('heroku-cli-util');
+let tls    = require('tls');
+let url    = require('url');
+let tty    = require('tty');
+let stream = require('stream');
+let cli    = require('heroku-cli-util');
 
 function buildCommand(args) {
   if (args.length === 1) {
@@ -38,22 +39,32 @@ function startDyno(heroku, app, size, command) {
   });
 }
 
-function attachToRendezvous(uri) {
-  let c = tls.connect(uri.port, uri.hostname);
-  c.setEncoding('utf8');
-  process.stdin.setEncoding('utf8');
-  c.on('connect', function () {
-    c.write(uri.path.substr(1) + '\r\n');
-  });
+function readStdin(c) {
+  let stdin = process.stdin;
+  stdin.setEncoding('utf8');
+  if (tty.isatty(0)) {
+    stdin.setRawMode(true);
+    stdin.pipe(c);
+  } else {
+    stdin.pipe(new stream.Transform({
+      objectMode: true,
+      transform: function (chunk, _, next) {
+        c.write(chunk, next);
+      },
+      flush: function (done) {
+        c.write("\x04", done);
+      }
+    }));
+  }
+}
+
+function readData(c) {
   let firstLine = true;
-  c.on('data', function (data) {
+  return function (data) {
     // discard first line
     if (firstLine) {
       firstLine = false;
-      if (tty.isatty(0)) {
-        process.stdin.setRawMode(true);
-      }
-      process.stdin.pipe(c);
+      readStdin(c);
       return;
     }
     data = data.replace('\r\n', '\n');
@@ -63,12 +74,21 @@ function attachToRendezvous(uri) {
       process.exit(exitCode[1]);
     }
     process.stdout.write(data);
+  };
+}
+
+function attachToRendezvous(uri) {
+  let c = tls.connect(uri.port, uri.hostname);
+  c.setEncoding('utf8');
+  c.on('connect', function () {
+    c.write(uri.path.substr(1) + '\r\n');
   });
+  c.on('data', readData(c));
   c.on('timeout', function () {
     cli.error('timed out');
   });
   c.on('end', function () {
-    process.exit(0);
+    process.exit(-1);
   });
   c.on('error', cli.errorHandler());
   process.once('SIGINT', function () {
