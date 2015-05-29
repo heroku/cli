cc1d8cab5d4a38c16ed09c5920061171d7b6bc43
dickeyxxx <jeff@dickeyxxx.com>
2015-05-29 09:47:26 -0700

    updated iojs to 2.1.0

--- plugins.go
+++ plugins.go
@@ -30,8 +30,8 @@ var node = gode.NewClient(AppDir)
 
 func init() {
 	node.Registry = "https://d3nfsbmspisrno.cloudfront.net"
-	node.NodeVersion = "2.0.2"
-	node.NpmVersion = "2.9.0"
+	node.NodeVersion = "2.1.0"
+	node.NpmVersion = "2.10.1"
 }
 
 // SetupNode sets up node and npm in ~/.heroku
