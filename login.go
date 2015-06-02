c676e92ac1c8d969ab805ba941d5ccc74db79c9f
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2015-06-02 11:23:37 -0700

    hide login command in v4

--- login.go
+++ login.go
@@ -16,6 +16,7 @@ var loginTopic = &Topic{
 
 var loginCmd = &Command{
 	Topic:       "login",
+	Hidden:      true,
 	Description: "Login with your Heroku credentials.",
 	Run: func(ctx *Context) {
 		login()
