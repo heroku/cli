33b3b13051b42906cb191267779c560f367140fd
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2015-06-19 17:53:36 -0700

    removed heroku-client

--- packages/heroku-apps/commands/info.js
+++ packages/heroku-apps/commands/info.js
@@ -1,10 +1,11 @@
 'use strict';
+
 let sprintf = require('sprintf-js');
 let co = require('co');
-let Heroku = require('heroku-client');
 let filesize = require('filesize');
 let _ = require('lodash');
 let S = require('string');
+let cli = require('heroku-cli-util');
 
 function shellLine(name, value) {
   return `${S(name).slugify()}=${value}`;
@@ -17,6 +18,40 @@ function outputLine(name, value) {
   return sprintf.sprintf('%-15s%s', name, value);
 }
 
+function* run (context, heroku) {
+  let line = outputLine;
+  if (context.args.shell) {
+    line = shellLine;
+  }
+  console.log(`=== ${context.app}`);
+  let info = yield {
+    addons: heroku.apps(context.app).addons().list(),
+    app: heroku.apps(context.app).info(),
+    dynos: heroku.apps(context.app).dynos().list()
+  };
+  let header = 'Addons';
+  for (let addon of info.addons) {
+    console.log(line(header, addon.plan.name));
+    header = '';
+  }
+  console.log(line('Git URL', info.app.git_url));
+  console.log(line('Owner', info.app.owner.email));
+  console.log(line('Region', info.app.region.name));
+  if (context.args.shell) {
+    console.log(line('Repo Size', info.app.repo_size));
+    console.log(line('Slug Size', info.app.slug_size));
+  } else {
+    console.log(line('Repo Size', filesize(info.app.repo_size, {round: 0})));
+    console.log(line('Slug Size', filesize(info.app.slug_size, {round: 0})));
+  }
+  console.log(line('Stack', info.app.stack.name));
+  console.log(line('Web URL', info.app.web_url));
+  console.log(line('Dynos', info.dynos.length));
+  _.forOwn(_.countBy(info.dynos, 'type'), function (count, type) {
+    console.log(line(`  ${type}`, count));
+  });
+}
+
 module.exports = {
   topic: 'apps',
   command: '_info',
@@ -25,42 +60,5 @@ module.exports = {
   needsApp: true,
   needsAuth: true,
   flags: [{name: 'shell', char: 's', description: 'output more shell friendly key/value pairs'}],
-  run: function (context) {
-    co(function *() {
-      let line = outputLine;
-      if (context.args.shell) {
-        line = shellLine;
-      }
-      let heroku = new Heroku({token: context.auth.password});
-      console.log(`=== ${context.app}`);
-      let info = yield {
-        addons: heroku.apps(context.app).addons().list(),
-        app: heroku.apps(context.app).info(),
-        dynos: heroku.apps(context.app).dynos().list()
-      };
-      let header = 'Addons';
-      for (let addon of info.addons) {
-        console.log(line(header, addon.plan.name));
-        header = '';
-      }
-      console.log(line('Git URL', info.app.git_url));
-      console.log(line('Owner', info.app.owner.email));
-      console.log(line('Region', info.app.region.name));
-      if (context.args.shell) {
-        console.log(line('Repo Size', info.app.repo_size));
-        console.log(line('Slug Size', info.app.slug_size));
-      } else {
-        console.log(line('Repo Size', filesize(info.app.repo_size, {round: 0})));
-        console.log(line('Slug Size', filesize(info.app.slug_size, {round: 0})));
-      }
-      console.log(line('Stack', info.app.stack.name));
-      console.log(line('Web URL', info.app.web_url));
-      console.log(line('Dynos', info.dynos.length));
-      _.forOwn(_.countBy(info.dynos, 'type'), function (count, type) {
-        console.log(line(`  ${type}`, count));
-      });
-    }).catch(function (err) {
-      console.error(err.stack);
-    });
-  }
+  run: cli.command(co.wrap(run))
 };
