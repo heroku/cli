6accd98c0a5821b6d3da4be4a18e8bc46412983f
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2015-08-03 14:35:56 -0700

    added 2fa alias

--- two_factor.go
+++ two_factor.go
@@ -5,23 +5,36 @@ var twoFactorTopic = &Topic{
 	Description: "manage two-factor authentication settings",
 }
 
+var twoFactorTopicAlias = &Topic{
+	Name:        "2fa",
+	Description: "manage two-factor authentication settings",
+}
+
 var twoFactorCmd = &Command{
 	Topic:     "twofactor",
 	NeedsAuth: true,
-	Run: func(ctx *Context) {
-		req := apiRequest(ctx.APIToken)
-		req.Method = "GET"
-		req.Uri = req.Uri + "/account"
-		res, err := req.Do()
-		ExitIfError(err)
-		var doc map[string]bool
-		res.Body.FromJsonTo(&doc)
-		if doc["two_factor_authentication"] {
-			Println("Two-factor authentication is enabled")
-		} else {
-			Println("Two-factor authentication is not enabled")
-		}
-	},
+	Run:       twoFactorRun,
+}
+
+var twoFactorCmdAlias = &Command{
+	Topic:     "2fa",
+	NeedsAuth: true,
+	Run:       twoFactorRun,
+}
+
+func twoFactorRun(ctx *Context) {
+	req := apiRequest(ctx.APIToken)
+	req.Method = "GET"
+	req.Uri = req.Uri + "/account"
+	res, err := req.Do()
+	ExitIfError(err)
+	var doc map[string]bool
+	res.Body.FromJsonTo(&doc)
+	if doc["two_factor_authentication"] {
+		Println("Two-factor authentication is enabled")
+	} else {
+		Println("Two-factor authentication is not enabled")
+	}
 }
 
 var twoFactorGenerateCmd = &Command{
@@ -29,21 +42,31 @@ var twoFactorGenerateCmd = &Command{
 	Command:     "generate-recovery-codes",
 	Description: "Generates and replaces recovery codes",
 	NeedsAuth:   true,
-	Run: func(ctx *Context) {
-		req := apiRequest(ctx.APIToken)
-		req.Method = "POST"
-		req.Uri = req.Uri + "/account/recovery-codes"
-		req.AddHeader("Heroku-Password", getPassword())
-		req.AddHeader("Heroku-Two-Factor-Code", getString("Two-factor code: "))
-		res, err := req.Do()
-		ExitIfError(err)
-		var codes []interface{}
-		res.Body.FromJsonTo(&codes)
-		Println("Recovery codes:")
-		for _, code := range codes {
-			Println(code)
-		}
-	},
+	Run:         twoFactorGenerateRun,
+}
+
+var twoFactorGenerateCmdAlias = &Command{
+	Topic:       "2fa",
+	Command:     "generate-recovery-codes",
+	Description: "Generates and replaces recovery codes",
+	NeedsAuth:   true,
+	Run:         twoFactorGenerateRun,
+}
+
+func twoFactorGenerateRun(ctx *Context) {
+	req := apiRequest(ctx.APIToken)
+	req.Method = "POST"
+	req.Uri = req.Uri + "/account/recovery-codes"
+	req.AddHeader("Heroku-Password", getPassword())
+	req.AddHeader("Heroku-Two-Factor-Code", getString("Two-factor code: "))
+	res, err := req.Do()
+	ExitIfError(err)
+	var codes []interface{}
+	res.Body.FromJsonTo(&codes)
+	Println("Recovery codes:")
+	for _, code := range codes {
+		Println(code)
+	}
 }
 
 var twoFactorDisableCmd = &Command{
@@ -51,22 +74,32 @@ var twoFactorDisableCmd = &Command{
 	Command:     "disable",
 	Description: "Disable two-factor authentication for your account",
 	NeedsAuth:   true,
-	Run: func(ctx *Context) {
-		req := apiRequest(ctx.APIToken)
-		req.Method = "PATCH"
-		req.Uri = req.Uri + "/account/"
-		req.Body = map[string]interface{}{
-			"two_factor_authentication": "false",
-			"password":                  getPassword(),
-		}
-		res, err := req.Do()
-		ExitIfError(err)
-		if res.StatusCode != 200 {
-			var doc map[string]string
-			res.Body.FromJsonTo(&doc)
-			Error(doc["message"])
-			return
-		}
-		Println("disabled two-factor authentication")
-	},
+	Run:         twoFactorDisableRun,
+}
+
+var twoFactorDisableCmdAlias = &Command{
+	Topic:       "2fa",
+	Command:     "disable",
+	Description: "Disable two-factor authentication for your account",
+	NeedsAuth:   true,
+	Run:         twoFactorDisableRun,
+}
+
+func twoFactorDisableRun(ctx *Context) {
+	req := apiRequest(ctx.APIToken)
+	req.Method = "PATCH"
+	req.Uri = req.Uri + "/account/"
+	req.Body = map[string]interface{}{
+		"two_factor_authentication": "false",
+		"password":                  getPassword(),
+	}
+	res, err := req.Do()
+	ExitIfError(err)
+	if res.StatusCode != 200 {
+		var doc map[string]string
+		res.Body.FromJsonTo(&doc)
+		Error(doc["message"])
+		return
+	}
+	Println("disabled two-factor authentication")
 }
