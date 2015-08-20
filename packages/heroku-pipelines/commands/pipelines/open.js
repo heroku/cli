696067919f748ab54ac695bb03cadad66aba4c33
Mark Pundsack <mp@heroku.com>
2015-08-20 15:55:45 -0500

    Add pipelines:open

---
+++ packages/heroku-pipelines/commands/pipelines/open.js
@@ -0,0 +1,33 @@
+'use strict';
+
+let cli = require('heroku-cli-util');
+let disambiguate = require('../../lib/disambiguate');
+var util = require('util')
+var exec = require('child_process').exec;
+let co  = require('co');
+
+function puts(error, stdout, stderr) {
+  console.log(stdout)
+};
+
+module.exports = {
+  topic: 'pipelines',
+  command: 'open',
+  description: 'open a pipeline',
+  help: 'Open a pipeline in Dashboard.',
+  needsApp: false,
+  needsAuth: true,
+  args: [
+    {name: 'pipeline', description: 'name of pipeline', optional: false},
+  ],
+  run: cli.command(function* (context, heroku) {
+    let pipeline = yield disambiguate(heroku, context.args.pipeline);
+    let promise = new Promise(function (fulfill) {
+      exec(`open https://dashboard.heroku.com/pipelines/${pipeline.id}`, function(error, stdout, stderr) {
+        fulfill(stdout);
+      });
+    });
+    let output = yield cli.action("Opening dashboard", promise);
+    if (output) console.log(output);
+  })
+};
