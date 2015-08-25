26e529c6a12bb826f8527103e44525e4bb430c4f
Mark Pundsack <mp@heroku.com>
2015-08-24 20:43:58 -0700

    Refactor infer and add tests

--- packages/heroku-pipelines/lib/infer.js
+++ packages/heroku-pipelines/lib/infer.js
@@ -1,50 +1,32 @@
 'use strict';
 
-module.exports = function infer(app) {
-  // Set defaults
-  let basename = app;
-  let stage = "production";
+const stageMap = [
+  {
+    stage: 'development',
+    regex: /-(dev|development)$/
+  },{
+    stage: 'test',
+    regex: /-(uat|tst|test)$/
+  },{
+    stage: 'qa',
+    regex: /-qa$/
+  },{
+    stage: 'staging',
+    regex: /-(stg|staging)$/
+  },{
+    stage: 'production',
+    regex: /-(prd|prod|production|admin|demo)$/
+  },{
+    stage: 'review',
+    regex: /-pr-(\d+)$/
+  }
+];
 
-  let pattern = /-[a-z]+$/;
-  let match = pattern.exec(app);
-  if(match) {
-    switch (match[0]) {
-      case "-dev":
-      case "-development":
-        basename = app.replace(pattern,"");
-        stage = "development";
-        break;
-      case "-uat":
-      case "-tst":
-      case "-test":
-        basename = app.replace(pattern,"");
-        stage = "test";
-        break;
-      case "-qa":
-        basename = app.replace(pattern,"");
-        stage = "qa";
-        break;
-      case "-stg":
-      case "-staging":
-        basename = app.replace(pattern,"");
-        stage = "staging";
-        break;
-      case "-admin":
-      case "-demo":
-      case "-prod":
-      case "-production":
-        basename = app.replace(pattern,"");
-        stage = "production";
-        break;
-      default:
-        basename = app;
-        stage = "production";
+module.exports = function infer(app) {
+  for (var val of stageMap) {
+    if(val.regex.test(app)) {
+      return [app.replace(val.regex,""), val.stage];
     }
   }
-  pattern = /-pr-(\d+)$/;
-  if (pattern.test(app)) {
-    basename = app.replace(pattern,"");
-    stage = "review";
-  }
-  return [basename,stage];
+  return [app,"production"];
 };
