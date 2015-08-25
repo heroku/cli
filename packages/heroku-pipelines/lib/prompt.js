3de4fe76bbb3150c4103a4aa6413a13aad360dee
Mark Pundsack <mp@heroku.com>
2015-08-24 18:00:28 -0700

    Add prompt.js wwrapper around inquirer

---
+++ packages/heroku-pipelines/lib/prompt.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = function prompt(questions) {
+  let inquirer = require('inquirer');
+  return new Promise(function (fulfill) {
+    inquirer.prompt(questions, function (answers) {
+      fulfill(answers);
+    });
+  });
+}
