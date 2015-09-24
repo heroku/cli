dce5c1f9d550929fa581284a89a63ac32b8d0923
dickeyxxx <jeff@dickeyxxx.com>
2015-09-24 16:57:01 -0700

    v4.23.7

--- gode/constants.go
+++ gode/constants.go
@@ -7,19 +7,19 @@ package gode
 //
 
 // Version is the requested node version
-const Version = "4.1.0"
+const Version = "4.1.1"
 
 // NpmVersion is the requested npm version
-const NpmVersion = "2.14.3"
+const NpmVersion = "2.14.4"
 
-const npmSha = "0578ad05ca5ace2cbeff0c765ee629dd17bfb8cb22cc9fac44477e3cd33eddc4"
-const npmURL = "https://cli-assets.heroku.com/npm/v2.14.3.zip"
+const npmSha = "f70c72d2972b3b28124b993b11b7e7fbe0402e69b156929baa4dd0cb5cf90032"
+const npmURL = "https://cli-assets.heroku.com/npm/v2.14.4.zip"
 
 var targets = []Target{
-	{"386", "linux", "https://cli-assets.heroku.com/node/v4.1.0/node-v4.1.0-linux-x86.tar.gz", "node-v4.1.0-linux-x86", "2ceaf5e66993da50e4514ddc5c270dc15ee712d00c29a2108655d9d259b3b9bb"},
-	{"amd64", "linux", "https://cli-assets.heroku.com/node/v4.1.0/node-v4.1.0-linux-x64.tar.gz", "node-v4.1.0-linux-x64", "7c6055e08127143d9a8f779aa56f3fe42035fff8843c2652b0b2726204556382"},
-	{"arm", "linux", "https://cli-assets.heroku.com/node/v4.1.0/node-v4.1.0-linux-armv7.tar.gz", "node-v4.1.0-linux-armv7", "3b728cdb93d155cf532a8a7f57f5a86f6f67a73e57a7abe657ff88166995e99e"},
-	{"amd64", "darwin", "https://cli-assets.heroku.com/node/v4.1.0/node-v4.1.0-darwin-x64.tar.gz", "node-v4.1.0-darwin-x64", "ff1c4b13a10c0789709cd73980a496b27e434207802989377ec4cb56302a1ebc"},
-	{"386", "windows", "https://cli-assets.heroku.com/node/v4.1.0/win-x86/node.exe", "node-v4.1.0-windows-x86", "62dca0c50648c3b87d9af1e469f290e82ab009f60892b164e1d1f6655c7310e3"},
-	{"amd64", "windows", "https://cli-assets.heroku.com/node/v4.1.0/win-x64/node.exe", "node-v4.1.0-windows-x64", "95199ec14462edc982e01538d81ecd9af35ae3f9ef18c33be53102a5f93053e0"},
+	{"386", "linux", "https://cli-assets.heroku.com/node/v4.1.1/node-v4.1.1-linux-x86.tar.gz", "node-v4.1.1-linux-x86", "3f9836b8a7e6e3d6591af6ef59e6055255439420518c3f77e0e65832a8486be1"},
+	{"amd64", "linux", "https://cli-assets.heroku.com/node/v4.1.1/node-v4.1.1-linux-x64.tar.gz", "node-v4.1.1-linux-x64", "f5f7e11a503c997486d50d8683741a554bdda1d1181125a05ac5844cb29d1572"},
+	{"arm", "linux", "https://cli-assets.heroku.com/node/v4.1.1/node-v4.1.1-linux-armv7l.tar.gz", "node-v4.1.1-linux-armv7l", "2896f0ab7c53bb7b489a09f7344e059f898ae929c2a9bfb7dfce85a5846ab9d2"},
+	{"amd64", "darwin", "https://cli-assets.heroku.com/node/v4.1.1/node-v4.1.1-darwin-x64.tar.gz", "node-v4.1.1-darwin-x64", "b7e72bf8364c35992a8bebc57bf68c596d622c33d409c0943bf7d24ca7205c76"},
+	{"386", "windows", "https://cli-assets.heroku.com/node/v4.1.1/win-x86/node.exe", "node-v4.1.1-windows-x86", "8fe8b23e11e6356b6ab50f18060939c3e7a9f56d8ca2189fc556c8185f1a5083"},
+	{"amd64", "windows", "https://cli-assets.heroku.com/node/v4.1.1/win-x64/node.exe", "node-v4.1.1-windows-x64", "9e985444df6374fb9efaa8c43630a26ca4fc77dcdcb5564abf7c30a62033dd53"},
 }
