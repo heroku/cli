05ebca69a2c706ab7113dd5cf6b5c613630a9d1f
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2015-10-09 14:25:30 -0700

    remove request library

--- packages/heroku-local/lib/download.js
+++ packages/heroku-local/lib/download.js
@@ -1,18 +1,16 @@
 'use strict';
 
 let fs      = require('fs');
-let request = require('request');
+let https   = require('https');
 
 function file (url, path, opts) {
   let tmpPath = path + '.tmp';
   return new Promise(function (fulfill, reject) {
-    request(url, function (err, res) {
-      if (err) return reject(err);
+    https.get(url, function (res) {
       if (res.statusCode > 200 || res.statusCode >= 300) return reject(new Error(res.body));
-      // for some reason this seems necessary
-      setTimeout(fulfill, 500);
-    })
-    .pipe(fs.createWriteStream(tmpPath, opts));
+      res.pipe(fs.createWriteStream(tmpPath, opts));
+      res.on('end', fulfill);
+    });
   })
   .then(() => fs.renameSync(tmpPath, path));
 }
