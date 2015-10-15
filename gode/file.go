0215bc485e9e39e597e751fdade6ac16bec99d44
dickeyxxx <jeff@dickeyxxx.com>
2015-10-15 10:58:56 -0700

    use os.Stat on windows

--- gode/file.go
+++ gode/file.go
@@ -2,18 +2,10 @@ package gode
 
 import (
 	"os"
-	"runtime"
 )
 
 func fileExists(path string) (bool, error) {
-	var err error
-	if runtime.GOOS == "windows" {
-		// Windows doesn't seem to like using os.Stat
-		_, err = os.Open(path)
-	} else {
-		_, err = os.Stat(path)
-	}
-	if err != nil {
+	if _, err := os.Stat(path); err != nil {
 		if os.IsNotExist(err) {
 			return false, nil
 		}
