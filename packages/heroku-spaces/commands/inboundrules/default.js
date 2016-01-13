350a31e30dbe1b0b91e78384554ec4d48650a214
Jeff Dickey <dickeyxxx@gmail.com>
2016-01-13 14:29:10 -0800

    Merge pull request #13 from heroku/default-getter
    Allow using spaces:inboundrules:default as a getter or setter

--- packages/heroku-spaces/commands/inboundrules/default.js
+++ packages/heroku-spaces/commands/inboundrules/default.js
@@ -6,12 +6,18 @@ let co  = require('co');
 function* run(context, heroku) {
   let lib = require('../../lib/inboundrules')(heroku);
   let space = context.flags.space;
-  yield cli.confirmApp(space, context.flags.confirm, `Destructive Action\nThis command will affect the space ${cli.color.bold.red(space)}`);
   let rules = yield lib.getRules(space);
-  rules.default_action = context.args['default'];
-  rules = yield lib.putRules(space, rules);
-  lib.displayRules(rules);
-  cli.warn('It may take a few moments for the changes to take effect.');
+  if (context.args['default']) {
+    // set default
+    yield cli.confirmApp(space, context.flags.confirm, `Destructive Action\nThis command will affect the space ${cli.color.bold.red(space)}`);
+    rules.default_action = context.args['default'];
+    rules = yield lib.putRules(space, rules);
+    lib.displayRules(rules);
+    cli.warn('It may take a few moments for the changes to take effect.');
+  } else {
+    // get default
+    cli.log(rules.default_action);
+  }
 }
 
 module.exports = {
@@ -33,9 +39,9 @@ Example:
   `,
   needsApp: false,
   needsAuth: true,
-  args: [{name: 'default'}],
+  args: [{name: 'default', optional: true}],
   flags: [
-    {name: 'space', char: 's', hasValue: true, description: 'space to set default action of'},
+    {name: 'space', char: 's', required: true, hasValue: true, description: 'space to set default action of'},
     {name: 'confirm', hasValue: true, description: 'set to space name to bypass confirm prompt'},
   ],
   run: cli.command(co.wrap(run))
