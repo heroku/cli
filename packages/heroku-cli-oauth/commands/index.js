1d820618b43e30ca3b1eb6733345f880ecc59712
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2016-01-20 17:25:26 -0800

    added clients command

--- packages/heroku-cli-oauth/commands/index.js
+++ packages/heroku-cli-oauth/commands/index.js
@@ -6,20 +6,27 @@ let cli = require('heroku-cli-util');
 function* run (context, heroku) {
   let clients = yield heroku.request({
     path: '/oauth/clients',
-    headers: {Accept: 'application/json'}
+    headers: {Range: null},
   });
   if (context.flags.json) {
     cli.log(JSON.stringify(clients, null, 2));
   } else if (clients.length === 0) {
     cli.log('No oauth clients.');
   } else {
-    cli.debug(clients);
+    cli.table(clients, {
+      columns: [
+        {key: 'name'},
+        {key: 'id'},
+        {key: 'redirect_uri'},
+      ]
+    });
   }
 }
 
 module.exports = {
   topic: 'clients',
   description: 'list your OAuth clients',
+  needsAuth: true,
   flags: [
     {name: 'json', description: 'output in json format'}
   ],
