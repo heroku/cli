349818fa7b1c5274d7c60b55771cb8694d82cd95
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2016-01-21 10:58:05 -0800

    init testing

---
+++ packages/heroku-cli-oauth/test/clients/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let nock   = require('nock');
+let cmd    = require('../../commands/clients/index');
+let expect = require('chai').expect;
+
+describe('clients', function() {
+  beforeEach(() => cli.mockConsole());
+
+  it('lists the clients', function() {
+    let api = nock('https://api.heroku.com:443')
+      .get('/oauth/clients')
+      .reply(200, [{name: 'awesome', id: 'f6e8d969-129f-42d2-854b-c2eca9d5a42e', redirect_uri: 'https://myapp.com'}]);
+    return cmd.run({flags: {}})
+    .then(() => expect(cli.stdout).to.equal('awesome  f6e8d969-129f-42d2-854b-c2eca9d5a42e  https://myapp.com\n'))
+    .then(() => api.done());
+  });
+});
---
+++ packages/heroku-cli-oauth/test/helpers.js
@@ -0,0 +1,4 @@
+'use strict';
+global.cli = require('heroku-cli-util');
+cli.raiseErrors   = true;
+cli.color.enabled = false;
