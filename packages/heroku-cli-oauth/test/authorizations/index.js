a984d9dcef15243f98b71ca9644e54f9ff99e98d
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2016-01-21 11:36:17 -0800

    testing

---
+++ packages/heroku-cli-oauth/test/authorizations/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let nock   = require('nock');
+let cmd    = require('../../commands/authorizations/index');
+
+describe('authorizations', function() {
+  beforeEach(() => cli.mockConsole());
+
+  it('lists the authorizations', function() {
+    let api = nock('https://api.heroku.com:443')
+      .get('/oauth/authorizations')
+      .reply(200, [{description: 'awesome', id: 'f6e8d969-129f-42d2-854b-c2eca9d5a42e', scope: ['app', 'user']}]);
+    return cmd.run({flags: {}})
+    .then(() => expect(cli.stdout).to.equal('awesome  f6e8d969-129f-42d2-854b-c2eca9d5a42e  app,user\n'))
+    .then(() => api.done());
+  });
+});
