57dfd8f6c637cc2f898b3c6303ce7dbb8fc60725
Ransom Briggs <rbriggs+github@heroku.com>
2016-02-10 14:52:00 -0800

    Merge pull request #10 from heroku/forego-https-proxy
    Conditionally tunnel forego download through proxy

--- packages/heroku-local/lib/download.js
+++ packages/heroku-local/lib/download.js
@@ -2,15 +2,40 @@
 
 let fs      = require('fs');
 let https   = require('https');
+let url     = require('url');
+let cli     = require('heroku-cli-util');
+let tunnel  = require('tunnel-agent');
 
-function file (url, path, opts) {
+function file (urlStr, path, opts) {
   let tmpPath = path + '.tmp';
   return new Promise(function (fulfill, reject) {
-    https.get(url, function (res) {
+    let httpsProxy = process.env.HTTPS_PROXY || process.env.https_proxy;
+    let agent;
+    if (httpsProxy) {
+      cli.hush(`proxy set to ${httpsProxy}`);
+      let proxy = url.parse(httpsProxy);
+
+      agent = tunnel.httpsOverHttp({
+        proxy: {
+          host: proxy.hostname,
+          port: proxy.port || 8080
+        }
+      });
+    } else {
+      agent = new https.Agent();
+    }
+
+    let requestOptions = url.parse(urlStr);
+    requestOptions.agent = agent;
+
+    let req = https.request(requestOptions, function (res) {
       if (res.statusCode > 200 || res.statusCode >= 300) return reject(new Error(res.body));
       res.pipe(fs.createWriteStream(tmpPath, opts));
       res.on('end', fulfill);
     });
+    req.on('error', reject);
+    req.end();
+
   })
   .then(() => fs.renameSync(tmpPath, path));
 }
