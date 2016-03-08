e581a24e92ade578e19e2083c46c2eb7390abfaa
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2016-03-08 06:43:02 -0800

    minor testing changes for style guide

--- packages/heroku-apps/test/commands/maintenance/index.js
+++ packages/heroku-apps/test/commands/maintenance/index.js
@@ -1,22 +1,30 @@
 'use strict';
 
-let nock   = require('nock');
-let expect = require('chai').expect;
-let cmd    = require('../../../commands/maintenance');
+// get command from index.js
+let cmd = commands.find(c => c.topic === 'maintenance');
 
-describe('maintenance', function() {
+describe('maintenance', () => {
+  // prevent stdout/stderr from displaying
+  // redirects to cli.stdout/cli.stderr instead
   beforeEach(() => cli.mockConsole());
 
-  it('shows the maintenance is on', function() {
+  it('shows the maintenance is on', () => {
+    // mock out API
     let api = nock('https://api.heroku.com:443')
       .get('/apps/myapp')
       .reply(200, {maintenance: true});
+
+    // run the command
     return cmd.run({app: 'myapp'})
+    // check stdout
     .then(() => expect(cli.stdout).to.equal('on\n'))
+    // check stderr
+    .then(() => expect(cli.stderr).to.equal(''))
+    // ensure all nock HTTP expectations are met
     .then(() => api.done());
   });
 
-  it('shows the maintenance is off', function() {
+  it('shows the maintenance is off', () => {
     let api = nock('https://api.heroku.com:443')
       .get('/apps/myapp')
       .reply(200, {maintenance: false});
--- packages/heroku-apps/test/helpers.js
+++ packages/heroku-apps/test/helpers.js
@@ -1,5 +1,10 @@
 'use strict';
-process.env.TZ = 'UTC';
-global.cli = require('heroku-cli-util');
-cli.raiseErrors   = true;
-cli.color.enabled = false;
+
+global.commands = require('../index').commands; // Load plugin commands
+global.cli = require('heroku-cli-util');        // Load heroku-cli-util helpers
+global.expect = require('chai').expect;         // Load chai
+global.nock = require('nock');                  // Load nock
+
+process.env.TZ    = 'UTC';                      // Use UTC time always
+cli.raiseErrors   = true;                       // Fully raise exceptions
+cli.color.enabled = false;                      // Disable color
