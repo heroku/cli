6d00f0623a800fb95f53126b78b1e6f93aca5d7d
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2016-03-24 12:57:31 -0700

    Merge pull request #36 from heroku/apps-open
    added apps:open

---
+++ packages/heroku-apps/commands/apps/open.js
@@ -0,0 +1,35 @@
+'use strict';
+
+let co       = require('co');
+let cli      = require('heroku-cli-util');
+let extend   = require('util')._extend;
+let url      = require('url');
+
+function* run (context, heroku) {
+  let app = yield heroku.apps(context.app).info();
+  yield cli.open(url.resolve(app.web_url, context.args.path || ''));
+}
+
+let cmd = {
+  topic: 'apps',
+  command: 'open',
+  description: 'open the app in a web browser',
+  help: `
+Examples:
+
+  $ heroku open -a myapp
+  # opens https://myapp.herokuapp.com
+
+  $ heroku open -a myapp /foo
+  # opens https://myapp.herokuapp.com/foo
+  `,
+  needsApp: true,
+  needsAuth: true,
+  args:  [{name: 'path', optional: true}],
+  run: cli.command(co.wrap(run))
+};
+
+module.exports.open = cmd;
+module.exports.root = extend({}, cmd);
+module.exports.root.topic = 'open';
+delete module.exports.root.command;
