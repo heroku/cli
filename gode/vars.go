06a92c2844233004cde9860f228f46469d6749cb
Jeff Dickey <dickeyxxx@gmail.com>
2016-03-30 12:33:51 -0700

    allow setting HEROKU_NPM_REGISTRY env var

--- gode/vars.go
+++ gode/vars.go
@@ -9,12 +9,19 @@ import (
 	"strings"
 )
 
-const registry = "https://cli-npm.heroku.com"
+var registry string
 
 var rootPath string
 var lockPath string
 var nodePath string
 var npmPath string
+
+func init() {
+	registry = os.Getenv("HEROKU_NPM_REGISTRY")
+	if registry == "" {
+		registry = "https://cli-npm.heroku.com"
+	}
+}
 
 // SetRootPath sets the root for gode
 func SetRootPath(root string) {
