24b83974d5146f47b4875ebe5c842ac47f9925ff
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2016-04-18 14:36:38 -0700

    added labs test (#67)

--- packages/heroku-apps/test/commands/labs/disable.js
+++ packages/heroku-apps/test/commands/labs/disable.js
@@ -2,9 +2,11 @@
 
 let nock = require('nock');
 let cmd  = require('../../../commands/labs/disable');
+let expect = require('unexpected');
 
 describe('labs:disable', function() {
   beforeEach(() => cli.mockConsole());
+  afterEach(() => nock.cleanAll());
 
   it('disables a user lab feature', function() {
     let api = nock('https://api.heroku.com:443')
@@ -20,6 +22,25 @@ describe('labs:disable', function() {
       .patch('/account/features/feature-a', {enabled: false})
       .reply(200);
     return cmd.run({args: {feature: 'feature-a'}})
+    .then(() => expect(cli.stdout, 'to be empty'))
+    .then(() => expect(cli.stderr, 'to equal', 'Disabling feature-a for jeff@heroku.com... done\n'))
+    .then(() => api.done());
+  });
+
+  it('disables an app feature', function() {
+    let api = nock('https://api.heroku.com:443')
+      .get('/account/features/feature-a').reply(404)
+      .get('/apps/myapp/features/feature-a')
+      .reply(200, {
+        enabled: true,
+        name: 'feature-a',
+        description: 'a user lab feature',
+        doc_url: 'https://devcenter.heroku.com',
+      })
+      .patch('/apps/myapp/features/feature-a', {enabled: false}).reply(200);
+    return cmd.run({app: 'myapp', args: {feature: 'feature-a'}})
+    .then(() => expect(cli.stdout, 'to be empty'))
+    .then(() => expect(cli.stderr, 'to equal', 'Disabling feature-a for myapp... done\n'))
     .then(() => api.done());
   });
 });
