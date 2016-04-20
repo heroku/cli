e3b64430228440d53a5f187f4d6bb2af40f12d8f
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2016-04-19 19:32:39 -0700

    added addons:attach and addons:detach (#24)
    * added addons:attach
    
    * added addons:detach
    
    * test case for confirmation

---
+++ packages/heroku-cli-addons/commands/addons/attach.js
@@ -0,0 +1,56 @@
+'use strict';
+
+let cli      = require('heroku-cli-util');
+let co       = require('co');
+
+function* run (context, heroku) {
+  let app = context.app;
+  let addon = yield heroku.get(`/addons/${context.args.addon_name}`);
+
+  function createAttachment(app, as, confirm) {
+    return cli.action(
+      `Attaching ${cli.color.addon(addon.name)}${as ? ' as ' + cli.color.attachment(as) : ''} to ${cli.color.app(app)}`,
+      heroku.request({
+        path: '/addon-attachments',
+        method: 'POST',
+        body: {
+          name:  as,
+          app:   {name: app},
+          addon: {name: addon.name},
+          confirm,
+        }
+      })
+    );
+  }
+
+  let attachment = yield createAttachment(app, context.flags.as, context.flags.confirm)
+  .catch(err => {
+    if (!err.body || err.body.id !== 'confirmation_required') throw err;
+    return cli.confirmApp(app, context.flags.confirm, err.body.message)
+    .then(() => createAttachment(app, context.flags.as, app));
+  });
+  let releases = yield cli.action(
+    `Setting ${cli.color.attachment(attachment.name)} config vars and restarting ${cli.color.app(app)}`,
+    {success: false},
+    heroku.request({
+      path: `/apps/${app}/releases`,
+      partial: true,
+      headers: { 'Range': `version ..; max=1, order=desc` },
+    })
+  );
+  cli.console.error(`done, v${releases[0].version}`);
+}
+
+module.exports = {
+  topic: 'addons',
+  command: 'attach',
+  description: 'attach add-on resource to a new app',
+  needsAuth: true,
+  needsApp: true,
+  flags: [
+    {name: 'as',      description: 'name for add-on attachment', hasValue: true},
+    {name: 'confirm', description: 'overwrite existing add-on attachment with same name', hasValue: true},
+  ],
+  args: [{name: 'addon_name'}],
+  run: cli.command(co.wrap(run))
+};
--- packages/heroku-cli-addons/index.js
+++ packages/heroku-cli-addons/index.js
@@ -7,6 +7,8 @@ exports.topic = {
 
 exports.commands = [
     require('./commands/addons'),
+    require('./commands/addons/attach'),
+    require('./commands/addons/detach'),
     require('./commands/addons/info'),
     require('./commands/addons/open'),
     require('./commands/addons/plans'),
