d778690b87a36535c59abb46ec38505481a1ec27
Jeff Dickey <dickeyxxx@gmail.com>
2016-04-22 19:04:15 -0700

    standardify

--- gulp/http.js
+++ gulp/http.js
@@ -1,15 +1,15 @@
-'use strict';
+'use strict'
 
-let gutil = require('gulp-util');
-let https = require('https');
+let gutil = require('gulp-util')
+let https = require('https')
 
 exports.get = url => {
-  gutil.log(`HTTP GET ${url}`);
+  gutil.log(`HTTP GET ${url}`)
   return new Promise((ok, fail) => {
     https.get(url, res => {
-      if (res.statusCode >= 300 && res.statusCode <= 399) return ok(exports.get(res.headers.location));
-      if (res.statusCode <= 199 || res.statusCode >= 400) return fail(new Error(`${res.statusCode}:${url}`));
-      ok(res);
-    });
-  });
-};
+      if (res.statusCode >= 300 && res.statusCode <= 399) return ok(exports.get(res.headers.location))
+      if (res.statusCode <= 199 || res.statusCode >= 400) return fail(new Error(`${res.statusCode}:${url}`))
+      ok(res)
+    })
+  })
+}
