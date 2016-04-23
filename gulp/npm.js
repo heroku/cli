c454d9903444d4d4ac19374c4206d1f58ecf0280
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2016-04-22 19:04:15 -0700

    standardify

--- gulp/npm.js
+++ gulp/npm.js
@@ -1,27 +1,27 @@
-'use strict';
+'use strict'
 
-let gulp     = require('gulp');
-let gutil    = require('gulp-util');
-let fs       = require('mz/fs');
-let config   = require('../config.json');
-let http     = require('./http');
-let zlib     = require('zlib');
-let tar      = require('tar');
+let gulp = require('gulp')
+let gutil = require('gulp-util')
+let fs = require('mz/fs')
+let config = require('../config.json')
+let http = require('./http')
+let zlib = require('zlib')
+let tar = require('tar')
 
-const npmPath = `./tmp/heroku/lib/npm-${config.npmVersion}`;
+const npmPath = `./tmp/heroku/lib/npm-${config.npmVersion}`
 
 gulp.task('build:workspace:npm', () => {
   return fs.exists(npmPath)
-  .then(exists => {
-    if (exists) return;
-    gutil.log(`${npmPath} not found, fetching`);
-    return http.get(`https://github.com/npm/npm/archive/v${config.npmVersion}.tar.gz`)
-    .then(res => {
-      return new Promise((ok, fail) => {
-        let gunzip = zlib.createGunzip().on('error', fail);
-        let extractor = tar.Extract({path: './tmp/heroku/lib'}).on('error', fail).on('end', ok);
-        res.pipe(gunzip).pipe(extractor);
-      });
-    });
-  });
-});
+    .then(exists => {
+      if (exists) return
+      gutil.log(`${npmPath} not found, fetching`)
+      return http.get(`https://github.com/npm/npm/archive/v${config.npmVersion}.tar.gz`)
+        .then(res => {
+          return new Promise((ok, fail) => {
+            let gunzip = zlib.createGunzip().on('error', fail)
+            let extractor = tar.Extract({path: './tmp/heroku/lib'}).on('error', fail).on('end', ok)
+            res.pipe(gunzip).pipe(extractor)
+          })
+        })
+    })
+})
