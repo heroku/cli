d23801832eb34f80672bd6c9ee7f667439149ad9
Ransom Briggs <rbriggs@heroku.com>
2016-04-25 14:23:29 -0500

    Extract display_table function

--- packages/heroku-cli-plugin-certs-v5/commands/certs/index.js
+++ packages/heroku-cli-plugin-certs-v5/commands/certs/index.js
@@ -3,8 +3,8 @@
 let co      = require('co');
 let cli     = require('heroku-cli-util');
 
-let endpoints   = require('../../lib/endpoints.js').all;
-let format_date = require('../../lib/format_date.js');
+let endpoints     = require('../../lib/endpoints.js').all;
+let display_table = require('../../lib/display_table.js');
 
 function* run(context, heroku) {
   let certs = yield endpoints(context.app, heroku);
@@ -12,24 +12,7 @@ function* run(context, heroku) {
   if (certs.length === 0) {
     cli.log(`${context.app} has no SSL certificates.\nUse \`heroku _certs:add CRT KEY\` to add one.`);
   } else {
-    let mapped = certs.filter(function(f) { return f.ssl_cert; }).map(function(f) {
-      return {
-        name:         f.name,
-        cname:        f.cname,
-        cert_domains: f.ssl_cert.cert_domains,
-        expires_at:   f.ssl_cert.expires_at,
-        ca_signed:    f.ssl_cert['ca_signed?'],
-        type:         f._meta.type
-      };
-    });
-    cli.table(mapped, {columns: [
-      {label: 'Name', key: 'name'},
-      {label: 'Endpoint', key: 'cname'},
-      {label: 'Common Name(s)', key: 'cert_domains', format: function(f) { return f.join(', '); }},
-      {label: 'Expires', key: 'expires_at', format: format_date},
-      {label: 'Trusted', key: 'ca_signed', format: function(f) { return f ? 'True' : 'False'; }},
-      {label: 'Type', key: 'type'}
-    ]});
+    display_table(certs);
   }
 }
 
