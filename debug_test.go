2c1f205e7c55536e938f9a6d31cac5439746b630
Jeff Dickey <dickeyxxx@gmail.com>
2016-04-27 20:54:51 -0700

    fix debug test

--- debug_test.go
+++ debug_test.go
@@ -22,9 +22,9 @@ var _ = Describe("debug", func() {
 		}
 		f.WriteString(butt)
 		f.Close()
+		cli.ExitFn = func(code int) {}
 		cli.Start("heroku", "debug:errlog")
 		stdout = vtclean.Clean(cli.Stdout.(*bytes.Buffer).String(), false)
-		cli.ExitFn = func(code int) {}
 	})
 	AfterEach(func() { cli.Stdout = os.Stdout })
 
