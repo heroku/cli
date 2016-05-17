94ccbdadcd8332fa3bcfe9d92e2d8e7ea05f6a7d
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2016-05-17 11:49:19 -0700

    sessions index

---
+++ packages/heroku-cli-oauth/commands/sessions/index.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const co = require('co')
+const cli = require('heroku-cli-util')
+
+function * run (context, heroku) {
+  let sessions = yield heroku.get('/oauth/sessions')
+  if (context.flags.json) {
+    cli.styledJSON(sessions)
+  } else if (sessions.length === 0) {
+    cli.log('No OAuth sessions.')
+  } else {
+    cli.table(sessions, {
+      printHeader: null,
+      columns: [
+        {key: 'description', format: name => cli.color.green(name)},
+        {key: 'id'}
+      ]
+    })
+  }
+}
+
+module.exports = {
+  topic: 'sessions',
+  description: 'list your OAuth sessions',
+  needsAuth: true,
+  flags: [
+    {name: 'json', description: 'output in json format'}
+  ],
+  run: cli.command(co.wrap(run))
+}
--- packages/heroku-cli-oauth/index.js
+++ packages/heroku-cli-oauth/index.js
@@ -1,9 +1,10 @@
 'use strict'
 
-exports.topic = {
-  name: 'clients',
-  description: 'manage OAuth clients on the platform'
-}
+exports.topics = [
+  {name: 'authorizations', description: 'OAuth authorizations'},
+  {name: 'clients', description: 'OAuth clients on the platform'},
+  {name: 'sessions', description: 'OAuth sessions'}
+]
 
 exports.commands = [
   require('./commands/authorizations'),
@@ -11,5 +12,6 @@ exports.commands = [
   require('./commands/clients/create'),
   require('./commands/clients/info'),
   require('./commands/clients/update'),
-  require('./commands/clients/destroy')
+  require('./commands/clients/destroy'),
+  require('./commands/sessions')
 ]
