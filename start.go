7a8fb5ee69438a61a87c63658e5e93c466602d27
Jeff Dickey <dickeyxxx@gmail.com>
2016-05-16 19:55:38 -0700

    do not run under sudo
    Fixes #156

--- start.go
+++ start.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"strings"
 )
 
@@ -28,6 +30,7 @@ var Args []string
 func Start(args ...string) {
 	Args = args
 	loadNewCLI()
+	stopIfSudo()
 
 	ShowDebugInfo()
 
@@ -79,4 +82,9 @@ func ShowDebugInfo() {
 		info = append(info, fmt.Sprintf("cmd: %s", Args[1]))
 	}
 	Debugln(strings.Join(info, " "))
+}
+func stopIfSudo() {
+	if os.Getenv("USER") == "root" && os.Getenv("SUDO_USER") != "" {
+		log.Fatalln("Refusing to run under sudo")
+	}
 }
