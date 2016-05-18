f570a78b6306136d99d2c8674532c4db68ff177d
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2016-05-18 12:36:31 -0700

    alternative stdout intercept

--- packages/heroku-run/test/commands/run.js
+++ packages/heroku-run/test/commands/run.js
@@ -1,20 +1,17 @@
 'use strict'
-/* globals describe it beforeEach commands apikey */
+/* globals describe it commands apikey */
 
 const cmd = commands.find(c => c.topic === 'run' && !c.command)
 const expect = require('unexpected')
-const intercept = require('intercept-stdout')
+const StdOutFixture = require('fixture-stdout')
+const fixture = new StdOutFixture()
 
 describe('run', () => {
-  let stdout, unhook
-
-  beforeEach(() => {
-    unhook = intercept(o => { stdout = o })
-  })
-
   it('runs a command', () => {
+    let stdout = ''
+    fixture.capture(s => { stdout += s })
     return cmd.run({app: 'heroku-run-test-app', flags: {}, auth: {password: apikey}, args: ['echo', '1', '2', '3']})
-    .then(() => unhook())
+    .then(() => fixture.release())
     .then(() => expect(stdout, 'to equal', '1 2 3\n'))
   })
 })
