fbb3af4733efe1f09297264a6027dac2e1ebffc4
Josh Sullivan <josh@dipperstove.com>
2016-05-18 10:57:20 -0700

    Merge pull request #20 from heroku/space-creation-spend-alert
    spaces: add spend alert to space creation output

--- packages/heroku-spaces/commands/create.js
+++ packages/heroku-spaces/commands/create.js
@@ -17,7 +17,10 @@ function * run (context, heroku) {
       log_drain_url: context.flags['log-drain-url']
     }
   })
-  space = yield cli.action(`Creating space ${cli.color.green(space)} in organization ${cli.color.cyan(context.org)}`, request)
+  space = yield cli.action(`
+${cli.color.bold('Spend Alert.')} Each Heroku Private Space costs $1000 in Add-on Credits/month (pro-rated to the second).
+
+Creating space ${cli.color.green(space)} in organization ${cli.color.cyan(context.org)}`, request)
   cli.styledHeader(space.name)
   cli.styledObject({
     ID: space.id,
