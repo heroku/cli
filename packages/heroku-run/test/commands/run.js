ec202fa8fa16afbb3412397956a7c78ac9265491
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2016-05-18 14:07:40 -0700

    testing env vars

--- packages/heroku-run/test/commands/run.js
+++ packages/heroku-run/test/commands/run.js
@@ -18,6 +18,15 @@ describe('run', () => {
     .then(() => expect(stdout, 'to equal', '1 2 3\n'))
   })
 
+  it('runs a command with env vars', () => {
+    let stdout = ''
+    let fixture = new StdOutFixture()
+    fixture.capture(s => { stdout += s })
+    return cmd.run({app: 'heroku-run-test-app', flags: {env: 'FOO=bar'}, auth: {password: apikey}, args: ['env']})
+    .then(() => fixture.release())
+    .then(() => expect(stdout, 'to contain', 'FOO=bar'))
+  })
+
   it('gets 127 status for invalid command', () => {
     return expect(cmd.run({app: 'heroku-run-test-app', flags: {'exit-code': true}, auth: {password: apikey}, args: ['invalid-command']})
                   , 'to be rejected with', 'Process exited with code 127')
