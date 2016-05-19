0ff993f30e266de5654fefbac33fafadd0773759
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2016-05-19 12:28:12 -0700

    updated deps

--- packages/heroku-status/commands/status.js
+++ packages/heroku-status/commands/status.js
@@ -1,9 +1,7 @@
 'use strict'
 
-let cli = require('heroku-cli-util')
-let moment = require('moment')
-let co = require('co')
-let _ = require('lodash')
+const cli = require('heroku-cli-util')
+const co = require('co')
 
 function capitalize (str) {
   return str.substr(0, 1).toUpperCase() + str.substr(1)
@@ -20,6 +18,10 @@ function printStatus (status) {
 }
 
 function * run (context) {
+  const moment = require('moment')
+  const maxBy = require('lodash.maxby')
+  const padEnd = require('lodash.padend')
+
   let host = process.env.HEROKU_STATUS_HOST || 'https://status.heroku.com'
   let response = (yield cli.got(host + '/api/v3/current-status', {
     path: '/api/v3/current-status',
@@ -39,9 +41,9 @@ function * run (context) {
     cli.log()
     cli.styledHeader(`${incident.title} ${cli.color.yellow(moment(incident.created_at).format('LT'))} ${cli.color.cyan(incident.full_url)}`)
 
-    let padding = _.maxBy(incident.updates, 'update_type.length').update_type.length + 1
+    let padding = maxBy(incident.updates, 'update_type.length').update_type.length + 1
     incident.updates.forEach((u) => {
-      cli.log(`${cli.color.yellow(_.padEnd(u.update_type, padding))} ${moment(u.updated_at).format('LT')} (${moment(u.updated_at).fromNow()})`)
+      cli.log(`${cli.color.yellow(padEnd(u.update_type, padding))} ${moment(u.updated_at).format('LT')} (${moment(u.updated_at).fromNow()})`)
       cli.log(`${u.contents}
 `)
     })
