382927917a0471316fc8bd9b13e92183b24219e2
Jingwen Owen Ou <jingweno@gmail.com>
2016-07-27 13:07:10 -0700

    Add filter for private and common runtimes (#117)
    * Add filter for private and common runtimes
    
    Add flag `--private` to shows private runtimes and flag `--common` to show common runtimes.
    
    * Use es6 `filter`
    
    * More concise functions

--- packages/heroku-apps/commands/regions/index.js
+++ packages/heroku-apps/commands/regions/index.js
@@ -7,7 +7,13 @@ function * run (context, heroku) {
   const sortBy = require('lodash.sortby')
 
   let regions = yield heroku.get('/regions')
+  if (context.flags.private) {
+    regions = regions.filter(region => region.private_capable)
+  } else if (context.flags.common) {
+    regions = regions.filter(region => !region.private_capable)
+  }
   regions = sortBy(regions, ['private_capable', 'name'])
+
   if (context.flags.json) {
     cli.log(JSON.stringify(regions, 0, 2))
   } else {
@@ -26,7 +32,9 @@ module.exports = {
   description: 'list available regions for deployment',
   needsAuth: true,
   flags: [
-    {name: 'json', description: 'output in json format'}
+    {name: 'json', description: 'output in json format'},
+    {name: 'private', description: 'show regions for private spaces'},
+    {name: 'common', description: 'show regions for common runtime'}
   ],
   run: cli.command(co.wrap(run))
 }
--- packages/heroku-apps/test/commands/regions/index.js
+++ packages/heroku-apps/test/commands/regions/index.js
@@ -13,16 +13,48 @@ describe('regions', function () {
     let api = nock('https://api.heroku.com:443')
       .get('/regions')
       .reply(200, [
-        {name: 'eu', description: 'Europe'},
-        {name: 'us', description: 'United States'},
+        {name: 'eu', description: 'Europe', private_capable: false},
+        {name: 'us', description: 'United States', private_capable: false},
         {name: 'oregon', description: 'Oregon, United States', private_capable: true}
       ])
     return cmd.run({flags: {}})
       .then(() => expect(cli.stdout).to.equal(`ID      Location               Runtime
 ──────  ─────────────────────  ──────────────
-oregon  Oregon, United States  Private Spaces
 eu      Europe                 Common Runtime
 us      United States          Common Runtime
+oregon  Oregon, United States  Private Spaces
+`))
+      .then(() => api.done())
+  })
+
+  it('filters private regions', function () {
+    let api = nock('https://api.heroku.com:443')
+      .get('/regions')
+      .reply(200, [
+        {name: 'eu', description: 'Europe', private_capable: false},
+        {name: 'us', description: 'United States', private_capable: false},
+        {name: 'oregon', description: 'Oregon, United States', private_capable: true}
+      ])
+    return cmd.run({flags: { private: true }})
+      .then(() => expect(cli.stdout).to.equal(`ID      Location               Runtime
+──────  ─────────────────────  ──────────────
+oregon  Oregon, United States  Private Spaces
+`))
+      .then(() => api.done())
+  })
+
+  it('filters common regions', function () {
+    let api = nock('https://api.heroku.com:443')
+      .get('/regions')
+      .reply(200, [
+        {name: 'eu', description: 'Europe', private_capable: false},
+        {name: 'us', description: 'United States', private_capable: false}
+      ])
+    return cmd.run({flags: { common: true }})
+      .then(() => expect(cli.stdout).to.equal(`ID  Location       Runtime
+──  ─────────────  ──────────────
+eu  Europe         Common Runtime
+us  United States  Common Runtime
 `))
       .then(() => api.done())
   })
