10172001a9261ee6f7588dd4589f8e7689900f85
Mark Turner <mark@amerine.net>
2016-08-06 19:41:50 -0700

    peering: Add peering support.
    This provide the full suite of vpc-peering related CLI commands.

---
+++ packages/heroku-spaces/lib/peering.js
@@ -0,0 +1,92 @@
+'use strict'
+
+let cli = require('heroku-cli-util')
+
+module.exports = function (heroku) {
+  function getPeeringInfo (space) {
+    return request('GET', `/spaces/${space}/peering-info`)
+  }
+
+  function getPeerings (space) {
+    return request('GET', `/spaces/${space}/peerings`)
+  }
+
+  function acceptPeeringRequest (space, pcxID) {
+    return request('POST', `/spaces/${space}/peerings`, {pcx_id: pcxID})
+  }
+
+  function destroyPeeringRequest (space, pcxID) {
+    return request('DELETE', `/spaces/${space}/peerings/${pcxID}`)
+  }
+
+  function displayCIDR (cidr) {
+    if (!cidr || cidr.length === 0) return ''
+    return cidr.join(', ')
+  }
+
+  function displayPeeringInfo (space, info) {
+    cli.styledHeader(`${space} Peering Info`)
+    cli.styledObject({
+      'AWS Account ID': info.aws_account_id,
+      'AWS Region': info.aws_region,
+      'AWS VPC ID': info.vpc_id,
+      'AWS VPC CIDR': info.vpc_cidr,
+      'Dyno CIDRs': displayCIDR(info.dyno_cidr_blocks),
+      'Unavailable CIDRs': displayCIDR(info.unavailable_cidr_blocks)
+    }, ['AWS Account ID', 'AWS Region', 'AWS VPC ID', 'AWS VPC CIDR', 'Dyno CIDRs', 'Unavailable CIDRs'])
+  }
+
+  function displayPeers (space, peers) {
+    var sF = function (s) {
+      var colored = s
+      switch (s) {
+        case 'active':
+          colored = `${cli.color.green(colored)}`
+          break
+        case 'pending-acceptance':
+        case 'provisioning':
+          colored = `${cli.color.yellow(colored)}`
+          break
+        case 'expired':
+        case 'failed':
+        case 'deleted':
+        case 'rejected':
+          colored = `${cli.color.red(colored)}`
+          break
+      }
+
+      return colored
+    }
+
+    cli.styledHeader(`${space} Peerings`)
+    cli.table(peers, {
+      columns: [
+        {key: 'pcx_id', label: 'PCX ID'},
+        {key: 'type', label: 'Type'},
+        {key: 'cidr_block', label: 'CIDR Block'},
+        {key: 'status', label: 'Status', format: status => sF(status)},
+        {key: 'aws_vpc_id', label: 'VPC ID'},
+        {key: 'aws_account_id', label: 'AWS Account ID'},
+        {key: 'expires', label: 'Expires'}
+      ]
+    })
+  }
+
+  function request (method, path, body) {
+    return heroku.request({
+      method: method,
+      path: path,
+      body: body,
+      headers: {Accept: 'application/vnd.heroku+json; version=3.dogwood'}
+    })
+  }
+
+  return {
+    getPeeringInfo,
+    displayPeeringInfo,
+    displayPeers,
+    getPeerings,
+    acceptPeeringRequest,
+    destroyPeeringRequest
+  }
+}
