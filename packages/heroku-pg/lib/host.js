fddc523eaeee14ef539483001774f797c35cf68f
Timothée Peignier <timothee.peignier@tryphon.org>
2016-08-22 15:41:32 -0700

    Don't make assumption about hostname (#18)

--- packages/heroku-pg/lib/host.js
+++ packages/heroku-pg/lib/host.js
@@ -1,14 +1,14 @@
 'use strict'
 
 module.exports = function (addon) {
-  let appHost = process.env.HEROKU_POSTGRESQL_HOST
+  let host = process.env.HEROKU_POSTGRESQL_HOST
   let plan = addon.plan.name
 
   if (plan.endsWith('dev') || plan.endsWith('basic')) {
-    if (appHost) return `https://${appHost}.herokuapp.com`
+    if (host) return `https://${host}`
     return 'https://postgres-starter-api.heroku.com'
   } else {
-    if (appHost) return `https://${appHost}.herokuapp.com`
+    if (host) return `https://${host}`
     return 'https://postgres-api.heroku.com'
   }
 }
--- packages/heroku-pg/test/lib/host.js
+++ packages/heroku-pg/test/lib/host.js
@@ -14,7 +14,7 @@ describe('host', () => {
   })
 
   context('with HEROKU_POSTGRESQL_HOST set', () => {
-    beforeEach(() => { process.env.HEROKU_POSTGRESQL_HOST = 'foo' })
+    beforeEach(() => { process.env.HEROKU_POSTGRESQL_HOST = 'foo.herokuapp.com' })
     afterEach(() => delete process.env.HEROKU_POSTGRESQL_HOST)
 
     it('shows dev host', () => {
