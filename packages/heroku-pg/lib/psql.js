55b6c0a4216fb8d24eeaca16e4446809bab2ea1b
Jeff Dickey <dickeyxxx@gmail.com>
2016-08-30 15:29:48 -0700

    fix psql on windows by not using execa (#19)
    * fix psql on windows by not using execa
    
    * pipe stderr to display
    
    * reuse exit status of psql
    
    * require sslmode

--- packages/heroku-pg/lib/psql.js
+++ packages/heroku-pg/lib/psql.js
@@ -4,28 +4,29 @@ const co = require('co')
 const debug = require('debug')('psql')
 
 function * exec (db, query) {
-  const execa = require('execa')
+  const stripEOF = require('strip-eof')
+  const {spawnSync} = require('child_process')
   const cli = require('heroku-cli-util')
 
   debug(query)
-  let env = {
+  let env = Object.assign({}, process.env, {
     PGAPPNAME: 'psql non-interactive',
+    PGSSLMODE: 'require',
     PGUSER: db.user,
     PGPASSWORD: db.password,
     PGDATABASE: db.database,
     PGPORT: db.port,
     PGHOST: db.host
-  }
-  try {
-    let {stdout, stderr} = yield execa.shell(`psql -c "${query}"`, {env})
-    process.stderr.write(stderr)
-    return stdout
-  } catch (err) {
+  })
+  let {stdout, error: err, status} = spawnSync('psql', ['--command', query], {env, encoding: 'utf8', stdio: [0, 'pipe', 2]})
+  if (err) {
     if (err.code !== 'ENOENT') throw err
     cli.error(`The local psql command could not be located.
 For help installing psql, see https://devcenter.heroku.com/articles/heroku-postgresql#local-setup`)
     process.exit(1)
   }
+  if (status !== 0) process.exit(status)
+  return stripEOF(stdout)
 }
 
 module.exports = {
