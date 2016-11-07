7d26ca50d453f865e6811830d99b6a9fb1c72953
Andrew Appleton <andysapple@gmail.com>
2016-11-07 11:23:47 +0000

    Add heroku ci:info

--- packages/heroku-ci/commands/ci/info.js
+++ packages/heroku-ci/commands/ci/info.js
@@ -1,10 +1,32 @@
+const cli = require('heroku-cli-util')
+const co = require('co')
+const api = require('../../lib/heroku-api')
+
+function* run (context, heroku) {
+  const coupling = yield api.pipelineCoupling(heroku, context.app)
+  const pipelineID = coupling.pipeline.id
+  const pipelineRepository = yield api.pipelineRepository(heroku, pipelineID)
+
+  cli.styledHeader(`CI settings for ${coupling.pipeline.name}`)
+  const output = {
+    repository: pipelineRepository.repository.name,
+    automatic_test_runs: pipelineRepository.ci
+  }
+
+  if (pipelineRepository.organization) {
+    output.organization = pipelineRepository.organization.name
+  }
+
+  cli.styledHash(output)
+}
+
 module.exports = {
   topic: 'ci',
   command: 'info',
   default: true,
-  description: 'CI overivew',
+  needsApp: true,
+  needsAuth: true,
+  description: 'CI overview',
   help: 'display CI information for the given pipeline',
-  run: function (context) {
-    console.log('Hello, World!')
-  }
+  run: cli.command(co.wrap(run))
 }
---
+++ packages/heroku-ci/test/commands/ci/info-test.js
@@ -0,0 +1,50 @@
+const nock = require('nock')
+const expect = require('chai').expect
+const cli = require('heroku-cli-util')
+const cmd = require('../../../commands/ci/info')
+
+describe('heroku ci:info', function () {
+  let app, coupling, pipelineRepository
+
+  beforeEach(function () {
+    cli.mockConsole()
+    app = '123-app'
+
+    coupling = {
+      pipeline: {
+        id: '123-abc',
+        name: 'test-pipeline'
+      }
+    }
+
+    pipelineRepository = {
+      ci: true,
+      organization: {
+        name: 'heroku-devex'
+      },
+
+      repository: {
+        name: 'heroku/heroku'
+      }
+    }
+  })
+
+  it('displays pipeline and repo info', function () {
+    const api = nock('https://api.heroku.com')
+      .get(`/apps/${app}/pipeline-couplings`)
+      .reply(200, coupling)
+
+    const kolkrabbi = nock('https://kolkrabbi.herokuapp.com')
+      .get(`/pipelines/${coupling.pipeline.id}/repository`)
+      .reply(200, pipelineRepository)
+
+    return cmd.run({ app }).then(() => {
+      expect(cli.stdout).to.contain(coupling.pipeline.name)
+      expect(cli.stdout).to.contain(pipelineRepository.repository.name)
+      expect(cli.stdout).to.contain(pipelineRepository.organization.name)
+
+      api.done()
+      kolkrabbi.done()
+    })
+  })
+})
