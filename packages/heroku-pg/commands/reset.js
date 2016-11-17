4b86a2a1083ed029bc907b1165deca7bb2bb4d04
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2016-11-17 10:20:21 -0800

    added pg:reset (#39)

---
+++ packages/heroku-pg/commands/reset.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const cli = require('heroku-cli-util')
+const co = require('co')
+
+function * run (context, heroku) {
+  const host = require('../lib/host')
+  const fetcher = require('../lib/fetcher')(heroku)
+  let {app, args, flags} = context
+  let db = yield fetcher.addon(app, args.database)
+
+  if (!app) app = db.app.name
+
+  yield cli.confirmApp(app, flags.confirm, `WARNING: Destructive action
+${cli.color.addon(db.name)} will lose all of its data
+`)
+
+  yield cli.action(`Resetting ${cli.color.addon(db.name)}`, co(function * () {
+    yield heroku.put(`/client/v11/databases/${db.name}/reset`, {host: host(db)})
+  }))
+}
+
+module.exports = {
+  topic: 'pg',
+  command: 'reset',
+  description: 'delete all data in DATABASE',
+  wantsApp: true,
+  needsAuth: true,
+  args: [{name: 'database', optional: true}],
+  flags: [{name: 'confirm', char: 'c', hasValue: true}],
+  run: cli.command({preauth: true}, co.wrap(run))
+}
---
+++ packages/heroku-pg/test/commands/reset.js
@@ -0,0 +1,43 @@
+'use strict'
+/* global describe it beforeEach afterEach */
+
+const cli = require('heroku-cli-util')
+const expect = require('unexpected')
+const nock = require('nock')
+const proxyquire = require('proxyquire')
+
+const addon = {
+  name: 'postgres-1',
+  plan: {name: 'heroku-postgresql:standard-0'}
+}
+const fetcher = () => {
+  return {
+    addon: () => addon
+  }
+}
+
+const cmd = proxyquire('../../commands/reset', {
+  '../lib/fetcher': fetcher
+})
+
+describe('pg:reset', () => {
+  let api, pg
+
+  beforeEach(() => {
+    api = nock('https://api.heroku.com')
+    pg = nock('https://postgres-api.heroku.com')
+    cli.mockConsole()
+  })
+
+  afterEach(() => {
+    nock.cleanAll()
+    pg.done()
+    api.done()
+  })
+
+  it('reset db', () => {
+    pg.put('/client/v11/databases/postgres-1/reset').reply(200)
+    return cmd.run({app: 'myapp', args: {}, flags: {confirm: 'myapp'}})
+    .then(() => expect(cli.stderr, 'to equal', 'Resetting postgres-1... done\n'))
+  })
+})
