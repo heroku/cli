0023448a52536995dd243664fa4f090085a21a71
Ransom Briggs <rbriggs@heroku.com>
2016-11-17 08:39:01 -0600

    Set stdout / stderr with to 80 to fix tests

--- packages/heroku-cli-plugin-certs-v5/test/helpers.js
+++ packages/heroku-cli-plugin-certs-v5/test/helpers.js
@@ -5,5 +5,8 @@ global.cli = require('heroku-cli-util')
 cli.raiseErrors = true
 cli.color.enabled = false
 
+process.stdout.columns = 80
+process.stderr.columns = 80
+
 let nock = require('nock')
 nock.disableNetConnect()
