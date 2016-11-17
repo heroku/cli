36bd75c84a2964c8284bfc6f885798cf4dd0e111
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2016-11-17 11:53:44 -0800

    added pg:backups:cancel (#27)
    * added pg:backups:cancel
    
    * fixed test descriptions

--- packages/heroku-pg/index.js
+++ packages/heroku-pg/index.js
@@ -8,6 +8,7 @@ exports.topics = [
 
 exports.commands = flatten([
   require('./commands/backups'),
+  require('./commands/backups/cancel'),
   require('./commands/backups/delete'),
   require('./commands/backups/info'),
   require('./commands/backups/schedule'),
---
+++ packages/heroku-pg/test/commands/backups/cancel.js
@@ -0,0 +1,48 @@
+'use strict'
+/* global describe it beforeEach afterEach context */
+
+const cli = require('heroku-cli-util')
+const expect = require('unexpected')
+const nock = require('nock')
+const cmd = require('../../..').commands.find(c => c.topic === 'pg' && c.command === 'backups:cancel')
+
+describe('pg:backups:cancel', () => {
+  let pg
+
+  beforeEach(() => {
+    pg = nock('https://postgres-api.heroku.com')
+    pg.post('/client/v11/apps/myapp/transfers/100-001/actions/cancel').reply(200, {})
+    cli.mockConsole()
+  })
+
+  afterEach(() => {
+    nock.cleanAll()
+    pg.done()
+  })
+
+  context('with no id', () => {
+    beforeEach(() => {
+      pg.get('/client/v11/apps/myapp/transfers').reply(200, [
+        {succeeded: true, to_type: 'gof3r', num: '3', uuid: '100-001'}
+      ])
+    })
+
+    it('cancels backup', () => {
+      return cmd.run({app: 'myapp', args: {}})
+      .then(() => expect(cli.stderr, 'to equal', 'Cancelling b003... done\n'))
+    })
+  })
+
+  context('with id', () => {
+    beforeEach(() => {
+      pg.get('/client/v11/apps/myapp/transfers/3').reply(200, {
+        succeeded: true, to_type: 'gof3r', num: '3', uuid: '100-001'
+      })
+    })
+
+    it('cancels backup', () => {
+      return cmd.run({app: 'myapp', args: {backup_id: 'b003'}})
+      .then(() => expect(cli.stderr, 'to equal', 'Cancelling b003... done\n'))
+    })
+  })
+})
