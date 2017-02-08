d7216885e261abf1b8c26de188d991d189ff63ac
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-02-07 19:20:12 -0800

    integration test

--- test/cli.js
+++ test/cli.js
@@ -2,17 +2,24 @@ const {describe, it, beforeEach, afterEach} = require('mocha')
 
 describe('cli', () => {
   beforeEach(function () {
+    this.log = console.log
+    this.argv = process.argv
     this.exit = process.exit
-    process.exit = code => { this.code = code }
   })
 
   afterEach(function () {
+    console.log = this.log
     process.exit = this.exit
-    delete this.code
+    process.argv = this.argv
   })
 
   it('runs the version command', async function () {
-    await require('../cli')('version')
+    this.output = ''
+    console.log = output => { this.output += output + '\n' }
+    process.exit = code => { this.code = code }
+    process.argv = ['node', 'heroku', 'version']
+    await require('../cli')
+    this.output.should.match(/^heroku-cli/)
     this.code.should.eq(0)
   })
 })
