17855e3a463b02ab3c2a5669944613dbed2f274f
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2017-02-08 17:01:29 -0800

    show commands

--- cli.js
+++ cli.js
@@ -24,6 +24,7 @@ async function main () {
   try {
     console.dir(process.argv)
     console.dir(argv)
+    console.dir(plugins.commands)
     if (argv.length < 2) await help()
     await run(plugins.commands.find(argv[1]))
     plugins.load()
