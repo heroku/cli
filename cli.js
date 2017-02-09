080d477bfd7a393b464f024c57fe1aa008417c85
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-02-08 20:11:57 -0800

    starting work on updater

--- cli.js
+++ cli.js
@@ -2,7 +2,18 @@ const plugins = require('./lib/plugins')
 let argv = process.argv.slice(2)
 argv.unshift('heroku')
 
+function onexit (options) {
+  const ansi = require('ansi-escapes')
+  process.stderr.write(ansi.cursorShow)
+  if (options.exit) process.exit(1)
+}
+
+process.on('exit', onexit)
+process.on('SIGINT', onexit.bind(null, {exit: true}))
+process.on('uncaughtException', onexit.bind(null, {exit: true}))
+
 async function main () {
+  let command
   try {
     let Command = plugins.commands.find(argv[1])
     if (!Command) Command = plugins.load().commands.find(argv[1])
@@ -12,13 +23,14 @@ async function main () {
       const {convertLegacy} = require('heroku-cli-command')
       Command = convertLegacy(Command)
     }
-    let command = new Command({argv})
+    command = new Command({argv})
     await command.init()
     await command.run()
     await command.done()
     process.exit(0)
   } catch (err) {
-    console.error(err)
+    if (command) command.error(err)
+    else console.error(err)
     process.exit(1)
   }
 }
