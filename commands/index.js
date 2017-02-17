df85cff66c0fb9688e5fbd1e5bec8706fe4f59ec
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2017-02-17 10:38:39 -0800

    migrating to cli-engine

--- commands/index.js
+++
@@ -1,9 +0,0 @@
-const klaw = require('klaw-sync')
-const {Topic} = require('heroku-cli-command')
-
-class Plugins extends Topic {}
-Plugins.topic = 'plugins'
-Plugins.description = 'manage heroku plugins'
-exports.topics = [Plugins]
-
-exports.commands = klaw(__dirname, {nodir: true}).map(f => require(f.path))
