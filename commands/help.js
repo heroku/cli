bf17e167737e7984c3765f62288f7ad687d7bf00
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2017-02-16 18:27:50 -0800

    fix command listing

--- commands/help.js
+++ commands/help.js
@@ -1,10 +1,12 @@
 const {Command} = require('heroku-cli-command')
 const util = require('../lib/util')
 
+const argv0 = 'heroku'
+
 class Help extends Command {
   async run () {
     let cmd = this.args.find(arg => !['help', '-h', '--help'].includes(arg))
-    if (!cmd) return this.topics()
+    if (!cmd) return this.topics({argv0})
     let topic = this.plugins.topics[cmd.split(':')[0]]
     let matchedCommand = this.plugins.commands[cmd]
     if (!topic && !matchedCommand) throw new Error(`command ${cmd} not found`)
@@ -19,27 +21,27 @@ class Help extends Command {
       Topic.description = topic.description
     }
     topic = new Topic({flags: this.flags})
-    await topic.help({commands, args: this.args, matchedCommand, argv0: 'heroku'})
+    await topic.help({commands, args: this.args, matchedCommand, argv0})
   }
 
   get plugins () {
     return require('../lib/plugins')
   }
 
-  topics () {
+  topics ({argv0}) {
     const max = require('lodash.maxby')
     const S = require('string')
 
     this.log(`Usage: ${this.argv[0]} COMMAND [--app APP] [command-specific-options]
 
-  Help topics, type ${this.color.cmd(this.argv[0] + ' help TOPIC')} for more details:
+  Help topics, type ${this.color.cmd(argv0 + ' help TOPIC')} for more details:
   `)
     let topics = Object.keys(this.plugins.topics).map(t => this.plugins.topics[t])
     topics = topics.filter(t => !t.hidden)
-    topics.sort(util.compare('name'))
-    let maxlength = max(topics, 'name.length').name.length
+    topics.sort(util.compare('topic'))
+    let maxlength = max(topics, 'topic.length').topic.length
     for (let topic of topics) {
-      this.log(`  ${this.argv[0]} ${S(topic.name).padRight(maxlength)}${topic.description ? ' # ' + topic.description : ''}`)
+      this.log(`  ${argv0} ${S(topic.topic).padRight(maxlength)}${topic.description ? ' # ' + topic.description : ''}`)
     }
 
     this.log()
