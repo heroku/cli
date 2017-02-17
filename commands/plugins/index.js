49dafbc0c5dce4eb04d461e27fb87f085c006380
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-02-16 16:14:17 -0800

    added plugins index

--- commands/plugins/index.js
+++ commands/plugins/index.js
@@ -1,9 +1,19 @@
 const {Command} = require('heroku-cli-command')
+const util = require('../../lib/util')
 
 class Plugins extends Command {
   async run () {
-    const plugins = require('../../lib/plugins')
-    console.dir(plugins.list())
+    let plugins = require('../../lib/plugins')
+    plugins = plugins.list()
+    plugins = Object.keys(plugins).map(p => plugins[p])
+    plugins = plugins.filter(p => p.type !== 'builtin')
+    plugins.sort(util.compare('name'))
+    if (!this.flags.core) plugins = plugins.filter(p => p.type !== 'core')
+    for (let plugin of plugins) {
+      let output = `${plugin.name} ${plugin.version}`
+      if (plugin.type !== 'user') output += ` (${plugin.type})`
+      this.log(output)
+    }
   }
 }
 
