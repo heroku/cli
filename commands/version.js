2254c005e7762cd6a163f9d7739033e443baaa9e
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-02-16 20:34:55 -0800

    added dev output

--- commands/version.js
+++ commands/version.js
@@ -3,6 +3,7 @@ const {Command} = require('heroku-cli-command')
 class Version extends Command {
   async run () {
     let version = require('../lib/version')
+    if (process.env.HEROKU_DEV) this.log(`HEROKU_DEV=${process.env.HEROKU_DEV}`)
     this.log(`heroku-cli/${version} (${process.platform}-${process.arch}) node-${process.version}`)
   }
 }
