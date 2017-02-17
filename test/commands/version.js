48cec60f52cd566a7afbc891c01dace1ca559a8c
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-02-17 11:18:38 -0800

    generalizing work

--- test/commands/version.js
+++
@@ -1,9 +0,0 @@
-const {describe, it} = require('mocha')
-const exec = require('heroku-cli-test')(require('../../commands'))
-
-describe('version', () => {
-  it('shows the version', async function () {
-    let {stdout} = await exec('version')
-    stdout.should.match(/^heroku-cli/)
-  })
-})
