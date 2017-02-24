400942f473b90c184a1f66bd63c6475bbf4428a1
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2017-02-24 15:57:45 -0800

    update cli-engine

--- bin/run.js
+++ bin/run.js
@@ -1,3 +1,8 @@
-require('cli-engine')({
-  disableUpdate: `Update CLI with \`npm update -g heroku-cli\``
+const path = require('path')
+const CLI = require('cli-engine').default
+const cli = new CLI({
+  root: path.join(__dirname, '..'),
+  updateDisabled: `Update CLI with \`npm update -g heroku-cli\``,
+  argv: process.argv.slice(1)
 })
+cli.run()
