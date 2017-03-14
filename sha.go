f154d612b58303b5fcf25362129ad46b17536618
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2017-03-13 19:39:23 -0700

    v5.99

--- sha.go
+++ sha.go
@@ -5,7 +5,6 @@ import (
 	"encoding/hex"
 	"io"
 	"io/ioutil"
-	"os"
 )
 
 // gets the sha of a stream and returns a tee to the same stream
@@ -18,17 +17,4 @@ func computeSha(reader io.Reader) (func() string, io.Reader) {
 		return hex.EncodeToString(hasher.Sum(nil))
 	}
 	return getSha, tee
-}
-
-func fileSha256(path string) (string, error) {
-	hasher := sha256.New()
-	f, err := os.Open(path)
-	if err != nil {
-		return "", err
-	}
-	defer f.Close()
-	if _, err := io.Copy(hasher, f); err != nil {
-		return "", err
-	}
-	return hex.EncodeToString(hasher.Sum(nil)), nil
 }
