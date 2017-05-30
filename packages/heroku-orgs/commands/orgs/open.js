5cafe486dbb347a44c10d12b3265a10bdbe31696
RasPhilCo <capt.pn@gmail.com>
2017-05-30 14:03:47 -0700

    Check --team before erroring (#77)

--- packages/heroku-orgs/commands/orgs/open.js
+++ packages/heroku-orgs/commands/orgs/open.js
@@ -5,7 +5,9 @@ let co = require('co')
 const {flags} = require('cli-engine-heroku')
 
 function * run (context, heroku) {
-  let org = yield heroku.get(`/organizations/${context.org || context.team || context.flags.team}`)
+  let team = context.org || context.team || context.flags.team
+  if (!team) throw new Error('No organization specified')
+  let org = yield heroku.get(`/organizations/${team}`)
   yield cli.open(`https://dashboard.heroku.com/orgs/${org.name}`)
 }
 
@@ -15,7 +17,7 @@ module.exports = {
   description: 'open the organization interface in a browser window',
   needsAuth: true,
   flags: [
-    flags.org({name: 'org', hasValue: true, description: 'org to use', hidden: false, required: true}),
+    flags.org({name: 'org', hasValue: true, description: 'org to use', hidden: false}),
     flags.team({name: 'team', hasValue: true, hidden: true})
   ],
   run: cli.command(co.wrap(run))
