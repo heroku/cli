3554fd2aa3df0cc8c1d0f9827fc88f1ca05bff6c
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-06-05 10:49:11 -0700

    show arm warning about not receiving updates

--- main.go
+++ main.go
@@ -10,6 +10,9 @@ import (
 func main() {
 	defer handlePanic()
 	runtime.GOMAXPROCS(1) // more procs causes runtime: failed to create new OS thread on Ubuntu
+	if runtime.GOARCH == "arm" {
+		Warn("Using deprecated v5 Heroku CLI.\nWe no longer provide autoupdates for arm systems.\nPlease reinstall following npm instructions at https://cli.heroku.com to receive further updates.")
+	}
 
 	// handle sigint
 	handleSignal(os.Interrupt, func() {
