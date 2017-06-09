1b3f09bb9a8d14df34ab660c52da2f8f45585e8f
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2017-06-09 16:51:33 -0700

    install updates

--- resources/deb/install-ubuntu.sh
+++ resources/deb/install-ubuntu.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
 {
-    echo "This script requires superuser access to install apt packages."
-    echo "You will be prompted for your password by sudo."
-
-    # clear any previous sudo permission
-    sudo -k
+    set -ex
+    SUDO=''
+    if [ "$EUID" != "0" ] && [ "$(id -u)" != "0" ]; then
+      SUDO='sudo'
+      echo "This script requires superuser access to install apt packages."
+      echo "You will be prompted for your password by sudo."
+      # clear any previous sudo permission
+      sudo -k
+    fi
 
     # run inside sudo
-    sudo sh <<SCRIPT
+    $SUDO sh <<SCRIPT
   set -ex
 
-  # remove toolbelt
-  rm -f /etc/apt/sources.list.d/heroku.list
-  apt-get remove -y heroku-toolbelt heroku || true
-
   # add heroku repository to apt
-  add-apt-repository "deb https://cli-assets.heroku.com/branches/stable/apt ./"
+  echo "deb https://cli-assets.heroku.com/branches/stable/apt ./" > /etc/apt/sources.list.d/heroku.list
+
+  # remove toolbelt
+  apt-get remove -y heroku-toolbelt heroku || true
 
   # install heroku's release key for package verification
   wget -qO- https://cli-assets.heroku.com/apt/release.key | apt-key add -
