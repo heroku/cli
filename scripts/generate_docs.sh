acaa040b8ea576835c359af8b7b05ecc56dfcbd4
karatecowboy <moriger@heroku.com>
2017-06-15 13:27:37 -0500

    CLI doc generation

--- scripts/generate_docs.sh
+++ scripts/generate_docs.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-heroku-plugin-readme-generator ../heroku-apps ../heroku-certs ../heroku-ci ../heroku-cli-addons ../heroku-cli-oauth ../heroku-container-registry ../heroku-fork ../heroku-git ../heroku-local ../heroku-orgs ../heroku-pg ../heroku-pipelines ../heroku-ps-exec ../heroku-run ../heroku-spaces ../heroku-status > /tmp/cli-dev-center-docs.md
+heroku-plugin-readme-generator
