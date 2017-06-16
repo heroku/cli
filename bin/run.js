2fdd070de243c6b1de41e7555b59f1e8940122d9
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2017-06-16 11:41:26 -0700

    check node version

--- bin/run.js
+++ bin/run.js
@@ -1,11 +1,18 @@
 const path = require('path')
-const CLI = require('cli-engine').default
 const root = path.join(__dirname, '..')
+const pjson = require(path.join(root, 'package.json'))
+const semver = require('semver')
+const nodeVersion = process.version.split('v')[1]
+if (!semver.satisfies(nodeVersion, pjson.engines.node)) {
+  process.stderr.write(`WARNING\nWARNING Node version must be ${pjson.engines.node} to use the Heroku CLI\nWARNING\n`)
+}
+
+const CLI = require('cli-engine').default
 const cli = new CLI({
   argv: process.argv.slice(1),
   config: {
     root,
-    pjson: require(path.join(root, 'package.json')),
+    pjson,
     updateDisabled: 'Update CLI with `npm update -g heroku-cli`'
   }
 })
