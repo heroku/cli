80732339f7986c4aa72b2cf5dbac064fea56cfa4
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2017-06-23 12:53:28 -0700

    clear out old sources

--- resources/deb/install-ubuntu.sh
+++ resources/deb/install-ubuntu.sh
@@ -13,7 +13,12 @@
     # run inside sudo
     $SUDO sh <<SCRIPT
   set -ex
-  dpkg -s apt-transport-https 1>/dev/null 2>/dev/null || (apt-get update && apt-get install -y apt-transport-https)
+
+  # if apt-transport-https is not installed, clear out old sources, update, then install apt-transport-https
+  dpkg -s apt-transport-https 1>/dev/null 2>/dev/null || \
+    (echo "" > /etc/apt/sources.list.d/heroku.list \
+      && apt-get update \
+      && apt-get install -y apt-transport-https)
 
   # add heroku repository to apt
   echo "deb https://cli-assets.heroku.com/branches/stable/apt ./" > /etc/apt/sources.list.d/heroku.list
