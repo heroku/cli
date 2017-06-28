ac64ff66ada00cfb7abc226dfceb1b9f34f9698b
Matthew O <moriger@mykolab.com>
2017-06-28 18:01:38 -0500

    Examples and h4 (#84)
    * add overview to topic
    
    * fix failing test
    
    * remove newline

--- packages/heroku-cli-addons/commands/addons/upgrade.js
+++ packages/heroku-cli-addons/commands/addons/upgrade.js
@@ -107,17 +107,17 @@ See available plans with \`heroku addons:plans SERVICE\`.
 Note that \`heroku addons:upgrade\` and \`heroku addons:downgrade\` are the same.
 Either one can be used to change an add-on plan up or down.
 
-https://devcenter.heroku.com/articles/managing-add-ons
+[https://devcenter.heroku.com/articles/managing-add-ons](https://devcenter.heroku.com/articles/managing-add-ons)
 
 Examples:
 
   Upgrade an add-on by service name:
 
-  $ heroku addons:upgrade heroku-redis:premium-2
+    $ heroku addons:upgrade heroku-redis:premium-2
 
   Upgrade a specific add-on:
 
-  $ heroku addons:upgrade swimming-briskly-123 heroku-redis:premium-2
+    $ heroku addons:upgrade swimming-briskly-123 heroku-redis:premium-2
   `,
   needsAuth: true,
   wantsApp: true,
