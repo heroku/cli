513fa1b850bcc36c6026b06e462668f0f1220b12
Ransom Briggs <rbriggs@heroku.com>
2017-08-07 12:40:21 -0700

    Update heroku.cmd to check LOCALAPPDATA (#569)

--- resources/exe/heroku.cmd
+++ resources/exe/heroku.cmd
@@ -1,3 +1,10 @@
 @echo off
+
 set CLI_BINPATH=%~dp0\heroku.cmd
-"%~dp0\..\cli\bin\node.exe" "%~dp0\..\cli\bin\heroku.js" %*
+set CLI_LOCAL_BIN=%LOCALAPPDATA%\heroku\client\bin\heroku.cmd
+
+if exist "%CLI_LOCAL_BIN%" (
+  "%CLI_LOCAL_BIN%" %*
+) else (
+  "%~dp0\..\cli\bin\node.exe" "%~dp0\..\cli\bin\heroku.js" %*
+)
