99e333024a65f45f7b04ade55caf963cc7a6194d
Ransom Briggs <rbriggs@heroku.com>
2017-08-08 14:37:16 -0500

    Remove env var so as to not add to environment

--- resources/exe/heroku.cmd
+++ resources/exe/heroku.cmd
@@ -1,10 +1,9 @@
 @echo off
 
 set CLI_BINPATH=%~dp0\heroku.cmd
-set CLI_LOCAL_BIN=%LOCALAPPDATA%\heroku\client\bin\heroku.cmd
 
-if exist "%CLI_LOCAL_BIN%" (
-  "%CLI_LOCAL_BIN%" %*
+if exist "%LOCALAPPDATA%\heroku\client\bin\heroku.cmd" (
+  "%LOCALAPPDATA%\heroku\client\bin\heroku.cmd" %*
 ) else (
   "%~dp0\..\cli\bin\node.exe" "%~dp0\..\cli\bin\heroku.js" %*
 )
