7c7dad533b2b38bfdc02ff44ba63486f5c762270
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-08-31 20:59:51 -0700

    use env var for cf

--- bin/run.js
+++ bin/run.js
@@ -7,13 +7,8 @@ if (!semver.satisfies(nodeVersion, pjson.engines.node)) {
   process.stderr.write(`WARNING\nWARNING Node version must be ${pjson.engines.node} to use the Heroku CLI\nWARNING\n`)
 }
 
-const CLI = require('cli-engine').default
-const cli = new CLI({
-  argv: process.argv.slice(1),
-  config: {
-    root,
-    pjson,
-    updateDisabled: 'Update CLI with `npm update -g heroku-cli`'
-  }
-})
-cli.run()
+const {run} = require('cli-engine')
+const config = {
+  updateDisabled: 'Update CLI with `npm update -g heroku-cli`'
+}
+run({config})
