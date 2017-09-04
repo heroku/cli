dbb905f208a0a060d6478ec3618b88b80e3c4e91
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-09-04 11:19:20 -0700

    added update step

---
+++ src/hooks/update/analytics.js
@@ -0,0 +1,19 @@
+// @flow
+
+import type {Config} from 'cli-engine-config'
+
+const debug = require('debug')('heroku:analytics')
+
+async function run (config: Config) {
+  try {
+    console.dir(config)
+    debug('update')
+    const Analytics = require('../../analytics').default
+    const analytics = new Analytics(config)
+    await analytics.submit()
+  } catch (err) {
+    debug(err)
+  }
+}
+
+module.exports = run
