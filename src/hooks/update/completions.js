360a83fbb148cf2d7d1b87b8f0c8e8ebbeb36445
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-09-27 17:55:01 -0400

    fix completions

--- src/hooks/update/completions.js
+++ src/hooks/update/completions.js
@@ -1,6 +1,6 @@
 // @flow
 
-import Plugins from 'cli-engine'
+import Plugins from 'cli-engine/lib/plugins'
 import type {Config} from 'cli-engine-config'
 import type {PreRun} from 'cli-engine/lib/hooks'
 import {AppCompletion, PipelineCompletion, SpaceCompletion, TeamCompletion} from 'cli-engine-heroku/lib/completions'
@@ -9,21 +9,21 @@ const debug = require('debug')('heroku:completions')
 
 async function run (config: Config, opts: PreRun) {
   try {
-    if (this.config.windows) {
+    if (config.windows) {
       debug('skipping autocomplete on windows')
     } else {
-      const plugins = await new Plugins(this.config).list()
+      const plugins = await (new Plugins(config)).list()
       const acPlugin = plugins.find(p => p.name === 'heroku-cli-autocomplete')
       if (acPlugin) {
         let ac = await acPlugin.findCommand('autocomplete:init')
-        if (ac) await ac.run(this.config)
+        if (ac) await ac.run(config)
       } else {
         debug('skipping autocomplete, not installed')
       }
-      await AppCompletion.options({config: this.config})
-      await PipelineCompletion.options({config: this.config})
-      await SpaceCompletion.options({config: this.config})
-      await TeamCompletion.options({config: this.config})
+      await AppCompletion.options({config})
+      await PipelineCompletion.options({config})
+      await SpaceCompletion.options({config})
+      await TeamCompletion.options({config})
     }
   } catch (err) {
     debug(err)
