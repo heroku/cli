9e617be796890cfa9009ddbb56a1d5449900bd87
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2017-10-24 14:48:41 -0700

    update index.test

--- packages/heroku-cli-autocomplete/src/commands/autocomplete/index.test.js
+++ packages/heroku-cli-autocomplete/src/commands/autocomplete/index.test.js
@@ -10,13 +10,37 @@ let skipWindows = (os.platform() === 'windows' || os.platform() === 'win32') ? x
 skipWindows('outputs install instructions for zsh', async () => {
   cli.config.mock = true
   await Autocomplete.mock('zsh')
-  expect(cli.stdout.output).toMatch(`Add the autocomplete setup script to your .zshrc via:
+  expect(cli.stdout.output).toMatch(`Setup Instructions for Heroku CLI Autocomplete ---
+
+1) Add the autocomplete env vars to your zsh profile
 
 $ printf "$(heroku autocomplete:script zsh)" >> ~/.zshrc
 
-Run the following zsh command to ensure no permissions conflicts:
+2) Run compaudit to ensure no permissions conflicts are present (some versions of zsh may not have this command)
 
 $ compaudit
 
-Lastly, restart your shell`)
+3) Source your updated zsh profile
+
+$ source ~/.zshrc
+
+4) Test command completion by pressing <TAB>, e.g.:
+
+$ heroku <TAB>
+
+5) Test flag completion by pressing <TAB>, e.g.:
+
+$ heroku apps:info --<TAB>
+
+6) Test flag options completion by pressing <TAB>, e.g.:
+
+$ heroku apps:info --app=<TAB>
+
+
+To uninstall Heroku CLI Autocomplete:
+-- Uninstall this plugin from your CLI (for help see: heroku help plugins:uninstall)
+-- Delete the env vars from your zsh profile & restart your terminal
+
+
+Enjoy!`)
 })
