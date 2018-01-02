b5fb2faf94862447225569d363886506a99c6948
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-01-02 11:47:25 -0800

    only tidy if cache != data

--- src/hooks/update/tidy.ts
+++ src/hooks/update/tidy.ts
@@ -8,14 +8,12 @@ const debug = require('debug')('heroku:tidy')
 export default class extends Hook<'update'> {
   async run() {
     try {
+      await deps.file.removeEmptyDirs(path.join(this.config.dataDir, 'tmp'))
       if (this.config.configDir !== this.config.dataDir) {
         await deps.file.removeEmptyDirs(this.config.configDir)
       }
-      await deps.file.removeEmptyDirs(path.join(this.config.dataDir, 'tmp'))
-      try {
+      if (this.config.cacheDir !== this.config.dataDir) {
         await this.cleanupPlugins()
-      } catch (err) {
-        if (err.code !== 'ENOENT') throw err
       }
     } catch (err) {
       debug(err)
@@ -25,7 +23,13 @@ export default class extends Hook<'update'> {
   private async cleanupPlugins() {
     let pluginsDir = path.join(this.config.dataDir, 'plugins')
     if (await deps.file.exists(path.join(pluginsDir, 'plugins.json'))) return
-    let pjson = await deps.file.readJSON(path.join(pluginsDir, 'package.json'))
+    let pjson
+    try {
+      pjson = await deps.file.readJSON(path.join(pluginsDir, 'package.json'))
+    } catch (err) {
+      if (err.code !== 'ENOENT') throw err
+      return
+    }
     if (!pjson.dependencies || pjson.dependencies === {}) {
       await deps.file.remove(path.join(pluginsDir))
     }
