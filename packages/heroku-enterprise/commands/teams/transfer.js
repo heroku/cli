8a4d757b43d9f9f79c5a7b23161653739fc3908d
Naaman Newbold <naaman@heroku.com>
2018-01-11 10:54:08 -0800

    Add the internal flag

--- packages/heroku-enterprise/commands/teams/transfer.js
+++ packages/heroku-enterprise/commands/teams/transfer.js
@@ -4,8 +4,9 @@ let cmd = require('../../lib/cmd')
 function * transfer(context, heroku) {
   let team = context.args.team
   let enterpriseAccount = context.flags['enterprise-account']
+  let internal = context.flags['internal']
 
-  let params = { body: { enterprise_account: enterpriseAccount } }
+  let params = { body: { enterprise_account: enterpriseAccount, internal: internal } }
   let transfer = heroku.post(`/teams/${team}/actions/transfer`, params)
 
   yield cli.action(`Transferring ${cli.color.cyan(team)} to ${cli.color.green(enterpriseAccount)}`, transfer)
@@ -22,6 +23,11 @@ module.exports = {
       description: 'enterprise account name',
       hasValue: true,
       required: true
+    },
+    {
+      name: 'internal',
+      description: 'mark the billing for the team as internal',
+      hasValue: false
     }
   ],
   args: [{ name: 'team', description: 'name of the team to transfer' } ],
