e4ea4e0331de3bbb113d3c60b763932175005faa
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-01-24 11:04:26 -0800

    fixed plugin test

--- test/acceptance/plugin.js
+++ test/acceptance/plugin.js
@@ -1,30 +1,23 @@
 #!/usr/bin/env node
 
-import * as execa from 'execa'
-import * as fs from 'fs-extra'
-import * as path from 'path'
-
+const execa = require('execa')
+const fs = require('fs-extra')
+const path = require('path')
 const readPkg = require('read-pkg')
 
 const plugins = ['heroku-ps-exec']
 
-jest.setTimeout(10000000)
-
-Promise.all(
-  plugins.map(plugin => {
-    skipWindowsIntegrationTest(plugin, async () => {
+plugins.forEach(plugin => {
+  describe(plugin, () => {
+    it('yarn test', async () => {
       const cwd = path.join(__dirname, '../tmp/plugin', plugin)
       await fs.remove(cwd)
-      const pkg = await readPkg(path.join(__dirname, '../node_modules', plugin, 'package.json'))
+      const pkg = await readPkg(path.join(__dirname, '../../node_modules', plugin, 'package.json'))
       await execa('git', ['clone', pkg.repository.url.split('+')[1], cwd])
       const opts = { cwd, stdio: [0, 1, 2] }
       await execa('git', ['checkout', `v${pkg.version}`], opts)
       await execa('yarn', [], opts)
       await execa('yarn', ['test'], opts)
     })
-  }),
-).catch(err => {
-  // tslint:disable-next-line
-  console.error(err)
-  process.exit(1)
+  })
 })
