acd9d26e7afd51a012b7e341b4db7766ff6716d8
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-01-24 11:20:30 -0800

    manually set repo

--- test/acceptance/plugin.js
+++ test/acceptance/plugin.js
@@ -3,19 +3,19 @@
 const sh = require('shelljs')
 const fs = require('fs-extra')
 const path = require('path')
-const readPkg = require('read-pkg')
 
-const plugins = ['heroku-ps-exec']
+const plugins = [
+  {name: 'heroku-ps-exec', repo: 'heroku/heroku-ps-exec'},
+]
 
 sh.set('-ev')
 
 plugins.forEach(plugin => {
-  describe(plugin, () => {
+  describe(plugin.name, () => {
     it('yarn test', () => {
       const cwd = path.join(__dirname, '../../tmp/plugin', plugin)
       sh.rm('-rf', cwd)
-      const pkg = readPkg.sync(path.join(__dirname, '../../node_modules', plugin, 'package.json'))
-      sh.exec(`git clone ${pkg.repository.url.split('+')[1]} ${cwd}`)
+      sh.exec(`git clone git@github.com:${plugin.repo} ${cwd}`)
       sh.cd(cwd)
       sh.exec(`git checkout v${pkg.version}`)
       sh.exec('yarn')
