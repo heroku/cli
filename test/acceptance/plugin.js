dbc6ec5fdf86c9ca39b19705faf5b64466ee6ff4
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-01-24 11:12:16 -0800

    fix plugin test

--- test/acceptance/plugin.js
+++ test/acceptance/plugin.js
@@ -1,23 +1,25 @@
 #!/usr/bin/env node
 
-const execa = require('execa')
+const sh = require('shelljs')
 const fs = require('fs-extra')
 const path = require('path')
 const readPkg = require('read-pkg')
 
 const plugins = ['heroku-ps-exec']
 
+sh.set('-ev')
+
 plugins.forEach(plugin => {
   describe(plugin, () => {
-    it('yarn test', async () => {
-      const cwd = path.join(__dirname, '../tmp/plugin', plugin)
-      await fs.remove(cwd)
-      const pkg = await readPkg(path.join(__dirname, '../../node_modules', plugin, 'package.json'))
-      await execa('git', ['clone', pkg.repository.url.split('+')[1], cwd])
-      const opts = { cwd, stdio: [0, 1, 2] }
-      await execa('git', ['checkout', `v${pkg.version}`], opts)
-      await execa('yarn', [], opts)
-      await execa('yarn', ['test'], opts)
+    it('yarn test', () => {
+      const cwd = path.join(__dirname, '../../tmp/plugin', plugin)
+      sh.rm('-rf', cwd)
+      const pkg = readPkg.sync(path.join(__dirname, '../../node_modules', plugin, 'package.json'))
+      sh.exec(`git clone ${pkg.repository.url.split('+')[1]} ${cwd}`)
+      sh.cd(cwd)
+      sh.exec(`git checkout v${pkg.version}`)
+      sh.exec('yarn')
+      sh.exec('yarn test')
     })
   })
 })
