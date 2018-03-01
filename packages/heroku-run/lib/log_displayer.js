3d9f9755145b58631bdc4817ab41546953686f0f
Cyril David <cyx@users.noreply.github.com>
2018-02-28 19:26:12 -0500

    logs: utilize eventsource (#42)
    * logs: utilize eventsource
    
    * Also add eventsource to yarn.lock
    
    * Miscellaneous fixes
    
     * split log lines so we can colorize them properly
     * close the event source when done so we can exit
     * address lint
    
    * Add user-agent with default, but customizable
    
    * Bring back v1 stuff until we're GA
    
    * Move the trim to the beginning: fixes excessive newlines

--- packages/heroku-run/lib/log_displayer.js
+++ packages/heroku-run/lib/log_displayer.js
@@ -1,6 +1,8 @@
 'use strict'
 
 let cli = require('heroku-cli-util')
+let EventSource = require('eventsource')
+let url = require('url')
 let liner = require('../lib/line_transform')
 
 const COLORS = [
@@ -40,6 +42,15 @@ function colorize (line) {
 }
 
 function readLogs (logplexURL) {
+  let u = url.parse(logplexURL)
+  if (u.query && u.query.includes('srv')) {
+    return readLogsV1(logplexURL)
+  } else {
+    return readLogsV2(logplexURL)
+  }
+}
+
+function readLogsV1 (logplexURL) {
   return new Promise(function (resolve, reject) {
     let res = cli.got.stream(logplexURL)
     res.setEncoding('utf8')
@@ -48,6 +59,37 @@ function readLogs (logplexURL) {
     liner.on('data', line => cli.log(colorize(line)))
     res.on('end', resolve)
     res.on('error', reject)
+  })
+}
+
+function readLogsV2 (logplexURL) {
+  return new Promise(function (resolve, reject) {
+    let u = url.parse(logplexURL, true)
+    let isTail = u.query.tail && u.query.tail === 'true'
+    let userAgent = process.env.HEROKU_DEBUG_USER_AGENT || 'heroku-run'
+    let es = new EventSource(logplexURL, {
+      headers: {
+        'User-Agent': userAgent
+      }
+    })
+
+    es.onerror = function (err) {
+      if (!isTail) {
+        resolve()
+        es.close()
+      }
+
+      if (err && (err.status === 404 || err.status === 403)) {
+        reject(new Error('Log stream timed out. Please try again.'))
+        es.close()
+      }
+    }
+
+    es.onmessage = function (e) {
+      e.data.trim().split(/\n+/).forEach((line) => {
+        cli.log(colorize(line))
+      })
+    }
   })
 }
 
