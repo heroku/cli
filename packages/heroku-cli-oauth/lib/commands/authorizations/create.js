b0c52adfcb355dd898d51e456944febe2c424056
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-03-16 14:30:55 -0700

    add expiration notice

--- packages/heroku-cli-oauth/lib/commands/authorizations/create.js
+++ packages/heroku-cli-oauth/lib/commands/authorizations/create.js
@@ -46,7 +46,7 @@ module.exports = {
   flags: [
     {char: 'd', name: 'description', hasValue: true, description: 'set a custom authorization description'},
     {char: 's', name: 'scope', hasValue: true, description: 'set custom OAuth scopes', completion: ScopeCompletion},
-    {char: 'e', name: 'expires-in', hasValue: true, description: 'set expiration in seconds'},
+    {char: 'e', name: 'expires-in', hasValue: true, description: 'set expiration in seconds (default no expiration)'},
     {name: 'short', description: 'only output token'},
     {name: 'json', description: 'output in json format'}
   ],
