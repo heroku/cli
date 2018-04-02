2f7f2171bc3c5c935c4487ead695602f68802fc5
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2018-04-02 12:37:57 -0700

    fix: netrc-parser test mock

--- packages/heroku-apps/test/commands/auth/token.js
+++ packages/heroku-apps/test/commands/auth/token.js
@@ -5,6 +5,14 @@ const proxyquire = require('proxyquire')
 
 const netrc = td.constructor(require('netrc-parser'))
 netrc['@global'] = true
+
+let mockMachines = {}
+netrc.default = new class {
+  loadSync = () => {
+    this.machines = mockMachines
+  }
+}()
+
 const cli = proxyquire('heroku-cli-util', {'netrc-parser': netrc})
 
 const cmd = proxyquire('../../../src/commands/auth/token', {'heroku-cli-util': cli})
@@ -14,7 +22,7 @@ describe('auth:token', () => {
   beforeEach(() => cli.mockConsole())
 
   it('shows logged in user', () => {
-    netrc.prototype.machines = {'api.heroku.com': {password: 'myapikey'}}
+    mockMachines = {'api.heroku.com': {password: 'myapikey'}}
     return cmd.run({})
       .then(() => expect(cli.stdout, 'to equal', 'myapikey\n'))
       .then(() => expect(cli.stderr, 'to be empty'))
