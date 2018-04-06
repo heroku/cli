30c217d415ffa4beb263af5224b007dea9cbdf2f
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-04-06 13:00:06 -0700

    fix: tests

--- packages/heroku-cli-plugin-auth/test/commands/labs/disable.test.ts
+++ packages/heroku-cli-plugin-auth/test/commands/labs/disable.test.ts
@@ -21,53 +21,21 @@ describe('labs:disable', () => {
     expect(stderr).to.contain('Disabling feature-a for jeff@heroku.com...\n')
   })
 
-// test('disables an app feature', async () => {
-//   api
-//     .get('/account/features/feature-a').reply(404)
-//     .get('/apps/myapp/features/feature-a')
-//     .reply(200, {
-//       enabled: true,
-//       name: 'feature-a',
-//       description: 'a user lab feature',
-//       doc_url: 'https://devcenter.heroku.com'
-//     })
-//     .patch('/apps/myapp/features/feature-a', {enabled: false}).reply(200)
-//   let {stdout, stderr} = await LabsDisable.mock(['feature-a', '--app=myapp'])
-//   expect(stdout).toEqual('')
-//   expect(stderr).toEqual('Disabling feature-a for myapp... done\n')
-// })
-
-// describe('requires confirmation to disable a secure feature', () => {
-//   beforeEach(() => {
-//     api
-//       .get('/account/features/spaces-strict-tls').reply(404)
-//       .get('/apps/myapp/features/spaces-strict-tls')
-//       .reply(200, {
-//         enabled: true,
-//         name: 'spaces-strict-tls',
-//         description: 'a user lab feature',
-//         doc_url: 'https://devcenter.heroku.com'
-//       })
-//       .patch('/apps/myapp/features/spaces-strict-tls', {enabled: false}).reply(200)
-//   })
-
-//   test('warns and prompts for confirmation', async () => {
-//     cli.prompt = function () { return Promise.resolve('myapp') }
-//     let {stdout, stderr} = await LabsDisable.mock(['spaces-strict-tls', '--app=myapp'])
-//     expect(stdout).toEqual('')
-//     expect(stderr).toEqual(` ▸    WARNING: Insecure Action
-//  ▸    You are enabling an older security protocol, TLS 1.0, which some
-//  ▸    organizations may not deem secure.
-//  ▸    To proceed, type myapp or re-run this command with --confirm myapp
-// Disabling spaces-strict-tls for myapp... done
-// `)
-//   })
-
-//   test('uses confirm flag and does not warn', async () => {
-//     let {stdout, stderr} = await LabsDisable.mock(['spaces-strict-tls', '--app=myapp', '--confirm=myapp'])
-//     expect(stdout).toEqual('')
-//     expect(stderr).toEqual('Disabling spaces-strict-tls for myapp... done\n')
-//   })
-// })
-
+  test
+  .nock('https://api.heroku.com', api => api
+    .get('/account/features/feature-a').reply(404)
+    .get('/apps/myapp/features/feature-a')
+    .reply(200, {
+      enabled: true,
+      name: 'feature-a',
+      description: 'a user lab feature',
+      doc_url: 'https://devcenter.heroku.com'
+    })
+    .patch('/apps/myapp/features/feature-a', {enabled: false}).reply(200)
+  )
+  .stderr()
+  .command(['labs:disable', 'feature-a', '--app=myapp'])
+  .it('disables an app feature', ({stderr}) => {
+    expect(stderr).to.contain('Disabling feature-a for myapp...')
+  })
 })
