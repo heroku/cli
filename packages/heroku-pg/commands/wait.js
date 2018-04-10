1ba82900bab41db68be09b2273f2e5ac9f6e8ba4
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-04-10 13:40:33 -0700

    feat: added notifications to pg:wait (#182)
    * feat: added notifications to pg:wait
    
    * fix: improve heroku.png
    
    * added error/success pngs

--- packages/heroku-pg/commands/wait.js
+++ packages/heroku-pg/commands/wait.js
@@ -2,56 +2,82 @@
 
 const cli = require('heroku-cli-util')
 const co = require('co')
+const path = require('path')
 
-function * run (context, heroku) {
+async function run (context, heroku) {
+  const debug = require('debug')('heroku-pg')
   const fetcher = require('../lib/fetcher')(heroku)
   const host = require('../lib/host')
   const app = context.app
   const db = context.args.database
+  const notify = require('../lib/notify')(context)
 
-  let waitFor = co.wrap(function * waitFor (db) {
+  let waitFor = async function waitFor (db) {
     const wait = require('co-wait')
     let interval = parseInt(context.flags['wait-interval'])
     if (!interval || interval < 0) interval = 5
 
     let status
     let waiting = false
+    let name = 'db'
+    let retries = 20
 
     while (true) {
-      status = yield heroku.request({
-        host: host(db),
-        path: `/client/v11/databases/${db.id}/wait_status`
-      })
+      try {
+        status = await heroku.request({
+          host: host(db),
+          path: `/client/v11/databases/${db.id}/wait_status`
+        })
+      } catch (err) {
+        debug(err)
+        if (!retries || err.statusCode !== 404) throw err
+        retries--
+        status = {'waiting?': true}
+      }
 
       if (status['error?']) {
+        notify({
+          sound: true,
+          subtitle: 'error',
+          message: `${name} ${status['message']}`,
+          contentImage: path.join(__dirname, '../assets/error.png')
+        })
         cli.error(status['message'])
         cli.exit(1)
       }
 
       if (!status['waiting?']) {
-        if (waiting) cli.action.done(status.message)
+        if (waiting) {
+          notify({
+            sound: true,
+            message: `${name} is ${status['message']}`,
+            contentImage: path.join(__dirname, '../assets/success.png')
+          })
+          cli.action.done(status.message)
+        }
         return
       }
 
       if (!waiting) {
         waiting = true
+        name = db.name
         cli.action.start(`Waiting for database ${cli.color.addon(db.name)}`)
       }
 
       cli.action.status(status.message)
 
-      yield wait(interval * 1000)
+      await wait(interval * 1000)
     }
-  })
+  }
 
   let dbs = []
   if (db) {
-    dbs = yield [fetcher.addon(app, db)]
+    dbs = await [fetcher.addon(app, db)]
   } else {
-    dbs = yield fetcher.all(app)
+    dbs = await fetcher.all(app)
   }
 
-  for (let db of dbs) yield waitFor(db)
+  for (let db of dbs) await waitFor(db)
 }
 
 module.exports = {
@@ -61,6 +87,9 @@ module.exports = {
   needsApp: true,
   needsAuth: true,
   args: [{name: 'database', optional: true}],
-  flags: [{name: 'wait-interval', description: 'how frequently to poll in seconds (to avoid rate limiting)', hasValue: true}],
+  flags: [
+    {name: 'wait-interval', description: 'how frequently to poll in seconds (to avoid rate limiting)', hasValue: true},
+    {name: 'no-notify', description: 'do not show OS notification'}
+  ],
   run: cli.command({preauth: true}, co.wrap(run))
 }
