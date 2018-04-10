85ab5db6a7c0d673545ed0c01489cb6ad97ac9d1
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-04-10 00:36:36 -0700

    use dev-cli for deb/win

--- resources/exe/heroku.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set HEROKU_CLI_BINPATH=%~dp0\heroku.cmd
-
-if exist "%LOCALAPPDATA%\heroku\client\bin\heroku.cmd" (
-  "%LOCALAPPDATA%\heroku\client\bin\heroku.cmd" %*
-) else (
-  "%~dp0\..\client\bin\node.exe" "%~dp0\..\client\bin\heroku.js" %*
-)
