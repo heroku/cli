12e74370be99b03e482720f84790fb23a17a65a2
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-04-23 13:34:16 -0700

    add release channels

---
+++ scripts/release/update_channel.js
@@ -0,0 +1,13 @@
+const qq = require('qqjs')
+
+module.exports = async () => {
+  const pjson = await qq.readJSON('./package.json')
+  if (process.env.CIRCLE_TAG.startsWith('v')) {
+    pjson.version = pjson.version.split('-')[0]
+  } else if (process.env.CIRCLE_BRANCH === 'master') {
+    pjson.version = pjson.version.split('-')[0] + '-beta'
+  } else {
+    pjson.version = pjson.version.split('-')[0] + '-dev'
+  }
+  await qq.writeJSON('./package.json', pjson)
+}
