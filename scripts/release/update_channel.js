657af3bd23384cf4189bc201ea870e1e561cdcde
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-04-23 13:36:54 -0700

    fix CIRCLE_TAG check

--- scripts/release/update_channel.js
+++ scripts/release/update_channel.js
@@ -2,7 +2,7 @@ const qq = require('qqjs')
 
 module.exports = async () => {
   const pjson = await qq.readJSON('./package.json')
-  if (process.env.CIRCLE_TAG.startsWith('v')) {
+  if (process.env.CIRCLE_TAG && process.env.CIRCLE_TAG.startsWith('v')) {
     pjson.version = pjson.version.split('-')[0]
   } else if (process.env.CIRCLE_BRANCH === 'master') {
     pjson.version = pjson.version.split('-')[0] + '-beta'
