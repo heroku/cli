3d262dfff46f92514573e2d934d7ee396430c933
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-05-07 22:42:25 -0700

    remove analytics hook

--- src/hooks/update/analytics.ts
+++
@@ -1,8 +0,0 @@
-import {Hook} from '@oclif/config'
-
-import Analytics from '../../analytics'
-
-export const analytics: Hook<'update'> = async function () {
-  const analytics = new Analytics(this.config)
-  await analytics.submit()
-}
