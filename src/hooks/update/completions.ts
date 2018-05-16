329e541873e62ada1aa2a5cad84cec4d2591e0c0
RasPhilCo <capt.pn@gmail.com>
2018-05-16 11:44:13 -0700

    update completions hook (#850)

--- src/hooks/update/completions.ts
+++ src/hooks/update/completions.ts
@@ -1,5 +1,5 @@
 import {AppCompletion, PipelineCompletion, SpaceCompletion, TeamCompletion} from '@heroku-cli/command/lib/completions'
-import {Hook, IConfig, load} from '@oclif/config'
+import {Hook, IConfig} from '@oclif/config'
 import cli from 'cli-ux'
 
 const debug = require('debug')('heroku:completions')
@@ -8,16 +8,18 @@ export const completions: Hook<'update'> = async function () {
   if (this.config.windows) {
     debug('skipping autocomplete on windows')
   } else {
-    const acPlugin = this.config.plugins.find(p => p.name === 'heroku-cli-autocomplete')
+    const acPlugin = this.config.plugins.find(p => p.name === '@heroku-cli/plugin-autocomplete')
     if (acPlugin) {
       cli.action.start('Updating completions')
-      let ac = await acPlugin.findCommand('autocomplete:buildcache')
-      if (ac) await ac.run([], this.config)
-      let config: IConfig = Object.assign(await load(), this.config)
-      await AppCompletion.options({config})
-      await PipelineCompletion.options({config})
-      await SpaceCompletion.options({config})
-      await TeamCompletion.options({config})
+      let acCreate = await acPlugin.findCommand('autocomplete:create')
+      if (acCreate) {
+        const config: IConfig = this.config
+        await acCreate.run([], config)
+        await AppCompletion.options({config})
+        await PipelineCompletion.options({config})
+        await SpaceCompletion.options({config})
+        await TeamCompletion.options({config})
+      }
     } else {
       debug('skipping autocomplete, not installed')
     }
