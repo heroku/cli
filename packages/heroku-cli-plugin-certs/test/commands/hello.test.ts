459846e2d7a73c2f8506c4e6322b1ca9946cc5dc
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-05-22 17:35:46 -0700

    fix: added stub for certs:auto:wait

--- packages/heroku-cli-plugin-certs/test/commands/hello.test.ts
+++ packages/heroku-cli-plugin-certs/test/commands/hello.test.ts
@@ -1,17 +1,7 @@
-import {expect, test} from '@oclif/test'
+import {test} from '@oclif/test'
 
-describe('hello', () => {
+describe('certs:auto:wait', () => {
   test
   .stdout()
-  .command(['hello'])
-  .it('runs hello', ctx => {
-    expect(ctx.stdout).to.contain('hello world')
-  })
-
-  test
-  .stdout()
-  .command(['hello', '--name', 'jeff'])
-  .it('runs hello --name jeff', ctx => {
-    expect(ctx.stdout).to.contain('hello jeff')
-  })
+  .it('runs certs:auto:wait', () => { })
 })
