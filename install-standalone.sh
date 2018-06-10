7b11bdd81482ca4e62fe32ae641302bc11cd997b
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-06-10 06:06:32 -0700

    fix: remove old heroku bin if exist

--- install-standalone.sh
+++ install-standalone.sh
@@ -46,10 +46,8 @@
   rm -rf heroku
   rm -rf ~/.local/share/heroku/client
   curl https://cli-assets.heroku.com/heroku-\$OS-\$ARCH.tar.xz | tar xJ
-  if [ $(which heroku) != /usr/local/bin/heroku ]; then
-    # delete incorrect heroku path
-    rm -f $(which heroku)
-  fi
+  # delete old heroku bin if exists
+  rm -f $(which heroku) || true
   rm -f /usr/local/bin/heroku
   ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
 
