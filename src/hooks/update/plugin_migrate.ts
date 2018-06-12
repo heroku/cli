2854501baab47c26250a444e55d732eb0cb4489e
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-06-11 19:06:13 -0700

    move plugin migration into oclif-plugins

--- src/hooks/update/plugin_migrate.ts
+++ src/hooks/update/plugin_migrate.ts
@@ -7,22 +7,6 @@ const exec = (cmd: string, args: string[]) => {
   return execa(cmd, args, {stdio: 'inherit'})
 }
 
-const deprecated: {[k: string]: string | null} = {
-  // rename these plugins
-  'heroku-api-plugin': 'api',
-  'heroku-cli-api': 'api',
-  'heroku-cli-autocomplete': 'autocomplete',
-
-  // remove these plugins
-  '@heroku-cli/config-edit': null,
-  'heroku-cli-config-edit': null,
-  'heroku-cli-plugin-generator': null,
-  'heroku-container-registry': null,
-  'heroku-pipelines': null,
-  'heroku-sudo': 'sudo',
-  'heroku-webhooks': null,
-}
-
 export const migrate: Hook<'init'> = async function () {
   if (process.argv[2] && process.argv[2].startsWith('plugins')) return
   const pluginsDir = path.join(this.config.dataDir, 'plugins')
@@ -35,8 +19,6 @@ export const migrate: Hook<'init'> = async function () {
       if (await fs.pathExists(p)) {
         const {manifest} = await fs.readJSON(p)
         for (let plugin of Object.keys(manifest.plugins)) {
-          if (deprecated[plugin] === null) continue
-          plugin = deprecated[plugin] || plugin
           process.stderr.write(`heroku-cli: migrating ${plugin}\n`)
           await exec('heroku', ['plugins:install', plugin])
         }
@@ -60,25 +42,5 @@ export const migrate: Hook<'init'> = async function () {
     process.stderr.write('heroku: done migrating plugins\n')
   }
 
-  const migrateDeprecatedPlugins = async () => {
-    for (let [name, v] of Object.entries(deprecated)) {
-      const plugin = this.config.plugins.find(p => p.name === name)
-      if (!plugin) continue
-      try {
-        if (v) {
-            process.stderr.write(`heroku: migrating plugin ${name} to ${v}\n`)
-            await exec('heroku', ['plugins:uninstall', name])
-            await exec('heroku', ['plugins:install', v])
-        } else {
-          process.stderr.write(`heroku: removing deprecated plugin ${name}\n`)
-          await exec('heroku', ['plugins:uninstall', name])
-        }
-      } catch (err) {
-        this.warn(err)
-      }
-    }
-  }
-
   await migrateV6Plugins()
-  await migrateDeprecatedPlugins()
 }
