9300b3d688b8a3a647dd3a95444ec043cfa3c89f
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2018-06-12 18:01:24 -0700

    fix tests

--- packages/heroku-orgs/test/commands/orgs/index.js
+++ packages/heroku-orgs/test/commands/orgs/index.js
@@ -8,19 +8,6 @@ describe('heroku orgs', () => {
   beforeEach(() => cli.mockConsole())
   afterEach(() => nock.cleanAll())
 
-  it('shows both Enterprise orgs and teams including a warning', () => {
-    let apiGetOrgs = stubGet.orgs()
-
-    return cmd.run({flags: {}})
-      .then(() => expect(
-        `org a          collaborator
-org b          admin
-team a         collaborator
-team b         admin\n`).to.eq(cli.stdout))
-      .then(() => expect(' ▸    To list your teams only you can use heroku teams\n').to.eq(cli.stderr))
-      .then(() => apiGetOrgs.done())
-  })
-
   it('shows Enterprise orgs only when passing the --enterprise flag', () => {
     let apiGetOrgs = stubGet.orgs()
 
@@ -32,18 +19,7 @@ org b          admin\n`).to.eq(cli.stdout))
       .then(() => apiGetOrgs.done())
   })
 
-  it('shows Teams only when passing the --teams flag', () => {
-    let apiGetOrgs = stubGet.orgs()
-
-    return cmd.run({flags: { teams: true }})
-      .then(() => expect(
-        `team a         collaborator
-team b         admin\n`).to.eq(cli.stdout))
-      .then(() => expect('').to.eq(cli.stderr))
-      .then(() => apiGetOrgs.done())
-  })
-
-  it("does not show the warning when there user doesn't have teams", () => {
+  it('shows orgs (now called teams)', () => {
     let apiGetOrgsOnly = stubGet.orgs([
       {name: 'org a', role: 'collaborator', type: 'enterprise'},
       {name: 'org b', role: 'admin', type: 'enterprise'}
