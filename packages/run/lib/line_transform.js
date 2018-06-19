06dfdbcf8ae89767bd86e0fc6d7c3b3005818d4b
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-06-19 09:40:13 -0700

    added lib files

---
+++ packages/run/lib/line_transform.js
@@ -0,0 +1,24 @@
+'use strict'
+
+let stream = require('stream')
+
+// this splits a stream into lines
+let transform = new stream.Transform({decodeStrings: false})
+transform._transform = function (chunk, encoding, next) {
+  let data = chunk
+  if (this._lastLineData) data = this._lastLineData + data
+
+  let lines = data.split('\n')
+  this._lastLineData = lines.splice(lines.length - 1, 1)[0]
+
+  lines.forEach(this.push.bind(this))
+  next()
+}
+
+transform._flush = function (done) {
+  if (this._lastLineData) this.push(this._lastLineData)
+  this._lastLineData = null
+  done()
+}
+
+module.exports = transform
