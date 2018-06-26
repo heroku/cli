927d9d020fec185d096edd58ea7b712d12c0dcf2
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-06-26 11:58:09 -0700

    Merge pull request #11 from heroku/mwoods79/allow-optional-domain-when-creating-enterprise-teams
    Allow optional domains when creating enterprise teams

--- packages/heroku-enterprise/commands/enterprises/create.js
+++ packages/heroku-enterprise/commands/enterprises/create.js
@@ -4,9 +4,10 @@ let cmd = require('../../lib/cmd')
 let cli = require('heroku-cli-util')
 
 function * create (context, heroku) {
-  let enterpriseAccountName = context.args.name
+  let name = context.args.name
   let managers = context.flags.managers.split(',')
-  let params = { body: { name: enterpriseAccountName, managers: managers } }
+  let domain = context.flags.domain
+  let params = {body: {name, managers, domain}}
   let create = heroku.post(`/admin/enterprise-accounts`, params)
   yield cli.action(`Creating ${enterpriseAccountName}`, create)
 }
@@ -16,7 +17,10 @@ module.exports = {
   command: 'create',
   description: '(sudo) create an enterprise account',
   needsAuth: true,
-  flags: [{name: 'managers', hasValue: true, required: true}],
+  flags: [
+    {name: 'managers', hasValue: true, required: true}
+    {name: 'domain',   hasValue: true, required: true}
+  ],
   args: [{name: 'name', hasValue: true, required: true}],
   run: cmd.run_admin(create)
 }
