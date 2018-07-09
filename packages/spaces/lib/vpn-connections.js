a43a335e7ff56afa04772eaa4bb25df5c709f6eb
Bernerd Schaefer <bj.schaefer@gmail.com>
2018-07-09 15:34:17 -0700

    spaces: add vpn:connect command

---
+++ packages/spaces/lib/vpn-connections.js
@@ -0,0 +1,24 @@
+'use strict'
+
+module.exports = function (heroku) {
+  function postVPNConnections (space, name, ip, cidrs) {
+    return request('POST', `/spaces/${space}/vpn-connections`, {
+      name: name,
+      public_ip: ip,
+      routable_cidrs: cidrs
+    })
+  }
+
+  function request (method, path, body) {
+    return heroku.request({
+      method: method,
+      path: path,
+      body: body,
+      headers: { Accept: 'application/vnd.heroku+json; version=3.dogwood' }
+    })
+  }
+
+  return {
+    postVPNConnections
+  }
+}
