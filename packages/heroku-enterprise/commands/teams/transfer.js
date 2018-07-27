9c67111da4d19cc08273214e061b9cf8b4679e0e
vinil jain <jvinil05@gmail.com>
2018-07-27 12:05:37 -0700

    Merge pull request #13 from heroku/vinil/add_notes_transfer
    add a note to the output of teams:transfer command

--- packages/heroku-enterprise/commands/teams/transfer.js
+++ packages/heroku-enterprise/commands/teams/transfer.js
@@ -10,6 +10,10 @@ function * transfer(context, heroku) {
   let transfer = heroku.post(`/admin/teams/${team}/actions/transfer`, params)
 
   yield cli.action(`Transferring ${cli.color.cyan(team)} to ${cli.color.green(enterpriseAccount)}`, transfer)
+
+  console.log(`Transfer enqueued; it may take up to 10-15 minutes to process. `+
+    `Check the output of "heroku sudo enterprises:teams --enterprise-account ${enterpriseAccount} "` +
+    `to monitor progress; the team will show up there once the transfer has completed.`)
 }
 
 module.exports = {
