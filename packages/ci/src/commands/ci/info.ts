34391d8e4587bdd0d6e034810983ebe2d8691600
Raúl Barroso <ra.barroso@gmail.com>
2018-08-07 12:55:39 +0200

    Update dependencies and config
    - Update tsconfig
    - Move out utilities

--- packages/ci/src/commands/ci/info.ts
+++ packages/ci/src/commands/ci/info.ts
@@ -1,31 +1,76 @@
-import {Command, flags} from '@oclif/command'
+
+import {Command, flags} from '@heroku-cli/command'
+
+import * as HerokuCI from '../../lib/interface'
+import {getPipeline} from '../../lib/utils/pipelines'
 
 export default class Info extends Command {
-  static description = 'describe the command here'
+  static description = 'show the status of a specific test run'
 
   static examples = [
-    `$ oclif-example ci:info
-showing info from ./src/ci/info.ts!
+    `$ heroku ci:info 1288 --app murmuring-headland-14719
 `,
   ]
 
   static flags = {
-    help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
+    app: flags.app({required: false}),
+    node: flags.string({description: 'the node number to show its setup and output', required: false}),
+    pipeline: flags.pipeline({required: false})
   }
 
-  static args = [{name: 'file'}]
+  static args = [{name: 'test-run', required: true}]
 
   async run() {
     const {args, flags} = this.parse(Info)
 
-    const name = flags.name || 'world'
-    this.log(`hi ${name} from ./src/commands/ci/info.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+    const pipeline = await getPipeline(flags, this)
+    const headers = {Accept: 'application/vnd.heroku+json; version=3.ci'}
+
+    try {
+      const {body: testRun} = await this.heroku.get<HerokuCI.TestRun>(`/pipelines/${pipeline.id}/test-runs/${args['test-run']}`, {headers})
+      const {body: testNodes} = await this.heroku.get(`/test-runs/${testRun.id}/test-nodes`, {headers})
+
+      this.log(testRun)
+      this.log(testNodes)
+
+    } catch (e) {
+      this.error(e.body.message) // This currently shows a  ›   Error: Not found.
     }
+
+    //  if (flags.node) {
+    //   if (testNodes.length > 1) {
+    //     testNode = testNodes[flags.node]
+
+    //     if (!testNode) {
+    //       cli.error(`There isn't a test node ${flags.node} for test run ${args['test-run']}`)
+    //     }
+    //   } else {
+    //     testNode = testNodes[0]
+    //   }
+
+    //   await renderNodeOutput(testRun, testNode)
+
+    //   if (testNodes.length === 1) {
+    //     this.log(/* newline 💃 */)
+    //     this.warn('This pipeline doesn\'t have parallel test runs, but you specified a node')
+    //     this.warn('See https://devcenter.heroku.com/articles/heroku-ci-parallel-test-runs for more info')
+    //   }
+    //   process.exit(testNode.exit_code)
+    // } else {
+    //   if (testNodes.length > 1) {
+    //     this.log(RenderTestRuns.printLine(testRun))
+    //     this.log(/* newline 💃 */)
+    //     testNodes.forEach((testNode) => {
+    //       this.log(RenderTestRuns.printLineTestNode(testNode))
+    //     })
+    //   } else {
+    //     testNode = testNodes[0]
+    //     await renderNodeOutput(testRun, testNode);
+    //     process.exit(testNode.exit_code)
+    //   }
+    // }
+
+    // if node is specified, show output and setup stream url for that node
+    // if no node is specified, show minimal information
   }
 }
---
+++ packages/ci/src/lib/interface.ts
@@ -0,0 +1,3 @@
+export interface TestRun {
+  id?: string
+}
