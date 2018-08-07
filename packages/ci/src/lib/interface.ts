8e0855d87016240df6f63104ccff5775a3fc2be6
Raúl Barroso <ra.barroso@gmail.com>
2018-08-07 17:29:42 +0200

    Update heroku ci interfaces

--- packages/ci/src/lib/interface.ts
+++ packages/ci/src/lib/interface.ts
@@ -1,3 +1,13 @@
+// TODO: Pull from the Heroku variant
+
 export interface TestRun {
-  id?: string
+  id?: string,
+
+}
+
+export interface TestNode {
+  id?: string,
+  exit_code?: number | undefined,
+  setup_stream_url?: string,
+  output_stream_url?: string,
 }
