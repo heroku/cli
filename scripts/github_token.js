a0286cd07860624eac9c564d586387ab51cf2598
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-08-17 14:47:19 -0700

    chore: use github app for macos and homebrew repo jobs

---
+++ scripts/github_token.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+// gets a github auth token for a github app
+
+const jwt = require('jsonwebtoken')
+const {HTTP} = require('http-call')
+const exp = new Date()
+exp.setMinutes(exp.getMinutes() + 10)
+const env = k => {
+  if (process.env[k]) return process.env[k]
+  else throw new Error(`Must set ${k}`)
+}
+
+const token = jwt.sign({
+  // iss: env('GITHUB_APP_ID')
+}, env('GITHUB_APP_PEM'), {
+  issuer: env('GITHUB_APP_ID'),
+  algorithm: 'RS256',
+  expiresIn: '10m'
+})
+
+const installation = process.argv[2]
+
+async function run () {
+  const {body} = await HTTP.post(`https://api.github.com/app/installations/${installation}/access_tokens`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: 'application/vnd.github.machine-man-preview+json"'
+    }
+  })
+  console.log(body.token)
+}
+
+run()
+  .catch(err => {
+    console.error(err)
+    process.exit(1)
+  })
