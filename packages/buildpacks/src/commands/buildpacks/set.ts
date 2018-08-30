c33f51826aa3e1731fffccb84c47f96fe47ead2b
Terence Lee <hone02@gmail.com>
2018-08-30 19:28:33 +0200

    feat(buildpacks): added search, info, and versions commands (#1000)

---
+++ packages/buildpacks/src/commands/buildpacks/set.ts
@@ -0,0 +1,41 @@
+import {Command, flags as Flags} from '@heroku-cli/command'
+
+import {BuildpackCommand} from '../../buildpacks'
+
+export default class Set extends Command {
+  static description: 'set new app buildpack, overwriting into list of buildpacks if necessary'
+  static flags = {
+    app: Flags.app({required: true}),
+    remote: Flags.remote(),
+    index: Flags.integer({
+      description: 'the 1-based index of the URL in the list of URLs',
+      char: 'i'
+    })
+  }
+  static args = [
+    {
+      name: 'buildpack',
+      required: true,
+      description: 'namespace/name of the buildpack'
+    }
+  ]
+
+  async run() {
+    let {args, flags} = this.parse(Set)
+    let buildpackCommand = new BuildpackCommand(this.heroku)
+    let buildpacks = await buildpackCommand.fetch(flags.app)
+
+    await buildpackCommand.validateUrlNotSet(buildpacks, args.buildpack)
+
+    let spliceIndex: number
+    if (flags.index === undefined) {
+      spliceIndex = 0
+    } else {
+      let foundIndex = buildpackCommand.findIndex(buildpacks, flags.index)
+      spliceIndex = (foundIndex === -1) ? buildpacks.length : foundIndex
+    }
+
+    let buildpackUpdates = await buildpackCommand.mutate(flags.app, buildpacks, spliceIndex, args.buildpack, 'set')
+    buildpackCommand.displayUpdate(flags.app, flags.remote || '', buildpackUpdates, 'set')
+  }
+}
---
+++ packages/buildpacks/src/commands/buildpacks/versions.ts
@@ -0,0 +1,55 @@
+import {Command} from '@heroku-cli/command'
+import {cli} from 'cli-ux'
+import {Result} from 'true-myth'
+
+import {BuildpackRegistry, RevisionBody} from 'buildpack-registry'
+
+export default class Versions extends Command {
+  static description = 'list versions of a buildpack'
+  static args = [
+    {
+      name: 'buildpack',
+      required: true,
+      description: 'namespace/name of the buildpack'
+    }
+  ]
+
+  async run() {
+    let {args} = this.parse(Versions)
+    let registry: BuildpackRegistry
+    let herokuAuth = this.heroku.auth || ''
+    if (herokuAuth === '') {
+      this.error('You need to be logged in to run this command.')
+    }
+    registry = new BuildpackRegistry()
+
+    Result.match({
+      Ok: _ => {},
+      Err: err => {
+        this.error(`Could not find the buildpack.\n${err}`)
+      },
+    }, BuildpackRegistry.isValidBuildpackSlug(args.buildpack))
+
+    let result = await registry.listVersions(args.buildpack)
+    Result.match({
+      Ok: versions => {
+        cli.table(versions.sort((a: RevisionBody, b: RevisionBody) => {
+          return a.release > b.release ? -1 : 1
+        }), {
+          columns: [
+            {key: 'release', label: 'Version'},
+            {key: 'created_at', label: 'Released At'},
+            {key: 'status', label: 'Status'}
+          ]
+        })
+      },
+      Err: err => {
+        if (err.status === 404) {
+          this.error(`Could not find '${args.buildpack}'`)
+        } else {
+          this.error(`Problem fetching versions, ${err.status}: ${err.description}`)
+        }
+      }
+    }, result)
+  }
+}
