ce84d869824228cc3ad38737c700542912eff77a
Jesse Brown <jabrown85@gmail.com>
2018-09-07 16:01:02 -0500

    fix(ci): unknown local commit path (#1019)
    Fix ci:run when it encounters an unknown commit

--- packages/ci/src/utils/source.ts
+++ packages/ci/src/utils/source.ts
@@ -4,6 +4,7 @@ const git = require('./git')
 import {Command} from '@heroku-cli/command'
 
 import * as fs from 'async-file'
+import {ux} from 'cli-ux'
 
 async function uploadArchive(url: string, filePath: string) {
   const request = got.stream.put(url, {
@@ -21,10 +22,8 @@ async function uploadArchive(url: string, filePath: string) {
 }
 
 async function prepareSource(ref: any, command: Command) {
-  const [filePath, source] = await [
-    git.createArchive(ref),
-    command.heroku.post('/sources', {body: command})
-  ]
+  const filePath = await git.createArchive(ref)
+  const {body: source} = await command.heroku.post<any>('/sources')
   await uploadArchive(source.source_blob.put_url, filePath)
   return Promise.resolve(source)
 }
@@ -42,7 +41,10 @@ export async function createSourceBlob(ref: any, command: Command) {
     if (await urlExists(archiveLink.archive_link)) {
       return archiveLink.archive_link
     }
-  } catch (ex) { command.error(ex) }
+  } catch (ex) {
+    // the commit isn't in the repo, we will package the local git commit instead
+    ux.debug(`Commit not found in pipeline repository: ${ex}`)
+  }
 
   const sourceBlob = await prepareSource(ref, command)
   return sourceBlob.source_blob.get_url
