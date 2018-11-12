455f0565359f1779178257f042c4c8a564b6f5c9
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2018-11-12 15:46:42 -0800

    fix(webhooks-v5): add remote flag

--- packages/webhooks-v5/commands/webhooks/events/info.js
+++ packages/webhooks-v5/commands/webhooks/events/info.js
@@ -29,7 +29,8 @@ Info.examples = [
 ]
 
 Info.flags = {
-  app: flags.app({ char: 'a' }),
+  app: flags.app(),
+  remote: flags.remote(),
   pipeline: flags.string({ char: 'p', description: 'pipeline on which to list', hidden: true })
 }
 
