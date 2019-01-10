61e5f4e8cb86029aaf2cfc0e149046702549673b
Tom Crayford <tcrayford@googlemail.com>
2019-01-10 15:57:23 +0000

    fix

--- packages/pg-v5/index.js
+++ packages/pg-v5/index.js
@@ -20,7 +20,7 @@ exports.commands = flatten([
   require('./commands/backups/url'),
   require('./commands/bloat'),
   require('./commands/blocking'),
-  require('./commands/connection_pooling')
+  require('./commands/connection_pooling'),
   require('./commands/copy'),
   require('./commands/credentials'),
   require('./commands/credentials/create'),
