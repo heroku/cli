a96283c4d644c863e34771372e43fb3e374556ca
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2019-01-23 12:11:58 -0800

    add color

--- packages/addons/src/commands/_addons/index.ts
+++ packages/addons/src/commands/_addons/index.ts
@@ -3,46 +3,44 @@
 
 // THOUGHTS:
 // - Maybe we should deprecate support on `--all` considering how little performant it is
-import * as Heroku from '@heroku-cli/schema'
-
+import color from '@heroku-cli/color'
 import {Command, flags as Flags} from '@heroku-cli/command'
-
-// import {string} from '@oclif/parser/lib/flags'
+import * as Heroku from '@heroku-cli/schema'
 import ux, {cli} from 'cli-ux'
 
 function getAddonName(addon: Heroku.AddOn) {
   let addonService: Heroku.AddOnService | undefined = addon.addon_service
+  let name = color.addon(addon.name!)
   if (addonService && addonService.human_name) {
-    let out = `${addonService.human_name} (${addon.name})`
+    let out = `${addonService.human_name} (${name})`
     if (addon.config_vars && addon.config_vars.length > 0) {
-      addon.config_vars.forEach((v: string) => {
+      addon.config_vars.forEach((v: string, i: number) => {
+        let branch = (i < addon.config_vars!.length - 1) ? ' ├─' : ' └─'
         out += '\n'
-        out += `└─ ${v}`
+        out += `${branch} as ${color.configVar(v)}`
       })
     }
     return out
   } else {
-    return addon.name
+    return name
   }
 }
 
 function getPrice(addon: Heroku.AddOn, app: string | undefined) {
   if (addon.billing_entity) {
-    if (app) {
-      if ((addon.billing_entity.type === 'app') && (addon.billing_entity.name === app)) {
-        if ((addon.plan) && (addon.plan.price)) {
-          const price = addon.plan.price
-          if (price.contract) return 'contract'
-          if (price.cents === 0) return 'free'
-          return `$${(price.cents / 100).toFixed(2)}`
-        }
-      } else {
-        let output = 'Billed to '
-        // TODO: Check whether I could still use `cli.color.app` from `heroku-cli-util
-        output += addon.billing_entity.type === 'app' ? addon.billing_entity.name : addon.billing_entity.name
-        output += ` (${addon.billing_entity.type})`
-        return output
+    if ((addon.billing_entity.type === 'app') && (addon.billing_entity.name === app)) {
+      if (addon.billed_price) {
+        const price = addon.billed_price
+        if (price.contract) return 'contract'
+        if (price.cents === 0) return 'free'
+        return `$${(price.cents / 100).toFixed(2)}`
       }
+    } else {
+      let output = 'Billed to '
+      output += addon.billing_entity.type === 'app' ? color.app(addon.billing_entity.name!) : addon.billing_entity.name
+      // waiting for 'team' type
+      // output += ` (${addon.billing_entity.type})`
+      return output
     }
   }
 }
@@ -74,16 +72,12 @@ export default class AddonsIndex extends Command {
     all: Flags.boolean({char: 'A', description: 'show add-ons and attachments for all accessible apps', required: false}),
     app: Flags.app(),
     json: Flags.boolean({description: 'returns add-ons in json format', required: false}),
-    ...cli.table.flags({})
+    ...cli.table.flags({except: 'csv'})
   }
 
   async run() {
     const {flags} = this.parse(AddonsIndex)
     let url
-
-    if ((!flags.app) && (!flags.all)) {
-      cli.error('You need to specify the scope via --app or --all')
-    }
 
     if (flags.app) {
       url = `/apps/${flags.app}/addons`
