f0055eabdcf5d43d1b483457500a83a5cacf92c4
Jason Draper <jason@drapergeek.com>
2019-01-29 10:46:22 -0500

    Come on, so close

--- packages/orgs-v5/test/stub/delete.js
+++ packages/orgs-v5/test/stub/delete.js
@@ -6,7 +6,7 @@ function collaboratorsOrgApp (app, email) {
   return nock('https://api.heroku.com:443', {
     reqheaders: { Accept: 'application/vnd.heroku+json; version=3' }
   })
-    .delete(`/organizations/apps/${app}/collaborators/${email}`).reply(200, {})
+    .delete(`/teams/apps/${app}/collaborators/${email}`).reply(200, {})
 }
 
 function collaboratorsPersonalApp (app, email) {
