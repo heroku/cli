fca3899c7f9b5191aa9b2228b5d0413434497a3c
Jason Draper <jason@drapergeek.com>
2019-01-29 09:10:29 -0500

    teamInvites

--- packages/orgs-v5/test/stub/get.js
+++ packages/orgs-v5/test/stub/get.js
@@ -168,7 +168,7 @@ function variableSizeTeamInvites (teamSize) {
   return nock('https://api.heroku.com:443', {
     reqheaders: { Accept: 'application/vnd.heroku+json; version=3.team-invitations' }
   })
-    .get('/organizations/myorg/invitations')
+    .get('/teams/myorg/invitations')
     .reply(200, invites)
 }
 
