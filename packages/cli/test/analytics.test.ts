e5d91857a741ab422cf8c5e7ffa80f28bbe625ce
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2019-02-26 15:07:40 -0800

    break out to separate tests

--- packages/cli/test/analytics.test.ts
+++ packages/cli/test/analytics.test.ts
@@ -1,43 +1,45 @@
-import * as Config from '@oclif/config'
-import AnalyticsCommand, {AnalyticsInterface} from '../src/analytics'
 import Login from '@heroku-cli/plugin-auth/src/commands/auth/login'
-import UserConfig from '../src/user-config'
+import * as Config from '@oclif/config'
+import {expect} from 'chai'
 import nock from 'nock'
 import sinon from 'sinon'
-import {expect} from 'chai'
+
+import AnalyticsCommand, {AnalyticsInterface} from '../src/analytics'
+import UserConfig from '../src/user-config'
+
+function backboardClosure(expectedCbk: (data: AnalyticsInterface) => any, actual: any) {
+  // mocks
+  let backboard = nock('https://backboard.heroku.com/', {
+    reqheaders: {
+      'user-agent': '@oclif/command/1.5.6 darwin-x64 node-v10.2.1',
+    }
+  })
+    .get('/hamurai')
+    .query(({data: analyticsData}: { data: string }) => {
+      const data: AnalyticsInterface = JSON.parse(Buffer.from(analyticsData, 'base64').toString())
+      expect(expectedCbk(data)).to.eq(actual)
+      return true
+    })
+    .reply(200)
+
+  return backboard
+}
+
+async function runAnalyticsTest(analytics: AnalyticsCommand, expectedCbk: (data: AnalyticsInterface) => any, actual: any) {
+  let backboard = backboardClosure(expectedCbk, actual)
+  await analytics.record({
+    Command: Login, argv: ['foo', 'bar']
+  })
+  backboard.done()
+}
 
 describe('analytics', () => {
-  it('emits analytics event', async () => {
-    // mocks
-    let backboard = nock('https://backboard.heroku.com/', {
-      reqheaders: {
-        'user-agent': '@oclif/command/1.5.6 darwin-x64 node-v10.2.1',
-      }
-    })
-      .get('/hamurai')
-      .query(({data: analyticsData}: {data: string}) => {
-        const data: AnalyticsInterface = JSON.parse(Buffer.from(analyticsData, 'base64').toString())
+  let analytics: any
+  let sandbox: any
 
-        expect(data.source).to.eq('cli')
-        expect(data.event).to.eq('login')
-        expect(data.properties.cli).to.eq('heroku')
-        expect(data.properties.command).to.eq('login')
-        expect(data.properties.completion).to.eq(0)
-        expect(data.properties.version).to.eq('1')
-        expect(data.properties.plugin).to.eq('foo')
-        expect(data.properties.plugin_version).to.eq('123')
-        expect(data.properties.os).to.eq('win32')
-        expect(data.properties.shell).to.eq('fish')
-        expect(data.properties.valid).to.eq(true)
-        expect(data.properties.language).to.eq('node')
-        expect(data.properties.install_id).to.eq('abcde')
-
-        return true
-      })
-      .reply(200)
-
+  before(async () => {
     // stubs
-    const sandbox = sinon.createSandbox()
+    sandbox = sinon.createSandbox()
     sandbox.stub(UserConfig.prototype, 'install').get(() => 'abcde')
     const config = await Config.load()
     config.platform = 'win32'
@@ -45,14 +47,64 @@ describe('analytics', () => {
     config.version = '1'
     config.userAgent = '@oclif/command/1.5.6 darwin-x64 node-v10.2.1'
     config.name = 'heroku'
-    const analytics = new AnalyticsCommand(config)
+    analytics = new AnalyticsCommand(config)
     Login.plugin = {name: 'foo', version: '123'} as any
     Login.id = 'login'
+  })
 
-    await analytics.record({
-      Command: Login, argv: ['foo', 'bar']
-    })
-    backboard.done()
+  it('emits source', async () => {
+    await runAnalyticsTest(analytics, (d: AnalyticsInterface) => d.source, 'cli')
+  })
+
+  it('emits event', async () => {
+    await runAnalyticsTest(analytics, (d: AnalyticsInterface) => d.event, 'login')
+  })
+
+  it('emits property cli', async () => {
+    await runAnalyticsTest(analytics, (d: AnalyticsInterface) => d.properties.cli, 'heroku')
+  })
+
+  it('emits property command', async () => {
+    await runAnalyticsTest(analytics, (d: AnalyticsInterface) => d.properties.command, 'login')
+  })
+
+  it('emits property completion', async () => {
+    await runAnalyticsTest(analytics, (d: AnalyticsInterface) => d.properties.completion, 0)
+  })
+
+  it('emits property version', async () => {
+    await runAnalyticsTest(analytics, (d: AnalyticsInterface) => d.properties.version, '1')
+  })
+
+  it('emits property plugin', async () => {
+    await runAnalyticsTest(analytics, (d: AnalyticsInterface) => d.properties.plugin, 'foo')
+  })
+
+  it('emits property plugin_version', async () => {
+    await runAnalyticsTest(analytics, (d: AnalyticsInterface) => d.properties.plugin_version, '123')
+  })
+
+  it('emits property os', async () => {
+    await runAnalyticsTest(analytics, (d: AnalyticsInterface) => d.properties.os, 'win32')
+  })
+
+  it('emits property shell', async () => {
+    await runAnalyticsTest(analytics, (d: AnalyticsInterface) => d.properties.shell, 'fish')
+  })
+
+  it('emits property valid', async () => {
+    await runAnalyticsTest(analytics, (d: AnalyticsInterface) => d.properties.valid, true)
+  })
+
+  it('emits property language', async () => {
+    await runAnalyticsTest(analytics, (d: AnalyticsInterface) => d.properties.language, 'node')
+  })
+
+  it('emits property install_id', async () => {
+    await runAnalyticsTest(analytics, (d: AnalyticsInterface) => d.properties.install_id, 'abcde')
+  })
+
+  after(() => {
     sandbox.restore()
   })
 })
