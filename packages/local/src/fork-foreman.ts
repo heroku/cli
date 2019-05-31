57f269b07160bf78c06c7d084cff37e593a33f7b
Chad Carbert <chadcarbert@me.com>
2019-05-31 13:27:41 -0400

    Change run_foreman.js -> run-foreman.js

--- packages/local/src/fork-foreman.ts
+++ packages/local/src/fork-foreman.ts
@@ -2,7 +2,7 @@ import {fork} from 'child_process'
 import * as path from 'path'
 
 export default function (argv: string[]): Promise<undefined> {
-  let script = path.join(__dirname, 'run_foreman.js')
+  let script = path.join(__dirname, 'run-foreman.js')
   let nf = fork(script, argv, {stdio: 'inherit'})
 
   return new Promise(resolve => {
