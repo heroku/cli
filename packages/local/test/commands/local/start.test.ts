a06ed61c7613ac958a2ef9b561a636046989f93f
Chad Carbert <chadcarbert@me.com>
2019-05-31 17:02:10 -0400

    Clean up local:start test

--- packages/local/test/commands/local/start.test.ts
+++ packages/local/test/commands/local/start.test.ts
@@ -1,19 +1,19 @@
-import {expect, test} from '@oclif/test'
+import { expect, test } from '@oclif/test'
 
 describe('local:start', () => {
   test
     .stdout()
     .command(['local:start', '--restart'])
-    .catch( e => {
+    .catch(e => {
       expect(e.message).to.equal('--restart is no longer available\nUse forego instead: https://github.com/ddollar/forego')
     })
-    .it('errors with proper usage suggestion')
+    .it('errors with deprecated restart flag message')
 
-    test
+  test
     .stdout()
-    .command(['local:start', '--cconcurrency'])
-    .catch( e => {
-      expect(e.message).to.equal('--restart is no longer available\nUse forego instead: https://github.com/ddollar/forego')
+    .command(['local:start', '--concurrency', 'web=2'])
+    .catch(e => {
+      expect(e.message).to.equal('--concurrency is no longer available\nUse forego instead: https://github.com/ddollar/forego')
     })
-    .it('errors with proper usage suggestion')
+    .it('errors with deprecated concurrency flag')
 })
