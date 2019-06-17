ed142c3fd2a4461e15bedc81df5e5d1140598d08
Chad Carbert <chadcarbert@me.com>
2019-06-17 16:55:21 -0400

    Harcode screen width for testing purposes for consistent test results
    Tests can be an issue when using cli-ux features that use oclif/screen.
    Depending on the width of the terminal the output might wrap or truncate.
    To get around this we set the global `columns` value, which is checked by
    oclif/screen. This specifically impacted tables and error message
    wrapping. 140 characters appears to be a width that works for the tests
    currently.
    
    See https://github.com/oclif/screen

---
+++ packages/webhooks/test/helpers/init.js
@@ -0,0 +1,6 @@
+// hardcodes `columns` terminal width for tests which is
+// important for things like tables and other places where
+// dynamic wrapping or truncating is an issue.
+// see: https://github.com/oclif/screen
+
+global.columns = 140
