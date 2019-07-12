be9381043c5ddc5fdd2d1ba43a788397849125be
Johnny Domino <jdomino@jdomino-ltm.internal.salesforce.com>
2019-07-12 15:59:38 -0400

    add heroku spaces:transfer

---
+++ packages/spaces/commands/transfer.js
@@ -0,0 +1,33 @@
+'use strict'
+
+let cli = require('heroku-cli-util')
+let co = require('co')
+
+function * run (context, heroku) {
+  let space = context.flags.space
+  let team = context.flags.team
+  let request = heroku.request({
+    method: 'POST',
+    path: `/spaces/${space}/transfer`,
+    body: { 'new_owner': team }
+  })
+  yield cli.action(`Transferring Space ${space} to Team ${cli.color.green(team)}`, request)
+}
+
+module.exports = {
+  topic: 'spaces',
+  command: 'transfer',
+  description: 'transfers a space',
+  help: `Example:
+
+    $ heroku spaces:transfer --space=space-name --team=team-name
+    Transferring Space space-name to Team team-name... done
+`,
+  needsApp: false,
+  needsAuth: true,
+  flags: [
+    { name: 'space', hasValue: true, required: true, description: 'name of space' },
+    { name: 'team', hasValue: true, required: true, description: 'desired owner of space' }
+  ],
+  run: cli.command(co.wrap(run))
+}
