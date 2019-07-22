df55d89e2110d4ce0e700999a5a6cd445ec883b1
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2019-07-22 11:42:42 -0700

    clean up cmd

--- packages/pipelines/src/commands/reviewapps/enable.ts
+++ packages/pipelines/src/commands/reviewapps/enable.ts
@@ -1,19 +1,19 @@
 import {Command, flags} from '@heroku-cli/command'
 import * as Heroku from '@heroku-cli/schema'
+import cli from 'cli-ux'
 
 export default class ReviewappsEnable extends Command {
   static description = 'enable review apps and/or settings on an existing pipeline'
 
   static examples = [
-    `$ heroku reviewapps:enable -p mypipeline --a myapp --autodeploy --autodestroy
-`,
+    '$ heroku reviewapps:enable -p mypipeline -a myapp --autodeploy --autodestroy',
   ]
 
   static flags = {
     app: flags.string({char: 'a', description: 'parent app used by review apps'}),
     pipeline: flags.string({char: 'p', description: 'name of pipeline'}),
-    autodeploy: flags.boolean({description: 'autodeploy the review app', required: false}),
-    autodestroy: flags.boolean({description: 'autodestroy the review app', required: false}),
+    autodeploy: flags.boolean({description: 'autodeploy the review app'}),
+    autodestroy: flags.boolean({description: 'autodestroy the review app'}),
   }
 
   async run() {
@@ -36,7 +36,7 @@ export default class ReviewappsEnable extends Command {
       this.log('Enabling auto destroy...')
       settings.pull_requests.auto_destroy = true
     }
-    this.cli.action.start('Configuring pipeline')
+    cli.action.start('Configuring pipeline')
 
     let {body: app} = await this.heroku.get<Heroku.App>(`/apps/${flags.app}`)
 
@@ -45,6 +45,6 @@ export default class ReviewappsEnable extends Command {
       body: settings
     })
 
-    this.cli.action.stop()
+    cli.action.stop()
   }
 }
