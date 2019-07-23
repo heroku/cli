a61de1d7747a636abaad2032ae4f6d88deb63218
RasPhilCo <capt.pn@gmail.com>
2019-07-23 11:55:34 -0700

    oclif conversion edits (#1270)
    * run lint
    
    * update pjson
    
    * clean up cmd
    
    * pjson
    
    * Add required flags to pipeline and app

--- packages/pipelines/src/commands/reviewapps/enable.ts
+++ packages/pipelines/src/commands/reviewapps/enable.ts
@@ -1,23 +1,23 @@
 import {Command, flags} from '@heroku-cli/command'
 import * as Heroku from '@heroku-cli/schema'
+import cli from 'cli-ux'
 
 export default class ReviewappsEnable extends Command {
   static description = 'enable review apps and/or settings on an existing pipeline'
 
   static examples = [
-    `$ heroku reviewapps:enable -p mypipeline --a myapp --autodeploy --autodestroy
-`,
+    '$ heroku reviewapps:enable -p mypipeline -a myapp --autodeploy --autodestroy',
   ]
 
   static flags = {
-    app: flags.string({ char: 'a', description: 'parent app used by review apps' }),
-    pipeline: flags.string({ char: 'p', description: 'name of pipeline' }),
-    autodeploy: flags.boolean({ description: 'autodeploy the review app', required: false }),
-    autodestroy: flags.boolean({ description: 'autodestroy the review app', required: false }),
+    app: flags.string({char: 'a', description: 'parent app used by review apps', required: true}),
+    pipeline: flags.string({char: 'p', description: 'name of pipeline', required: true}),
+    autodeploy: flags.boolean({description: 'autodeploy the review app'}),
+    autodestroy: flags.boolean({description: 'autodestroy the review app'}),
   }
 
   async run() {
-    const {args, flags} = this.parse(ReviewappsEnable)
+    const {flags} = this.parse(ReviewappsEnable)
 
     const settings = {
       pull_requests: {
@@ -36,15 +36,15 @@ export default class ReviewappsEnable extends Command {
       this.log('Enabling auto destroy...')
       settings.pull_requests.auto_destroy = true
     }
-    this.cli.action.start('Configuring pipeline')
+    cli.action.start('Configuring pipeline')
 
-    let { body: app } = await this.heroku.get<Heroku.App>(`/apps/${flags.app}`)
+    let {body: app} = await this.heroku.get<Heroku.App>(`/apps/${flags.app}`)
 
     await this.heroku.patch(`/apps/${app.id}/github`, {
       hostname: 'kolkrabbi.heroku.com',
       body: settings
     })
 
-    this.cli.action.stop()
+    cli.action.stop()
   }
 }
