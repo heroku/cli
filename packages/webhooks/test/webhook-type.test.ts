bc601f315865f975249f5fdc20b14ef5baf3a080
Zane Whitfield <zwhitfield@salesforce.com>
2019-07-31 14:16:26 -0700

    Fix webhook-type tests

--- packages/webhooks/test/webhook-type.test.ts
+++ packages/webhooks/test/webhook-type.test.ts
@@ -1,10 +1,28 @@
 import {expect, test} from '@oclif/test'
 import webhookType from '../src/webhook-type'
 
-describe('webhooks:add', () => {
+describe('webhooks type', () => {
   test
-    .do(function(){
-        webhookType()
+    .stdout()
+    .do(function () {
+      const webhookInfo = webhookType({pipeline: 'randomPipeline', app: ''})
+      expect(webhookInfo).to.deep.equal({path: '/pipelines/randomPipeline', display: 'randomPipeline'})
     })
-    .it('adds a specific app webhook')
+    .it('returns correct pipeline path and display info')
+
+  test
+    .stdout()
+    .do(function () {
+      const webhookInfo = webhookType({pipeline: '', app: 'randomApp'})
+      expect(webhookInfo).to.deep.equal({path: '/apps/randomApp', display: '\u001b[38;5;104m⬢ randomApp\u001b[0m'})
+    })
+    .it('returns correct app path and display info')
+
+  test
+    .stdout()
+    .do(function () {
+      webhookType({pipeline: '', app: ''})
+    })
+    .catch(e => expect(e.message).to.equal('No app specified'))
+    .it('returns error if no arguments are given')
 })
