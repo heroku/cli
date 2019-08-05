be90cf791e0eec30b2da9e26c6c05eb8b5d1b498
Chris Freeman <cfreeman.code@gmail.com>
2019-08-05 16:38:47 -0600

    first pass at converting Dyno and Run

--- packages/run/src/commands/run.ts
+++ packages/run/src/commands/run.ts
@@ -1,25 +1,92 @@
-import {Command, flags} from '@oclif/command'
+import {Command, flags} from '@heroku-cli/command'
+import {DynoSizeCompletion, ProcessTypeCompletion} from '@heroku-cli/command/lib/completions'
+import cli from 'cli-ux'
+import DebugFactory from 'debug'
+
+import Dyno from '../lib/dyno'
+import {buildCommand, buildEnvFromFlag} from '../lib/helpers'
+
+const debug = DebugFactory('heroku:run')
 
 export default class Run extends Command {
-  static description = 'describe the command here'
+  static description = 'run a one-off process inside a heroku dyno'
+
+  static examples = [
+    '$ heroku run bash',
+    '$ heroku run -s hobby -- myscript.sh -a arg1 -s arg2'
+  ]
+
+  // This is to allow for variable length arguments
+  static strict = false
 
   static flags = {
-    help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
+    app: flags.app({description: 'parent app used by review apps', required: true}),
+    size: flags.string({char: 's', description: 'dyno size', completion: DynoSizeCompletion}),
+    type: flags.string({description: 'process type', completion: ProcessTypeCompletion}),
+    'exit-code': flags.boolean({char: 'x', description: 'passthrough the exit code of the remote command'}),
+    env: flags.string({char: 'e', description: "environment variables to set (use ';' to split multiple vars)"}),
+    'no-tty': flags.boolean({description: 'force the command to not run in a tty'}),
+    listen: flags.boolean({description: 'listen on a local port', hidden: true}),
+    'no-notify': flags.boolean({description: 'disables notification when dyno is up (alternatively use HEROKU_NOTIFICATIONS=0)'})
   }
 
-  static args = [{name: 'file'}]
-
   async run() {
-    const {args, flags} = this.parse(Run)
+    const {argv, flags} = this.parse(Run)
 
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from /Users/chris.freeman/heroku/cli/packages/run/src/commands/run.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+    let opts = {
+      'exit-code': flags['exit-code'],
+      'no-tty': flags['no-tty'],
+      app: flags.app,
+      attach: true,
+      command: buildCommand(argv),
+      env: flags.env,
+      heroku: this.heroku,
+      listen: flags.listen,
+      notify: !flags['no-notify'],
+      size: flags.size,
+      type: flags.type
+    }
+
+    if (!opts.command) {
+      throw new Error('Usage: heroku run COMMAND\n\nExample: heroku run bash')
+    }
+
+    let dyno = new Dyno(opts)
+    try {
+      await dyno.start()
+      debug('done running')
+    } catch (err) {
+      debug(err)
+      if (err.exitCode) {
+        cli.exit(err.exitCode)
+      } else {
+        throw err
+      }
     }
   }
 }
+
+// module.exports = {
+//   topic: 'run',
+//   description: 'run a one-off process inside a heroku dyno',
+//   help: 'Shows a notification if the dyno takes more than 20 seconds to start.',
+//   examples: `$ heroku run bash
+// Running bash on app.... up, run.1
+// ~ $
+
+// $ heroku run -s hobby -- myscript.sh -a arg1 -s arg2
+// Running myscript.sh -a arg1 -s arg2 on app.... up, run.1`,
+//   variableArgs: true,
+//   needsAuth: true,
+//   needsApp: true,
+//   flags: [
+//     {name: 'size', char: 's', description: 'dyno size', hasValue: true, completion: DynoSizeCompletion},
+//     {name: 'type', description: 'process type', hasValue: true, completion: ProcessTypeCompletion},
+//     {name: 'exit-code', char: 'x', description: 'passthrough the exit code of the remote command'},
+//     {name: 'env', char: 'e', description: "environment variables to set (use ';' to split multiple vars)", hasValue: true},
+//     {name: 'no-tty', description: 'force the command to not run in a tty', hasValue: false},
+//     {name: 'listen', description: 'listen on a local port', hasValue: false, hidden: true},
+//     {name: 'no-notify', description: 'disables notification when dyno is up (alternatively use HEROKU_NOTIFICATIONS=0)', hasValue: false}
+//   ],
+//   run: cli.command(run)
+// }
--- packages/run/test/commands/run.test.ts
+++ packages/run/test/commands/run.test.ts
@@ -3,15 +3,66 @@ import {expect, test} from '@oclif/test'
 describe('run', () => {
   test
     .stdout()
-    .command(['run'])
-    .it('runs hello', ctx => {
-      expect(ctx.stdout).to.contain('hello world')
-    })
-
-  test
-    .stdout()
-    .command(['run', '--name', 'jeff'])
-    .it('runs hello --name jeff', ctx => {
-      expect(ctx.stdout).to.contain('hello jeff')
+    .command(['run', '--app=heroku-run-test-app', 'echo 1 2 3'])
+    .it('runs a command', ctx => {
+      expect(ctx.stdout).to.include('1 2 3\n')
     })
 })
+
+
+// 'use strict'
+
+// const cli = require('heroku-cli-util')
+// const cmd = require('../../commands/run')
+// const expect = require('unexpected')
+// const StdOutFixture = require('fixture-stdout')
+// const assertExit = require('../assert_exit')
+
+// describe('run', () => {
+//   beforeEach(() => cli.mockConsole())
+
+//   it('runs a command', () => {
+//     let stdout = ''
+//     let fixture = new StdOutFixture()
+//     fixture.capture(s => { stdout += s }) //     return cmd.run({ app: 'heroku-run-test-app', flags: {}, auth: { password: global.apikey }, args: ['echo', '1', '2', '3'] })
+//       .then(() => fixture.release())
+//       .then(() => expect(stdout, 'to contain', '1 2 3\n'))
+//   })
+
+//   it('runs a command with spaces', () => {
+//     let stdout = ''
+//     let fixture = new StdOutFixture()
+//     fixture.capture(s => { stdout += s })
+//     return cmd.run({ app: 'heroku-run-test-app', flags: {}, auth: { password: global.apikey }, args: ['ruby', '-e', 'puts ARGV[0]', '{"foo": "bar"} '] })
+//       .then(() => fixture.release())
+//       .then(() => expect(stdout, 'to equal', '{"foo": "bar"} \n'))
+//   })
+
+//   it('runs a command with quotes', () => {
+//     let stdout = ''
+//     let fixture = new StdOutFixture()
+//     fixture.capture(s => { stdout += s })
+//     return cmd.run({ app: 'heroku-run-test-app', flags: {}, auth: { password: global.apikey }, args: ['ruby', '-e', 'puts ARGV[0]', '{"foo":"bar"}'] })
+//       .then(() => fixture.release())
+//       .then(() => expect(stdout, 'to equal', '{"foo":"bar"}\n'))
+//   })
+
+//   it('runs a command with env vars', () => {
+//     let stdout = ''
+//     let fixture = new StdOutFixture()
+//     fixture.capture(s => { stdout += s })
+//     return cmd.run({ app: 'heroku-run-test-app', flags: { env: 'FOO=bar' }, auth: { password: global.apikey }, args: ['env'] })
+//       .then(() => fixture.release())
+//       .then(() => expect(stdout, 'to contain', 'FOO=bar'))
+//   })
+
+//   it('gets 127 status for invalid command', () => {
+//     cli.exit.mock()
+//     return assertExit(127, cmd.run({
+//       app: 'heroku-run-test-app',
+//       flags: { 'exit-code': true },
+//       auth: { password: global.apikey },
+//       args: ['invalid-command']
+//     }))
+//   })
+// })
