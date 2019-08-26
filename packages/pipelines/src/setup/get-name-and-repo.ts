192642ddc70acdb0459f8500609073ab560a0a81
Chris Freeman <cfreeman.code@gmail.com>
2019-08-26 13:40:39 -0600

    refactor away from inquirer

--- packages/pipelines/src/setup/get-name-and-repo.ts
+++ packages/pipelines/src/setup/get-name-and-repo.ts
@@ -1,33 +1,52 @@
-import {prompt} from 'inquirer'
+import cli from 'cli-ux'
 
 import {pipelineName, repoName} from './validate'
 
 function filter(obj: any) {
   const ret: any = {}
-  Object.keys(obj).filter((key: any) => obj[key] !== undefined).forEach((key: string) => { ret[key] = obj[key] })
+  Object.keys(obj)
+    .filter((key: any) => obj[key] !== undefined)
+    .forEach((key: string) => { ret[key] = obj[key] })
   return ret
 }
 
+interface GetNameAndRepoAnswer {
+  name: string | boolean
+  repo: string | boolean
+}
+
 export default async function getNameAndRepo(args: any) {
-  const answer = await prompt([{
-    type: 'input',
-    name: 'name',
-    message: 'Pipeline name',
-    when() { return !args.name },
-    validate(input) {
-      const [valid, msg] = pipelineName(input)
-      return valid || msg
+  let answer: GetNameAndRepoAnswer = {
+    name: '',
+    repo: ''
+  }
+
+  if (!args.name) {
+    let name = await cli.prompt('Pipeline name', {
+      required: true
+    })
+
+    const [valid, msg] = pipelineName(name)
+
+    if (valid) {
+      answer.name = name
+    } else {
+      cli.error(msg as string)
     }
-  }, {
-    type: 'input',
-    name: 'repo',
-    message: 'GitHub repository to connect to (e.g. rails/rails)',
-    when() { return !args.repo },
-    validate(input) {
-      const [valid, msg] = repoName(input)
-      return valid || msg
+  }
+
+  if (!args.repo) {
+    let repo = await cli.prompt('GitHub repository to connect to (e.g. rails/rails)', {
+      required: true
+    })
+
+    const [valid, msg] = repoName(repo)
+    if (valid) {
+      answer.repo = repo
+    } else {
+      cli.error(msg as string)
     }
-  }])
+  }
 
   const reply: any = Object.assign(filter(answer), filter(args))
   reply.name = reply.name.toLowerCase().replace(/\s/g, '-')
