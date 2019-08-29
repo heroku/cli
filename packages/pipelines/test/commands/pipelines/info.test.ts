061a415d7156113ff0e1aceeca4f9fc1361dda67
Zane Whitfield <zwhitfield@salesforce.com>
2019-08-29 15:17:36 -0700

    Fix beginning test & app code

--- packages/pipelines/test/commands/pipelines/info.test.ts
+++ packages/pipelines/test/commands/pipelines/info.test.ts
@@ -1,8 +1,9 @@
 import {expect, test} from '@oclif/test'
+import Heroku from '@heroku-cli/schema'
 
 describe('pipelines:info', () => {
-  let pipelines, couplings: any , apps: any , api: any, pipeline: any, stage
-
+  type Stage = 'test' | 'review' | 'development' | 'staging' | 'production'
+  let stage: Stage
   const appNames = [
     'development-app-1',
     'development-app-2',
@@ -14,73 +15,74 @@ describe('pipelines:info', () => {
     'staging-app-2',
     'production-app-1'
   ]
+  let owner: any = null
+  let pipeline = {name: 'example', id: '0123', owner}
+  let pipelines = [pipeline]
 
-  function setup(owner: any) {
-    pipeline = {name: 'example', id: '0123', owner}
-
-    if (owner && owner.type === 'team') {
-      api.get(`/teams/${owner.id}`).reply(200, {
-        id: owner.id,
-        name: 'my-team'
-      })
-    }
-
-    pipelines = [pipeline]
-
-    apps = []
-    couplings = []
+  let apps: Array<Heroku.App> = []
+  let couplings: Array<Heroku.PipelineCoupling> = []
 
           // Build couplings
-    appNames.forEach((name, id) => {
-      stage = name.split('-')[0]
-      couplings.push({
-        stage,
-        app: {id: `app-${id + 1}`}
-      })
+  appNames.forEach((name, id) => {
+    stage = name.split('-')[0] as Stage
+    couplings.push({
+      stage,
+      app: {id: `app-${id + 1}`}
     })
+  })
 
           // Build apps
-    appNames.forEach((name, id) => {
-      apps.push(
-        {
-          id: `app-${id + 1}`,
-          name,
-          pipeline,
-          owner: {id: '1234', email: 'foo@user.com'}
-        }
+  appNames.forEach((name, id) => {
+    apps.push(
+      {
+        id: `app-${id + 1}`,
+        name,
+        pipeline,
+        owner: {id: '1234', email: 'foo@user.com'}
+      }
             )
-    })
+  })
 
-    api
-      .get('/pipelines')
-      .query(true)
-      .reply(200, pipelines)
-      .get('/pipelines/0123/pipeline-couplings')
-      .reply(200, couplings)
-      .post('/filters/apps')
-      .reply(200, apps)
+  const addMocks = (testInstance: typeof test) => {
+    return testInstance
+      .nock('https://api.heroku.com', api => {
+        api
+          .get('/pipelines')
+          .query(true)
+          .reply(200, pipelines)
+          .get('/pipelines/0123/pipeline-couplings')
+          .reply(200, couplings)
+          .post('/filters/apps')
+          .reply(200, apps)
+
+        if (owner && owner.type === 'team') {
+          api.get(`/teams/${owner.id}`).reply(200, {
+            id: owner.id,
+            name: 'my-team'
+          })
+        }
+      })
   }
 
   function itShowsPipelineApps(ctx: any) {
-    expect(ctx.stderr).to.include('=== example')
+    expect(ctx.stdout).to.include('=== example')
     appNames.forEach(name => {
-      expect(ctx.stderr).to.include(name)
+      expect(ctx.stdout).to.include(name)
     })
-    expect(ctx.stderr).to.include(`
-    app name           stage
-    ─────────────────  ───────────
-    development-app-1  development
-    development-app-2  development
-    review-app-1       review
-    review-app-2       review
-    review-app-3       review
-    review-app-4       review
-    staging-app-1      staging
-    staging-app-2      staging
-    production-app-1   production`)
+    expect(ctx.stdout).to.include(`
+app name            stage       
+⬢ development-app-1 development 
+⬢ development-app-2 development 
+⬢ review-app-1      review      
+⬢ review-app-2      review      
+⬢ review-app-3      review      
+⬢ review-app-4      review      
+⬢ staging-app-1     staging     
+⬢ staging-app-2     staging     
+⬢ production-app-1  production  `)
   }
 
-  test
+  addMocks(test)
     .stderr()
     .stdout()
     // .nock('https://api.heroku.com', api => {
@@ -91,24 +93,23 @@ describe('pipelines:info', () => {
       itShowsPipelineApps(ctx)
     })
 
-  describe(`when pipeline doesn't have an owner`, () => {
-    test
-      .stderr()
-      .stderr()
-      .command(['pipelines:info', 'example'])
-      .it(`doesn't display the owner`, ctx => {
-        expect(ctx.stderr).to.not.contain('owner: foo@user.com')
-      })
+  // describe(`when pipeline doesn't have an owner`, () => {
+  //   test
+  //     .stderr()
+  //     .stderr()
+  //     .command(['pipelines:info', 'example'])
+  //     .it(`doesn't display the owner`, ctx => {
+  //       expect(ctx.stderr).to.not.contain('owner: foo@user.com')
+  //     })
 
-      // describe(`when pipeline doesn't have an owner`, () => {
-      //   test
-      //     .stderr()
-      //     .stderr()
-      //     .command(['pipeline:info', 'example', '--json'])
-      //     .it('displays json format', ctx => {
-      //       JSON.parse(ctx.stderr).pipeline.name.to.eq('example')
-      //       JSON.parse(ctx.stderr).apps.length.to.eq(9)
-      //       itShowsPipelineApps(ctx)
-      //     })
-  })
+  //   test
+  //     .stderr()
+  //     .stderr()
+  //     .command(['pipeline:info', 'example'])
+  //     .it('displays json format', ctx => {
+  //       JSON.parse(ctx.stderr).pipeline.name.to.eq('example')
+  //       JSON.parse(ctx.stderr).apps.length.to.eq(9)
+  //       itShowsPipelineApps(ctx)
+  //     })
+  // })
 })
