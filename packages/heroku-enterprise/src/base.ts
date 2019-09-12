13dda95561fd220faf5d822ee576ca5129ed863c
Todd Evanoff <evanoff@gmail.com>
2019-09-12 09:23:47 -0700

    Merge pull request #75 from heroku/jd/ga
    [day of GA] remove beta warning

--- packages/heroku-enterprise/src/base.ts
+++ packages/heroku-enterprise/src/base.ts
@@ -1,17 +1,7 @@
 import {Command} from '@heroku-cli/command'
-import {IConfig, LoadOptions} from '@oclif/config'
-import {cli} from 'cli-ux'
+import {IConfig} from '@oclif/config'
 
 export default abstract class extends Command {
-  static async run(argv?: string[], config?: LoadOptions) {
-    // TODO: remove after GA
-    cli.warn(`heroku-enterprise is part of the Heroku Enterprise Account Beta program.
-For more info, please see "http://github.com/heroku/heroku-enterprise" or contact your CSA.
-`)
-
-    return super.run(argv, config)
-  }
-
   protected constructor(argv: string[], config: IConfig) {
     super(argv, config)
     this.heroku.defaults.headers = {
