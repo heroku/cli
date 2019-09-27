1e7c38a856c09937152dcf1d0d68bd949b018adb
Sama <31874211+samarao@users.noreply.github.com>
2019-09-27 13:18:19 -0700

    Revert "Revert "feat(pipelines): update disable for review apps 2.0 (#1322)" (#1337)"
    This reverts commit e1d95c33f8f84d3f7501e1dc142556cffcaadbe3.

--- packages/pipelines/src/commands/reviewapps/disable.ts
+++ packages/pipelines/src/commands/reviewapps/disable.ts
@@ -1,27 +1,26 @@
-import {Command, flags as HerokuFlags} from '@heroku-cli/command'
-import Heroku from '@heroku-cli/schema'
+import {Command, flags} from '@heroku-cli/command'
+import * as Heroku from '@heroku-cli/schema'
 import cli from 'cli-ux'
 
 export default class ReviewappsDisable extends Command {
-  static description = 'disable review apps or settings on an existing pipeline'
+  static description = 'disable review apps and/or settings on an existing pipeline'
 
   static examples = [
     '$ heroku reviewapps:disable -p my-pipeline -a my-app --autodeploy'
   ]
 
   static flags = {
-    app: HerokuFlags.app({
+    app: flags.app({
       description: 'parent app used by review apps',
+    }),
+    remote: flags.remote(),
+    pipeline: flags.pipeline({
       required: true,
     }),
-    pipeline: HerokuFlags.pipeline({
-      required: true,
-    }),
-    remote: HerokuFlags.remote(),
-    autodeploy: HerokuFlags.boolean({
+    autodeploy: flags.boolean({
       description: 'disable autodeployments'
     }),
-    autodestroy: HerokuFlags.boolean({
+    autodestroy: flags.boolean({
       description: 'disable automatically destroying review apps'
     }),
   }
@@ -29,33 +28,54 @@ export default class ReviewappsDisable extends Command {
   async run() {
     const {flags} = this.parse(ReviewappsDisable)
 
-    // if no flags are passed then the user is disabling review apps
-    let disable = !flags.autodeploy && !flags.autodestroy
+    if (flags.app) {
+      // remove app & remote flags when Review Apps 1.0 is deprecated
+      this.warn('Specifying an app via --app or --remote is no longer needed for this command')
+    }
 
-    let settings = {
-      pull_requests: {
-        enabled: !disable,
-        auto_deploy: false,
-        auto_destroy: false
-      }
+    let settings: {
+      automatic_review_apps: boolean,
+      destroy_stale_apps: boolean,
+      pipeline?: string,
+      repo?: string
+    } = {
+      automatic_review_apps: true,
+      destroy_stale_apps: true,
+      pipeline: undefined,
+      repo: undefined
     }
 
     if (flags.autodeploy) {
       this.log('Disabling auto deployment...')
-      settings.pull_requests.auto_deploy = false
+      settings.automatic_review_apps = false
     }
+
     if (flags.autodestroy) {
       this.log('Disabling auto destroy...')
-      settings.pull_requests.auto_destroy = false
+      settings.destroy_stale_apps = false
     }
 
     cli.action.start('Configuring pipeline')
 
-    let {body: app} = await this.heroku.get<Heroku.App>(`/apps/${flags.app}`)
+    let {body: pipeline} = await this.heroku.get<Heroku.Pipeline>(`/pipelines/${flags.pipeline}`)
 
-    await this.heroku.patch(`/apps/${app.id}/github`, {
-      hostname: 'kolkrabbi.heroku.com',
-      body: settings
+    settings.pipeline = pipeline.id
+
+    let {body: feature} = await this.heroku.get<Heroku.AccountFeature>('/account/features/dashboard-repositories-api')
+
+    if (feature.enabled) {
+      let {body: repo} = await this.heroku.get(`/pipelines/${pipeline.id}/repo`, {
+        headers: {Accept: 'application/vnd.heroku+json; version=3.repositories-api'}
+      })
+      settings.repo = repo.full_name
+    } else {
+      let {body: repo} = await this.heroku.get(`/pipelines/${pipeline.id}/repository`)
+      settings.repo = repo.repository.name
+    }
+
+    await this.heroku.post(`/pipelines/${pipeline.id}/review-app-config`, {
+      body: settings,
+      headers: {Accept: 'application/vnd.heroku+json; version=3.review-apps'}
     })
 
     cli.action.stop()
