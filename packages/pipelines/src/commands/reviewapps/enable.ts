ea2e5ad0736316c6ef1f7a8395e90ce51695fc2d
RasPhilCo <capt.pn@gmail.com>
2019-09-27 10:42:50 -0700

    Revert "feat(pipelines): update enable for review apps 2.0 (#1321)"
    This reverts commit f4406073719afda34ad4dca8b519d3e101264940.

--- packages/pipelines/src/commands/reviewapps/enable.ts
+++ packages/pipelines/src/commands/reviewapps/enable.ts
@@ -12,6 +12,7 @@ export default class ReviewappsEnable extends Command {
   static flags = {
     app: flags.app({
       description: 'parent app used by review apps',
+      required: true,
     }),
     remote: flags.remote(),
     pipeline: flags.string({
@@ -30,54 +31,30 @@ export default class ReviewappsEnable extends Command {
   async run() {
     const {flags} = this.parse(ReviewappsEnable)
 
-    if (flags.app) {
-      // remove app & remote flags when Review Apps 1.0 is deprecated
-      this.warn('Specifying an app via --app or --remote is no longer needed for this command')
-    }
-
-    let settings: {
-      automatic_review_apps: boolean,
-      destroy_stale_apps: boolean,
-      pipeline?: string,
-      repo?: string
-    } = {
-      automatic_review_apps: false,
-      destroy_stale_apps: false,
-      pipeline: undefined,
-      repo: undefined
+    const settings = {
+      pull_requests: {
+        enabled: true,
+        auto_deploy: false,
+        auto_destroy: false
+      }
     }
 
     if (flags.autodeploy) {
       this.log('Enabling auto deployment...')
-      settings.automatic_review_apps = true
+      settings.pull_requests.auto_deploy = true
     }
 
     if (flags.autodestroy) {
       this.log('Enabling auto destroy...')
-      settings.destroy_stale_apps = true
+      settings.pull_requests.auto_destroy = true
     }
-
     cli.action.start('Configuring pipeline')
 
-    let {body: pipeline} = await this.heroku.get<Heroku.Pipeline>(`/pipelines/${flags.pipeline}`)
+    let {body: app} = await this.heroku.get<Heroku.App>(`/apps/${flags.app}`)
 
-    settings.pipeline = pipeline.id
-
-    let {body: feature} = await this.heroku.get<Heroku.AccountFeature>('/account/features/dashboard-repositories-api')
-
-    if (feature.enabled) {
-      let {body: repo} = await this.heroku.get(`/pipelines/${pipeline.id}/repo`, {
-        headers: {Accept: 'application/vnd.heroku+json; version=3.repositories-api'}
-      })
-      settings.repo = repo.full_name
-    } else {
-      let {body: repo} = await this.heroku.get(`/pipelines/${pipeline.id}/repository`)
-      settings.repo = repo.repository.name
-    }
-
-    await this.heroku.post(`/pipelines/${pipeline.id}/review-app-config`, {
-      body: settings,
-      headers: {Accept: 'application/vnd.heroku+json; version=3.review-apps'}
+    await this.heroku.patch(`/apps/${app.id}/github`, {
+      hostname: 'kolkrabbi.heroku.com',
+      body: settings
     })
 
     cli.action.stop()
