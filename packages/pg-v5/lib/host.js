824e2be5e39154b21b50a2033d41a9717267623a
Tim Carey-Smith <tim@spork.in>
2019-10-31 11:20:11 -0700

    feat(pg-v5): support overriding the postgres hobby tier host

--- packages/pg-v5/lib/host.js
+++ packages/pg-v5/lib/host.js
@@ -4,8 +4,12 @@ const util = require('./util')
 
 module.exports = function (addon) {
   let host = process.env.HEROKU_POSTGRESQL_HOST
+  let starterHost = process.env.HEROKU_POSTGRESQL_STARTER_HOST
 
+  if (addon && util.starterPlan(addon)) {
+    if (starterHost) return `https://${starterHost}`
+    return 'https://postgres-starter-api.heroku.com'
+  }
   if (host) return `https://${host}`
-  if (addon && util.starterPlan(addon)) return 'https://postgres-starter-api.heroku.com'
   return 'https://postgres-api.heroku.com'
 }
