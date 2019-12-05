b07e2b24ec0da2aed3f719c4585adaa24a171780
Chad Carbert <chadcarbert@me.com>
2019-12-05 17:18:18 -0500

    chore(ps): autofix with eslint

--- packages/ps/src/commands/ps/autoscale/enable.ts
+++ packages/ps/src/commands/ps/autoscale/enable.ts
@@ -11,13 +11,14 @@ const isPerfOrPrivateTier = (size: string) => {
 
 export default class Enable extends Command {
   static description = 'enable web dyno autoscaling'
+
   static flags = {
     app: flags.app({required: true}),
     remote: flags.remote(),
     min: flags.integer({required: true, description: 'minimum number of dynos'}),
     max: flags.integer({required: true, description: 'maximum number of dynos'}),
     p95: flags.integer({description: 'desired p95 response time'}),
-    notifications: flags.boolean({description: 'receive email notifications when the max dyno limit is reached'})
+    notifications: flags.boolean({description: 'receive email notifications when the max dyno limit is reached'}),
   }
 
   async run() {
@@ -26,7 +27,7 @@ export default class Enable extends Command {
 
     const [appResponse, formationResponse] = await Promise.all([
       this.heroku.get<Heroku.App>(`/apps/${flags.app}`),
-      this.heroku.get<Heroku.Formation[]>(`/apps/${flags.app}/formation`)
+      this.heroku.get<Heroku.Formation[]>(`/apps/${flags.app}/formation`),
     ])
     const app = appResponse.body
     const formations = formationResponse.body
@@ -40,7 +41,7 @@ export default class Enable extends Command {
     }
 
     const {body} = await this.heroku.get<Heroku.Formation[]>(`/apps/${app.id}/formation/web/monitors`, {
-      hostname: METRICS_HOST
+      hostname: METRICS_HOST,
     })
     const scaleMonitor = (body || []).find((m: any) => m.action_type === 'scale')
 
@@ -50,32 +51,32 @@ export default class Enable extends Command {
       notification_period: 1440,
       op: 'GREATER_OR_EQUAL',
       period: 1,
-      notification_channels: flags.notifications ? ['app'] : []
+      notification_channels: flags.notifications ? ['app'] : [],
     }
 
     if (scaleMonitor) {
       updatedValues = {...updatedValues,
         min_quantity: flags.min || scaleMonitor.min_quantity,
         max_quantity: flags.max || scaleMonitor.max_quantity,
-        value: flags.p95 ? flags.p95 : scaleMonitor.value
+        value: flags.p95 ? flags.p95 : scaleMonitor.value,
       }
 
       await this.heroku.patch(`/apps/${app.id}/formation/web/monitors/${scaleMonitor.id}`,
         {
           body: updatedValues,
-          hostname: METRICS_HOST
+          hostname: METRICS_HOST,
         })
     } else {
       updatedValues = {...updatedValues,
         name: 'LATENCY_SCALE',
         min_quantity: flags.min,
         max_quantity: flags.max,
-        value: flags.p95 ? flags.p95 : 1000
+        value: flags.p95 ? flags.p95 : 1000,
       }
 
       await this.heroku.post(`/apps/${app.id}/formation/web/monitors`, {
         hostname: METRICS_HOST,
-        body: updatedValues
+        body: updatedValues,
       })
     }
 
