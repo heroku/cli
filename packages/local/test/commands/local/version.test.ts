ae7c697c9e1d6b477371f8daef010877bfadece9
Chad Carbert <chadcarbert@me.com>
2019-12-17 15:45:44 -0700

    chore(local): upgrade local to eslint (#1403)

--- packages/local/test/commands/local/version.test.ts
+++ packages/local/test/commands/local/version.test.ts
@@ -4,14 +4,14 @@ import * as foreman from '../../../src/fork-foreman'
 
 describe('local:version', () => {
   test
-    .stub(foreman, 'fork', (argv: string[]) => {
-      expect(argv).is.eql(['--version'])
-    })
-    .command(['local:version'])
-    .it('is passes the --version flag to foreman')
+  .stub(foreman, 'fork', (argv: string[]) => {
+    expect(argv).is.eql(['--version'])
+  })
+  .command(['local:version'])
+  .it('is passes the --version flag to foreman')
 
   test
-    .command(['local:version', 'extra'])
-    .catch(e => expect(e.message).to.equal('Unexpected argument: extra\nSee more help with --help'))
-    .it('is throws an error when extra arguments are passed in')
+  .command(['local:version', 'extra'])
+  .catch(error => expect(error.message).to.equal('Unexpected argument: extra\nSee more help with --help'))
+  .it('is throws an error when extra arguments are passed in')
 })
