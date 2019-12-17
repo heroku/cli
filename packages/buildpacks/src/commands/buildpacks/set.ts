bb45760bfe8d863b19bd26d05cc9f109ec0e5715
Chad Carbert <chadcarbert@me.com>
2019-12-17 15:50:21 -0700

    chore(buildpacks): upgrade buildpacks to eslint (#1411)

--- packages/buildpacks/src/commands/buildpacks/set.ts
+++ packages/buildpacks/src/commands/buildpacks/set.ts
@@ -4,31 +4,33 @@ import {BuildpackCommand} from '../../buildpacks'
 
 export default class Set extends Command {
   static description: 'set new app buildpack, overwriting into list of buildpacks if necessary'
+
   static flags = {
     app: Flags.app({required: true}),
     remote: Flags.remote(),
     index: Flags.integer({
       description: 'the 1-based index of the URL in the list of URLs',
-      char: 'i'
-    })
+      char: 'i',
+    }),
   }
+
   static args = [
     {
       name: 'buildpack',
       required: true,
-      description: 'namespace/name of the buildpack'
-    }
+      description: 'namespace/name of the buildpack',
+    },
   ]
 
   async run() {
-    let {args, flags} = this.parse(Set)
+    const {args, flags} = this.parse(Set)
 
     if (flags.index && flags.index < 0) {
       this.error('Invalid index. Must be greater than 0.')
     }
 
-    let buildpackCommand = new BuildpackCommand(this.heroku)
-    let buildpacks = await buildpackCommand.fetch(flags.app)
+    const buildpackCommand = new BuildpackCommand(this.heroku)
+    const buildpacks = await buildpackCommand.fetch(flags.app)
 
     await buildpackCommand.validateUrlNotSet(buildpacks, args.buildpack)
 
@@ -36,11 +38,11 @@ export default class Set extends Command {
     if (flags.index === undefined) {
       spliceIndex = 0
     } else {
-      let foundIndex = buildpackCommand.findIndex(buildpacks, flags.index)
+      const foundIndex = buildpackCommand.findIndex(buildpacks, flags.index)
       spliceIndex = (foundIndex === -1) ? buildpacks.length : foundIndex
     }
 
-    let buildpackUpdates = await buildpackCommand.mutate(flags.app, buildpacks, spliceIndex, args.buildpack, 'set')
+    const buildpackUpdates = await buildpackCommand.mutate(flags.app, buildpacks, spliceIndex, args.buildpack, 'set')
     buildpackCommand.displayUpdate(flags.app, flags.remote || '', buildpackUpdates, 'set')
   }
 }
