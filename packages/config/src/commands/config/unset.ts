fdac605171027d78c0934b1008e7de7fd24d0956
Chad Carbert <chadcarbert@me.com>
2019-12-17 15:47:25 -0700

    chore(config): upgrade config to eslint (#1397)

--- packages/config/src/commands/config/unset.ts
+++ packages/config/src/commands/config/unset.ts
@@ -8,14 +8,18 @@ export class ConfigUnset extends Command {
   static aliases = [
     'config:remove',
   ]
+
   static description = 'unset one or more config vars'
+
   static examples = [
     `$ heroku config:unset RAILS_ENV
 Unsetting RAILS_ENV and restarting example... done, v10`,
     `$ heroku config:unset RAILS_ENV RACK_ENV
 Unsetting RAILS_ENV, RACK_ENV and restarting example... done, v10`,
   ]
+
   static strict = false
+
   static flags = {
     app: flags.app({char: 'a', required: true}),
     remote: flags.remote({char: 'r'}),
@@ -26,7 +30,7 @@ Unsetting RAILS_ENV, RACK_ENV and restarting example... done, v10`,
     const lastRelease = async () => {
       const {body: releases} = await this.heroku.get<Heroku.Release[]>(`/apps/${flags.app}/releases`, {
         partial: true,
-        headers: {Range: 'version ..; order=desc,max=1'}
+        headers: {Range: 'version ..; order=desc,max=1'},
       })
       return releases[0]
     }
@@ -35,14 +39,17 @@ Unsetting RAILS_ENV, RACK_ENV and restarting example... done, v10`,
       this.error('Usage: heroku config:unset KEY1 [KEY2 ...]\nMust specify KEY to unset.')
     }
 
-    let vars = argv.map(v => color.configVar(v)).join(', ')
+    const vars = argv.map(v => color.configVar(v)).join(', ')
 
     ux.action.start(`Unsetting ${vars} and restarting ${color.app(flags.app)}`)
     await this.heroku.patch(`/apps/${flags.app}/config-vars`, {
       // body will be like {FOO: null, BAR: null}
-      body: _.reduce(argv, (vars, v) => { vars[v] = null; return vars }, {} as {[k: string]: null})
+      body: _.reduce(argv, (vars, v) => {
+        vars[v] = null
+        return vars
+      }, {} as {[k: string]: null}),
     })
-    let release = await lastRelease()
+    const release = await lastRelease()
     ux.action.stop('done, ' + color.release(`v${release.version}`))
   }
 }
