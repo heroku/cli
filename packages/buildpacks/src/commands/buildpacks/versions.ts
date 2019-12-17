c3acdaea9edb0d186481b6da9a25788680dede5c
Chad Carbert <chadcarbert@me.com>
2019-12-17 15:50:21 -0700

    chore(buildpacks): upgrade buildpacks to eslint (#1411)

--- packages/buildpacks/src/commands/buildpacks/versions.ts
+++ packages/buildpacks/src/commands/buildpacks/versions.ts
@@ -6,31 +6,32 @@ import {BuildpackRegistry, RevisionBody} from '@heroku/buildpack-registry'
 
 export default class Versions extends Command {
   static description = 'list versions of a buildpack'
+
   static args = [
     {
       name: 'buildpack',
       required: true,
-      description: 'namespace/name of the buildpack'
-    }
+      description: 'namespace/name of the buildpack',
+    },
   ]
 
   async run() {
-    let {args} = this.parse(Versions)
-    let registry: BuildpackRegistry
-    let herokuAuth = this.heroku.auth || ''
+    const {args} = this.parse(Versions)
+    const herokuAuth = this.heroku.auth || ''
     if (herokuAuth === '') {
       this.error('You need to be logged in to run this command.')
     }
-    registry = new BuildpackRegistry()
+    const registry = new BuildpackRegistry()
 
     Result.match({
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
       Ok: _ => {},
       Err: err => {
         this.error(`Could not find the buildpack.\n${err}`)
       },
     }, BuildpackRegistry.isValidBuildpackSlug(args.buildpack))
 
-    let result = await registry.listVersions(args.buildpack)
+    const result = await registry.listVersions(args.buildpack)
     Result.match({
       Ok: versions => {
         cli.table(versions.sort((a: RevisionBody, b: RevisionBody) => {
@@ -39,8 +40,8 @@ export default class Versions extends Command {
           columns: [
             {key: 'release', label: 'Version'},
             {key: 'created_at', label: 'Released At'},
-            {key: 'status', label: 'Status'}
-          ]
+            {key: 'status', label: 'Status'},
+          ],
         })
       },
       Err: err => {
@@ -49,7 +50,7 @@ export default class Versions extends Command {
         } else {
           this.error(`Problem fetching versions, ${err.status}: ${err.description}`)
         }
-      }
+      },
     }, result)
   }
 }
