ee9ba4986bf4eba4c5c97d3d492ff3bf1bf3757c
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2020-01-27 15:00:21 -0800

    chore(webhooks): tslint -> eslint

--- packages/webhooks/test/commands/base.test.ts
+++ packages/webhooks/test/commands/base.test.ts
@@ -5,10 +5,14 @@ import * as path from 'path'
 import webhooksAbstractClass from '../../src/commands/base'
 
 class Webhooks extends webhooksAbstractClass {
+  // eslint-disable-next-line no-useless-constructor
   constructor(argv: string[], config: IConfig) {
     super(argv, config)
   }
-  async run() {}
+
+  async run() {
+    'empty run function'
+  }
 }
 const root = path.resolve(__dirname, '../package.json')
 const config = new Config({root})
@@ -16,26 +20,26 @@ const webhookObject = new Webhooks([], config)
 
 describe('webhooks type', () => {
   test
-    .stdout()
-    .do(function () {
-      const webhookInfo = webhookObject.webhookType({pipeline: 'randomPipeline', app: ''})
-      expect(webhookInfo).to.deep.equal({path: '/pipelines/randomPipeline', display: 'randomPipeline'})
-    })
-    .it('returns correct pipeline path and display info')
+  .stdout()
+  .do(function () {
+    const webhookInfo = webhookObject.webhookType({pipeline: 'randomPipeline', app: ''})
+    expect(webhookInfo).to.deep.equal({path: '/pipelines/randomPipeline', display: 'randomPipeline'})
+  })
+  .it('returns correct pipeline path and display info')
 
   test
-    .stdout()
-    .do(function () {
-      const webhookInfo = webhookObject.webhookType({pipeline: '', app: 'randomApp'})
-      expect(webhookInfo).to.deep.equal({path: '/apps/randomApp', display: 'randomApp'})
-    })
-    .it('returns correct app path and display info')
+  .stdout()
+  .do(function () {
+    const webhookInfo = webhookObject.webhookType({pipeline: '', app: 'randomApp'})
+    expect(webhookInfo).to.deep.equal({path: '/apps/randomApp', display: 'randomApp'})
+  })
+  .it('returns correct app path and display info')
 
   test
-    .stdout()
-    .do(function () {
-      webhookObject.webhookType({pipeline: '', app: ''})
-    })
-    .catch(e => expect(e.message).to.equal('No app specified'))
-    .it('returns error if no arguments are given')
+  .stdout()
+  .do(function () {
+    webhookObject.webhookType({pipeline: '', app: ''})
+  })
+  .catch(error => expect(error.message).to.equal('No app specified'))
+  .it('returns error if no arguments are given')
 })
