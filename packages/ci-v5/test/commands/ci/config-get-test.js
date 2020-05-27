5ee3331cb1b6946abb463ac54e493e79a8d25796
Chad Carbert <chadcarbert@me.com>
2020-05-27 16:26:24 -0400

    fix(ci-v5): switch generator tests to async/await

--- packages/ci-v5/test/commands/ci/config-get-test.js
+++ packages/ci-v5/test/commands/ci/config-get-test.js
@@ -16,27 +16,27 @@ describe('heroku ci:config:get', function () {
     pipeline = Factory.pipeline
   })
 
-  it('displays the config value', function * () {
+  it('displays the config value', async function () {
     const api = nock('https://api.heroku.com')
       .get(`/pipelines/${pipeline.id}`)
       .reply(200, pipeline)
       .get(`/pipelines/${pipeline.id}/stage/test/config-vars`)
       .reply(200, { [key]: value })
 
-    yield cmd.run({ args: { key }, flags: { pipeline: pipeline.id } })
+    await cmd.run({ args: { key }, flags: { pipeline: pipeline.id } })
 
     expect(cli.stdout).to.equal(`${value}\n`)
     api.done()
   })
 
-  it('displays config formatted for shell', function * () {
+  it('displays config formatted for shell', async function () {
     const api = nock('https://api.heroku.com')
       .get(`/pipelines/${pipeline.id}`)
       .reply(200, pipeline)
       .get(`/pipelines/${pipeline.id}/stage/test/config-vars`)
       .reply(200, { [key]: value })
 
-    yield cmd.run({ args: { key }, flags: { shell: true, pipeline: pipeline.id } })
+    await cmd.run({ args: { key }, flags: { shell: true, pipeline: pipeline.id } })
 
     expect(cli.stdout).to.equal(`${key}=${value}\n`)
     api.done()
