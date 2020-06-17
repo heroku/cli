ed73629557830b0f84f0da31cff02e9ca39e5ad7
Stanley Stuart <sstuart@salesforce.com>
2020-06-16 20:31:08 -0700

    chore(apps): ensure '@heroku-cli/color' disables color in tests

--- packages/apps/test/helpers/init.js
+++ packages/apps/test/helpers/init.js
@@ -1,5 +1,7 @@
 const chalk = require('chalk')
 chalk.enabled = false
+const {color} = require('@heroku-cli/color')
+color.enabled = false
 
 process.stdout.columns = 80
 process.stderr.columns = 80
