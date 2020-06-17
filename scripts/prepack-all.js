37f44e6199ac08301f8332ee6df1802a67a6f97f
Stanley Stuart <sstuart@salesforce.com>
2020-06-16 21:41:31 -0700

    try prepacking

---
+++ scripts/prepack-all.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+const glob = require('glob')
+const concurrently = require('concurrently')
+const path = require('path')
+const os = require('os')
+
+const root = path.join(__dirname, '..')
+const packages = glob.sync(`${root}/packages/*`)
+
+const commands = packages.map(packagePath => {
+  const packageName = path.relative(root, path.basename(packagePath));
+  return {
+    name: `packages/${packageName}`,
+    command: `yarn --cwd="${path.normalize(packagePath)}" prepack`,
+    env: {
+      FORCE_COLOR: '0'
+    },
+    prefixColor: 'white'
+  }
+});
+
+async function run() {
+  const SIGINT_HANDLER = () => {
+    console.log('Received ctrl+c. Stopping scripts/prepack-all.js');
+    process.stdout.write('\n');
+    process.exit(1)
+  }
+  try {
+    process.once('SIGINT', SIGINT_HANDLER);
+    await concurrently(commands, {
+      maxProcesses: process.env.CI ? os.cpus() : 4,
+      killOthers: ['failure']
+    })
+  } catch (err) {
+    console.log('Error running tests: ', err);
+    process.exit(1);
+  } finally {
+    process.removeListener('SIGINT', SIGINT_HANDLER);
+  }
+}
+
+run()
+
