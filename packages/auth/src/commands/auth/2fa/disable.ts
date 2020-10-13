31113844548191d8e439a09e5f4da706d08b7dbc
Jessie A. Young <jessie.young@salesforce.com>
2020-10-12 21:45:51 -0700

    try something similar to https://github.com/heroku/cli/pull/1656/commits/cc3f414920698838b46a322290468bc707df1b47

--- packages/auth/src/commands/auth/2fa/disable.ts
+++ packages/auth/src/commands/auth/2fa/disable.ts
@@ -1,6 +1,6 @@
 import {Command} from '@heroku-cli/command'
 import * as Heroku from '@heroku-cli/schema'
-import ux from 'cli-ux'
+import cli from 'cli-ux'
 
 export default class Auth2faGenerate extends Command {
   static description = 'disables 2fa on account'
@@ -14,14 +14,13 @@ export default class Auth2faGenerate extends Command {
 Disabling 2fa on me@example.com... done`
 
   async run() {
-    ux.action.start('Disabling 2fa')
-    const {body: account} = await this.heroku.get<Heroku.Account>('/account', {
-      headers: {Accept: 'application/vnd.heroku+json; version=3.with_vaas_info'},
-    })
-    ux.action.start(`Disabling 2fa on ${account.email}`)
+    cli.action.start('Disabling 2fa')
+    const headers = { 'Accept': 'application/vnd.heroku+json; version=3.with_vaas_info' }
+    const {body: account} = await this.heroku.get<Heroku.Account>('/account', {headers})
+    cli.action.start(`Disabling 2fa on ${account.email}`)
     if (account.vaas_enrolled) this.error('Cannot disable 2fa via CLI\nPlease visit your Account page on the Heroku Dashboad to manage 2fa')
     if (!account.two_factor_authentication) this.error('2fa is already disabled')
-    const password = await ux.prompt('Password', {type: 'hide'})
+    const password = await cli.prompt('Password', {type: 'hide'})
     await this.heroku.patch('/account', {body: {password, two_factor_authentication: false}})
   }
 }
