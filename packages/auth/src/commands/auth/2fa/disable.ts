b5f9bf64371fed052f999710c9c032bafb3cbb93
Jessie A. Young <jessieay@users.noreply.github.com>
2020-10-14 09:19:23 -0700

    Update packages/auth/src/commands/auth/2fa/disable.ts
    Co-authored-by: Stanley Stuart <fivetanley@users.noreply.github.com>

--- packages/auth/src/commands/auth/2fa/disable.ts
+++ packages/auth/src/commands/auth/2fa/disable.ts
@@ -18,7 +18,7 @@ Disabling 2fa on me@example.com... done`
     const headers = {Accept: 'application/vnd.heroku+json; version=3.with_vaas_info'}
     const {body: account} = await this.heroku.get<Heroku.Account>('/account', {headers})
     cli.action.start(`Disabling 2fa on ${account.email}`)
-    if (account.vaas_enrolled) this.error('Cannot disable 2fa via CLI\nPlease visit your Account page on the Heroku Dashboad to manage 2fa')
+    if (account.vaas_enrolled) this.error('Cannot disable 2fa via CLI\nPlease visit your Account page on the Heroku Dashboard to manage 2fa: https://dashboard.heroku.com/account')
     if (!account.two_factor_authentication) this.error('2fa is already disabled')
     const password = await cli.prompt('Password', {type: 'hide'})
     await this.heroku.patch('/account', {body: {password, two_factor_authentication: false}})
