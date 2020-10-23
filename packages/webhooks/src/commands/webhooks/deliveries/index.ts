cde41cb80f2b5a8a5a094d8b86be3c4b5671c19d
Dan Peterson <dpiddy@gmail.com>
2020-10-23 11:52:03 -0300

    webhooks: show 20 deliveries by default, add --num flag
    The previously hard-coded number of 1000 puts strain on the webhooks
    API subsystem and can cause requests to fail. While that's not ideal,
    there's also no way to use a different number to work around the
    issue.
    
    1000 is probably a bit much for normal use, too, since it can easily
    scroll most results off the screen. 20 is used as the default,
    matching `heroku releases`.
    
    The new --num flag, also matching `heroku releases`, can be used to
    specify a different number.

--- packages/webhooks/src/commands/webhooks/deliveries/index.ts
+++ packages/webhooks/src/commands/webhooks/deliveries/index.ts
@@ -15,6 +15,7 @@ export default class Deliveries extends BaseCommand {
     remote: flags.remote(),
     status: flags.string({char: 's', description: 'filter deliveries by status'}),
     pipeline: flags.pipeline({char: 'p', description: 'pipeline on which to list', hidden: true}),
+    num: flags.integer({char: 'n', description: 'number of deliveries to show (default: 20)'}),
   }
 
   async run() {
@@ -22,16 +23,18 @@ export default class Deliveries extends BaseCommand {
     const webhookType = this.webhookType(flags)
     let {path} = webhookType
     const {display} = webhookType
-    const max = 1000
 
     path = `${path}/webhook-deliveries`
     if (flags.status) {
       path += `?eq[status]=${encodeURIComponent(flags.status)}`
     }
 
-    const {body: deliveries} = await this.webhooksClient.get(path, {
+    if (!flags.num) {
+      flags.num = 20
+    }
+    const {body: deliveries, statusCode: status} = await this.webhooksClient.get(path, {
       headers: {
-        Range: `seq ..; order=desc,max=${max}`,
+        Range: `seq ..; order=desc,max=${flags.num}`,
       },
       partial: true,
     })
@@ -45,8 +48,8 @@ export default class Deliveries extends BaseCommand {
 
       deliveries.reverse()
 
-      if (deliveries.length === max) {
-        this.warn(`Only showing the ${max} most recent deliveries`)
+      if (status === 206) {
+        this.warn(`Only showing the ${flags.num} most recent deliveries`)
         this.warn('It is possible to filter deliveries by using the --status flag')
       }
 
