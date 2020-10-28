d343463faeca18d7400e5f300799973c955a24e1
Chris Freeman <cfreeman.code@gmail.com>
2020-10-28 14:27:23 -0600

    cleanup update command and make it public

--- packages/apps/src/commands/domains/update.ts
+++ packages/apps/src/commands/domains/update.ts
@@ -5,15 +5,16 @@ import cli from 'cli-ux'
 export default class DomainsUpdate extends Command {
   static description = 'update a domain to use a different SSL certificate on an app'
 
-  static examples = ['heroku domains:update www.example.com --cert-id mycert']
-
-  static hidden = true
+  static examples = ['heroku domains:update www.example.com --cert mycert']
 
   static flags = {
     help: flags.help({char: 'h'}),
     app: flags.app({required: true}),
     remote: flags.remote(),
-    'cert-id': flags.string({required: true}),
+    cert: flags.string({
+      required: true,
+      description: 'the name or id of the certificate you want to use for this domain',
+    }),
   }
 
   static args = [{name: 'hostname'}]
@@ -22,12 +23,12 @@ export default class DomainsUpdate extends Command {
     const {args, flags} = this.parse(DomainsUpdate)
     const {hostname} = args
     try {
-      cli.action.start(`Updating ${color.cyan(hostname)} to use ${color.cyan(flags['cert-id'])} certificate`)
+      cli.action.start(`Updating ${color.cyan(hostname)} to use ${color.cyan(flags.cert)} certificate`)
       await this.heroku.patch<string>(`/apps/${flags.app}/domains/${hostname}`, {
         headers: {
           Accept: 'application/vnd.heroku+json; version=3.allow_multiple_sni_endpoints',
         },
-        body: {sni_endpoint: flags['cert-id']},
+        body: {sni_endpoint: flags.cert},
       })
     } catch (error) {
       cli.error(error)
