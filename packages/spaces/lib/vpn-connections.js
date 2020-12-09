3f96a296a4cfda6309478de36e1d50bea043cc7c
Yannick Schutz <yschutz@salesforce.com>
2020-12-09 16:58:55 +0100

    Standard fix

--- packages/spaces/lib/vpn-connections.js
+++ packages/spaces/lib/vpn-connections.js
@@ -1,7 +1,7 @@
 'use strict'
 
 module.exports = function (heroku) {
-  function postVPNConnections(space, name, ip, cidrs) {
+  function postVPNConnections (space, name, ip, cidrs) {
     return request('POST', `/spaces/${space}/vpn-connections`, {
       name: name,
       public_ip: ip,
@@ -9,25 +9,25 @@ module.exports = function (heroku) {
     })
   }
 
-  function patchVPNConnections(space, name, cidrs) {
+  function patchVPNConnections (space, name, cidrs) {
     return request('PATCH', `/spaces/${space}/vpn-connections/${name}`, {
       routable_cidrs: cidrs
     })
   }
 
-  function getVPNConnections(space) {
+  function getVPNConnections (space) {
     return request('GET', `/spaces/${space}/vpn-connections`)
   }
 
-  function getVPNConnection(space, name) {
+  function getVPNConnection (space, name) {
     return request('GET', `/spaces/${space}/vpn-connections/${name}`)
   }
 
-  function deleteVPNConnection(space, name) {
+  function deleteVPNConnection (space, name) {
     return request('DELETE', `/spaces/${space}/vpn-connections/${name}`)
   }
 
-  function request(method, path, body) {
+  function request (method, path, body) {
     return heroku.request({
       method: method,
       path: encodeURI(path),
