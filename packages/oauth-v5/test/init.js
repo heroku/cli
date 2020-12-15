af4f56fdae86e742952cac8016d7f1253b12315e
Stanley Stuart <sstuart@salesforce.com>
2020-12-14 18:56:47 -0800

    test(oauth-v5): remove unexpected module; add chai

--- packages/oauth-v5/test/init.js
+++ packages/oauth-v5/test/init.js
@@ -2,6 +2,8 @@
 
 const cli = require('heroku-cli-util')
 const nock = require('nock')
+const chai = require('chai')
+chai.use(require('chai-as-promised'))
 
 cli.raiseErrors = true
 nock.disableNetConnect()
