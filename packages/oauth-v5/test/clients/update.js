61b1cb93f3aef20566052c5f63f0286103ce228f
Stanley Stuart <sstuart@salesforce.com>
2020-12-14 17:21:42 -0800

    test(oauth-v5): remove un unexpected usage

--- packages/oauth-v5/test/clients/update.js
+++ packages/oauth-v5/test/clients/update.js
@@ -40,7 +40,7 @@ describe('clients:update', () => {
   })
 
   it('does nothing with no changes', () => {
-    return expect(cmd.run({ args: { id: 'f6e8d969-129f-42d2-854b-c2eca9d5a42e' }, flags: {} }),
-      'to be rejected with', 'No changes provided.')
+    return expect(cmd.run({ args: { id: 'f6e8d969-129f-42d2-854b-c2eca9d5a42e' }, flags: {} }))
+      .to.be.rejectedWith(Error, 'No changes provided.')
   })
 })
