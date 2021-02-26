a779949daffbd29e911c4d93ce1198cb91116526
Stanley Stuart <sstuart@salesforce.com>
2021-02-26 09:24:45 -0800

    fix(ci-v5): remove co-wait

--- packages/ci-v5/lib/test-run.js
+++ packages/ci-v5/lib/test-run.js
@@ -1,10 +1,9 @@
 const api = require('./heroku-api')
-const wait = require('co-wait')
 
 async function waitForStates(states, testRun, { heroku }) {
   while (!states.includes(testRun.status)) {
     testRun = await api.testRun(heroku, testRun.pipeline.id, testRun.number)
-    await wait(1000)
+    await new Promise((resolve) => setTimeout(resolve, 1000))
   }
   return testRun
 }
