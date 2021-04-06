1976ca132a12debb2c7764761377a605b232e193
Jessie A. Young <jessie.young@salesforce.com>
2021-04-06 16:29:44 -0700

    add back in example

--- packages/auth/src/commands/auth/2fa/disable.ts
+++ packages/auth/src/commands/auth/2fa/disable.ts
@@ -4,6 +4,7 @@ import cli from 'cli-ux'
 
 export default class Auth2faGenerate extends Command {
   static description = 'disables 2fa on account'
+  static example = `$ heroku auth:2fa:disable`
 
   static aliases = [
     'twofactor:disable',
