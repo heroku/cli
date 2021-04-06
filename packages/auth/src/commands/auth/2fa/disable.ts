57e1d00e9de9a65fd12efedb17e67ac79abdbcff
Jessie A. Young <jessie.young@salesforce.com>
2021-04-06 15:25:04 -0700

    feat(auth): Update auth:2fa:disable
    * Deprecation warning
    * This has already been deprecated for anyone in the MFA beta, which
      went to GA on April 5, 2021.
    * Next step will be to remove entirely
    * https://salesforce.quip.com/JYsuAbKPJa0W#UaWABAZB3C9

--- packages/auth/src/commands/auth/2fa/disable.ts
+++ packages/auth/src/commands/auth/2fa/disable.ts
@@ -10,17 +10,7 @@ export default class Auth2faGenerate extends Command {
     '2fa:disable',
   ]
 
-  static example = `$ heroku auth:2fa:disable
-Disabling 2fa on me@example.com... done`
-
   async run() {
-    cli.action.start('Disabling 2fa')
-    const headers = {Accept: 'application/vnd.heroku+json; version=3.with_vaas_info'}
-    const {body: account} = await this.heroku.get<Heroku.Account>('/account', {headers})
-    cli.action.start(`Disabling 2fa on ${account.email}`)
-    if (account.vaas_enrolled) this.error('Cannot disable 2fa via CLI\nPlease visit your Account page on the Heroku Dashboard to manage 2fa: https://dashboard.heroku.com/account')
-    if (!account.two_factor_authentication) this.error('2fa is already disabled')
-    const password = await cli.prompt('Password', {type: 'hide'})
-    await this.heroku.patch('/account', {body: {password, two_factor_authentication: false}})
+    cli.warn('DEPRECATION WARNING: this command has been removed, in favor of disabling MFA in your Account Settings in a browser.')
   }
 }
