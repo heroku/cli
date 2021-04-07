647970fa628cdd2e807832f7382be02936bb276c
Jessie A. Young <jessie.young@salesforce.com>
2021-04-07 09:48:34 -0700

    more lint

--- packages/auth/src/commands/auth/2fa/disable.ts
+++ packages/auth/src/commands/auth/2fa/disable.ts
@@ -3,7 +3,9 @@ import cli from 'cli-ux'
 
 export default class Auth2faGenerate extends Command {
   static description = 'disables 2fa on account'
+
   static example = '$ heroku auth:2fa:disable'
+
   static aliases = [
     'twofactor:disable',
     '2fa:disable',
