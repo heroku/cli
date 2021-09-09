da13b0b9c7538eee684389ffa84cad29884cc265
Justin Downing <jdowning@heroku.com>
2021-09-09 14:45:11 -0700

    surface message during redis:wait

--- packages/redis-v5/commands/wait.js
+++ packages/redis-v5/commands/wait.js
@@ -1,6 +1,46 @@
 'use strict'
 
 const cli = require('heroku-cli-util')
+const wait = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+async function run (context, heroku) {
+  const api = require('../lib/shared')(context, heroku)
+  const addon = await api.getRedisAddon()
+
+  let waitFor = async function waitFor() {
+    let interval = parseInt(context.flags['wait-interval'])
+    if (!interval || interval < 0) interval = 5
+    let status
+    let waiting = false
+
+    while (true) {
+      try {
+        status = await api.request(`/redis/v0/databases/${addon.name}/wait`, 'GET')
+      } catch (err) {
+        if (err.statusCode !== 404) throw err
+        status = { 'waiting?': true }
+      }
+
+      if (!status['waiting?']) {
+        if (waiting) {
+          cli.action.done(status.message)
+        }
+        return
+      }
+
+      if (!waiting) {
+        waiting = true
+        cli.action.start(`Waiting for database ${cli.color.addon(addon.name)}`)
+      }
+
+      cli.action.status(status.message)
+
+      await wait(interval * 1000)
+    }
+  }
+
+  await waitFor()
+}
 
 module.exports = {
   topic: 'redis',
@@ -9,19 +49,8 @@ module.exports = {
   needsAuth: true,
   args: [{ name: 'database', optional: true }],
   description: 'wait for Redis instance to be available',
-  run: cli.command(async (context, heroku) => {
-    const api = require('../lib/shared')(context, heroku)
-    const addon = await api.getRedisAddon()
-
-    let interval = setInterval(function () {
-      api.request(`/redis/v0/databases/${addon.name}/wait`, 'GET').then(function (status) {
-        if (!status['waiting?']) {
-          clearInterval(interval)
-        }
-      }, function (error) {
-        cli.error(error)
-        clearInterval(interval)
-      })
-    }, 500)
-  })
+  flags: [
+    { name: 'wait-interval', description: 'how frequently to poll in seconds', hasValue: true },
+  ],
+  run: cli.command({ preauth: true }, run)
 }
