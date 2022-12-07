85c30fb9b9e2e987cc5e375ece831e5d46b07a2e
Katy Bowman <katy.bowman@salesforce.com>
2022-12-07 11:53:20 -0500

    refactor(auth): refactor the auth package to use oclif/core

--- packages/auth/src/commands/labs/disable.ts
+++ packages/auth/src/commands/labs/disable.ts
@@ -1,13 +1,13 @@
 import color from '@heroku-cli/color'
 import {Command, flags} from '@heroku-cli/command'
 import * as Heroku from '@heroku-cli/schema'
-import {cli} from 'cli-ux'
+import {CliUx, Interfaces} from '@oclif/core'
 
 const SecurityExceptionFeatures: any = {
   'spaces-strict-tls': {
     async prompt(out: any, app: string): Promise<string> {
       out.warn('Insecure Action')
-      const name = await cli.prompt(`You are enabling an older security protocol, TLS 1.0, which some organizations may not deem secure.
+      const name = await CliUx.ux.prompt(`You are enabling an older security protocol, TLS 1.0, which some organizations may not deem secure.
 To proceed, type ${app} or re-run this command with --confirm ${app}`)
       return name
     },
@@ -19,14 +19,14 @@ export default class LabsDisable extends Command {
 
   static args = [{name: 'feature'}]
 
-  static flags = {
+  static flags: Interfaces.FlagInput = {
     app: flags.app(),
     remote: flags.remote(),
     confirm: flags.string({required: false}),
   }
 
   async run() {
-    const {args, flags} = this.parse(LabsDisable)
+    const {args, flags} = await this.parse(LabsDisable)
     const feature = args.feature
     let request
     let target
@@ -34,7 +34,7 @@ export default class LabsDisable extends Command {
     if (SecurityExceptionFeatures[feature]) {
       if (flags.confirm !== flags.app) {
         const prompt = SecurityExceptionFeatures[feature].prompt
-        const confirm = await prompt(cli, flags.app)
+        const confirm = await prompt(CliUx, flags.app)
         if (confirm !== flags.app) {
           this.error('Confirmation name did not match app name. Try again.')
         }
@@ -54,9 +54,9 @@ export default class LabsDisable extends Command {
       target = flags.app
     }
 
-    cli.action.start(`Disabling ${color.green(feature)} for ${color.cyan(target!)}`)
+    CliUx.ux.action.start(`Disabling ${color.green(feature)} for ${color.cyan(target!)}`)
     await request
-    cli.action.stop()
+    CliUx.ux.action.stop()
   }
 
   disableFeature(feature: string, app?: string): Promise<any> {
