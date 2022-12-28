aa7ee702adc6d3948a86e57a4b4ddedc2c6a07cd
Jennette <jheikes@salesforce.com>
2022-12-28 17:14:45 -0500

    chore: troubleshooting

--- packages/webhooks/test/commands/webhooks/add.test.ts
+++ packages/webhooks/test/commands/webhooks/add.test.ts
@@ -27,6 +27,8 @@ describe('webhooks:add', () => {
     'http://foobar.com',
   ])
   .it('adds a specific app webhook', ctx => {
+    console.log("stdout=" + ctx.stdout)
+    console.log("stderr=" + ctx.stderr)
     expect(ctx.stdout.trim()).to.equal('')
     expect(ctx.stderr).to.include('Adding webhook to example-app... done')
   })
