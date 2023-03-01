69abaa24bfa250184dc7c6806566380e65eda3cb
Vera Gangeskar Johne <vera@gangeskar.com>
2023-03-01 14:13:18 -0500

    log message from Shogun after upgrade request

--- packages/redis-v5/test/commands/upgrade.js
+++ packages/redis-v5/test/commands/upgrade.js
@@ -20,12 +20,13 @@ describe('heroku redis:upgrade', () => {
       .get('/apps/example/addons').reply(200, [
         { name: 'redis-haiku', addon_service: { name: 'heroku-redis' }, config_vars: ['REDIS_URL'] }
       ])
+    
+    let message = 'Your Redis version is being upgraded to 6.2. The system is preparing a maintenance. Once the maintenance is ready you can run it with heroku data:maintenances:run. See: https://devcenter.heroku.com/articles/data-maintenance-cli-commands#heroku-data-maintenances-run.'
 
     let redis = nock('https://redis-api.heroku.com:443')
       .post('/redis/v0/databases/redis-haiku/upgrade', { version: '6.2' }).reply(200, {
-        message: 'Upgrading version now!'
+        message: message
       })
-
     return command.run({ app: 'example', flags: { confirm: 'example', version: '6.2' }, args: {}, auth: { username: 'foobar', password: 'password' } })
       .then(() => app.done())
       .then(() => redis.done())
