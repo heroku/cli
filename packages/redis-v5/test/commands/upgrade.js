33bdd2d09cde5e68b3adf1cb2d2e027303ad1138
Vera Gangeskar Johne <vera@gangeskar.com>
2023-03-01 15:46:53 -0500

    test

--- packages/redis-v5/test/commands/upgrade.js
+++ packages/redis-v5/test/commands/upgrade.js
@@ -30,7 +30,6 @@ describe('heroku redis:upgrade', () => {
       .then(() => app.done())
       .then(() => redis.done())
       .then(() => expect(cli.stderr).to.equal('Requesting upgrade of redis-haiku to 6.2... Upgrading version now!\n'))
-      .then(() => expect(cli.stdout).to.equal('Upgrading version now!\n')
   })
 
   it('# errors on missing version', function () {
