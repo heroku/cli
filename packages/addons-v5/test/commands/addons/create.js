f500830332cd498748a5b87969b7347e20b92dd4
Katy Bowman <katy.bowman@salesforce.com>
2023-04-13 16:37:10 -0400

    chore: eslint fixes for addons-v5 package

--- packages/addons-v5/test/commands/addons/create.js
+++ packages/addons-v5/test/commands/addons/create.js
@@ -217,6 +217,7 @@ Use heroku addons:docs heroku-db3 to view documentation
       })
 
       it('waits for response and notifies', () => {
+        // eslint-disable-next-line node/no-extraneous-require
         const notifySpy = sandbox.spy(require('@heroku-cli/notifications'), 'notify')
 
         const asyncAddon = _.clone(addon)
@@ -255,6 +256,7 @@ Use heroku addons:docs heroku-db3 to view documentation
       })
 
       it('notifies when provisioning failure occurs', () => {
+        // eslint-disable-next-line node/no-extraneous-require
         const notifySpy = sandbox.spy(require('@heroku-cli/notifications'), 'notify')
 
         const asyncAddon = _.clone(addon)
