5587e6086e243b8f8e9a81b4e988cf0a9913db5e
Katy Bowman <katy.bowman@salesforce.com>
2023-04-13 16:37:10 -0400

    chore: eslint fixes for addons-v5 package

--- packages/addons-v5/lib/notify.js
+++ packages/addons-v5/lib/notify.js
@@ -3,19 +3,21 @@
 const path = require('path')
 
 module.exports = {
-  notify: function (subtitle, message, success=true) {
+  notify: function (subtitle, message, success = true) {
     const contentImage = path.join(__dirname, `../assets/${success ? 'success' : 'error'}.png`)
     try {
-      const { notify } = require('@heroku-cli/notifications')
+      // eslint-disable-next-line node/no-extraneous-require
+      const {notify} = require('@heroku-cli/notifications')
       notify({
         title: 'heroku cli',
         subtitle,
         message,
         contentImage,
-        sound: true
+        sound: true,
       })
-    } catch (err) {
-      cli.warn(err)
+    } catch (error) {
+      // eslint-disable-next-line no-undef
+      cli.warn(error)
     }
-  }
+  },
 }
