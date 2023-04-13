14bce4bfbcda966e24f1383683b474b7ae5d6cd7
RyanDagg <ryandagg@users.noreply.github.com>
2023-04-13 16:22:02 -0600

    refactor: safe test deploy environment (#2297)
    * refactor: single source of truth for s3 bucket. No longer coming from CI env vars, but from a location that oclif expects
    rewrite install_scripts from bash to js to simplify getting bucket name.
    
    * fix: require over import

---
+++ scripts/postrelease/install_scripts.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const execa = require('execa')
+const getHerokuS3Bucket = require('../utils/getHerokuS3Bucket')
+const qq = require('qqjs')
+
+qq.config.silent = false
+qq.run(async () => {
+  const { GITHUB_REF_TYPE, GITHUB_REF_NAME } = process.env
+  if (GITHUB_REF_TYPE === 'tag' && GITHUB_REF_NAME.startsWith('v')) {
+    const HEROKU_S3_BUCKET = getHerokuS3Bucket()
+    await execa(`aws s3 cp --content-type text/plain --cache-control "max-age: 604800" ./install-standalone.sh "s3://${HEROKU_S3_BUCKET}/install-standalone.sh"`)
+    await execa(`aws s3 cp --content-type text/plain --cache-control "max-age: 604800" ./install-standalone.sh "s3://${HEROKU_S3_BUCKET}/install.sh"`)
+    await execa(`aws s3 cp --content-type text/plain --cache-control "max-age: 604800" ./install-ubuntu.sh "s3://${HEROKU_S3_BUCKET}/install-ubuntu.sh"`)
+  } else {
+    console.log('Not on stable release, skipping updating install scripts')
+  }
+})
