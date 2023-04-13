09eb5172a16a33086cbba3e8ae256e1b18dfbb1f
RyanDagg <rdagg@salesforce.com>
2023-04-13 14:20:13 -0600

    refactor: single source of truth for s3 bucket. No longer coming from CI env vars, but from a location that oclif expects rewrite install_scripts from bash to js to simplify getting bucket name.

---
+++ scripts/utils/getHerokuS3Bucket.js
@@ -0,0 +1,15 @@
+import path from 'path'
+
+const getHerokuS3Bucket = () => {
+  const pjsonPath = path.join(__dirname, '..', '..', 'packages', 'cli', 'package.json')
+  const pjson = require(pjsonPath)
+  const bucket = pjson.oclif?.update?.s3?.bucket
+
+  if (!bucket) {
+    throw new Error('S3 bucket missing from packages/cli/package.json')
+  }
+
+  return bucket
+}
+
+module.exports = getHerokuS3Bucket
