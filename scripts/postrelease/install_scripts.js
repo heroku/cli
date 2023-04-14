4e94fb1fdc1a591bd1b744e2da69c0e0df03ed6e
RyanDagg <ryandagg@users.noreply.github.com>
2023-04-14 15:49:40 -0600

    fix: oclif promote step (#2301)
    * fix: upgrade to latest oclif for promote:deb fix.
    
    * fix: use correct path to install scripts
    
    * chore: update input description, remove unneeded continue-on-error

--- scripts/postrelease/install_scripts.js
+++ scripts/postrelease/install_scripts.js
@@ -9,9 +9,9 @@ qq.run(async () => {
   const { GITHUB_REF_TYPE, GITHUB_REF_NAME } = process.env
   if (GITHUB_REF_TYPE === 'tag' && GITHUB_REF_NAME.startsWith('v')) {
     const HEROKU_S3_BUCKET = getHerokuS3Bucket()
-    await execa(`aws s3 cp --content-type text/plain --cache-control "max-age: 604800" ./install-standalone.sh "s3://${HEROKU_S3_BUCKET}/install-standalone.sh"`)
-    await execa(`aws s3 cp --content-type text/plain --cache-control "max-age: 604800" ./install-standalone.sh "s3://${HEROKU_S3_BUCKET}/install.sh"`)
-    await execa(`aws s3 cp --content-type text/plain --cache-control "max-age: 604800" ./install-ubuntu.sh "s3://${HEROKU_S3_BUCKET}/install-ubuntu.sh"`)
+    await execa(`aws s3 cp --content-type text/plain --cache-control "max-age: 604800" ../../install-standalone.sh "s3://${HEROKU_S3_BUCKET}/install-standalone.sh"`)
+    await execa(`aws s3 cp --content-type text/plain --cache-control "max-age: 604800" ../../install-standalone.sh "s3://${HEROKU_S3_BUCKET}/install.sh"`)
+    await execa(`aws s3 cp --content-type text/plain --cache-control "max-age: 604800" ../../install-ubuntu.sh "s3://${HEROKU_S3_BUCKET}/install-ubuntu.sh"`)
   } else {
     console.log('Not on stable release, skipping updating install scripts')
   }
