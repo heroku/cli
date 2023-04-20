2365c5638c3657fe14ab499598b3a24b270d2c91
Zane Whitfield <zwhitfield@salesforce.com>
2023-04-20 13:50:22 -0700

    chore(orgs-v5): add code coverage reports (#2315)
    * Update leave tests
    
    * Update access:remove tests
    
    * Update orgs:open tests
    
    * Update packages/orgs-v5/test/commands/apps/leave.js
    
    Co-authored-by: Katy Bowman <katy.bowman@salesforce.com>
    
    * Update packages/orgs-v5/test/commands/apps/leave.js
    
    Co-authored-by: Katy Bowman <katy.bowman@salesforce.com>
    
    * Update packages/orgs-v5/test/commands/orgs/open.js
    
    Co-authored-by: Katy Bowman <katy.bowman@salesforce.com>
    
    * Update test description
    
    ---------
    
    Co-authored-by: Katy Bowman <katy.bowman@salesforce.com>

--- packages/orgs-v5/test/commands/access/remove.js
+++ packages/orgs-v5/test/commands/access/remove.js
@@ -1,7 +1,10 @@
 'use strict'
 /* globals describe it beforeEach afterEach context cli nock expect */
 
+let cli = require('heroku-cli-util')
 let cmd = require('../../../commands/access/remove')[0]
+const proxyquire = require('proxyquire')
+const sinon = require('sinon')
 let stubDelete = require('../../stub/delete')
 let apiDelete
 
@@ -19,6 +22,33 @@ describe('heroku access:remove', () => {
         .then(() => expect(`Removing raulb@heroku.com access from the app myapp... done
 `).to.eq(cli.stderr))
         .then(() => apiDelete.done())
+    })
+  })
+
+  context('using old command', () => {
+    let cmd2
+    let cliErrorStub
+    let processStub
+
+    before(() => {
+      cliErrorStub = sinon.stub(cli, 'error').returns(() => {})
+      cmd2 = proxyquire(('../../../commands/access/remove'), {
+        'cli': cliErrorStub
+      })
+      processStub = sinon.stub(process, 'exit').returns(() => {})
+      cli.mockConsole()
+    })
+    after(() => {
+      cliErrorStub.restore()
+      processStub.restore()
+      nock.cleanAll()
+    })
+
+    it('errors when attempting to use old command', () => {
+      cmd2[1].run({
+        app: 'myapp'
+      })
+      expect(cliErrorStub.called).to.equal(true)
     })
   })
 })
