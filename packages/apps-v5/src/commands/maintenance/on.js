e0f64b145b764d05808bc424ed5f6552b46ffaa6
Katy Bowman <katy.bowman@salesforce.com>
2023-04-26 14:39:19 -0400

    chore: eslint fixes for src/commands/maintenance/ & src/commands/notifications/

--- packages/apps-v5/src/commands/maintenance/on.js
+++ packages/apps-v5/src/commands/maintenance/on.js
@@ -4,7 +4,7 @@ const cli = require('heroku-cli-util')
 
 async function run(context, heroku) {
   let app = context.app
-  let p = heroku.patch(`/apps/${app}`, { body: { maintenance: true } })
+  let p = heroku.patch(`/apps/${app}`, {body: {maintenance: true}})
   await cli.action(`Enabling maintenance mode for ${cli.color.app(app)}`, p)
 }
 
@@ -14,5 +14,5 @@ module.exports = {
   description: 'put the app into maintenance mode',
   needsApp: true,
   needsAuth: true,
-  run: cli.command(run)
+  run: cli.command(run),
 }
