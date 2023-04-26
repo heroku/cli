a87be4a2a42f67bbf2fc9986c6e3128f6ac95c65
Katy Bowman <katy.bowman@salesforce.com>
2023-04-26 15:37:02 -0400

    chore: eslint fixes for test/commands/features/

--- packages/apps-v5/test/commands/features/enable.js
+++ packages/apps-v5/test/commands/features/enable.js
@@ -1,20 +1,20 @@
 'use strict'
-/* globals commands describe beforeEach it */
+/* globals commands beforeEach */
 
 const cli = require('heroku-cli-util')
 const nock = require('nock')
-const cmd = commands.find((c) => c.topic === 'features' && c.command === 'enable')
+const cmd = commands.find(c => c.topic === 'features' && c.command === 'enable')
 
 describe('features:enable', function () {
   beforeEach(() => cli.mockConsole())
 
   it('enables an app feature', function () {
     let api = nock('https://api.heroku.com:443')
-      .get('/apps/myapp/features/feature-a')
-      .reply(200, { enabled: false })
-      .patch('/apps/myapp/features/feature-a', { enabled: true })
-      .reply(200)
-    return cmd.run({ app: 'myapp', args: { feature: 'feature-a' } })
-      .then(() => api.done())
+    .get('/apps/myapp/features/feature-a')
+    .reply(200, {enabled: false})
+    .patch('/apps/myapp/features/feature-a', {enabled: true})
+    .reply(200)
+    return cmd.run({app: 'myapp', args: {feature: 'feature-a'}})
+    .then(() => api.done())
   })
 })
--- packages/apps-v5/test/commands/features/index.js
+++ packages/apps-v5/test/commands/features/index.js
@@ -1,25 +1,25 @@
 'use strict'
-/* globals commands describe beforeEach it */
+/* globals commands beforeEach */
 
 const cli = require('heroku-cli-util')
 const expect = require('chai').expect
 const nock = require('nock')
-const cmd = commands.find((c) => c.topic === 'features' && !c.command)
+const cmd = commands.find(c => c.topic === 'features' && !c.command)
 
 describe('features', () => {
   beforeEach(() => cli.mockConsole())
 
   it('shows the app features', function () {
     let api = nock('https://api.heroku.com:443')
-      .get('/apps/myapp/features')
-      .reply(200, [
-        { enabled: true, state: 'general', name: 'feature a', description: 'an app feature' }
-      ])
-    return cmd.run({ app: 'myapp', flags: {} })
-      .then(() => expect(cli.stderr).to.equal(''))
-      .then(() => expect(cli.stdout).to.equal(`=== App Features myapp
+    .get('/apps/myapp/features')
+    .reply(200, [
+      {enabled: true, state: 'general', name: 'feature a', description: 'an app feature'},
+    ])
+    return cmd.run({app: 'myapp', flags: {}})
+    .then(() => expect(cli.stderr).to.equal(''))
+    .then(() => expect(cli.stdout).to.equal(`=== App Features myapp
 [+] feature a  an app feature
 `))
-      .then(() => api.done())
+    .then(() => api.done())
   })
 })
