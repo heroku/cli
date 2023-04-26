c82b5ba4778db50ed618a02d8d8deea627839e4d
Katy Bowman <katy.bowman@salesforce.com>
2023-04-26 15:13:50 -0400

    chore: eslint fixes for test/commands/apps/favorites, notifications, & stack

--- packages/apps-v5/test/commands/apps/favorites/add.js
+++ packages/apps-v5/test/commands/apps/favorites/add.js
@@ -1,36 +1,36 @@
 'use strict'
-/* globals describe beforeEach it commands */
+/* globals beforeEach commands */
 
 const cli = require('heroku-cli-util')
 const nock = require('nock')
 const expect = require('chai').expect
-const cmd = commands.find((c) => c.topic === 'apps' && c.command === 'favorites:add')
+const cmd = commands.find(c => c.topic === 'apps' && c.command === 'favorites:add')
 
 describe('apps:favorites:add', () => {
   beforeEach(() => cli.mockConsole())
 
   it('adds the app as a favorite', () => {
     let api = nock('https://particleboard.heroku.com')
-      .get('/favorites?type=app')
-      .reply(200, [])
-      .post('/favorites', { type: 'app', resource_id: 'myapp' })
-      .reply(201)
+    .get('/favorites?type=app')
+    .reply(200, [])
+    .post('/favorites', {type: 'app', resource_id: 'myapp'})
+    .reply(201)
 
-    return cmd.run({ app: 'myapp' })
-      .then(() => expect(cli.stdout).to.equal(''))
-      .then(() => expect(cli.stderr).to.equal('Adding myapp to favorites... done\n'))
-      .then(() => api.done())
+    return cmd.run({app: 'myapp'})
+    .then(() => expect(cli.stdout).to.equal(''))
+    .then(() => expect(cli.stderr).to.equal('Adding myapp to favorites... done\n'))
+    .then(() => api.done())
   })
 
   it('errors if app is already favorited', () => {
     nock('https://particleboard.heroku.com')
-      .get('/favorites?type=app')
-      .reply(200, [{ resource_name: 'myapp' }])
+    .get('/favorites?type=app')
+    .reply(200, [{resource_name: 'myapp'}])
 
-    return cmd.run({ app: 'myapp' })
-      .catch(function (err) {
-        expect(err).to.be.an.instanceof(Error)
-        expect(err.message).to.equal('myapp is already a favorite app.')
-      })
+    return cmd.run({app: 'myapp'})
+    .catch(function (error) {
+      expect(error).to.be.an.instanceof(Error)
+      expect(error.message).to.equal('myapp is already a favorite app.')
+    })
   })
 })
