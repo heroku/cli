8691b10d840bb3c2b212760ea0d6babce3a44825
Katy Bowman <katy.bowman@salesforce.com>
2023-04-27 15:14:29 -0400

    chore: eslint fixes for test/commands/

--- packages/pg-v5/test/commands/outliers.js
+++ packages/pg-v5/test/commands/outliers.js
@@ -1,16 +1,16 @@
 'use strict'
-/* global describe it beforeEach afterEach */
+/* global beforeEach afterEach */
 
 const cli = require('heroku-cli-util')
-const { expect } = require('chai')
+const {expect} = require('chai')
 const nock = require('nock')
 const proxyquire = require('proxyquire')
-const { stdout } = require('stdout-stderr')
+const {stdout} = require('stdout-stderr')
 
 const db = {}
 const fetcher = () => {
   return {
-    database: () => db
+    database: () => db,
   }
 }
 
@@ -18,19 +18,19 @@ const EXPECTED_OUTPUT_TEXT = 'slow things'
 
 const psql = {
   serverVersion: '11.16',
-  fetchVersion: function (db) {
+  fetchVersion: function () {
     this._fetchVersionCalled = true
     return Promise.resolve(this.serverVersion)
   },
   exec: function (db, query) {
     this._query = query
     return Promise.resolve(EXPECTED_OUTPUT_TEXT)
-  }
+  },
 }
 
 const cmd = proxyquire('../../commands/outliers', {
   '../lib/fetcher': fetcher,
-  '../lib/psql': psql
+  '../lib/psql': psql,
 })
 
 describe('pg:outliers', () => {
@@ -49,14 +49,14 @@ describe('pg:outliers', () => {
   })
 
   it('reset queries stats', async () => {
-    await cmd.run({ app: 'myapp', args: {}, flags: { reset: true } })
+    await cmd.run({app: 'myapp', args: {}, flags: {reset: true}})
 
     expect(psql._query.trim()).to.equal('SELECT pg_stat_statements_reset()')
   })
 
   it('returns queries outliers', async () => {
     psql.serverVersion = '11.16'
-    await cmd.run({ app: 'myapp', args: {}, flags: {} })
+    await cmd.run({app: 'myapp', args: {}, flags: {}})
 
     expect(psql._fetchVersionCalled).to.eq(true)
     expect(psql._query.trim()).to.contain('total_time AS total_exec_time')
@@ -66,7 +66,7 @@ describe('pg:outliers', () => {
 
   it('uses an updated query for a newer version', async () => {
     psql.serverVersion = '13.7'
-    await cmd.run({ app: 'myapp', args: {}, flags: {} })
+    await cmd.run({app: 'myapp', args: {}, flags: {}})
     expect(psql._fetchVersionCalled).to.eq(true)
     expect(psql._query.trim()).to.contain('total_exec_time AS total_exec_time')
     expect(psql._query.trim()).to.contain('FROM pg_stat_statements')
