45ff1c9e72d0a6eb3fd7f877560eedfde26c0de2
Katy Bowman <katy.bowman@salesforce.com>
2023-04-27 10:26:32 -0400

    chore: eslint fixes for commands/backups/

--- packages/pg-v5/commands/backups/url.js
+++ packages/pg-v5/commands/backups/url.js
@@ -5,9 +5,9 @@ const cli = require('heroku-cli-util')
 async function run(context, heroku) {
   const host = require('../../lib/host')()
   const pgbackups = require('../../lib/pgbackups')(context, heroku)
-  const { sortBy } = require('lodash')
+  const {sortBy} = require('lodash')
 
-  const { app, args } = context
+  const {app, args} = context
 
   let num
 
@@ -15,13 +15,13 @@ async function run(context, heroku) {
     num = await pgbackups.transfer.num(args.backup_id)
     if (!num) throw new Error(`Invalid Backup: ${args.backup_id}`)
   } else {
-    let transfers = await heroku.get(`/client/v11/apps/${app}/transfers`, { host })
+    let transfers = await heroku.get(`/client/v11/apps/${app}/transfers`, {host})
     let lastBackup = sortBy(transfers.filter(t => t.succeeded && t.to_type === 'gof3r'), 'created_at').pop()
     if (!lastBackup) throw new Error(`No backups on ${cli.color.app(app)}. Capture one with ${cli.color.cmd('heroku pg:backups:capture')}`)
     num = lastBackup.num
   }
 
-  let info = await heroku.post(`/client/v11/apps/${app}/transfers/${num}/actions/public-url`, { host })
+  let info = await heroku.post(`/client/v11/apps/${app}/transfers/${num}/actions/public-url`, {host})
   cli.log(info.url)
 }
 
@@ -30,16 +30,16 @@ let cmd = {
   description: 'get secret but publicly accessible URL of a backup',
   needsApp: true,
   needsAuth: true,
-  args: [{ name: 'backup_id', optional: true }],
+  args: [{name: 'backup_id', optional: true}],
   flags: [
     // ignored but present for backwards compatibility
-    { name: 'quiet', char: 'q', hidden: true }
+    {name: 'quiet', char: 'q', hidden: true},
   ],
-  run: cli.command({ preauth: true }, run)
+  run: cli.command({preauth: true}, run),
 }
 
 module.exports = [
-  Object.assign({ command: 'backups:url' }, cmd),
-  Object.assign({ command: 'backups:public-url', hidden: true }, cmd),
-  Object.assign({ command: 'backups:publicurl', hidden: true }, cmd)
+  Object.assign({command: 'backups:url'}, cmd),
+  Object.assign({command: 'backups:public-url', hidden: true}, cmd),
+  Object.assign({command: 'backups:publicurl', hidden: true}, cmd),
 ]
