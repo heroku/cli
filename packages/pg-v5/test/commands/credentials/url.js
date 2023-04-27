fc5d2dc840dd902629be864cffad36f5f1210c06
Katy Bowman <katy.bowman@salesforce.com>
2023-04-27 14:06:14 -0400

    chore: eslint fixes for test/commands/credentials & test/commands/links

--- packages/pg-v5/test/commands/credentials/url.js
+++ packages/pg-v5/test/commands/credentials/url.js
@@ -1,8 +1,8 @@
 'use strict'
-/* global describe it beforeEach afterEach */
+/* global beforeEach afterEach */
 
 const cli = require('heroku-cli-util')
-const { expect } = require('chai')
+const {expect} = require('chai')
 const nock = require('nock')
 const proxyquire = require('proxyquire')
 
@@ -11,27 +11,29 @@ const db = {
   host: 'foo.com',
   user: 'jeff',
   password: 'pass',
-  url: { href: 'postgres://jeff:pass@foo.com/mydb' }
+  url: {href: 'postgres://jeff:pass@foo.com/mydb'},
 }
 
 const addon = {
   name: 'postgres-1',
-  plan: { name: 'heroku-postgresql:standard-0' }
+  plan: {name: 'heroku-postgresql:standard-0'},
 }
 
 const fetcher = () => {
   return {
     database: () => db,
-    addon: () => addon
+    addon: () => addon,
   }
 }
 
 const cmd = proxyquire('../../../commands/credentials/url', {
-  '../../lib/fetcher': fetcher
+  '../../lib/fetcher': fetcher,
 })
 
 describe('pg:credentials:url', () => {
-  let api, pg, starter
+  let api
+  let pg
+  let starter
 
   beforeEach(() => {
     api = nock('https://api.heroku.com')
@@ -57,19 +59,19 @@ describe('pg:credentials:url', () => {
         {
           user: 'jeff-rotating',
           password: 'passw0rd',
-          state: 'revoking'
+          state: 'revoking',
         },
         {
           user: 'jeff',
           password: 'hunter2',
-          state: 'active'
-        }
-      ]
+          state: 'active',
+        },
+      ],
     }
     pg.get('/postgres/v0/databases/postgres-1/credentials/jeff').reply(200, roleInfo)
 
-    return cmd.run({ app: 'myapp', args: {}, flags: { name: 'jeff' } })
-      .then(() => expect(cli.stdout).to.equal(`Connection information for jeff credential.\nConnection info string:
+    return cmd.run({app: 'myapp', args: {}, flags: {name: 'jeff'}})
+    .then(() => expect(cli.stdout).to.equal(`Connection information for jeff credential.\nConnection info string:
    "dbname=d123 host=localhost port=5442 user=jeff password=hunter2 sslmode=require"
 Connection URL:
    postgres://jeff:hunter2@localhost:5442/d123
@@ -79,39 +81,39 @@ Connection URL:
   it('throws an error when the db is starter plan but the name is specified', () => {
     const hobbyAddon = {
       name: 'postgres-1',
-      plan: { name: 'heroku-postgresql:hobby-dev' }
+      plan: {name: 'heroku-postgresql:hobby-dev'},
     }
 
     const fetcher = () => {
       return {
         database: () => db,
-        addon: () => hobbyAddon
+        addon: () => hobbyAddon,
       }
     }
 
     const cmd = proxyquire('../../../commands/credentials/url', {
-      '../../lib/fetcher': fetcher
+      '../../lib/fetcher': fetcher,
     })
 
     const err = 'Essential-tier databases support only one default credential.'
-    return expect(cmd.run({ app: 'myapp', args: {}, flags: { name: 'jeff' } })).to.be.rejectedWith(Error, err)
+    return expect(cmd.run({app: 'myapp', args: {}, flags: {name: 'jeff'}})).to.be.rejectedWith(Error, err)
   })
 
   it('shows the correct credentials with starter plan', () => {
     const hobbyAddon = {
       name: 'postgres-1',
-      plan: { name: 'heroku-postgresql:hobby-dev' }
+      plan: {name: 'heroku-postgresql:hobby-dev'},
     }
 
     const fetcher = () => {
       return {
         database: () => db,
-        addon: () => hobbyAddon
+        addon: () => hobbyAddon,
       }
     }
 
     const cmd = proxyquire('../../../commands/credentials/url', {
-      '../../lib/fetcher': fetcher
+      '../../lib/fetcher': fetcher,
     })
 
     let roleInfo = {
@@ -125,14 +127,14 @@ Connection URL:
         {
           user: 'abcdef',
           password: 'hunter2',
-          state: 'active'
-        }
-      ]
+          state: 'active',
+        },
+      ],
     }
     starter.get('/postgres/v0/databases/postgres-1/credentials/default').reply(200, roleInfo)
 
-    return cmd.run({ app: 'myapp', args: {}, flags: {} })
-      .then(() => expect(cli.stdout).to.equal(`Connection information for default credential.\nConnection info string:
+    return cmd.run({app: 'myapp', args: {}, flags: {}})
+    .then(() => expect(cli.stdout).to.equal(`Connection information for default credential.\nConnection info string:
    "dbname=d123 host=localhost port=5442 user=abcdef password=hunter2 sslmode=require"
 Connection URL:
    postgres://abcdef:hunter2@localhost:5442/d123
