1728fe085ba71020b0cc535ad4e9bcb777ce6fe6
Katy Bowman <katy.bowman@salesforce.com>
2023-04-28 15:17:11 -0400

    chore: eslint fixes for lib/

--- packages/spaces/lib/spaces.js
+++ packages/spaces/lib/spaces.js
@@ -1,12 +1,12 @@
 'use strict'
 
-function displayNat (nat) {
+function displayNat(nat) {
   if (!nat) return
   if (nat.state !== 'enabled') return nat.state
   return nat.sources.join(', ')
 }
 
-function displayShieldState (space) {
+function displayShieldState(space) {
   return space.shield ? 'on' : 'off'
 }
 
