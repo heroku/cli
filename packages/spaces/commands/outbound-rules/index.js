0cb67d8ba7d8a672981d7c746137010b8e3a1df5
Katy Bowman <katy.bowman@salesforce.com>
2023-04-28 15:06:23 -0400

    chore: eslint fixes for commands/drains, hosts/, & outbound-rules/

--- packages/spaces/commands/outbound-rules/index.js
+++ packages/spaces/commands/outbound-rules/index.js
@@ -1,13 +1,13 @@
 'use strict'
 
 let cli = require('heroku-cli-util')
-const { SpaceCompletion } = require('@heroku-cli/command/lib/completions')
+const {SpaceCompletion} = require('@heroku-cli/command/lib/completions')
 
-function displayJSON (rules) {
+function displayJSON(rules) {
   cli.log(JSON.stringify(rules, null, 2))
 }
 
-async function run (context, heroku) {
+async function run(context, heroku) {
   let lib = require('../../lib/outbound-rules')(heroku)
   let space = context.flags.space || context.args.space
   if (!space) throw new Error('Space name required.\nUSAGE: heroku outbound-rules --space my-space')
@@ -32,10 +32,10 @@ You can add specific rules that only allow your dyno to communicate with trusted
   needsApp: false,
   needsAuth: true,
   hidden: true,
-  args: [{ name: 'space', optional: true, hidden: true }],
+  args: [{name: 'space', optional: true, hidden: true}],
   flags: [
-    { name: 'space', char: 's', hasValue: true, description: 'space to get outbound rules from', completion: SpaceCompletion },
-    { name: 'json', description: 'output in json format' }
+    {name: 'space', char: 's', hasValue: true, description: 'space to get outbound rules from', completion: SpaceCompletion},
+    {name: 'json', description: 'output in json format'},
   ],
-  run: cli.command(run)
+  run: cli.command(run),
 }
