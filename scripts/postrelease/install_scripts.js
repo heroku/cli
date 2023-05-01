32a562d51ddfcf887c0bcbc2cc2e724390f5864d
Katy Bowman <katy.bowman@salesforce.com>
2023-05-01 16:00:38 -0400

    fix: add working directory for install scripts step (#2323)
    * fix: separate job for updating install scripts and update path
    
    * Update .github/workflows/pack-and-release.yml
    
    Co-authored-by: RyanDagg <ryandagg@users.noreply.github.com>
    
    * fix whitespace errors
    
    ---------
    
    Co-authored-by: RyanDagg <ryandagg@users.noreply.github.com>

--- scripts/postrelease/install_scripts.js
+++ scripts/postrelease/install_scripts.js
@@ -3,19 +3,15 @@
 const execa = require('execa')
 const getHerokuS3Bucket = require('../utils/getHerokuS3Bucket')
 const qq = require('qqjs')
-const path = require('path')
 
 qq.config.silent = false
 qq.run(async () => {
   const {GITHUB_REF_TYPE, GITHUB_REF_NAME} = process.env
   if (GITHUB_REF_TYPE === 'tag' && GITHUB_REF_NAME.startsWith('v')) {
     const HEROKU_S3_BUCKET = getHerokuS3Bucket()
-
-    const pathToRepoRoot = path.join(__dirname, '..', '..')
-    const pathToInstallStandalone = path.join(pathToRepoRoot, 'install-standalone.sh')
-    await execa(`aws s3 cp --content-type text/plain --cache-control "max-age: 604800" ${pathToInstallStandalone} "s3://${HEROKU_S3_BUCKET}/install-standalone.sh"`)
-    await execa(`aws s3 cp --content-type text/plain --cache-control "max-age: 604800" ${pathToInstallStandalone} "s3://${HEROKU_S3_BUCKET}/install.sh"`)
-    await execa(`aws s3 cp --content-type text/plain --cache-control "max-age: 604800" ${path.join(pathToRepoRoot, 'install-ubuntu.sh')} "s3://${HEROKU_S3_BUCKET}/install-ubuntu.sh"`)
+    await execa(`aws s3 cp --content-type text/plain --cache-control "max-age: 604800" ./install-standalone.sh "s3://${HEROKU_S3_BUCKET}/install-standalone.sh"`)
+    await execa(`aws s3 cp --content-type text/plain --cache-control "max-age: 604800" ./install-standalone.sh "s3://${HEROKU_S3_BUCKET}/install.sh"`)
+    await execa(`aws s3 cp --content-type text/plain --cache-control "max-age: 604800" ./install-ubuntu.sh "s3://${HEROKU_S3_BUCKET}/install-ubuntu.sh"`)
   } else {
     console.log('Not on stable release, skipping updating install scripts')
   }
