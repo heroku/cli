c9c5b46df21c2c2b27c547fd68d5808bbb44fcac
RyanDagg <rdagg@salesforce.com>
2023-05-02 10:10:37 -0600

    chore: fix usage of execa in install_scripts.js

--- scripts/postrelease/install_scripts.js
+++ scripts/postrelease/install_scripts.js
@@ -15,9 +15,9 @@ qq.run(async () => {
   const {GITHUB_REF_TYPE, GITHUB_REF_NAME} = process.env
   if (GITHUB_REF_TYPE === 'tag' && GITHUB_REF_NAME.startsWith('v')) {
     const HEROKU_S3_BUCKET = getHerokuS3Bucket()
-    await execa(`aws s3 cp --content-type text/plain --cache-control "max-age: 604800" ./install-standalone.sh "s3://${HEROKU_S3_BUCKET}/install-standalone.sh"`, opts)
-    await execa(`aws s3 cp --content-type text/plain --cache-control "max-age: 604800" ./install-standalone.sh "s3://${HEROKU_S3_BUCKET}/install.sh"`, opts)
-    await execa(`aws s3 cp --content-type text/plain --cache-control "max-age: 604800" ./install-ubuntu.sh "s3://${HEROKU_S3_BUCKET}/install-ubuntu.sh"`, opts)
+    await execa.command(`aws s3 cp --content-type text/plain --cache-control max-age=604800 ./install-standalone.sh s3://${HEROKU_S3_BUCKET}/install-standalone.sh`)
+    await execa.command(`aws s3 cp --content-type text/plain --cache-control max-age=604800 ./install-standalone.sh s3://${HEROKU_S3_BUCKET}/install.sh`, opts)
+    await execa.command(`aws s3 cp --content-type text/plain --cache-control max-age=604800 ./install-ubuntu.sh s3://${HEROKU_S3_BUCKET}/install-ubuntu.sh`, opts)
   } else {
     console.log('Not on stable release, skipping updating install scripts')
   }
