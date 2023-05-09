6b11439e12c18fd682e9524c2b0f17b87311d541
Katy Bowman <katy.bowman@salesforce.com>
2023-05-09 11:22:28 -0400

    chore: update indentations in auth

--- packages/auth/test/commands/2fa/index.test.ts
+++ packages/auth/test/commands/2fa/index.test.ts
@@ -2,24 +2,24 @@ import {expect, test} from '@oclif/test'
 
 describe('2fa', () => {
   test
-  .nock('https://api.heroku.com', api => api
-  .get('/account')
-  .reply(200, {two_factor_authentication: true}),
-  )
-  .stdout()
-  .command(['2fa'])
-  .it('shows 2fa is enabled', ({stdout}) => {
-    expect(stdout).to.equal('Two-factor authentication is enabled\n')
-  })
+    .nock('https://api.heroku.com', api => api
+      .get('/account')
+      .reply(200, {two_factor_authentication: true}),
+    )
+    .stdout()
+    .command(['2fa'])
+    .it('shows 2fa is enabled', ({stdout}) => {
+      expect(stdout).to.equal('Two-factor authentication is enabled\n')
+    })
 
   test
-  .nock('https://api.heroku.com', api => api
-  .get('/account')
-  .reply(200, {two_factor_authentication: false}),
-  )
-  .stdout()
-  .command(['2fa'])
-  .it('shows 2fa is not enabled', ({stdout}) => {
-    expect(stdout).to.equal('Two-factor authentication is not enabled\n')
-  })
+    .nock('https://api.heroku.com', api => api
+      .get('/account')
+      .reply(200, {two_factor_authentication: false}),
+    )
+    .stdout()
+    .command(['2fa'])
+    .it('shows 2fa is not enabled', ({stdout}) => {
+      expect(stdout).to.equal('Two-factor authentication is not enabled\n')
+    })
 })
