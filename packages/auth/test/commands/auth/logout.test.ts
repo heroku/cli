33ab1d24835cc7736f5fc54f48853ffa3fcc259a
Zane Whitfield <zwhitfield@salesforce.com>
2023-05-10 13:31:03 -0700

    chore(auth): add code coverage reports (#2348)
    * Remove index.ts from test coverage report
    
    * Add test coverage
    
    * Update test to pass CI
    
    * Add test coverage for logout.ts

---
+++ packages/auth/test/commands/auth/logout.test.ts
@@ -0,0 +1,18 @@
+import {expect, test} from '@oclif/test'
+import {CliUx} from '@oclif/core'
+import sinon from 'sinon'
+
+const cli = CliUx.ux.action
+const cliStub = sinon.stub(cli, 'start').callsFake(() => {})
+
+describe('auth:logout', async () => {
+  test
+    .stdout()
+    .stub(cli, 'start', cliStub)
+    .command(['logout'])
+    .it('shows cli logging user out', () => {
+      expect(cliStub.calledWith('Logging out')).to.equal(true)
+    })
+
+  after(() => cliStub.restore())
+})
