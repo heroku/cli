bf9911d488a90322481a22374519a8a67a253be2
Katy Bowman <katy.bowman@salesforce.com>
2023-05-18 15:46:22 -0400

    chore: add smoke test for auth package

--- packages/cli/test/acceptance/smoke.acceptance.test.ts
+++ packages/cli/test/acceptance/smoke.acceptance.test.ts
@@ -70,6 +70,11 @@ describe('@acceptance smoke tests', () => {
     expect(stdout).to.contain('has no webhooks')
   })
 
+  it('heroku auth:whoami', async () => {
+    const {stdout} = await run('auth:whoami')
+    expect(stdout).to.contain('heroku-cli@salesforce.com')
+  })
+
   // TODO: turn this test back on once the issue with listing plugins is fixed
   it.skip('asserts oclif plugins are in core', async () => {
     const cmd = await run('plugins --core')
