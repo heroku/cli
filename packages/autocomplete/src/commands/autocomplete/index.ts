0744e6cc68ab04eba76659cd348392cfe65ba127
Katy Bowman <katy.bowman@salesforce.com>
2023-05-22 09:55:08 -0400

    logging out the shell definitions

--- packages/autocomplete/src/commands/autocomplete/index.ts
+++ packages/autocomplete/src/commands/autocomplete/index.ts
@@ -27,6 +27,8 @@ export default class Index extends AutocompleteBase {
 
   async run() {
     const {args, flags} = await this.parse(Index)
+    console.log('args', args)
+    console.log('this.config', this.config)
     const shell = args.shell || this.config.shell
     this.errorIfNotSupportedShell(shell)
 
