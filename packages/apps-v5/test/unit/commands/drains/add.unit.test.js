0b52375a019f1a54e27b40a826b8a14eae83d510
Katy Bowman <katy.bowman@salesforce.com>
2023-05-30 17:22:56 -0400

    chore: add smoke tests for addons-v5, apps, and apps-v5 packages (#2370)
    * chore: add smoke test for addons-v5
    
    * chore: add smoke test for apps package
    
    * chore: add smoke test for apps-v5 package
    
    * chore: reorganize unit tests for addons-v5 package
    
    * chore: reorganize unit tests for apps package
    
    * chore: reorganize unit tests for apps-v5 package
    
    * fix: unit test fixes
    
    * fix: fix heroku apps smoke test

---
+++ packages/apps-v5/test/unit/commands/drains/add.unit.test.js
@@ -0,0 +1,20 @@
+'use strict'
+/* globals beforeEach */
+
+const cli = require('heroku-cli-util')
+const nock = require('nock')
+const cmd = require('../../../../src/commands/drains/add')
+const expect = require('chai').expect
+
+describe('drains:add', function () {
+  beforeEach(() => cli.mockConsole())
+
+  it('adds a log drain', function () {
+    let api = nock('https://api.heroku.com:443')
+      .post('/apps/myapp/log-drains', {url: 'syslog://logs.example.com'})
+      .reply(200, {url: 'syslog://logs.example.com'})
+    return cmd.run({app: 'myapp', args: {url: 'syslog://logs.example.com'}})
+      .then(() => expect(cli.stdout).to.equal('Successfully added drain syslog://logs.example.com\n'))
+      .then(() => api.done())
+  })
+})
---
+++ packages/apps-v5/test/unit/commands/maintenance/off.unit.test.js
@@ -0,0 +1,18 @@
+'use strict'
+/* globals beforeEach */
+
+const cli = require('heroku-cli-util')
+const nock = require('nock')
+const cmd = require('../../../../src/commands/maintenance/off')
+
+describe('maintenance:off', function () {
+  beforeEach(() => cli.mockConsole())
+
+  it('turns maintenance mode off', function () {
+    let api = nock('https://api.heroku.com:443')
+      .patch('/apps/myapp', {maintenance: false})
+      .reply(200)
+    return cmd.run({app: 'myapp'})
+      .then(() => api.done())
+  })
+})
