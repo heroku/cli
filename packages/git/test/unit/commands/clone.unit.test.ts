99e533b362a63b47902520b7914ef04cec3199de
Katy Bowman <katy.bowman@salesforce.com>
2023-06-13 13:40:36 -0400

    chore: remove broken tests form the git package

--- packages/git/test/unit/commands/clone.unit.test.ts
+++ packages/git/test/unit/commands/clone.unit.test.ts
@@ -1,17 +1,7 @@
-/* global beforeEach */
-
 'use strict'
 import {expect, test} from '@oclif/test'
-import sinon from 'sinon'
-import git from '../../../src/git'
 
 describe('git:clone', function () {
-  let gitMock: sinon.SinonMock
-
-  beforeEach(() => {
-    gitMock = sinon.mock(git)
-  })
-
   test
     .stderr()
     .command(['git:clone'])
@@ -19,17 +9,4 @@ describe('git:clone', function () {
       expect(error.message).to.contain('Missing required flag app')
     })
     .it('errors if no app given')
-
-  test
-    .nock('https://api.heroku.com', api => api
-      .get('/apps/myapp')
-      .reply(200, {name: 'myapp'}),
-    )
-    .stub(git, 'spawn', () => gitMock)
-    .stdout()
-    .command(['git:clone', '--app', 'myapp'])
-    .it('calls git spawn function with "clone" arg', () => {
-      gitMock.expects('spawn').once().withExactArgs(['clone', '-o', 'heroku', 'https://git.heroku.com/myapp.git', 'myapp']).returns(Promise.resolve())
-      gitMock.restore()
-    })
 })
--- packages/git/test/unit/commands/remote.unit.test.ts
+++ packages/git/test/unit/commands/remote.unit.test.ts
@@ -1,17 +1,7 @@
 'use strict'
-/* global beforeEach */
-
 import {expect, test} from '@oclif/test'
-import sinon from 'sinon'
-import git from '../../../src/git'
 
 describe('git:remote', function () {
-  let gitMock: sinon.SinonMock
-
-  beforeEach(() => {
-    gitMock = sinon.mock(git)
-  })
-
   test
     .stderr()
     .command(['git:remote'])
@@ -19,32 +9,4 @@ describe('git:remote', function () {
       expect(error.message).to.contain('Specify an app with --app')
     })
     .it('errors if no app given')
-
-  test
-    .nock('https://api.heroku.com', api => api
-      .get('/apps/myapp')
-      .reply(200, {name: 'myapp'}),
-    )
-    .stub(git, 'exec', () => gitMock)
-    .stdout()
-    .command(['git:remote', '--app', 'myapp'])
-    .it('calls git exec function with "remote" arg', ({stdout}) => {
-      gitMock.expects('exec').once().withExactArgs(['remote', 'set-url', 'heroku', 'https://git.heroku.com/myapp.git']).returns(Promise.resolve())
-      expect(stdout).to.contain('set git remote heroku to https://git.heroku.com/myapp.git\n')
-      gitMock.restore()
-    })
-
-  test
-    .nock('https://api.heroku.com', api => api
-      .get('/apps/myapp')
-      .reply(200, {name: 'myapp'}),
-    )
-    .stub(git, 'exec', () => gitMock)
-    .stdout()
-    .command(['git:remote', '--app', 'myapp'])
-    .it('calls git exec function with "remote" arg', ({stdout}) => {
-      gitMock.expects('exec').once().withExactArgs(['remote', 'add', 'heroku', 'https://git.heroku.com/myapp.git']).returns(Promise.resolve())
-      expect(stdout).to.contain('set git remote heroku to https://git.heroku.com/myapp.git\n')
-      gitMock.restore()
-    })
 })
