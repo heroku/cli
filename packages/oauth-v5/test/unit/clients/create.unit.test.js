5bba9244aede5ecd4693d89de9d094a5a7e21977
Zane Whitfield <zwhitfield@salesforce.com>
2023-06-14 14:25:15 -0700

    Update and add test coverage (#2388)

--- packages/oauth-v5/test/unit/clients/create.unit.test.js
+++ packages/oauth-v5/test/unit/clients/create.unit.test.js
@@ -27,4 +27,21 @@ HEROKU_OAUTH_SECRET=clientsecret
 `))
       .then(() => api.done())
   })
+
+  it('creates the client when json flag is passed', function () {
+    let api = nock('https://api.heroku.com:443')
+      .post('/oauth/clients', {
+        name: 'awesome',
+        redirect_uri: 'https://myapp.com',
+      })
+      .reply(201, {
+        name: 'awesome',
+        id: 'f6e8d969-129f-42d2-854b-c2eca9d5a42e',
+        redirect_uri: 'https://myapp.com',
+        secret: 'clientsecret',
+      })
+    return cmd.run({args: {name: 'awesome', redirect_uri: 'https://myapp.com'}, flags: {json: true}})
+      .then(() => expect(cli.stdout).to.equal('{\n  "name": "awesome",\n  "id": "f6e8d969-129f-42d2-854b-c2eca9d5a42e",\n  "redirect_uri": "https://myapp.com",\n  "secret": "clientsecret"\n}\n'))
+      .then(() => api.done())
+  })
 })
--- packages/oauth-v5/test/unit/clients/rotate.unit.test.js
+++ packages/oauth-v5/test/unit/clients/rotate.unit.test.js
@@ -3,6 +3,7 @@
 
 const cli = require('heroku-cli-util')
 const nock = require('nock')
+const {expect} = require('chai')
 const cmd = require('../../../lib/commands/clients/rotate')
 
 describe('clients:rotate', () => {
@@ -25,5 +26,33 @@ describe('clients:rotate', () => {
         secret: 'clientsecret',
       })
     return cmd.run({args: {id: 'f6e8d969-129f-42d2-854b-c2eca9d5a42e'}, flags: {url: 'https://heroku.com'}})
+      .then(() => expect(cli.stdout).to.equal('Updating f6e8d969-129f-42d2-854b-c2eca9d5a42e\n=== awesome\nid:           f6e8d969-129f-42d2-854b-c2eca9d5a42e\nname:         awesome\nredirect_uri: https://myapp.com\nsecret:       clientsecret\n'))
+      .then(() => api.done())
+  })
+
+  it('rotates the client secret when json flag is passed', () => {
+    api.post('/oauth/clients/f6e8d969-129f-42d2-854b-c2eca9d5a42e/actions/rotate-credentials')
+      .reply(200, {
+        name: 'awesome',
+        id: 'f6e8d969-129f-42d2-854b-c2eca9d5a42e',
+        redirect_uri: 'https://myapp.com',
+        secret: 'clientsecret',
+      })
+    return cmd.run({args: {id: 'f6e8d969-129f-42d2-854b-c2eca9d5a42e'}, flags: {url: 'https://heroku.com', json: true}})
+      .then(() => expect(cli.stdout).to.equal('Updating f6e8d969-129f-42d2-854b-c2eca9d5a42e\n{\n  "name": "awesome",\n  "id": "f6e8d969-129f-42d2-854b-c2eca9d5a42e",\n  "redirect_uri": "https://myapp.com",\n  "secret": "clientsecret"\n}\n'))
+      .then(() => api.done())
+  })
+
+  it('rotates the client secret when shell flag is passed', () => {
+    api.post('/oauth/clients/f6e8d969-129f-42d2-854b-c2eca9d5a42e/actions/rotate-credentials')
+      .reply(200, {
+        name: 'awesome',
+        id: 'f6e8d969-129f-42d2-854b-c2eca9d5a42e',
+        redirect_uri: 'https://myapp.com',
+        secret: 'clientsecret',
+      })
+    return cmd.run({args: {id: 'f6e8d969-129f-42d2-854b-c2eca9d5a42e'}, flags: {url: 'https://heroku.com', shell: true}})
+      .then(() => expect(cli.stdout).to.equal('Updating f6e8d969-129f-42d2-854b-c2eca9d5a42e\nHEROKU_OAUTH_ID=f6e8d969-129f-42d2-854b-c2eca9d5a42e\nHEROKU_OAUTH_SECRET=clientsecret\n'))
+      .then(() => api.done())
   })
 })
