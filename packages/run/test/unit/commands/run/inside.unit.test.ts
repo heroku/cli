d9fe928c0f09d6763d664539f8d7b722b5376a31
Katy Bowman <katy.bowman@salesforce.com>
2023-06-23 18:48:58 -0400

    chore: add a few more inside tests

--- packages/run/test/unit/commands/run/inside.unit.test.ts
+++ packages/run/test/unit/commands/run/inside.unit.test.ts
@@ -17,7 +17,7 @@ describe('run:inside', () => {
       })
   })
 
-  describe('errors without command', () => {
+  describe('run:inside errors', () => {
     test
       .stub(Dyno.prototype, 'start', sinon.stub().callsFake(function () {
         return Promise.resolve()
@@ -26,6 +26,25 @@ describe('run:inside', () => {
       .catch(error => {
         expect(error.message).to.contain('Usage: heroku run:inside DYNO COMMAND')
       })
-      .it('throws an error')
+      .it('errors when fewer than two args are passed')
+
+    test
+      .stub(Dyno.prototype, 'start', sinon.stub().callsFake(function () {
+        return Promise.reject(new Error('failed'))
+      }))
+      .command(['run:inside', '--app=heroku-cli-ci-smoke-test-app', 'web.1', 'bash'])
+      .catch(error => expect(error.message).to.equal('failed'))
+      .it('throws a generic error when the request fails and there is no exit code')
+
+    const errorWithCode: any = new Error('failed')
+    errorWithCode.exitCode = '403'
+
+    test
+      .stub(Dyno.prototype, 'start', sinon.stub().callsFake(function () {
+        return Promise.reject(errorWithCode)
+      }))
+      .command(['run:inside', '--app=heroku-cli-ci-smoke-test-app', 'web.1', 'bash'])
+      .catch(error => expect(error.message).to.equal('EEXIT: 403'))
+      .it('throws a stylized error when the request fails and there is an exit code')
   })
 })
