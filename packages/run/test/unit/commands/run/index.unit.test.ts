ef11500b5ee3dceccb26b913ce87e7597adc2505
Katy Bowman <katy.bowman@salesforce.com>
2023-06-26 17:48:52 -0400

    chore: add unit tests for the run package (#2396)
    * chore: add tests and fix linting for colorize.ts
    
    * chore: add tests for rake command
    
    * chore: add structure for logDisplayer tests
    
    * fix logdisplayer import
    
    * chore: lint and add tests for run helpers.ts
    
    * test: run/index
    
    * Add detached test coverage
    
    * test: run:inside
    
    * chore: add a few more inside tests
    
    * fix: windows test fixes
    
    * wip: attempt at dyno test run
    
    * chore: delete new test for dyno, which is very hard to unit test.
    
    * chore: move file location to mach pattern
    
    * chore: remove unneeded dependency
    
    * fix: async tests
    
    * chore: delete stubbed tests
    
    * fix: properly mock api responses
    
    * fix: remove unused nock
    
    ---------
    
    Co-authored-by: RyanDagg <rdagg@salesforce.com>
    Co-authored-by: Zane Whitfield <zwhitfield@salesforce.com>

---
+++ packages/run/test/unit/commands/run/index.unit.test.ts
@@ -0,0 +1,35 @@
+import {expect, test} from '@oclif/test'
+import * as sinon from 'sinon'
+
+import Dyno from '../../../../src/lib/dyno'
+
+describe('run/index', () => {
+  describe('runs correct command', () => {
+    let dynoOpts
+    test
+      .nock('https://api.heroku.com', api => {
+        api.get('/account')
+          .reply(200, {})
+      })
+      .stub(Dyno.prototype, 'start', sinon.stub().callsFake(function () {
+        dynoOpts = this.opts
+        return Promise.resolve()
+      }))
+      .command(['run', 'bash', '--app=heroku-cli-ci-smoke-test-app'])
+      .it('runs bash', () => {
+        expect(dynoOpts.command).to.equal('bash')
+      })
+  })
+
+  describe('errors without command', async () => {
+    test
+      .stub(Dyno.prototype, 'start', sinon.stub().callsFake(function () {
+        return Promise.resolve()
+      }))
+      .command(['run', '--app=heroku-cli-ci-smoke-test-app'])
+      .catch(error => {
+        expect(error.message).to.contain('Usage: heroku run COMMAND')
+      })
+      .it('throws an error')
+  })
+})
