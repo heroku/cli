e928fcadc262573c2f44ea35010611cabd54f99d
Katy Bowman <katy.bowman@salesforce.com>
2023-06-26 17:48:52 -0400

    chore: add unit tests for the run package (#2396)
    * chore: add tests and fix linting for colorize.ts
    
    * chore: add tests for rake command
    
    * chore: add structure for logDisplayer tests
    
    * fix logdisplayer import
    
    * chore: lint and add tests for run helpers.ts
    
    * test: run/index
    
    * Add detached test coverage
    
    * test: run:inside
    
    * chore: add a few more inside tests
    
    * fix: windows test fixes
    
    * wip: attempt at dyno test run
    
    * chore: delete new test for dyno, which is very hard to unit test.
    
    * chore: move file location to mach pattern
    
    * chore: remove unneeded dependency
    
    * fix: async tests
    
    * chore: delete stubbed tests
    
    * fix: properly mock api responses
    
    * fix: remove unused nock
    
    ---------
    
    Co-authored-by: RyanDagg <rdagg@salesforce.com>
    Co-authored-by: Zane Whitfield <zwhitfield@salesforce.com>

--- packages/run/test/unit/commands/rake.unit.test.ts
+++ packages/run/test/unit/commands/rake.unit.test.ts
@@ -15,4 +15,23 @@ describe('rake', () => {
     .it('runs rake', () => {
       expect(dynoOpts.command).to.equal('rake test')
     })
+
+  test
+    .stub(Dyno.prototype, 'start', sinon.stub().callsFake(function () {
+      const err:any = new Error('rake error')
+      err.exitCode = 1
+      throw err
+    }))
+    .command(['rake', '--app=heroku-cli-ci-smoke-test-app', 'test'])
+    .catch(error => expect(error.message).to.equal('rake error'))
+    .it('catches error with an exit code')
+
+  test
+    .stub(Dyno.prototype, 'start', sinon.stub().callsFake(function () {
+      const err = new Error('rake error')
+      throw err
+    }))
+    .command(['rake', '--app=heroku-cli-ci-smoke-test-app', 'test'])
+    .catch(error => expect(error.message).to.equal('rake error'))
+    .it('catches error without an exit code')
 })
