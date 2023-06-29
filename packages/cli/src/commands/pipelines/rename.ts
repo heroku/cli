abc787c17246cf077d7967333c0dc0e7cefd0dda
RyanDagg <ryandagg@users.noreply.github.com>
2023-06-29 13:58:00 -0600

    refactor(pipelines): move to cli package (#2403)
    * chore: move src & tests from local to cli
    
    * chore: finish moving files
    
    * refactor: finish moving/deleting files. Transpile and generate manifests before /cli unit tests to avoid oclif from doing it on every call of test.command.
    
    * fix: version unit test due to file structure change and oclif load order.
    
    * fix: drop node 14 since oclif does not support it and we are using their commands in unit tests now.
    
    * chore: revert unneeded change
    
    * chore: remove script that is no longer needed
    
    * refactor: move files from pipelines to cli
    
    * chore: move dependencies missing from pipelines to cli. delete remaining pipelines files.
    
    * fix: remove pipelines package from cli dependencies
    
    * fix: local:version test approach
    
    * fix: imports
    
    * fix: revert tsconfig change
    
    * fix: update @oclif/test for bug fixes in handling CliUx stdout/err

---
+++ packages/cli/src/commands/pipelines/rename.ts
@@ -0,0 +1,43 @@
+import color from '@heroku-cli/color'
+import {Command} from '@heroku-cli/command'
+import {CliUx} from '@oclif/core'
+
+import {updatePipeline} from '../../lib/pipelines/api'
+import disambiguate from '../../lib/pipelines/disambiguate'
+
+const cli = CliUx.ux
+
+export default class PipelinesRename extends Command {
+  static description = 'rename a pipeline'
+
+  static examples = [
+    '$ heroku pipelines:rename my-pipeline new-pipeline-name',
+  ]
+
+  static args = [
+    {
+      name: 'pipeline',
+      description: 'current name of pipeline',
+      required: true,
+    },
+    {
+      name: 'name',
+      description: 'new name of pipeline',
+      required: true,
+    },
+  ]
+
+  async run() {
+    const {args} = await this.parse(PipelinesRename)
+
+    const pipeline = await disambiguate(this.heroku, args.pipeline)
+
+    cli.action.start(`Renaming ${color.pipeline(pipeline.name!)} pipeline to ${color.pipeline(args.name)}`)
+
+    await updatePipeline(this.heroku, pipeline.id!, {
+      name: args.name,
+    })
+
+    cli.action.stop()
+  }
+}
---
+++ packages/cli/test/unit/commands/pipelines/transfer.unit.test.ts
@@ -0,0 +1,96 @@
+import {expect, test} from '@oclif/test'
+import {CliUx} from '@oclif/core'
+import * as sinon from 'sinon'
+
+const cli = CliUx.ux
+
+describe('pipelines:transfer', function () {
+  const pipeline = {
+    id: 'd214bea8-aaa0-4f98-91ba-e781cb26d50f',
+    name: 'test-pipeline',
+  }
+
+  const team = {
+    id: 'caee7555-c3ba-4959-a393-a0ae93bb958f',
+    name: 'foo-team',
+  }
+
+  const account = {
+    id: 'bcf91aa0-9465-4ad8-a38f-983cd177780f',
+    email: 'user@example.com',
+  }
+
+  const coupling = {
+    app: {
+      id: '579a2433-7afd-421c-8b87-1b89b5cfbf10',
+    },
+  }
+
+  const app = {
+    id: coupling.app.id,
+    name: 'my-app',
+  }
+
+  let update: {isDone(): boolean}
+
+  const addMocks = (testInstance: typeof test) => {
+    return testInstance
+      .nock('https://api.heroku.com', api => {
+        api.get(`/pipelines/${pipeline.id}`).reply(200, pipeline)
+        api.get(`/pipelines/${pipeline.id}/pipeline-couplings`).reply(200, [coupling])
+        api.post('/filters/apps').reply(200, [app])
+      })
+  }
+
+  addMocks(test)
+    .stderr()
+    .nock('https://api.heroku.com', api => {
+      api.get(`/teams/${team.name}`).reply(200, team)
+      update = api.post('/pipeline-transfers', {
+        new_owner: {id: team.id, type: 'team'},
+        pipeline: {id: pipeline.id},
+      }).reply(200, {})
+    })
+    .command(['pipelines:transfer', `--pipeline=${pipeline.id}`, `--confirm=${pipeline.name}`, team.name])
+    .retries(2)
+    .it('transfers to a team', ctx => {
+      expect(ctx.stderr).to.include(`Transferring ${pipeline.name} pipeline to the ${team.name} team... done`)
+      expect(update.isDone()).to.be.true
+    })
+
+  addMocks(test)
+    .stderr()
+    .nock('https://api.heroku.com', api => {
+      api.get(`/users/${account.email}`).reply(200, account)
+      update = api.post('/pipeline-transfers', {
+        new_owner: {id: account.id, type: 'user'},
+        pipeline: {id: pipeline.id},
+      }).reply(200, {})
+    })
+    .command(['pipelines:transfer', `--pipeline=${pipeline.id}`, `--confirm=${pipeline.name}`, account.email])
+    .it('transfers to an account', ctx => {
+      expect(ctx.stderr).to.include(`Transferring ${pipeline.name} pipeline to the ${account.email} account... done`)
+      expect(update.isDone()).to.be.true
+    })
+
+  const promptStub = sinon.stub()
+
+  addMocks(test)
+    .stderr()
+    .do(() => {
+      promptStub.onFirstCall().resolves(pipeline.name)
+    })
+    .nock('https://api.heroku.com', api => {
+      api.get(`/users/${account.email}`).reply(200, account)
+      update = api.post('/pipeline-transfers', {
+        new_owner: {id: account.id, type: 'user'},
+        pipeline: {id: pipeline.id},
+      }).reply(200, {})
+    })
+    .stub(cli, 'prompt', () => promptStub)
+    .command(['pipelines:transfer', `--pipeline=${pipeline.id}`, account.email])
+    .it('does not pass confirm flag', ctx => {
+      expect(ctx.stderr).to.include(`Transferring ${pipeline.name} pipeline to the ${account.email} account... done`)
+      expect(update.isDone()).to.be.true
+    })
+})
