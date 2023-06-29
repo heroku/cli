dff280dd67eb10798505fc7875d1425a9eb943ec
Zane Whitfield <zwhitfield@salesforce.com>
2023-06-29 09:42:20 -0700

    First pass with telemetry collection

--- packages/cli/src/hooks/prerun/analytics.ts
+++ packages/cli/src/hooks/prerun/analytics.ts
@@ -1,8 +1,16 @@
 import {Hook} from '@oclif/core'
 
 import Analytics from '../../analytics'
+import * as telemetry from '../../performance_analytics'
 
-export const analytics: Hook<'prerun'> = async function (options) {
-  const analytics = new Analytics(this.config)
-  await analytics.record(options)
+declare const global: telemetry.TelemetryGlobal
+
+const analytics: Hook<'prerun'> = async function (options) {
+  global.cliTelemetry = telemetry.setupTelemetry(this.config, options)
+  console.log('cliTelemetry: ', global.cliTelemetry)
+
+  // const analytics = new Analytics(this.config)
+  // await analytics.record(options)
 }
+
+export default analytics
