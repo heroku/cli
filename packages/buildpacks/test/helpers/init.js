b4f019f72d4737a620bbee41140132e49165f1e3
Katy Bowman <katy.bowman@salesforce.com>
2023-06-29 15:55:27 -0400

    refactor: remove buildpacks package

--- packages/buildpacks/test/helpers/init.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-process.env.TS_NODE_PROJECT = path.resolve('test/tsconfig.json')
-process.stderr.columns = 80
-
-let nock = require('nock')
-nock.disableNetConnect()
-if (process.env.ENABLE_NET_CONNECT === 'true') {
-  nock.enableNetConnect()
-}
