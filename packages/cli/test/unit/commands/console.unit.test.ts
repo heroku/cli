300b0922f993259fba2628b009e5f604eb63acc0
Katy Bowman <katy.bowman@salesforce.com>
2023-07-05 13:44:40 -0400

    refactor: move run package tests to cli package

--- packages/cli/test/unit/commands/console.unit.test.ts
+++ packages/cli/test/unit/commands/console.unit.test.ts
@@ -1,13 +1,15 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import {expect, test} from '@oclif/test'
 import * as sinon from 'sinon'
 
-import Dyno from '../../../src/lib/dyno'
+import Dyno from '../../../src/lib/run/dyno'
 
 describe('console', () => {
-  let dynoOpts
+  let dynoOpts: { command: any }
 
   test
     .stub(Dyno.prototype, 'start', sinon.stub().callsFake(function () {
+      // @ts-ignore
       dynoOpts = this.opts
       return Promise.resolve()
     }))
