03dd75015450017a5f75219309a930b2a3872232
RyanDagg <ryandagg@users.noreply.github.com>
2023-07-05 10:43:52 -0600

    refactor(webhooks): move to cli (#2413)
    * refactor: move webhooks into cli
    
    * chore: update dependencies. Delete /webhooks dir
    
    * fix: import errors
    
    * fix: use date-fns per v2 instead of v1
    
    * fix: unit tests

---
+++ packages/cli/src/commands/webhooks/info.ts
@@ -0,0 +1,35 @@
+import {flags} from '@heroku-cli/command'
+import {CliUx} from '@oclif/core'
+
+import BaseCommand from '../../lib/webhooks/base'
+
+export default class WebhooksInfo extends BaseCommand {
+  static description = 'info for a webhook on an app'
+
+  static example = ['$ heroku webhooks:info 99999999-9999-9999-9999-999999999999']
+
+  static flags = {
+    app: flags.app(),
+    remote: flags.remote(),
+    pipeline: flags.pipeline({char: 'p', description: 'pipeline on which to list', hidden: true}),
+  }
+
+  static args = [{name: 'id', required: true}]
+
+  async run() {
+    const {flags, args} = await this.parse(WebhooksInfo)
+    const {path} = this.webhookType(flags)
+
+    const {body: webhook}: {body: any} = await this.webhooksClient.get(`${path}/webhooks/${args.id}`)
+
+    const obj = {
+      'Webhook ID': webhook.id,
+      URL: webhook.url,
+      Include: webhook.include.join(','),
+      Level: webhook.level,
+    }
+
+    CliUx.ux.styledHeader(webhook.id)
+    CliUx.ux.styledObject(obj)
+  }
+}
