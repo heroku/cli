a54196499bf2fa895d7932e2b6625a34d0f33bc1
Zane Whitfield <zwhitfield@salesforce.com>
2023-07-06 14:59:08 -0700

    Setup cli data collection for telemetry

--- packages/cli/src/hooks/postrun/performance_analytics.ts
+++ packages/cli/src/hooks/postrun/performance_analytics.ts
@@ -5,14 +5,11 @@ import * as telemetry from '../../performance_analytics'
 declare const global: telemetry.TelemetryGlobal
 
 const performance_analytics: Hook<'postrun'> = async function (options) {
-    if(global.cliTelemetry) {
-        const cmdStartTime = global.cliTelemetry.commandRunDuration
-        global.cliTelemetry.commandRunDuration = telemetry.computeDuration(cmdStartTime)
-    }
-    console.log('cliTelemetry in postrun: ', global.cliTelemetry)
-
-  // const analytics = new Analytics(this.config)
-  // await analytics.record(options)
+  if (global.cliTelemetry) {
+    const cmdStartTime = global.cliTelemetry.commandRunDuration
+    global.cliTelemetry.commandRunDuration = telemetry.computeDuration(cmdStartTime)
+    global.cliTelemetry.lifecycleHookCompletion.postrun = true
+  }
 }
 
 export default performance_analytics
