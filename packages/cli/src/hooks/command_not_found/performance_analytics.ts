c961807255c81f458502ebb706212f22b77a4732
Zane Whitfield <zwhitfield@salesforce.com>
2023-07-06 14:59:08 -0700

    Setup cli data collection for telemetry

--- packages/cli/src/hooks/command_not_found/performance_analytics.ts
+++ packages/cli/src/hooks/command_not_found/performance_analytics.ts
@@ -5,17 +5,7 @@ import * as telemetry from '../../performance_analytics'
 declare const global: telemetry.TelemetryGlobal
 
 const performance_analytics: Hook<'command_not_found'> = async function (options) {
-    // console.log('options: ', options)
-    // console.log('this.config: ', this.config)
     global.cliTelemetry = telemetry.reportCmdNotFound(this.config)
-    // if(global.cliTelemetry) {
-    //     const cmdStartTime = global.cliTelemetry.commandRunDuration
-    //     global.cliTelemetry.commandRunDuration = telemetry.computeDuration(cmdStartTime)
-    // }
-    console.log('cliTelemetry in command_not_found: ', global.cliTelemetry)
-
-  // const analytics = new Analytics(this.config)
-  // await analytics.record(options)
 }
 
 export default performance_analytics
