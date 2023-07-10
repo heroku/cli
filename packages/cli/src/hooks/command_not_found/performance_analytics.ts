a89b63efa03b7031b35add4a36038c08ff547fbd
Zane Whitfield <zwhitfield@salesforce.com>
2023-07-10 16:28:25 -0700

    WIP rollbar not logging with basic catch

--- packages/cli/src/hooks/command_not_found/performance_analytics.ts
+++ packages/cli/src/hooks/command_not_found/performance_analytics.ts
@@ -5,7 +5,7 @@ import * as telemetry from '../../performance_analytics'
 declare const global: telemetry.TelemetryGlobal
 
 const performance_analytics: Hook<'command_not_found'> = async function (options) {
-    global.cliTelemetry = telemetry.reportCmdNotFound(this.config)
+  global.cliTelemetry = telemetry.reportCmdNotFound(this.config)
 }
 
 export default performance_analytics
