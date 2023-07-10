a3e9340ec5ea4645385b31e1420f7dfc88bf9369
Katy Bowman <katy.bowman@salesforce.com>
2023-07-10 14:09:25 -0400

    chore: add comment on piplines:open test

--- packages/cli/test/unit/commands/pipelines/open.unit.test.ts
+++ packages/cli/test/unit/commands/pipelines/open.unit.test.ts
@@ -18,6 +18,8 @@ describe('pipelines:open', () => {
     .command(['pipelines:open', pipeline.name])
     .it('opens the url', () => {
       const urlArgArray = spawnStub.getCall(0).args[1]
+      // For darwin-based platforms this arg is an array that contains the site url.
+      // For windows-based platforms this arg is an array that contains an encoded command that includes the url
       const hasCorrectUrl = urlArgArray.includes('https://dashboard.heroku.com/pipelines/0123') || urlArgArray.includes('UwB0AGEAcgB0ACAAIgBoAHQAdABwAHMAOgAvAC8AZABhAHMAaABiAG8AYQByAGQALgBoAGUAcgBvAGsAdQAuAGMAbwBtAC8AcABpAHAAZQBsAGkAbgBlAHMALwAwADEAMgAzACIA')
       expect(hasCorrectUrl).to.be.true
     })
