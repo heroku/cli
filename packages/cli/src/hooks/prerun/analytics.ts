19d131705d2d6fbe3c605998060a50697517f34b
Zane Whitfield <zwhitfield@salesforce.com>
2023-07-13 16:41:15 -0700

    Code clean up

--- packages/cli/src/hooks/prerun/analytics.ts
+++ packages/cli/src/hooks/prerun/analytics.ts
@@ -1,15 +1,14 @@
 import {Hook} from '@oclif/core'
 
 import Analytics from '../../analytics'
-import * as telemetry from '../../performance_analytics'
+import * as telemetry from '../../global_telemetry'
 
 declare const global: telemetry.TelemetryGlobal
 
 const analytics: Hook<'prerun'> = async function (options) {
   global.cliTelemetry = telemetry.setupTelemetry(this.config, options)
-
-  // const analytics = new Analytics(this.config)
-  // await analytics.record(options)
+  const analytics = new Analytics(this.config)
+  await analytics.record(options)
 }
 
 export default analytics
