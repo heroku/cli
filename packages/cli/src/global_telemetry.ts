2205f3d6eef9acd61f2863ed549c69c3763197de
Zane Whitfield <zwhitfield@salesforce.com>
2023-07-25 13:35:36 -0700

    WIP trace and span setup

--- packages/cli/src/global_telemetry.ts
+++ packages/cli/src/global_telemetry.ts
@@ -3,6 +3,7 @@ import {HoneycombSDK} from '@honeycombio/opentelemetry-node'
 import {getNodeAutoInstrumentations} from '@opentelemetry/auto-instrumentations-node'
 import 'dotenv/config'
 const isDev = process.env.IS_DEV_ENVIRONMENT === 'true'
+import opentelemetry from '@opentelemetry/api'
 
 const debug = require('debug')('global_telemetry')
 const rollbar = new Rollbar({
@@ -46,6 +47,7 @@ export interface TelemetryGlobal extends NodeJS.Global {
 
 export function honeycombStart() {
   honeycomb.start()
+  // console.log('honeycomb object:', honeycomb)
 }
 
 export function setupTelemetry(config: any, opts: any) {
@@ -109,7 +111,17 @@ export async function sendTelemetry(currentTelemetry: any) {
 
 export async function sendToHoneycomb(data: any) {
   try {
+    // const trace = opentelemetry.trace.getTracerProvider()
+    // console.log('tracerProver:', trace)
     // send telemetry to backboard
+    const tracer = opentelemetry.trace.getTracer('heroku-cli-tracer')
+    tracer.startActiveSpan('command.name.test', span => {
+      span.setAttribute('command', data.command)
+      // console.log('span isRecording:', span.isRecording())
+      // console.log('spanContext:', span.spanContext())
+      // console.log('activeSpan:', span)
+      span.end()
+    })
   } catch {
     debug('could not send telemetry')
   }
