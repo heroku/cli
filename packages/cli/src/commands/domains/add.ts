95ae2aeacc427bac70ea98ae906515d576cf8f68
RyanDagg <rdagg@salesforce.com>
2023-08-22 17:34:03 -0600

    whitespace for version

--- packages/cli/src/commands/domains/add.ts
+++ packages/cli/src/commands/domains/add.ts
@@ -16,6 +16,7 @@ export default class DomainsAdd extends Command {
   static description = 'add a domain to an app'
 
   static examples = ['heroku domains:add www.example.com']
+
   static flags = {
     help: flags.help({char: 'h'}),
     app: flags.app({required: true}),
