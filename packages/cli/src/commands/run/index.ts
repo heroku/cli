849413c267f27eedd2e82c23343ea84e9324e6cf
RyanDagg <rdagg@salesforce.com>
2023-08-30 13:04:25 -0600

    Revert "fix(run): downgrade @heroku-cli/plugins-run to @oclif/core v1 (#2460)"
    This reverts commit 76664592e108babd934e1d15c9481009f4fb422f.

---
+++ packages/cli/src/commands/run/index.ts
@@ -0,0 +1,70 @@
+import {Command, flags} from '@heroku-cli/command'
+import {DynoSizeCompletion, ProcessTypeCompletion} from '@heroku-cli/command/lib/completions'
+import {ux} from '@oclif/core'
+import debugFactory from 'debug'
+import * as Heroku from '@heroku-cli/schema'
+
+import Dyno from '../../lib/run/dyno'
+import {buildCommand} from '../../lib/run/helpers'
+
+const debug = debugFactory('heroku:run')
+
+export default class Run extends Command {
+  static description = 'run a one-off process inside a heroku dyno\nShows a notification if the dyno takes more than 20 seconds to start.'
+
+  static examples = [
+    '$ heroku run bash',
+    '$ heroku run -s standard-2x -- myscript.sh -a arg1 -s arg2',
+  ]
+
+  // This is to allow for variable length arguments
+  static strict = false
+
+  static flags = {
+    app: flags.app({description: 'parent app used by review apps', required: true}),
+    remote: flags.remote(),
+    size: flags.string({char: 's', description: 'dyno size', completion: DynoSizeCompletion}),
+    type: flags.string({description: 'process type', completion: ProcessTypeCompletion}),
+    'exit-code': flags.boolean({char: 'x', description: 'passthrough the exit code of the remote command'}),
+    env: flags.string({char: 'e', description: "environment variables to set (use ';' to split multiple vars)"}),
+    'no-tty': flags.boolean({description: 'force the command to not run in a tty'}),
+    listen: flags.boolean({description: 'listen on a local port', hidden: true}),
+    'no-notify': flags.boolean({description: 'disables notification when dyno is up (alternatively use HEROKU_NOTIFICATIONS=0)'}),
+  }
+
+  async run() {
+    const {argv, flags} = await this.parse(Run)
+
+    const opts = {
+      'exit-code': flags['exit-code'],
+      'no-tty': flags['no-tty'],
+      app: flags.app,
+      attach: true,
+      command: buildCommand(argv as string[]),
+      env: flags.env,
+      heroku: this.heroku,
+      listen: flags.listen,
+      notify: !flags['no-notify'],
+      size: flags.size,
+      type: flags.type,
+    }
+
+    if (!opts.command) {
+      throw new Error('Usage: heroku run COMMAND\n\nExample: heroku run bash')
+    }
+
+    await this.heroku.get<Heroku.Account>('/account')
+    const dyno = new Dyno(opts)
+    try {
+      await dyno.start()
+      debug('done running')
+    } catch (error: any) {
+      debug(error)
+      if (error.exitCode) {
+        ux.error(error.message, {code: error.exitCode, exit: error.exitCode})
+      } else {
+        throw error
+      }
+    }
+  }
+}
---
+++ packages/cli/test/unit/commands/run/inside.unit.test.ts
@@ -0,0 +1,52 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import {expect, test} from '@oclif/test'
+import * as sinon from 'sinon'
+
+import Dyno from '../../../../src/lib/run/dyno'
+
+describe('run:inside', () => {
+  describe('runs correct command', () => {
+    let dynoOpts: { command: any }
+    test
+      .stub(Dyno.prototype, 'start', sinon.stub().callsFake(function () {
+        // @ts-ignore
+        dynoOpts = this.opts
+        return Promise.resolve()
+      }))
+      .command(['run:inside', 'DYNO_NAME', 'bash', '--app=heroku-cli-ci-smoke-test-app'])
+      .it('runs bash', () => {
+        expect(dynoOpts.command).to.equal('bash')
+      })
+  })
+
+  describe('run:inside errors', () => {
+    test
+      .stub(Dyno.prototype, 'start', sinon.stub().callsFake(function () {
+        return Promise.resolve()
+      }))
+      .command(['run:inside', '--app=heroku-cli-ci-smoke-test-app'])
+      .catch(error => {
+        expect(error.message).to.contain('Usage: heroku run:inside DYNO COMMAND')
+      })
+      .it('errors when fewer than two args are passed')
+
+    test
+      .stub(Dyno.prototype, 'start', sinon.stub().callsFake(function () {
+        return Promise.reject(new Error('failed'))
+      }))
+      .command(['run:inside', '--app=heroku-cli-ci-smoke-test-app', 'web.1', 'bash'])
+      .catch(error => expect(error.message).to.equal('failed'))
+      .it('throws a generic error when the request fails and there is no exit code')
+
+    const errorWithCode: any = new Error('failed')
+    errorWithCode.exitCode = '403'
+
+    test
+      .stub(Dyno.prototype, 'start', sinon.stub().callsFake(function () {
+        return Promise.reject(errorWithCode)
+      }))
+      .command(['run:inside', '--app=heroku-cli-ci-smoke-test-app', 'web.1', 'bash'])
+      .catch(error => expect(error.message).to.equal('EEXIT: 403'))
+      .it('throws a stylized error when the request fails and there is an exit code')
+  })
+})
