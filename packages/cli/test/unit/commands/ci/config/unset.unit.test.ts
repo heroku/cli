d8eb1e996692bccc7e106feddb7f2706b230db75
Zane Whitfield <zwhitfield@salesforce.com>
2023-09-29 16:31:55 -0700

    WIP migrate test

---
+++ packages/cli/test/unit/commands/ci/config/unset.unit.test.ts
@@ -0,0 +1,22 @@
+import {expect, test} from '@oclif/test'
+
+const key = 'FOO'
+const pipeline =  {
+  id: '123e4567-e89b-12d3-a456-426655440000',
+  name: 'test-pipeline',
+}
+describe('heroku ci:config:unset', function () {
+  test
+    .stdout()
+    .stderr()
+    .nock('https://api.heroku.com', api => {
+      api.get(`/pipelines/${pipeline.id}`)
+        .reply(200, pipeline)
+        .get(`/pipelines/${pipeline.id}/stage/test/config-vars`)
+        .reply(200, {[key]: null})
+    })
+    .command(['ci:config:unset', `--pipeline=${pipeline.id}`, key])
+    .it('displays the config value', ({stderr}) => {
+      expect(stderr).to.contain('Unsetting FOO... done')
+    })
+})
