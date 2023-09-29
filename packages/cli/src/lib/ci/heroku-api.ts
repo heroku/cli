20622a73d377c650fbc626714634af8a0c05e8b3
Zane Whitfield <zwhitfield@salesforce.com>
2023-09-29 10:44:31 -0700

    Migrate setConfigVars

--- packages/cli/src/lib/ci/heroku-api.ts
+++ packages/cli/src/lib/ci/heroku-api.ts
@@ -1,5 +1,14 @@
 import {APIClient} from '@heroku-cli/command'
+const V3_HEADER = 'application/vnd.heroku+json; version=3'
+const PIPELINE_HEADER = `${V3_HEADER}.pipelines`
 
 export async function pipelineCoupling(client: APIClient, app: string) {
   return client.get(`/apps/${app}/pipeline-couplings`)
+}
+
+function setConfigVars(client: APIClient, pipelineID: string, body: any) {
+  return client.patch(`/pipelines/${pipelineID}/stage/test/config-vars`, {
+    headers: {Accept: PIPELINE_HEADER},
+    body,
+  })
 }
