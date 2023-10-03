5623c1270d42c96660daf149791c2b1e1740dad2
Zane Whitfield <zwhitfield@salesforce.com>
2023-10-03 15:59:34 -0700

    refactor(ci): move ci:open to CLI & upgrade to oclif (#2499)
    * Migrate ci:open command
    
    * Delete ci-v5 ci:open command
    
    * Fix type issue by passing heroku client

---
+++ packages/cli/src/commands/ci/open.ts
@@ -0,0 +1,23 @@
+import {Command, flags} from '@heroku-cli/command'
+import * as open from 'open'
+import {getPipeline} from '../../lib/ci/pipelines'
+
+export default class CiOpen extends Command {
+  static description = 'open the Dashboard version of Heroku CI'
+  static topic = 'ci'
+  static examples = [
+    '$ heroku ci:open --app murmuring-headland-14719',
+  ]
+
+  static flags = {
+    help: flags.help({char: 'h'}),
+    app: flags.app({required: true}),
+    pipeline: flags.pipeline({required: false}),
+  }
+
+  async run() {
+    const {flags} = await this.parse(CiOpen)
+    const pipeline = await getPipeline(flags, this.heroku)
+    await open(`https://dashboard.heroku.com/pipelines/${pipeline.id}/tests`)
+  }
+}
