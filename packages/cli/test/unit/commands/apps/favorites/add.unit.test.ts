156ce2a92965f9d5c8de89ce3fab02730ff2710a
RyanDagg <rdagg@salesforce.com>
2023-10-09 10:37:53 -0600

    chore: add test to cover "app not found" error

--- packages/cli/test/unit/commands/apps/favorites/add.unit.test.ts
+++ packages/cli/test/unit/commands/apps/favorites/add.unit.test.ts
@@ -30,4 +30,17 @@ describe('apps:favorites:add', () => {
       expect(error.message).to.contain('is already a favorite app.')
     })
     .it('errors if app is already favorited')
+
+  test
+    .stderr()
+    .nock('https://particleboard.heroku.com', api => {
+      api.get('/favorites?type=app')
+        .reply(200, [{resource_name: MY_APP}])
+    })
+    .command(['apps:favorites:add', '--app=NOT_AN_APP'])
+    .catch((error: any) => {
+      expect(error).to.be.an.instanceof(Error)
+      expect(error.message).to.contain('App not found')
+    })
+    .it('errors if app is already favorited')
 })
