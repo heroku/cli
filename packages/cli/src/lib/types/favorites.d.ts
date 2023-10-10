7510fa68c5c867296467ac4d201ba49d29e53ee2
Zane Whitfield <zwhitfield@salesforce.com>
2023-10-10 14:43:25 -0700

    refactor(app): move apps:favorites:index to CLI & upgrade to oclif (#2509)
    * Migrate command
    
    * Migrate tests
    
    * Remove v5 index file & test

---
+++ packages/cli/src/lib/types/favorites.d.ts
@@ -0,0 +1,8 @@
+export type Favorite = {
+  id: string;
+  resource_id: string;
+  resource_name: string;
+  type: string;
+}
+
+export type Favorites = Favorite[]
---
+++ packages/cli/test/unit/commands/apps/favorites/index.unit.test.ts
@@ -0,0 +1,31 @@
+import {expect, test} from '@oclif/test'
+
+const MY_APP = 'myapp'
+const MY_APP2 = 'myotherapp'
+describe('apps:favorites', () => {
+  test
+    .stdout()
+    .stderr()
+    .nock('https://particleboard.heroku.com', api => {
+      api.get('/favorites?type=app')
+        .reply(200, [{resource_name: MY_APP}, {resource_name: MY_APP2}])
+    })
+    .command(['apps:favorites'])
+    .it('shows all favorite apps', ({stdout, stderr}) => {
+      expect(stdout).to.contain('=== Favorited Apps\n\nmyapp\nmyotherapp\n')
+      expect(stderr).to.equal('')
+    })
+
+  test
+    .stdout()
+    .stderr()
+    .nock('https://particleboard.heroku.com', api => {
+      api.get('/favorites?type=app')
+        .reply(200, [{resource_name: MY_APP}, {resource_name: MY_APP2}])
+    })
+    .command(['apps:favorites', '--json'])
+    .it('shows all favorite apps as json', ({stdout, stderr}) => {
+      expect(stdout).to.contain('[\n  {\n    "resource_name": "myapp"\n  },\n  {\n    "resource_name": "myotherapp"\n  }\n]\n')
+      expect(stderr).to.equal('')
+    })
+})
