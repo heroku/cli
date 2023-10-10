6b3498723f9be12cf68733471e045a0cc967b145
Zane Whitfield <zwhitfield@salesforce.com>
2023-10-10 11:21:14 -0700

    WIP migrate command & test

---
+++ packages/cli/src/lib/types/stacks.d.ts
@@ -0,0 +1,10 @@
+export type Stack = {
+  default: boolean
+  created_at: string
+  id: string,
+  name: 'heroku-18',
+  state: 'public',
+  updated_at: '2012-01-01T12:00:00Z'
+}
+
+export type Stacks = Stack[]
