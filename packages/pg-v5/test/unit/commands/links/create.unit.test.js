48b4abc61d86c90b1a1fde6d14076ee8b55b76f6
Jon Daniel <jon.daniel@salesforce.com>
2023-10-10 10:51:00 -0400

    Adding tests for essential-tier addons with pg:links

--- packages/pg-v5/test/unit/commands/links/create.unit.test.js
+++ packages/pg-v5/test/unit/commands/links/create.unit.test.js
@@ -6,21 +6,18 @@ const {expect} = require('chai')
 const nock = require('nock')
 const proxyquire = require('proxyquire')
 
-const addon = {
-  id: 1,
-  name: 'postgres-1',
-  plan: {name: 'heroku-postgresql:standard-0'},
-}
 
-const cmd = proxyquire('../../../../commands/links/create', {
-  '@heroku-cli/plugin-addons': {
-    resolve: {addon: () => addon},
-  },
-  '../../lib/fetcher': () => ({
-    all: () => [addon],
-    addon: () => addon,
-  }),
-})
+const buildCmd = (addon) => {
+  return proxyquire('../../../../commands/links/create', {
+    '@heroku-cli/plugin-addons': {
+      resolve: {addon: () => addon},
+    },
+    '../../lib/fetcher': () => ({
+      all: () => [addon],
+      addon: () => addon,
+    }),
+  })
+}
 
 describe('pg:links:create', () => {
   let api
@@ -38,10 +35,35 @@ describe('pg:links:create', () => {
     pg.done()
   })
 
-  it('creates a link', () => {
-    pg.post('/client/v11/databases/1/links', {target: 'postgres-1', as: 'foobar'}).reply(200, {name: 'foobar'})
-    return cmd.run({app: 'myapp', args: {}, flags: {confirm: 'myapp', as: 'foobar'}})
-      .then(() => expect(cli.stdout).to.equal(''))
-      .then(() => expect(cli.stderr).to.equal('Adding link from postgres-1 to postgres-1... done, foobar\n'))
+  context("on an essential database", () => {
+    let addon = {
+      id: 2,
+      name: 'postgres-1',
+      plan: {name: 'heroku-postgresql:basic'},
+    }
+    let cmd = buildCmd(addon)
+
+    it('errors when attempting to create a link', () => {
+      return cmd.run({app: 'myapp', args: {}, flags: {confirm: 'myapp', as: 'foobar'}})
+        .catch(error => {
+          expect(error.message).to.equal('pg:links isn’t available for Essential-tier databases.')
+        })
+    })
+  })
+
+  context("on a production database", () => {
+    let addon = {
+      id: 1,
+      name: 'postgres-1',
+      plan: {name: 'heroku-postgresql:standard-0'},
+    }
+    let cmd = buildCmd(addon)
+
+    it('creates a link', () => {
+      pg.post('/client/v11/databases/1/links', {target: 'postgres-1', as: 'foobar'}).reply(200, {name: 'foobar'})
+      return cmd.run({app: 'myapp', args: {}, flags: {confirm: 'myapp', as: 'foobar'}})
+        .then(() => expect(cli.stdout).to.equal(''))
+        .then(() => expect(cli.stderr).to.equal('Adding link from postgres-1 to postgres-1... done, foobar\n'))
+    })
   })
 })
