3c695fc560c9e6c0284211561cfe5827cb55a358
Zane Whitfield <zwhitfield@salesforce.com>
2023-10-12 11:13:36 -0700

    refactor(app): move apps:favorites:remove to CLI & upgrade to oclif (#2508)
    * Migrate remove command
    
    * Migrate tests
    
    * Remove v5 remove file & test
    
    * Update test
    
    * Trigger github workflow
    
    * Update test

---
+++ packages/cli/src/commands/apps/favorites/remove.ts
@@ -0,0 +1,36 @@
+import color from '@heroku-cli/color'
+import {ux} from '@oclif/core'
+import {Command, flags} from '@heroku-cli/command'
+import {Favorites} from '../../../lib/types/favorites'
+
+export default class Remove extends Command {
+  static description = 'unfavorites an app'
+  static topic = 'apps'
+  static flags  = {
+    app: flags.app({required: true}),
+  }
+
+  async run() {
+    const {flags} = await this.parse(Remove)
+    const {app} = flags
+
+    ux.action.start(`Removing ${color.app(app)} from favorites`)
+
+    const {body: favorites} = await this.heroku.get<Favorites>(
+      '/favorites?type=app',
+      {hostname: 'particleboard.heroku.com'},
+    )
+
+    const favorite = favorites.find(f => f.resource_name === app)
+
+    if (!favorite) {
+      throw new Error(`${color.app(app)} is not already a favorite app.`)
+    }
+
+    await this.heroku.delete(`/favorites/${favorite.id}`, {
+      hostname: 'particleboard.heroku.com',
+    })
+
+    ux.action.stop()
+  }
+}
