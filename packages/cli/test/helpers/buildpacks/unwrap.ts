9f7ae1c1364926a28522d7df8555d5b7f6346d86
Zane Whitfield <zwhitfield@salesforce.com>
2023-10-30 13:03:07 -0700

    Consolidate unwrap (#2523)

--- packages/cli/test/helpers/buildpacks/unwrap.ts
+++
@@ -1,8 +0,0 @@
-const unwrap = function (str: string): string {
-  let sanitize = str.replace(/\n ([›»]) {3}/g, '')
-  sanitize = sanitize.replace(/ ([›»]) {3}/g, '')
-
-  return sanitize
-}
-
-export {unwrap}
