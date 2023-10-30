a61d59e42be6715044f7fd31d9ffd6ca9aca847c
RyanDagg <rdagg@salesforce.com>
2023-10-30 16:20:42 -0600

    refactor: migrate command to core & TS reimplement heroku-cli-util’s confirmApp in our lib

--- packages/cli/src/commands/apps/destroy.ts
+++ packages/cli/src/commands/apps/destroy.ts
@@ -1,43 +1,54 @@
-'use strict'
-
+import color from '@heroku-cli/color'
 import {Command, flags} from '@heroku-cli/command'
-import Git from '../../lib/git/git'
-
-const git = new Git()
+import {Args, ux} from '@oclif/core'
+import {uniq} from 'lodash'
+import confirmApp from '../../lib/apps/confirm-app'
+import * as git from '../../lib/ci/git'
 
 export default class Destroy extends Command {
   static description = 'permanently destroy an app'
   static help = 'This will also destroy all add-ons on the app.'
   static aliases = ['destroy', 'apps:delete']
   static flags = {
-    app: flags.app({hidden: true}),
+    app: flags.app(),
     confirm: flags.string({char: 'c'}),
   }
 
+  static args = {
+    app: Args.string({hidden: true}),
+  }
+
   async run() {
-    const app = context.args.app || context.app
+    const {flags, args} = await this.parse(Destroy)
+
+    const app = args.app || flags.app
     if (!app) throw new Error('No app specified.\nUSAGE: heroku apps:destroy APPNAME')
 
-    context.app = app // make sure context.app is always set for herkou-cli-util
+    // context.app = app // make sure context.app is always set for herkou-cli-util
 
-    await heroku.get(`/apps/${app}`)
-    await cli.confirmApp(app, context.flags.confirm, `WARNING: This will delete ${cli.color.app(app)} including all add-ons.`)
-    const request = heroku.request({
-      method: 'DELETE',
-      path: `/apps/${app}`,
-    })
-    await cli.action(`Destroying ${cli.color.app(app)} (including all add-ons)`, request)
+    // this appears to report errors if app not found
+    await this.heroku.get(`/apps/${app}`)
+    await confirmApp(app, flags.confirm, `WARNING: This will delete ${color.app(app)} including all add-ons.`)
+    ux.action.start(`Destroying ${color.app(app)} (including all add-ons)`)
+    await this.heroku.delete(`/apps/${app}`)
 
     if (git.inGitRepo()) {
       // delete git remotes pointing to this app
       await git.listRemotes()
-        .filter(r => git.gitUrl(app) === r[1] || git.sshGitUrl(app) === r[1])
-        .map(r => r[0])
-        .uniq()
-        .forEach(element => {
-          git.rmRemote(element)
+        .then(remotes => {
+          const transformed = remotes
+            .filter(r => git.gitUrl(app) === r[1] || git.sshGitUrl(app) === r[1])
+            .map(r => r[0])
+
+          const uniqueRemotes = uniq(transformed)
+
+          uniqueRemotes.forEach(element => {
+            git.rmRemote(element)
+          })
         })
     }
+
+    ux.action.stop()
   }
 }
 
