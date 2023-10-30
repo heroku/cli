21e8e65bbfe4b7074c781c2579ea92025529b45d
RyanDagg <rdagg@salesforce.com>
2023-10-30 15:40:03 -0600

    wip

--- packages/cli/src/commands/apps/destroy.ts
+++ packages/cli/src/commands/apps/destroy.ts
@@ -1,50 +1,43 @@
 'use strict'
 
-const cli = require('heroku-cli-util')
+import {Command, flags} from '@heroku-cli/command'
+import Git from '../../lib/git/git'
 
-async function run(context, heroku) {
-  const _ = require('lodash')
-  const git = require('../../git')(context)
+const git = new Git()
 
-  let app = context.args.app || context.app
-  if (!app) throw new Error('No app specified.\nUSAGE: heroku apps:destroy APPNAME')
+export default class Destroy extends Command {
+  static description = 'permanently destroy an app'
+  static help = 'This will also destroy all add-ons on the app.'
+  static aliases = ['destroy', 'apps:delete']
+  static flags = {
+    app: flags.app({hidden: true}),
+    confirm: flags.string({char: 'c'}),
+  }
 
-  context.app = app // make sure context.app is always set for herkou-cli-util
+  async run() {
+    const app = context.args.app || context.app
+    if (!app) throw new Error('No app specified.\nUSAGE: heroku apps:destroy APPNAME')
 
-  await heroku.get(`/apps/${app}`)
-  await cli.confirmApp(app, context.flags.confirm, `WARNING: This will delete ${cli.color.app(app)} including all add-ons.`)
-  let request = heroku.request({
-    method: 'DELETE',
-    path: `/apps/${app}`,
-  })
-  await cli.action(`Destroying ${cli.color.app(app)} (including all add-ons)`, request)
+    context.app = app // make sure context.app is always set for herkou-cli-util
 
-  if (git.inGitRepo()) {
-    // delete git remotes pointing to this app
-    _(await git.listRemotes())
-      .filter(r => git.gitUrl(app) === r[1] || git.sshGitUrl(app) === r[1])
-      .map(r => r[0])
-      .uniq()
-      .forEach(element => {
-        git.rmRemote(element)
-      })
+    await heroku.get(`/apps/${app}`)
+    await cli.confirmApp(app, context.flags.confirm, `WARNING: This will delete ${cli.color.app(app)} including all add-ons.`)
+    const request = heroku.request({
+      method: 'DELETE',
+      path: `/apps/${app}`,
+    })
+    await cli.action(`Destroying ${cli.color.app(app)} (including all add-ons)`, request)
+
+    if (git.inGitRepo()) {
+      // delete git remotes pointing to this app
+      await git.listRemotes()
+        .filter(r => git.gitUrl(app) === r[1] || git.sshGitUrl(app) === r[1])
+        .map(r => r[0])
+        .uniq()
+        .forEach(element => {
+          git.rmRemote(element)
+        })
+    }
   }
 }
 
-let cmd = {
-  description: 'permanently destroy an app',
-  help: 'This will also destroy all add-ons on the app.',
-  needsAuth: true,
-  wantsApp: true,
-  args: [{name: 'app', hidden: true, optional: true}],
-  flags: [
-    {name: 'confirm', char: 'c', hasValue: true},
-  ],
-  run: cli.command(run),
-}
-
-module.exports = [
-  Object.assign({topic: 'apps', command: 'destroy'}, cmd),
-  Object.assign({hidden: true, topic: 'destroy'}, cmd),
-  Object.assign({hidden: true, topic: 'apps', command: 'delete'}, cmd),
-]
