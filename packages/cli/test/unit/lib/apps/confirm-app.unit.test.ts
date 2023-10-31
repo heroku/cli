25c5e6e3a03162daa754c7947f92e119529a3f12
RyanDagg <rdagg@salesforce.com>
2023-10-31 11:27:10 -0600

    fix: test name

--- packages/cli/test/unit/lib/apps/confirm-app.unit.test.ts
+++ packages/cli/test/unit/lib/apps/confirm-app.unit.test.ts
@@ -4,11 +4,11 @@ import confirmApp from '../../../../src/lib/apps/confirm-app'
 
 const stripAnsi = require('strip-ansi')
 
-describe('display', () => {
+describe('confirmApp', () => {
   test
     .stdout()
     .stderr()
-    .do(async () => confirmApp('app', 'app'))
+    .do(() => confirmApp('app', 'app'))
     .it('should not error or prompt with confirm flag match', ({stderr, stdout}) => {
       expect(stderr).to.equal('')
       expect(stdout).to.equal('')
@@ -17,7 +17,7 @@ describe('display', () => {
   test
     .stdout()
     .stderr()
-    .do(async () => confirmApp('app', 'nope'))
+    .do(() => confirmApp('app', 'nope'))
     .catch((error: Error) => {
       expect(stripAnsi(error.message)).to.equal('Confirmation nope did not match app. Aborted.')
     })
@@ -47,7 +47,7 @@ describe('display', () => {
 
   test
     .stub(ux, 'prompt', () => Promise.resolve('nope'))
-    .do(async () => confirmApp('app'))
+    .do(() => confirmApp('app'))
     .catch((error: Error) => {
       expect(stripAnsi(error.message)).to.equal('Confirmation did not match app. Aborted.')
     })
