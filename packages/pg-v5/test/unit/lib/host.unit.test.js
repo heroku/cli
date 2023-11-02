b83b162f2eb0d30f8efcb943680c9e2a2abd215d
Jon Daniel <jon.daniel@salesforce.com>
2023-11-02 15:55:51 -0400

    More test fixes

--- packages/pg-v5/test/unit/lib/host.unit.test.js
+++ packages/pg-v5/test/unit/lib/host.unit.test.js
@@ -14,23 +14,14 @@ describe('host', () => {
   context('with HEROKU_DATA_HOST set', () => {
     beforeEach(() => {
       process.env.HEROKU_DATA_HOST = 'data-host.herokuapp.com'
-      process.env.HEROKU_POSTGRESQL_HOST = 'postgresql-host.herokuapp.com'
     })
     afterEach(() => delete process.env.HEROKU_DATA_HOST)
     afterEach(() => delete process.env.HEROKU_POSTGRESQL_HOST)
 
-    it('shows essential host', () => {
-      expect(host({plan: {name: 'heroku-postgresql:mini'}})).to.equal('https://postgres-starter-api.heroku.com')
-    })
-
-    it('shows data host', () => {
+    it('shows data-host.herokuapp.com host', () => {
+      expect(host({plan: {name: 'heroku-postgresql:mini'}})).to.equal('https://data-host.herokuapp.com')
       expect(host({plan: {name: 'heroku-postgresql:premium-0'}})).to.equal('https://data-host.herokuapp.com')
-    })
-
-    context('for numbered essential plans', () => {
-      it('shows data host', () => {
-        expect(host({plan: {name: 'heroku-postgresql:essential-0'}})).to.equal('https://data-host.herokuapp.com')
-      })
+      expect(host({plan: {name: 'heroku-postgresql:essential-0'}})).to.equal('https://data-host.herokuapp.com')
     })
   })
 
@@ -40,18 +31,10 @@ describe('host', () => {
     })
     afterEach(() => delete process.env.HEROKU_POSTGRESQL_HOST)
 
-    it('shows essential host', () => {
-      expect(host({plan: {name: 'heroku-postgresql:mini'}})).to.equal('https://postgres-starter-api.heroku.com')
-    })
-
-    it('shows postgresql host', () => {
+    it('shows postgresql-host.herokuapp.com host', () => {
+      expect(host({plan: {name: 'heroku-postgresql:mini'}})).to.equal('https://postgresql-host.herokuapp.com')
       expect(host({plan: {name: 'heroku-postgresql:premium-0'}})).to.equal('https://postgresql-host.herokuapp.com')
-    })
-
-    context('for numbered essential plans', () => {
-      it('shows data host', () => {
-        expect(host({plan: {name: 'heroku-postgresql:essential-0'}})).to.equal('https://postgresql-host.herokuapp.com')
-      })
+      expect(host({plan: {name: 'heroku-postgresql:essential-0'}})).to.equal('https://postgresql-host.herokuapp.com')
     })
   })
 })
