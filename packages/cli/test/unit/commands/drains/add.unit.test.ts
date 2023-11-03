21d0ad60642a15063cf911775586ba8ead0f0a61
RyanDagg <rdagg@salesforce.com>
2023-11-03 14:08:12 -0600

    refactor: move & change file ext

--- packages/cli/test/unit/commands/drains/add.unit.test.ts
+++ packages/cli/test/unit/commands/drains/add.unit.test.ts
@@ -3,14 +3,14 @@
 
 const cli = require('heroku-cli-util')
 const nock = require('nock')
-const cmd = require('../../../../src/commands/drains/add')
+const cmd = require('packages/cli/src/commands/drains/add')
 const expect = require('chai').expect
 
 describe('drains:add', function () {
   beforeEach(() => cli.mockConsole())
 
   it('adds a log drain', function () {
-    let api = nock('https://api.heroku.com:443')
+    const api = nock('https://api.heroku.com:443')
       .post('/apps/myapp/log-drains', {url: 'syslog://logs.example.com'})
       .reply(200, {url: 'syslog://logs.example.com'})
     return cmd.run({app: 'myapp', args: {url: 'syslog://logs.example.com'}})
