6ca1a43b7253d4f4057e39b6893e72e3a1d18818
RyanDagg <ryandagg@users.noreply.github.com>
2023-11-03 12:53:14 -0600

    refactor(app): move apps:destroy to CLI & upgrade to oclif (#2528)
    * refactor: move files & change ext to TS
    
    * wip
    
    * refactor: migrate command to core & TS
    reimplement heroku-cli-util’s confirmApp in our lib
    
    * upgrade test. Passing.
    
    * add test case for no app flag/arg
    
    * update commands test for aliases
    
    * add unit tests for confirmApp
    
    * fix: test name
    
    * fix: typo

---
+++ packages/cli/test/unit/lib/apps/confirm-app.unit.test.ts
@@ -0,0 +1,55 @@
+import {ux} from '@oclif/core'
+import {expect, test} from '@oclif/test'
+import confirmApp from '../../../../src/lib/apps/confirm-app'
+
+const stripAnsi = require('strip-ansi')
+
+describe('confirmApp', () => {
+  test
+    .stdout()
+    .stderr()
+    .do(() => confirmApp('app', 'app'))
+    .it('should not error or prompt with confirm flag match', ({stderr, stdout}) => {
+      expect(stderr).to.equal('')
+      expect(stdout).to.equal('')
+    })
+
+  test
+    .stdout()
+    .stderr()
+    .do(() => confirmApp('app', 'nope'))
+    .catch((error: Error) => {
+      expect(stripAnsi(error.message)).to.equal('Confirmation nope did not match app. Aborted.')
+    })
+    .it('should err on confirm flag mismatch')
+
+  test
+    .stdout()
+    .stderr()
+    .stub(ux, 'prompt', () => Promise.resolve('app'))
+    .do(() => confirmApp('app'))
+    .it('should not err on confirm prompt match', ({stderr, stdout}) => {
+      expect(stderr).to.contain('Warning: WARNING: Destructive Action')
+      expect(stdout).to.equal('')
+    })
+
+  const customMessage = 'custom message'
+
+  test
+    .stdout()
+    .stderr()
+    .stub(ux, 'prompt', () => Promise.resolve('app'))
+    .do(() => confirmApp('app', undefined, customMessage))
+    .it('should display custom message', ({stderr, stdout}) => {
+      expect(stderr).to.contain(customMessage)
+      expect(stdout).to.equal('')
+    })
+
+  test
+    .stub(ux, 'prompt', () => Promise.resolve('nope'))
+    .do(() => confirmApp('app'))
+    .catch((error: Error) => {
+      expect(stripAnsi(error.message)).to.equal('Confirmation did not match app. Aborted.')
+    })
+    .it('should err on confirm prompt mismatch')
+})
