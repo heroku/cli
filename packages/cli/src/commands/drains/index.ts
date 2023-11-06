bf59bde83214d52a74f299d56afa671000fb50d7
RyanDagg <ryandagg@users.noreply.github.com>
2023-11-06 14:44:31 -0700

    refactor(app-v5): move drains index to CLI & upgrade to oclif (#2538)
    * refactor: move files and change ext
    
    * refactor: upgrade command to TS & core
    
    * refactor: upgrade test to TS & core
    
    * chore: improve readability
    
    * chore: readability++

---
+++ packages/cli/src/commands/drains/index.ts
@@ -0,0 +1,55 @@
+import {ux} from '@oclif/core'
+import color from '@heroku-cli/color'
+import * as Heroku from '@heroku-cli/schema'
+import {flags, Command} from '@heroku-cli/command'
+import {partition} from 'lodash'
+
+function styledDrain(id: string, name: string, drain: Heroku.LogDrain) {
+  let output = `${id} (${name})`
+  if (drain.extended) output += ` drain_id=${drain.extended.drain_id}`
+  ux.log(output)
+}
+
+export default class Drains extends Command {
+  static description = 'display the log drains of an app'
+
+  static flags = {
+    app: flags.app({required: true}),
+    extended: flags.boolean({char: 'x', hidden: true}),
+    json: flags.boolean({description: 'output in json format'}),
+  }
+
+  async run() {
+    const {flags} = await this.parse(Drains)
+
+    let path = `/apps/${flags.app}/log-drains`
+    if (flags.extended) {
+      path += '?extended=true'
+    }
+
+    const {body: drains} = await this.heroku.get<Heroku.LogDrain[]>(path)
+
+    if (flags.json) {
+      ux.styledJSON(drains)
+    } else {
+      const [drainsWithAddons, drainsWithoutAddons] = partition<Heroku.LogDrain>(drains, 'addon')
+      if (drainsWithoutAddons.length > 0) {
+        ux.styledHeader('Drains')
+        drainsWithoutAddons.forEach((drain: Heroku.LogDrain) => {
+          styledDrain(drain.url || '', color.green(drain.token || ''), drain)
+        })
+      }
+
+      if (drainsWithAddons.length > 0) {
+        const addons = await Promise.all(
+          drainsWithAddons.map((d: Heroku.LogDrain) => this.heroku.get<Heroku.AddOn>(`/apps/${flags.app}/addons/${d.addon?.name}`)),
+        )
+        ux.styledHeader('Add-on Drains')
+        addons.forEach(({body: addon}, i) => {
+          styledDrain(color.yellow(addon.plan?.name || ''), color.green(addon.name || ''), drainsWithAddons[i])
+        })
+      }
+    }
+  }
+}
+
