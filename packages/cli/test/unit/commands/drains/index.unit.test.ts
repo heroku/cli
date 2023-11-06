5f5ebb31cda96681618d0b7671d1dfa034a3df19
RyanDagg <rdagg@salesforce.com>
2023-11-06 12:48:17 -0700

    refactor: upgrade test to TS & core

--- packages/cli/test/unit/commands/drains/index.unit.test.ts
+++ packages/cli/test/unit/commands/drains/index.unit.test.ts
@@ -1,72 +1,79 @@
-'use strict'
-/* globals beforeEach */
-
-const cli = require('heroku-cli-util')
-const nock = require('nock')
-const cmd = require('../../../../src/commands/drains')
-const expect = require('chai').expect
+import {expect, test} from '@oclif/test'
 
 describe('drains', function () {
-  beforeEach(() => cli.mockConsole())
+  test
+    .stderr()
+    .stdout()
+    .nock('https://api.heroku.com:443', api => {
+      api
+        .get('/apps/myapp/log-drains')
+        .reply(200, [{
+          token: 'd.8bf587e9-29d1-43c8-bd0e-36cdfaf35259',
+          url: 'https://forker.herokuapp.com'}])
+    })
+    .command(['drains', '-a', 'myapp'])
+    .it('shows log drains', ({stdout, stderr}) => {
+      expect(stderr).to.equal('')
+      expect(stdout).to.equal(`=== Drains
 
-  it('shows log drains', function () {
-    let api = nock('https://api.heroku.com:443')
-      .get('/apps/myapp/log-drains')
-      .reply(200, [{
-        token: 'd.8bf587e9-29d1-43c8-bd0e-36cdfaf35259',
-        url: 'https://forker.herokuapp.com'}])
-    return cmd.run({app: 'myapp', flags: {}})
-      .then(() => expect(cli.stderr).to.equal(''))
-      .then(() => expect(cli.stdout).to.equal(`=== Drains
 https://forker.herokuapp.com (d.8bf587e9-29d1-43c8-bd0e-36cdfaf35259)
-`))
-      .then(() => api.done())
-  })
+`)
+    })
 
-  it('shows add-on drains', function () {
-    let api = nock('https://api.heroku.com:443')
-      .get('/apps/myapp/log-drains')
-      .reply(200, [{
-        addon: {name: 'add-on-123'},
-        token: 'd.8bf587e9-29d1-43c8-bd0e-36cdfaf35259',
-        url: 'https://forker.herokuapp.com'}])
-      .get('/apps/myapp/addons/add-on-123')
-      .reply(200, {name: 'add-on-123', plan: {name: 'add-on:test'}})
-    return cmd.run({app: 'myapp', flags: {}})
-      .then(() => expect(cli.stderr).to.equal(''))
-      .then(() => expect(cli.stdout).to.equal(`=== Add-on Drains
+  test
+    .stderr()
+    .stdout()
+    .nock('https://api.heroku.com:443', api => {
+      api
+        .get('/apps/myapp/log-drains')
+        .reply(200, [{
+          addon: {name: 'add-on-123'},
+          token: 'd.8bf587e9-29d1-43c8-bd0e-36cdfaf35259',
+          url: 'https://forker.herokuapp.com'}])
+        .get('/apps/myapp/addons/add-on-123')
+        .reply(200, {name: 'add-on-123', plan: {name: 'add-on:test'}})
+    })
+    .command(['drains', '-a', 'myapp'])
+    .it('shows add-on drains', ({stdout, stderr}) => {
+      expect(stderr).to.equal('')
+      expect(stdout).to.equal(`=== Add-on Drains
+
 add-on:test (add-on-123)
-`))
-      .then(() => api.done())
-  })
+`)
+    })
 
-  it('shows drain_id for both', function () {
-    let api = nock('https://api.heroku.com:443')
-      .get('/apps/myapp/log-drains?extended=true')
-      .reply(200, [{
-        addon: {name: 'add-on-123'},
-        token: 'd.8bf587e9-29d1-43c8-bd0e-36cdfaf35259',
-        url: 'https://forker.herokuapp.com',
-        extended: {
-          drain_id: 12345,
-        },
-      }, {
-        token: 'd.8bf587e9-29d1-43c8-bd0e-36cdfaf35259',
-        url: 'https://forker.herokuapp.com',
-        extended: {
-          drain_id: 67890,
-        },
-      }])
-      .get('/apps/myapp/addons/add-on-123')
-      .reply(200, {name: 'add-on-123', plan: {name: 'add-on:test'}})
-    return cmd.run({app: 'myapp', flags: {extended: true}})
-      .then(() => expect(cli.stderr).to.equal(''))
-      .then(() => expect(cli.stdout).to.equal(
-        `=== Drains
+  test
+    .stderr()
+    .stdout()
+    .nock('https://api.heroku.com:443', api => {
+      api
+        .get('/apps/myapp/log-drains?extended=true')
+        .reply(200, [{
+          addon: {name: 'add-on-123'},
+          token: 'd.8bf587e9-29d1-43c8-bd0e-36cdfaf35259',
+          url: 'https://forker.herokuapp.com',
+          extended: {
+            drain_id: 12345,
+          },
+        }, {
+          token: 'd.8bf587e9-29d1-43c8-bd0e-36cdfaf35259',
+          url: 'https://forker.herokuapp.com',
+          extended: {
+            drain_id: 67890,
+          },
+        }])
+        .get('/apps/myapp/addons/add-on-123')
+        .reply(200, {name: 'add-on-123', plan: {name: 'add-on:test'}})
+    })
+    .command(['drains', '-a', 'myapp', '--extended'])
+    .it('shows drain_id for both', ({stdout, stderr}) => {
+      expect(stderr).to.equal('')
+      expect(stdout).to.equal(`=== Drains
+
 https://forker.herokuapp.com (d.8bf587e9-29d1-43c8-bd0e-36cdfaf35259) drain_id=67890
 === Add-on Drains
+
 add-on:test (add-on-123) drain_id=12345
-`))
-      .then(() => api.done())
-  })
+`)
+    })
 })
