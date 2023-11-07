deca1d3276e41d0a6b99f4584c1d492d4e87ba50
RyanDagg <rdagg@salesforce.com>
2023-11-07 16:38:04 -0700

    fix: pin core version to match cli-command

--- packages/certs-v5/commands/certs/index.js
+++ packages/certs-v5/commands/certs/index.js
@@ -7,7 +7,6 @@ let endpoints = require('../../lib/endpoints.js').all
 let displayTable = require('../../lib/display_table.js')
 
 async function run(context, heroku) {
-  console.log(context)
   let certs = await endpoints(context.app, heroku)
 
   if (certs.length === 0) {
