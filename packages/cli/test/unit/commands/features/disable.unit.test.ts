e192553f02f6303d7e6b053acebb1cfc8c0f67cb
RyanDagg <rdagg@salesforce.com>
2023-11-08 15:02:36 -0700

    refactor: upgrade test to TS & core

--- packages/cli/test/unit/commands/features/disable.unit.test.ts
+++ packages/cli/test/unit/commands/features/disable.unit.test.ts
@@ -1,20 +1,32 @@
-'use strict'
-/* globals commands beforeEach */
+import {expect, test} from '@oclif/test'
+import stripAnsi = require('strip-ansi')
 
-const cli = require('heroku-cli-util')
-const nock = require('nock')
-const cmd = commands.find(c => c.topic === 'features' && c.command === 'disable')
+describe('features:disable',  () => {
+  test
+    .stderr({print: true})
+    .stdout({print: true})
+    .nock('https://api.heroku.com:443', api => {
+      api
+        .get('/apps/myapp/features/feature-a')
+        .reply(200, {enabled: true})
+        .patch('/apps/myapp/features/feature-a', {enabled: false})
+        .reply(200)
+    })
+    .command(['features:disable', '-a', 'myapp', 'feature-a'])
+    .it('disables an app feature', ({stdout, stderr}) => {
+      expect(stderr).to.include('Disabling feature-a for ⬢ myapp... done')
+      expect(stdout).to.equal('')
+    })
 
-describe('features:disable', function () {
-  beforeEach(() => cli.mockConsole())
-
-  it('disables an app feature', function () {
-    let api = nock('https://api.heroku.com:443')
-      .get('/apps/myapp/features/feature-a')
-      .reply(200, {enabled: true})
-      .patch('/apps/myapp/features/feature-a', {enabled: false})
-      .reply(200)
-    return cmd.run({app: 'myapp', args: {feature: 'feature-a'}})
-      .then(() => api.done())
-  })
+  test
+    .nock('https://api.heroku.com:443', api => {
+      api
+        .get('/apps/myapp/features/feature-a')
+        .reply(200, {enabled: false})
+    })
+    .command(['features:disable', '-a', 'myapp', 'feature-a'])
+    .catch((error: any) => {
+      expect(stripAnsi(error.message)).to.equal('feature-a is already disabled.')
+    })
+    .it('errors if feature is already disabled')
 })
