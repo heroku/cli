0279630d098bf62d9eb0b4f1af92a0a04d940c75
RyanDagg <ryandagg@users.noreply.github.com>
2023-11-09 10:01:55 -0700

    refactor(app-v5): move features:disable to CLI & upgrade to oclif (#2547)
    * refactor: move file location and change ext
    
    * refactor: upgrade to TS & core
    
    * refactor: upgrade test to TS & core
    
    * chore: remove debug printing

---
+++ packages/cli/test/unit/commands/features/disable.unit.test.ts
@@ -0,0 +1,32 @@
+import {expect, test} from '@oclif/test'
+import stripAnsi = require('strip-ansi')
+
+describe('features:disable',  () => {
+  test
+    .stderr()
+    .stdout()
+    .nock('https://api.heroku.com:443', api => {
+      api
+        .get('/apps/myapp/features/feature-a')
+        .reply(200, {enabled: true})
+        .patch('/apps/myapp/features/feature-a', {enabled: false})
+        .reply(200)
+    })
+    .command(['features:disable', '-a', 'myapp', 'feature-a'])
+    .it('disables an app feature', ({stdout, stderr}) => {
+      expect(stderr).to.include('Disabling feature-a for ⬢ myapp... done')
+      expect(stdout).to.equal('')
+    })
+
+  test
+    .nock('https://api.heroku.com:443', api => {
+      api
+        .get('/apps/myapp/features/feature-a')
+        .reply(200, {enabled: false})
+    })
+    .command(['features:disable', '-a', 'myapp', 'feature-a'])
+    .catch((error: any) => {
+      expect(stripAnsi(error.message)).to.equal('feature-a is already disabled.')
+    })
+    .it('errors if feature is already disabled')
+})
