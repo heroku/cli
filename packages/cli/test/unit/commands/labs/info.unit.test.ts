45058be4b0b1d400a11f0a109f5a51e51a620102
Zane Whitfield <zwhitfield@salesforce.com>
2023-11-13 16:04:37 -0800

    Successfully add first test

--- packages/cli/test/unit/commands/labs/info.unit.test.ts
+++ packages/cli/test/unit/commands/labs/info.unit.test.ts
@@ -1,81 +1,23 @@
 import {expect, test} from '@oclif/test'
 import {ux} from '@oclif/core'
-import * as sinon from 'sinon'
 
-const promptStub = sinon.stub()
-
-describe('labs:disable', () => {
+describe('labs:info', () => {
   test
-    .nock('https://api.heroku.com', api => api
-      .get('/account')
-      .reply(200, {email: 'jeff@heroku.com'})
+    .stdout()
+    .stderr()
+    .nock('https://api.heroku.com:443', api => api
       .get('/account/features/feature-a')
       .reply(200, {
         enabled: true,
         name: 'feature-a',
         description: 'a user lab feature',
         doc_url: 'https://devcenter.heroku.com',
-      })
-      .patch('/account/features/feature-a', {enabled: false})
-      .reply(200),
+      }),
     )
     .stderr()
-    .command(['labs:disable', 'feature-a'])
-    .it('disables a user lab feature', () => {
-    // to-do: make this work on CI
-    // expect(stderr).to.contain('Disabling feature-a for jeff@heroku.com...')
-    })
-
-  test
-    .stderr()
-    .do(() => {
-      promptStub.onFirstCall().resolves('myapp')
-    })
-    .nock('https://api.heroku.com', api => api
-      .get('/account/features/spaces-strict-tls').reply(404)
-      .get('/apps/myapp/features/spaces-strict-tls')
-      .reply(200, {
-        enabled: true,
-        name: 'spaces-strict-tls',
-        description: 'a user lab feature',
-        doc_url: 'https://devcenter.heroku.com',
-      })
-      .patch('/apps/myapp/features/spaces-strict-tls', {enabled: false}).reply(200),
-    )
-    .stub(ux, 'prompt', () => Promise.resolve('myapp'))
-    .command(['labs:disable', 'spaces-strict-tls', '--app=myapp'])
-    .it('warns user of insecure action', ({stderr}) => {
-      expect(stderr).to.contain('Insecure Action\nDisabling spaces-strict-tls for myapp...')
-    })
-
-  test
-    .stderr()
-    .do(() => {
-      promptStub.onFirstCall().resolves('myapp')
-      promptStub.onSecondCall().resolves('notMyApp')
-    })
-    .stub(ux, 'prompt', () => promptStub)
-    .command(['labs:disable', 'spaces-strict-tls', '--app=myapp'])
-    .catch(error => {
-      expect(error.message).to.equal('Confirmation name did not match app name. Try again.')
-    })
-    .it('errors when confirmation name does not match')
-
-  test
-    .nock('https://api.heroku.com', api => api
-      .get('/account/features/feature-a').reply(404)
-      .get('/apps/myapp/features/feature-a')
-      .reply(200, {
-        enabled: true,
-        name: 'feature-a',
-        description: 'a user lab feature',
-        doc_url: 'https://devcenter.heroku.com',
-      })
-      .patch('/apps/myapp/features/feature-a', {enabled: false}).reply(200),
-    )
-    .stderr()
-    .command(['labs:disable', 'feature-a', '--app=myapp'])
-    .it('disables an app feature', ({stderr}) => {
-      expect(stderr).to.contain('Disabling feature-a for myapp...')
+    .command(['labs:info', 'feature-a'])
+    .it('shows user labs feature info', ({stdout, stderr}) => {
+      expect(stdout).to.equal('=== feature-a\n\nDescription: a user lab feature\nDocs:        https://devcenter.heroku.com\nEnabled:     true\n')
+      expect(stderr).to.be.empty
     })
 })
