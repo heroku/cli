575ce050f027f89c0a714b59a15feb8db9a9fc0e
Zane Whitfield <zwhitfield@salesforce.com>
2023-11-14 16:35:03 -0800

    Add test

--- packages/cli/test/unit/commands/labs/index.unit.test.ts
+++ packages/cli/test/unit/commands/labs/index.unit.test.ts
@@ -5,54 +5,21 @@ describe('labs', () => {
     .stdout()
     .stderr()
     .nock('https://api.heroku.com:443', api => api
-      .get('/account/features/feature-a')
-      .reply(200, {
-        enabled: true,
-        name: 'feature-a',
-        description: 'a user lab feature',
-        doc_url: 'https://devcenter.heroku.com',
-      }),
+      .get('/account')
+      .reply(200, {email: 'jeff@heroku.com'})
+      .get('/account/features')
+      .reply(200, [
+        {enabled: true, name: 'lab feature a', description: 'a user lab feature'},
+        {enabled: false, name: 'lab feature b', description: 'a user lab feature'},
+      ])
+      .get('/apps/myapp/features')
+      .reply(200, [
+        {enabled: true, name: 'lab feature c', description: 'an app lab feature'},
+      ]),
     )
-    .command(['labs:info', 'feature-a'])
-    .it('shows user labs feature info', ({stdout, stderr}) => {
-      expect(stdout).to.equal('=== feature-a\n\nDescription: a user lab feature\nDocs:        https://devcenter.heroku.com\nEnabled:     true\n')
-      expect(stderr).to.be.empty
-    })
-
-  test
-    .stdout()
-    .stderr()
-    .nock('https://api.heroku.com:443', api => api
-      .get('/account/features/feature-a')
-      .reply(200, {
-        enabled: true,
-        name: 'feature-a',
-        description: 'a user lab feature',
-        doc_url: 'https://devcenter.heroku.com',
-      }),
-    )
-    .command(['labs:info', 'feature-a', '--json'])
-    .it('shows user labs feature info as json', ({stdout, stderr}) => {
-      expect(stdout).to.equal('{\n  "enabled": true,\n  "name": "feature-a",\n  "description": "a user lab feature",\n  "doc_url": "https://devcenter.heroku.com"\n}\n')
-      expect(stderr).to.be.empty
-    })
-
-  test
-    .stdout()
-    .stderr()
-    .nock('https://api.heroku.com:443', api => api
-      .get('/account/features/feature-a').reply(404)
-      .get('/apps/myapp/features/feature-a')
-      .reply(200, {
-        enabled: true,
-        name: 'feature-a',
-        description: 'an app labs feature',
-        doc_url: 'https://devcenter.heroku.com',
-      }),
-    )
-    .command(['labs:info', 'feature-a', '-a', 'myapp'])
-    .it('shows app labs feature info', ({stdout, stderr}) => {
-      expect(stdout).to.equal('=== feature-a\n\nDescription: an app labs feature\nDocs:        https://devcenter.heroku.com\nEnabled:     true\n')
+    .command(['labs', 'myapp'])
+    .it('shows labs features', ({stdout, stderr}) => {
+      expect(stdout).to.equal('=== User Features jeff@heroku.com\n\n[+] lab feature a  a user lab feature\n[ ] lab feature b  a user lab feature\n\n=== App Features ⬢ myapp\n\n[+] lab feature c  an app lab feature\n')
       expect(stderr).to.be.empty
     })
 })
