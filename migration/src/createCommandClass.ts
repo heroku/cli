9e845be11251570ab7bc900190d1a90bd85ba2fd
Justin Wilaby <jwilaby@jwilaby-ltmzwtk.internal.salesforce.com>
2023-11-15 13:54:56 -0800

    feature(migration): Use TS compiler to migrate legacy commands

---
+++ migration/src/createCommandClass.ts
@@ -0,0 +1,5 @@
+import ts from 'typescript';
+
+export function createCommandClass(node: ts.FunctionDeclaration & { name: { text: 'run' }}, className: ts.Identifier | string) {
+    const classDecl = ts.factory.createClassDeclaration(undefined, className);
+}
