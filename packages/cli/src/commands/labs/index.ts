1cdbe386c5e9dfc1f370e208a14c7b179778628a
Zane Whitfield <zwhitfield@salesforce.com>
2023-11-14 16:29:26 -0800

    WIP migrate command part 3

--- packages/cli/src/commands/labs/index.ts
+++ packages/cli/src/commands/labs/index.ts
@@ -4,11 +4,17 @@ import * as Heroku from '@heroku-cli/schema'
 import {Args, ux} from '@oclif/core'
 import {sortBy} from 'lodash'
 
+interface Features {
+  currentUser: any,
+  user: any,
+  app?: any,
+}
+
 function printJSON(features: Heroku.Account | Heroku.AccountFeature | Heroku.AppFeature) {
   ux.log(JSON.stringify(features, null, 2))
 }
 
-function printFeatures(features: Heroku.AccountFeature[] | Heroku.AccountFeature) {
+function printFeatures(features: any) {
   features = sortBy(features, 'name')
   const longest = Math.max.apply(null, features.map((f: Record<string, string>) => f.name.length))
   for (const f of features) {
@@ -24,10 +30,11 @@ export default class LabsIndex extends Command {
   static topic = 'labs'
 
   static args = {
-    app: Args.string({required: true}),
+    app: Args.string({required: false}),
   }
 
   static flags = {
+    app: flags.app({required: false}),
     json: flags.boolean({description: 'display as json', required: false}),
   }
 
@@ -36,21 +43,32 @@ export default class LabsIndex extends Command {
     const [currentUserResponse, userResponse, appResponse] = await Promise.all([
       this.heroku.get<Heroku.Account>('/account'),
       this.heroku.get<Heroku.AccountFeature>('/account/features'),
-      args.app ? this.heroku.get<Heroku.AppFeature>(`/apps/${args.app}/features`) : null,
+      (args.app || flags.app) ? this.heroku.get<Heroku.AppFeature>(`/apps/${args.app ? args.app : flags.app}/features`) : null,
     ])
+
+    let app = null
     const currentUser = currentUserResponse.body
     const user = userResponse.body
-    const app = appResponse?.body
 
-    const features = {
+    const features: Features = {
       currentUser,
       user,
-      app,
     }
+
+    // makes sure app isn't added to json object if null
+    if (appResponse !== null) {
+      app = appResponse!.body
+      features.app = app
+    }
+
     // general features are managed via `features` not `labs`
     features.user = features.user.filter((f: Record<string, string>) => f.state !== 'general')
     if (features.app) features.app = features.app.filter((f: Record<string, string>) => f.state !== 'general')
     if (flags.json) {
+      // Added the comments below as the current logic breaks
+      // interface rules and other solutions are quite verbose
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
       delete features.currentUser
       printJSON(features)
     } else {
@@ -58,7 +76,7 @@ export default class LabsIndex extends Command {
       printFeatures(features.user)
       if (features.app) {
         ux.log()
-        ux.styledHeader(`App Features ${color.app(args.app)}`)
+        ux.styledHeader(`App Features ${color.app(args.app ? args.app! : flags.app!)}`)
         printFeatures(features.app)
       }
     }
