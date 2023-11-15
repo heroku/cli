71741f7a1a74fb133f72ff574c00b9013d12ffa5
Zane Whitfield <zwhitfield@salesforce.com>
2023-11-15 15:33:24 -0800

    Make enableFeatures function async

--- packages/cli/src/commands/labs/enable.ts
+++ packages/cli/src/commands/labs/enable.ts
@@ -3,7 +3,7 @@ import {APIClient, flags, Command} from '@heroku-cli/command'
 import * as Heroku from '@heroku-cli/schema'
 import {Args, ux} from '@oclif/core'
 
-function enableFeature(heroku: APIClient, feature: string, app?: string) {
+async function enableFeature(heroku: APIClient, feature: string, app?: string) {
   return heroku.patch<Heroku.AppFeature | Heroku.AccountFeature>(app ? `/apps/${app}/features/${feature}` : `/account/features/${feature}`, {
     body: {enabled: true},
   })
@@ -28,7 +28,7 @@ export default class LabsEnable extends Command {
 
     try {
       await this.heroku.get<Heroku.AccountFeature>(`/account/features/${feature}`)
-      enableFeature(this.heroku, feature)
+      await enableFeature(this.heroku, feature)
       const targetResponse = await this.heroku.get<Heroku.Account>('/account')
       target = targetResponse.body.email
     } catch (error: any) {
@@ -36,7 +36,7 @@ export default class LabsEnable extends Command {
       // might be an app feature
       if (!flags.app) throw error
       await this.heroku.get<Heroku.AppFeature>(`/apps/${flags.app}/features/${feature}`)
-      enableFeature(this.heroku, feature, flags.app)
+      await enableFeature(this.heroku, feature, flags.app)
       target = flags.app
     }
 
