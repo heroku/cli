c16c6ee1ff52d63e21eed3f53360280ac07d3859
Zane Whitfield <zwhitfield@salesforce.com>
2023-11-15 16:56:31 -0800

    Update command inherited class name

--- packages/cli/src/commands/maintenance/off.ts
+++ packages/cli/src/commands/maintenance/off.ts
@@ -3,7 +3,7 @@ import * as Heroku from '@heroku-cli/schema'
 import color from '@heroku-cli/color'
 import {ux} from '@oclif/core'
 
-export default class MaintenanceIndex extends Command {
+export default class MaintenanceOff extends Command {
   static description = 'take the app out of maintenance mode'
   static topic = 'maintenance'
 
@@ -12,7 +12,7 @@ export default class MaintenanceIndex extends Command {
   }
 
   async run() {
-    const {flags} = await this.parse(MaintenanceIndex)
+    const {flags} = await this.parse(MaintenanceOff)
     ux.action.start(`Disabling maintenance mode for ${color.app(flags.app)}`)
     await this.heroku.patch<Heroku.App>(`/apps/${flags.app}`, {body: {maintenance: false}})
     ux.action.stop()
