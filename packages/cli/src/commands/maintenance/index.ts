fdc5ce9379001e82e3de8a6d7386253aff6f61d4
Zane Whitfield <zwhitfield@salesforce.com>
2023-11-15 16:00:59 -0800

    WIP migrate command & tests part 2

--- packages/cli/src/commands/maintenance/index.ts
+++ packages/cli/src/commands/maintenance/index.ts
@@ -1,18 +1,18 @@
-import {Command} from '@heroku-cli/command'
+import {Command, flags} from '@heroku-cli/command'
 import * as Heroku from '@heroku-cli/schema'
-import {Args, ux} from '@oclif/core'
+import {ux} from '@oclif/core'
 
 export default class MaintenanceIndex extends Command {
   static description = 'display the current maintenance status of app'
   static topic = 'maintenance'
 
-  static args = {
-    app: Args.string({required: true}),
+  static flags = {
+    app: flags.app({required: true}),
   }
 
   async run() {
-    const {args} = await this.parse(MaintenanceIndex)
-    const appResponse = await this.heroku.get<Heroku.App>(`/apps/${args.app}`)
+    const {flags} = await this.parse(MaintenanceIndex)
+    const appResponse = await this.heroku.get<Heroku.App>(`/apps/${flags.app}`)
     const app = appResponse.body
     ux.log(app.maintenance ? 'on' : 'off')
   }
