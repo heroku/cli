ff818593ec5b64e7ba3bbc4b68f28b1e53e86a8e
Zane Whitfield <zwhitfield@salesforce.com>
2023-11-15 16:07:59 -0800

    Successfully migrate command & tests

--- packages/cli/test/unit/commands/maintenance/index.unit.test.ts
+++ packages/cli/test/unit/commands/maintenance/index.unit.test.ts
@@ -1,45 +1,29 @@
 import {expect, test} from '@oclif/test'
 
-describe('labs:enable', () => {
+describe('maintenance', () => {
   test
     .stdout()
     .stderr()
     .nock('https://api.heroku.com:443', api => api
-      .get('/account')
-      .reply(200, {email: 'jeff@heroku.com'})
-      .get('/account/features/feature-a')
-      .reply(200, {
-        enabled: false,
-        name: 'feature-a',
-        description: 'a user lab feature',
-        doc_url: 'https://devcenter.heroku.com',
-      })
-      .patch('/account/features/feature-a', {enabled: true})
-      .reply(200),
+      .get('/apps/myapp')
+      .reply(200, {maintenance: true}),
     )
-    .command(['labs:enable', 'feature-a'])
-    .it('enables a user lab feature', ({stdout, stderr}) => {
-      expect(stdout).to.be.empty
-      expect(stderr).to.equal('Enabling feature-a for jeff@heroku.com...\nEnabling feature-a for jeff@heroku.com... done\n')
+    .command(['maintenance', '-a', 'myapp'])
+    .it('shows that maintenance is on', ({stdout, stderr}) => {
+      expect(stdout).to.equal('on\n')
+      expect(stderr).to.be.empty
     })
 
   test
     .stdout()
     .stderr()
     .nock('https://api.heroku.com:443', api => api
-      .get('/account/features/feature-a').reply(404)
-      .get('/apps/myapp/features/feature-a')
-      .reply(200, {
-        enabled: false,
-        name: 'feature-a',
-        description: 'an app labs feature',
-        doc_url: 'https://devcenter.heroku.com',
-      })
-      .patch('/apps/myapp/features/feature-a', {enabled: true}).reply(200),
+      .get('/apps/myapp')
+      .reply(200, {maintenance: false}),
     )
-    .command(['labs:enable', 'feature-a', '-a', 'myapp'])
-    .it('enables an app feature', ({stdout, stderr}) => {
-      expect(stdout).to.be.empty
-      expect(stderr).to.contain('Enabling feature-a for myapp... done\n')
+    .command(['maintenance', '-a', 'myapp'])
+    .it('shows that maintenance is off', ({stdout, stderr}) => {
+      expect(stdout).to.equal('off\n')
+      expect(stderr).to.be.empty
     })
 })
