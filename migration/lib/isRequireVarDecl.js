949f9a2ec02585b8e9f30342d995a3bf2f9bc941
Justin Wilaby <jwilaby@jwilaby-ltmzwtk.internal.salesforce.com>
2023-11-17 11:38:48 -0800

    Programmatic Eslint and other small tweaks

--- migration/lib/isRequireVarDecl.js
+++
@@ -1,9 +0,0 @@
-import ts from 'typescript';
-export function isRequireVarDecl(node) {
-    return ts.isVariableDeclarationList(node) &&
-        ts.isVariableDeclaration(node.declarations[0]) &&
-        ts.isCallExpression(node.declarations[0].initializer) &&
-        ts.isIdentifier(node.declarations[0].initializer.expression) &&
-        node.declarations[0].initializer.expression.text === 'require';
-}
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaXNSZXF1aXJlVmFyRGVjbC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uL3NyYy9pc1JlcXVpcmVWYXJEZWNsLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBLE9BQU8sRUFBRSxNQUFNLFlBQVksQ0FBQTtBQUUzQixNQUFNLFVBQVUsZ0JBQWdCLENBQUMsSUFBYTtJQUM1QyxPQUFPLEVBQUUsQ0FBQyx5QkFBeUIsQ0FBQyxJQUFJLENBQUM7UUFDdkMsRUFBRSxDQUFDLHFCQUFxQixDQUFDLElBQUksQ0FBQyxZQUFZLENBQUMsQ0FBQyxDQUFDLENBQUM7UUFDMUMsRUFBRSxDQUFDLGdCQUFnQixDQUFDLElBQUksQ0FBQyxZQUFZLENBQUMsQ0FBQyxDQUFDLENBQUMsV0FBVyxDQUFDO1FBQ3JELEVBQUUsQ0FBQyxZQUFZLENBQUMsSUFBSSxDQUFDLFlBQVksQ0FBQyxDQUFDLENBQUMsQ0FBQyxXQUFXLENBQUMsVUFBVSxDQUFDO1FBQzVELElBQUksQ0FBQyxZQUFZLENBQUMsQ0FBQyxDQUFDLENBQUMsV0FBVyxDQUFDLFVBQVUsQ0FBQyxJQUFJLEtBQUssU0FBUyxDQUFBO0FBQ3RFLENBQUMifQ==
--- migration/src/index.ts
+++ migration/src/index.ts
@@ -1,6 +1,8 @@
 import path from 'node:path'
+import fs from 'node:fs/promises'
 import pascalcase from 'pascalcase'
 import ts from 'typescript'
+import {ESLint} from 'eslint'
 
 import {createClassElementsFromModuleExports} from './createClassElementFromModuleExports.js'
 import {createCommandClass} from './createCommandClass.js'
@@ -15,20 +17,25 @@ import {Command, flags} from '@heroku-cli/command'
 import {Args, ux} from '@oclif/core'
 import * as Heroku from '@heroku-cli/schema'
 
-const require = createRequire(imports.meta.url)
+const require = createRequire(import.meta.url)
 `
 export class CommandMigrationFactory {
+    private static outputLocation = 'converted' as const
+
     protected readonly program: ts.Program;
     protected readonly printer: ts.Printer;
+    protected readonly linter: ESLint
     private readonly files: string[];
 
     constructor(files: string[], compilerOptions: ts.CompilerOptions) {
       this.files = files
       this.program = ts.createProgram({rootNames: files, options: compilerOptions, host: ts.createCompilerHost(compilerOptions)})
       this.printer = ts.createPrinter({newLine: ts.NewLineKind.CarriageReturnLineFeed})
+      this.linter = new ESLint({fix: true, useEslintrc: true})
     }
 
-    public migrate(): void {
+    public async migrate(): Promise<void> {
+      const lintOperations: Promise<ESLint.LintResult[]>[] = []
       for (let i = 0; i < this.files.length; i++) {
         const file = this.files[i]
         let ast = this.program.getSourceFile(file)
@@ -38,11 +45,15 @@ export class CommandMigrationFactory {
 
         ast = this.migrateRunFunctionDecl(ast, file)
         ast = this.migrateModuleExports(ast)
-        ast = this.removeUnnededStatements(ast)
+        ast = this.updateOrRemoveStatements(ast)
         const sourceFile = ts.createSourceFile(path.basename(file), '', ts.ScriptTarget.Latest, false, ts.ScriptKind.TS)
-        const nodeStrings = commonImports + ast.statements.map(stmt => this.printer.printNode(ts.EmitHint.Unspecified, stmt, sourceFile))
-        this.writeSourceFile(nodeStrings, file)
+        const sourceStr = commonImports + this.printer.printList(ts.ListFormat.MultiLine, ast.statements, sourceFile)
+
+        lintOperations.push(this.linter.lintText(sourceStr, {filePath: file}))
       }
+
+      const lintResults = await Promise.all(lintOperations)
+      await Promise.all(lintResults.map(res => this.writeSourceFile(res[0].output, res[0].filePath)))
     }
 
     private migrateRunFunctionDecl(node: ts.SourceFile, filePath: string): ts.SourceFile {
@@ -70,7 +81,7 @@ export class CommandMigrationFactory {
       return ts.visitEachChild(node, visitor, nullTransformationContext)
     }
 
-    private removeUnnededStatements(node: ts.SourceFile) : ts.SourceFile {
+    private updateOrRemoveStatements(node: ts.SourceFile) : ts.SourceFile {
       const visitor = (node: ts.Node): ts.Node => {
         // 'use strict'
         if (ts.isExpressionStatement(node) && ts.isStringLiteral(node.expression) && node.expression.text === 'use strict') {
@@ -82,11 +93,25 @@ export class CommandMigrationFactory {
           return null
         }
 
+        // some string literals that are no longer aligned with
+        // the original source file do not print properly
+        // recreating them seems to workaround this issue.
+        if (ts.isStringLiteral(node)) {
+          return ts.factory.createStringLiteral(node.text)
+        }
+
         return ts.visitEachChild(node, visitor, nullTransformationContext)
       }
 
       return ts.visitEachChild(node, visitor, nullTransformationContext)
     }
 
-    private async writeSourceFile(content: string, originalFilePath: string): Promise<void> {}
+    private async writeSourceFile(content: string, originalFilePath: string): Promise<void> {
+      const {dir, name} = path.parse(originalFilePath)
+      const pathFromRoot = dir.replace(process.cwd(), '')
+      const finalPath = path.join(path.resolve(CommandMigrationFactory.outputLocation), pathFromRoot)
+
+      await fs.mkdir(finalPath, {recursive: true})
+      await fs.writeFile(path.join(finalPath, `${pascalcase(name)}.ts`), content)
+    }
 }
