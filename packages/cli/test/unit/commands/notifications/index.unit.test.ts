1ff001dc5bf303ae6d57ebf363276de8da4f1ee3
Zane Whitfield <zwhitfield@salesforce.com>
2023-11-20 11:12:49 -0800

    Successfully migrate first test

--- packages/cli/test/unit/commands/notifications/index.unit.test.ts
+++ packages/cli/test/unit/commands/notifications/index.unit.test.ts
@@ -1,16 +1,26 @@
 import {expect, test} from '@oclif/test'
+import * as time from '../../../../src/lib/notifications/time'
+import {unwrap} from '../../../helpers/utils/unwrap'
 
-describe('maintenance:on', () => {
-  test
-    .stdout()
-    .stderr()
-    .nock('https://api.heroku.com:443', api => api
-      .patch('/apps/myapp', {maintenance: true})
-      .reply(200),
-    )
-    .command(['maintenance:on', '-a', 'myapp'])
-    .it('turns maintenance mode on', ({stdout, stderr}) => {
-      expect(stdout).to.be.empty
-      expect(stderr).to.contain('Enabling maintenance mode for ⬢ myapp... done')
+describe('notifications', () => {
+  describe('no notifications', () => {
+    describe('with app', () => {
+      test
+        .stdout()
+        .stderr()
+        .nock('https://api.heroku.com:443', api => api
+          .get('/apps/myapp')
+          .reply(200, {id: 'myapp', name: 'myapp'}),
+        )
+        .nock('https://telex.heroku.com:443', api => api
+          .get('/user/notifications')
+          .reply(200, []),
+        )
+        .command(['notifications', '-a', 'myapp', '--read'])
+        .it('warns about no read notifications', ({stdout, stderr}) => {
+          expect(stdout).to.be.empty
+          expect(unwrap(stderr)).to.contain(' ▸    You have no notifications on myapp.\n ▸    Run heroku notifications --all to view notifications for all\n ▸    apps.\n')
+        })
     })
+  })
 })
