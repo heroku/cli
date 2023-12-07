ec2c8046ea1eba8d374536919b22e98b1736f356
RyanDagg <rdagg@salesforce.com>
2023-12-07 14:44:50 -0700

    experiment to determine if new npm setup is causing test failures.

--- scripts/migration/src/node-validators/isMigrationCandidate.ts
+++
@@ -1,6 +0,0 @@
-import {isRunFunctionDecl} from './isRunFunctionDecl.js'
-import ts from 'typescript'
-
-export function isMigrationCandidate(sourceFile: ts.SourceFile): boolean {
-  return sourceFile.statements.some(stmt => isRunFunctionDecl(stmt))
-}
--- scripts/migration/src/node-validators/isRequireVarDecl.ts
+++
@@ -1,9 +0,0 @@
-import ts from 'typescript'
-
-export function isRequireVarDecl(node: ts.Node): node is ts.VariableDeclarationList {
-  return ts.isVariableDeclarationList(node) &&
-    ts.isVariableDeclaration(node.declarations[0]) &&
-        ts.isCallExpression(node.declarations[0].initializer) &&
-        ts.isIdentifier(node.declarations[0].initializer.expression) &&
-        node.declarations[0].initializer.expression.text === 'require'
-}
