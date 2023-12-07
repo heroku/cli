8290ce9a730893ca33471611aba2d049d32863b6
RyanDagg <rdagg@salesforce.com>
2023-12-06 17:05:10 -0700

    chore: comments

--- scripts/migration/src/getCommandDeclaration.ts
+++ scripts/migration/src/getCommandDeclaration.ts
@@ -2,13 +2,6 @@ import ts from 'typescript'
 import {isModuleExportsObject} from './node-validators/isModuleExportsObject.js'
 import {isCommandDeclaration} from './node-validators/isCommandDeclaration.js'
 
-/**
- * Finds command declaration and returns it if found
- *
- * @param sourceFile
- * @returns undefined | ts.ObjectLiteralExpression
- */
-
 export function getCommandDeclaration(sourceFile: ts.SourceFile): undefined | ts.ObjectLiteralExpression {
   for (const node of sourceFile.statements) {
     if (isCommandDeclaration(node)) {
