15b4c17b3df0b1f43f7f28ffda546082c4f5ac8f
RyanDagg <rdagg@salesforce.com>
2023-12-07 17:07:34 -0700

    clean up

--- packages/migration/src/transforms/heroku-cli-utils/validators.ts
+++ packages/migration/src/transforms/heroku-cli-utils/validators.ts
@@ -7,19 +7,9 @@ type ExpressionTerminatesWithIdentifier = ts.PropertyAccessExpression & {
 export type CallWith1PrecedingPropertyAccess = ts.CallExpression & {
   expression: ExpressionTerminatesWithIdentifier
 }
-export type CallWith2PrecedingPropertyAccess = ts.CallExpression & {
-  expression: ts.PropertyAccessExpression & {
-    expression: ExpressionTerminatesWithIdentifier
-  }
-}
 export const isCallWith1PrecedingPropertyAccess = (node: ts.Node): node is CallWith1PrecedingPropertyAccess => (
   ts.isCallExpression(node) &&
   ts.isPropertyAccessExpression(node.expression) &&
   ts.isIdentifier(node.expression.expression)
 )
-export const isCallWith2PrecedingPropertyAccess = (node: ts.Node): node is CallWith2PrecedingPropertyAccess => (
-  ts.isCallExpression(node) &&
-  ts.isPropertyAccessExpression(node.expression) &&
-  ts.isPropertyAccessExpression(node.expression.expression) &&
-  ts.isIdentifier(node.expression.expression.expression)
-)
+
