3a3cc2b940339fab24fffa56206a9af9210e1054
RyanDagg <rdagg@salesforce.com>
2023-12-12 13:00:27 -0700

    wip: find nock blocks

--- packages/migration/src/transforms/unit-tests/migrateItCall.ts
+++ packages/migration/src/transforms/unit-tests/migrateItCall.ts
@@ -1,4 +1,5 @@
 import ts from 'typescript'
+import {nullTransformationContext} from '../../nullTransformationContext.js'
 
 const {factory} = ts
 
@@ -18,6 +19,32 @@ const createTestBase = () => factory.createCallExpression(
   [],
 )
 
+const isNockCall = (node: ts.Node) => (
+  ts.isCallExpression(node) &&
+    ts.isIdentifier(node.expression) &&
+    node.expression.escapedText === 'nock'
+)
+
+// output an array of CallExpression/PropertyAccessExpression pairs? Just replace the "last" `nock(`?
+const isNockBlock = (node: ts.Node): node is ts.VariableStatement => {
+  if (node && node?.getText()?.includes('nock')) {
+    debugger
+  }
+
+  let workingNode = node
+  /* nodes are "upside down" compared to human reading. Given:
+  * let api = nock('https://api.heroku.com:443')
+    .post('/account/keys', {public_key: key})
+    .reply(200)
+  * node starts as `.reply(200)`, and last CallExpression/PropertyAccessExpression pair is our target `nock('https://api.heroku.com:443')`
+  * */
+  while (ts.isCallExpression(workingNode) && ts.isPropertyAccessExpression(workingNode.expression)) {
+    workingNode = workingNode.expression.expression
+  }
+
+  return ts.isCallExpression(workingNode) &&  ts.isIdentifier(workingNode.expression) && workingNode.expression.escapedText === 'nock'
+}
+
 const migrateNock = (callEx: ts.CallExpression) =>  {
   // ???
 
@@ -29,8 +56,26 @@ const migrateNock = (callEx: ts.CallExpression) =>  {
   )
 }
 
+const findNock = (node: ts.Node) => {
+  const visitor = (innerNode: ts.Node): ts.Node => {
+    if (isNockBlock(innerNode)) {
+      debugger
+      return innerNode
+    }
+
+    return ts.visitEachChild(innerNode, visitor, nullTransformationContext)
+  }
+
+  return ts.visitEachChild(node, visitor, nullTransformationContext)
+}
+
 export const migrateItCall = (node: ts.Node) => {
+  // try bottom up creation? Start with smallest piece?
+  // replace entire section? Shouldn't delete something if it's there, but how to move it? Keep track of unknown parts?
+  // move ^ into a `do`? Likely not what's wanted
   const base = createTestBase()
 
-  return base
+  findNock(node)
+
+  return node
 }
