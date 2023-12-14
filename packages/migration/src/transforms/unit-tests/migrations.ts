0671dfbfcb08535d395db7338efcff866d4a2e59
RyanDagg <rdagg@salesforce.com>
2023-12-14 14:45:49 -0700

    building .nock(domain, api => {}) correctly. blocks intentionally empty

--- packages/migration/src/transforms/unit-tests/migrations.ts
+++ packages/migration/src/transforms/unit-tests/migrations.ts
@@ -21,31 +21,89 @@ const createTestBase = () => factory.createCallExpression(
   [],
 )
 
-export const migrations = (itCall: TestFunctionCall<'it'>, nestedNockInBeforeEach: NockNameCallPair[][]) => {
-  // try bottom up creation? Start with smallest piece?
-  // replace entire section? Shouldn't delete something if it's there, but how to move it? Keep track of unknown parts?
-  // move ^ into a `do`? Likely not what's wanted
-  const base = createTestBase()
-
-  const nockCalls = getNockMethodCallExpressions(itCall.arguments[1].body, nestedNockInBeforeEach)
-
-  // todo: anyway to find nock in beforeEach? example: packages/pg-v5/test/unit/commands/maintenance/run.unit.test.js
-  /* todo: handle separate definition then chaining. example: packages/pg-v5/test/unit/commands/backups/capture.unit.test.js
-  * look for nock, then decide if it's one expression or multiple?
-  *  */
-
-  return base
-}
-
 export const transformNode = <N extends ts.Node>(node: N, transform: (innerNode: ts.Node) => ts.Node) => {
-  const visitor = (vNode: ts.Node): ts.Node => ts.visitEachChild(transform(vNode), visitor, nullTransformationContext)
+  const visitor = (vNode: ts.Node): ts.Node => {
+    return ts.visitEachChild(transform(vNode), visitor, nullTransformationContext)
+  }
 
   return ts.visitEachChild(node, visitor, nullTransformationContext)
 }
 
 const transformIts = (node: ts.Node, nestedNockInBeforeEach: NockNameCallPair[][]): ts.Node => {
   if (isTestItCall(node)) {
-    return migrations(node, nestedNockInBeforeEach)
+    // try bottom up creation? Start with smallest piece?
+    // replace entire section? Shouldn't delete something if it's there, but how to move it? Keep track of unknown parts?
+    // move ^ into a `do`? Likely not what's wanted
+    let result = createTestBase()
+
+    const nockCalls = getNockMethodCallExpressions(node.arguments[1].body, nestedNockInBeforeEach)
+
+    for (const nockCall of nockCalls) {
+      result = factory.createCallExpression(
+        factory.createPropertyAccessExpression(
+          result,
+          factory.createIdentifier('nock'),
+        ),
+        undefined,
+        [
+          nockCall.instanceCall.arguments[0], // domain nock is instantiated with
+          factory.createArrowFunction(
+            undefined,
+            undefined,
+            [factory.createParameterDeclaration(
+              undefined,
+              undefined,
+              factory.createIdentifier(nockCall.varName),
+            )],
+            undefined,
+            factory.createToken(ts.SyntaxKind.EqualsGreaterThanToken), // "=>" in arrow function
+            factory.createBlock(
+              // nockCall.properties.map(callEx => {
+              //   return factory.createExpressionStatement(callEx)
+              // }),
+              [],
+              true,
+            ),
+            // factory.createBlock(
+            //   [ // map nockCall.properties
+            //     factory.createExpressionStatement(factory.createCallExpression(
+            //       factory.createPropertyAccessExpression(
+            //         factory.createCallExpression(
+            //           factory.createPropertyAccessExpression(
+            //             factory.createIdentifier(nockCall.varName),
+            //             factory.createIdentifier('get'),
+            //           ),
+            //           undefined,
+            //           [factory.createStringLiteral('/apps/myapp')], //path
+            //         ),
+            //         factory.createIdentifier('reply'), //
+            //       ),
+            //       undefined,
+            //       [
+            //         factory.createNumericLiteral('200'),
+            //         factory.createObjectLiteralExpression(
+            //           [factory.createPropertyAssignment(
+            //             factory.createIdentifier('maintenance'),
+            //             factory.createTrue(),
+            //           )],
+            //           false,
+            //         ),
+            //       ],
+            //     )),
+            //   ],
+            //   true,
+            // ),
+          ),
+        ],
+      )
+    }
+
+    // todo: anyway to find nock in beforeEach? example: packages/pg-v5/test/unit/commands/maintenance/run.unit.test.js
+    /* todo: handle separate definition then chaining. example: packages/pg-v5/test/unit/commands/backups/capture.unit.test.js
+    * look for nock, then decide if it's one expression or multiple?
+    *  */
+
+    return result
   }
 
   if (isTestDescribeCall(node)) {
@@ -63,7 +121,15 @@ export const transformDescribes = (node: ts.Node, nestedNockInBeforeEach: NockNa
       nestedNockInBeforeEach.push(nockInBeforeEach)
     }
 
-    return transformIts(node.arguments[1].body, nestedNockInBeforeEach)
+    const itVisitor = (iNode: ts.Node): ts.Node => {
+      if (isTestItCall(iNode)) {
+        return transformIts(iNode, nestedNockInBeforeEach)
+      }
+
+      return ts.visitEachChild(iNode, itVisitor, nullTransformationContext)
+    }
+
+    return ts.visitEachChild(node, itVisitor, nullTransformationContext)
   }
 
   return ts.visitEachChild(node, _node => transformDescribes(_node, nestedNockInBeforeEach), nullTransformationContext)
