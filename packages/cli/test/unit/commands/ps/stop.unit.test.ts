b65ef00f5b365b67a6496720052be1b459c589d9
Zane Whitfield <zwhitfield@salesforce.com>
2023-12-14 13:47:34 -0800

    Create command & test file

---
+++ packages/cli/test/unit/commands/ps/stop.unit.test.ts
@@ -0,0 +1,16 @@
+import {expect, test} from '@oclif/test'
+
+describe('ps:stop', () => {
+  test
+    .stdout()
+    .stderr()
+    .nock('https://api.heroku.com:443', api => api
+      .delete('/apps/myapp/dynos')
+      .reply(200),
+    )
+    .command(['ps:restart', '-a', 'myapp'])
+    .it('restarts all dynos', ({stdout, stderr}) => {
+      expect(stdout).to.be.empty
+      expect(stderr).to.equal('Restarting dynos on ⬢ myapp...\nRestarting dynos on ⬢ myapp... done\n')
+    })
+})
