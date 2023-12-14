e617c91bd93c948d23c079a32f50791b77015751
Zane Whitfield <zwhitfield@salesforce.com>
2023-12-14 15:54:00 -0800

    Successfully migrate tests

--- packages/cli/test/unit/commands/ps/stop.unit.test.ts
+++ packages/cli/test/unit/commands/ps/stop.unit.test.ts
@@ -5,12 +5,25 @@ describe('ps:stop', () => {
     .stdout()
     .stderr()
     .nock('https://api.heroku.com:443', api => api
-      .delete('/apps/myapp/dynos')
+      .post('/apps/myapp/dynos/web/actions/stop')
       .reply(200),
     )
-    .command(['ps:restart', '-a', 'myapp'])
-    .it('restarts all dynos', ({stdout, stderr}) => {
+    .command(['ps:stop', 'web', '-a', 'myapp'])
+    .it('stops all web dynos', ({stdout, stderr}) => {
       expect(stdout).to.be.empty
-      expect(stderr).to.equal('Restarting dynos on ⬢ myapp...\nRestarting dynos on ⬢ myapp... done\n')
+      expect(stderr).to.contains('Stopping web dynos on ⬢ myapp... done\n')
+    })
+
+  test
+    .stdout()
+    .stderr()
+    .nock('https://api.heroku.com:443', api => api
+      .post('/apps/myapp/dynos/run.10/actions/stop')
+      .reply(200),
+    )
+    .command(['ps:stop', 'run.10', '-a', 'myapp'])
+    .it('stops run.10 dyno', ({stdout, stderr}) => {
+      expect(stdout).to.be.empty
+      expect(stderr).to.contains('Stopping run.10 dyno on ⬢ myapp... done\n')
     })
 })
