c3d2b12863ea80b72a3deb52894401aec9c6720b
RyanDagg <rdagg@salesforce.com>
2024-01-04 14:06:46 -0700

    fix: typing for runCommand

--- packages/cli/test/helpers/runCommand.ts
+++ packages/cli/test/helpers/runCommand.ts
@@ -1,8 +1,8 @@
-import {getConfig} from './testInstances.js'
+import {getConfig} from './testInstances'
 import {Command} from '@heroku-cli/command'
 import {Config} from '@oclif/core'
 
-type CmdConstructor = new<T extends Command>(args: string[], config: Config) => T
+type CmdConstructor<T extends Command = Command> = new(args: string[], config: Config) => T
 
 const runCommand = (Cmd: CmdConstructor, args: string[]) => {
   const instance = new Cmd(args, getConfig())
