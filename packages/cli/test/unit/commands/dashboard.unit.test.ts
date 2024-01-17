d83f64655a795880c2ae4c03468f5667dc28da8c
RyanDagg <rdagg@salesforce.com>
2024-01-16 17:49:37 -0700

    ci failure test

--- packages/cli/test/unit/commands/dashboard.unit.test.ts
+++ packages/cli/test/unit/commands/dashboard.unit.test.ts
@@ -16,8 +16,6 @@ describe('dashboard', function () {
   beforeEach(() => {
     stdout.start()
     stderr.start()
-    stdout.print = true
-    stderr.print = true
   })
 
   afterEach(() => {
@@ -58,7 +56,7 @@ describe('dashboard', function () {
         .reply(200, [])
 
       return runCommand(Cmd, [])
-        .then(() => expect(stdout.output).to.equal('See all add-ons with heroku addons\nSee all apps with heroku apps --all\n\nSee other CLI commands with heroku help\n\n'))
+        .then(() => expect(stdout.output).to.equal('Seeasdf all add-ons with heroku addons\nSee all apps with heroku apps --all\n\nSee other CLI commands with heroku help\n\n'))
         .then(() => expect(unwrap(stderr.output)).to.contain('Loading... doneWarning: Add apps to this dashboard by favoriting them with heroku apps:favorites:add\n'))
         .then(() => longboard.done())
         .then(() => telex.done())
