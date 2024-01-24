9e25038430c3f4bcc034f409472b9751a76c1e69
RyanDagg <ryandagg@users.noreply.github.com>
2024-01-24 11:46:32 -0700

    refactor(addons-v5): move addons:docs to CLI & upgrade to oclif (#2605)
    * wip: working through tests. Found an issue with the codemod
    
    * tests working
    
    * wip
    
    * fix: delete OG files
    
    * wip on unit tests
    
    * fix: change command to have a required arg and allow variable args
    
    * fix: unit tests passing
    
    * chore: delete unused files that were migrated.
    
    * chore: clean up. Remove unused waitForAddonDeprovisioning
    
    * refactor: migrate to core & TS. wip
    
    * fix: unit tests passing
    
    * fix: tests passing
    
    * Delete unused files
    
    * add alias. cleanup
    
    * wip: converting resolve.unit.test. 'does not memoize errors' test is hanging and can't find a way to catch the error.
    
    * wip: attempt to change test to account for @heroku-cli/command's APIClient now retrying on two_factor 403
    
    * fix: test 500 instead of 403 that is handled by APIClient retry
    
    * chore: clean up.
    
    * wip: convert files w/codemod
    
    * fix: tests. mocking open
    
    * chore: stub open in entire test
    
    * remove unused files

---
+++ packages/cli/src/commands/addons/docs.ts
@@ -0,0 +1,37 @@
+import color from '@heroku-cli/color'
+import {Command, flags} from '@heroku-cli/command'
+import {Args, ux} from '@oclif/core'
+import * as Heroku from '@heroku-cli/schema'
+import {resolveAddon} from '../../lib/addons/resolve'
+import * as open from 'open'
+
+export default class Docs extends Command {
+    static topic = 'addons';
+    static description = "open an add-on's Dev Center documentation in your browser";
+    static flags = {
+      'show-url': flags.boolean({description: 'show URL, do not open browser'}),
+      app: flags.app(),
+    };
+
+    static args = {
+      addon: Args.string({required: true}),
+    };
+
+    public async run(): Promise<void> {
+      const {flags,  args} = await this.parse(Docs)
+      const {app} = flags
+      const id = args.addon.split(':')[0]
+      const addonResponse = await this.heroku.get<Heroku.AddOn>(`/addon-services/${encodeURIComponent(id)}`)
+        .catch(() => null)
+
+      const addon = addonResponse?.body ? addonResponse.body : (await resolveAddon(this.heroku, app, id)).addon_service
+
+      const url = `https://devcenter.heroku.com/articles/${addon.name}`
+      if (flags['show-url']) {
+        ux.log(url)
+      } else {
+        ux.log(`Opening ${color.cyan(url)}...`)
+        await open(url)
+      }
+    }
+}
---
+++ packages/cli/test/unit/commands/addons/docs.unit.test.ts
@@ -0,0 +1,67 @@
+import {stdout, stderr} from 'stdout-stderr'
+import runCommand from '../../../helpers/runCommand'
+import * as proxyquire from 'proxyquire'
+import * as nock from 'nock'
+import * as sinon from 'sinon'
+import {expect} from 'chai'
+
+const {default: Cmd} =  proxyquire(
+  '../../../../src/commands/addons/docs',
+  {open: sinon.stub()},
+)
+
+describe('addons:docs', function () {
+  it('opens an addon by name', async function () {
+    const api = nock('https://api.heroku.com:443')
+      .get('/addon-services/slowdb')
+      .reply(200, {name: 'slowdb'})
+
+    await runCommand(Cmd, ['--show-url', 'slowdb'])
+
+    expect(stdout.output).to.equal('https://devcenter.heroku.com/articles/slowdb\n')
+    // expect(stderr.output).to.equal('')
+    api.done()
+  })
+
+  it('opens an addon by name with no url flag passed', async function () {
+    const api = nock('https://api.heroku.com:443')
+      .get('/addon-services/slowdb')
+      .reply(200, {name: 'slowdb'})
+
+    await runCommand(Cmd, ['slowdb'])
+
+    expect(stdout.output).to.equal('Opening https://devcenter.heroku.com/articles/slowdb...\n')
+    api.done()
+  })
+  it('opens an addon by attachment name', async function () {
+    const api = nock('https://api.heroku.com:443')
+      .get('/addon-services/my-attachment-1111')
+      .reply(404)
+      .post('/actions/addons/resolve', {addon: 'my-attachment-1111', app: null})
+      .reply(200, [{addon_service: {name: 'slowdb'}}])
+
+    await runCommand(Cmd, ['--show-url', 'my-attachment-1111'])
+
+    expect(stdout.output).to.equal('https://devcenter.heroku.com/articles/slowdb\n')
+    expect(stderr.output).to.equal('')
+    api.done()
+  })
+  it('opens an addon by app/attachment name', async function () {
+    const api = nock('https://api.heroku.com:443')
+      .get('/addon-services/my-attachment-1111')
+      .reply(404)
+      .post('/actions/addons/resolve', {app: 'myapp', addon: 'my-attachment-1111'})
+      .reply(200, [{addon_service: {name: 'slowdb'}}])
+
+    await runCommand(Cmd, [
+      '--app',
+      'myapp',
+      '--show-url',
+      'my-attachment-1111',
+    ])
+
+    expect(stdout.output).to.equal('https://devcenter.heroku.com/articles/slowdb\n')
+    expect(stderr.output).to.equal('')
+    api.done()
+  })
+})
