23f8e2dae8ecef0b3945c67e84a5d93ab53ed9c0
Zane Whitfield <zwhitfield@salesforce.com>
2024-01-30 09:54:21 -0800

    feat(ps): add larger dyno information to ps:type and ps:scale (#2598)
    * Add additional dyno options
    
    * Add additional price logic for shield dynos
    
    * Update dyno names from feedback
    
    * Update tests
    
    * Update case in dyno naming
    
    * WIP create larger dyno feature flag
    
    * Add feature flag and update tests
    
    * Add additional tests for feature flag
    
    * Add feature flag to ps:scale and update tests
    
    * Add additional tests for ps:scale using feature flag
    
    * Update feature flag naming & if statements
    
    * WIP update ps:type to include available dyno sizes & update tests
    
    * Clean up code & tests
    
    * Add availableDynoSizes list & update tests
    
    * Update available dyno sizes & tests for ps:type & ps:scale
    
    * Remove unnecessary returns for clarity

--- packages/apps-v5/test/unit/commands/ps/type.unit.test.js
+++ packages/apps-v5/test/unit/commands/ps/type.unit.test.js
@@ -6,6 +6,14 @@ const cmd = commands.find(c => c.topic === 'ps' && c.command === 'type')
 const nock = require('nock')
 const expect = require('chai').expect
 
+// will remove this flag once we have
+// successfully launched larger dyno sizes
+function featureFlagPayload(isEnabled = false) {
+  return {
+    enabled: isEnabled,
+  }
+}
+
 function app(args = {}) {
   let base = {name: 'myapp'}
   return Object.assign(base, args)
@@ -19,6 +27,8 @@ describe('ps:type', function () {
 
   it('displays cost/hour and max cost/month for all individually-priced dyno sizes', function () {
     let api = nock('https://api.heroku.com')
+      .get('/account/features/frontend-larger-dynos')
+      .reply(200, featureFlagPayload(true))
       .get('/apps/myapp')
       .reply(200, app())
       .get('/apps/myapp/formation')
@@ -29,28 +39,37 @@ describe('ps:type', function () {
         {type: 'web', quantity: 1, size: 'Standard-2X'},
         {type: 'web', quantity: 1, size: 'Performance-M'},
         {type: 'web', quantity: 1, size: 'Performance-L'},
+        {type: 'web', quantity: 1, size: 'Performance-L-RAM'},
+        {type: 'web', quantity: 1, size: 'Performance-XL'},
+        {type: 'web', quantity: 1, size: 'Performance-2XL'},
       ])
 
     return cmd.run({app: 'myapp'})
       .then(() => {
         expect(cli.stdout).to.eq(`=== Dyno Types
-type  size           qty  cost/hour  max cost/month
-────  ─────────────  ───  ─────────  ──────────────
-web   Eco            1
-web   Basic          1    ~$0.010    $7
-web   Standard-1X    1    ~$0.035    $25
-web   Standard-2X    1    ~$0.069    $50
-web   Performance-M  1    ~$0.347    $250
-web   Performance-L  1    ~$0.694    $500
+type  size               qty  cost/hour  max cost/month
+────  ─────────────────  ───  ─────────  ──────────────
+web   Eco                1
+web   Basic              1    ~$0.010    $7
+web   Standard-1X        1    ~$0.035    $25
+web   Standard-2X        1    ~$0.069    $50
+web   Performance-M      1    ~$0.347    $250
+web   Performance-L      1    ~$0.694    $500
+web   Performance-L-RAM  1    ~$0.694    $500
+web   Performance-XL     1    ~$1.042    $750
+web   Performance-2XL    1    ~$2.083    $1500
 === Dyno Totals
-type           total
-─────────────  ─────
-Eco            1
-Basic          1
-Standard-1X    1
-Standard-2X    1
-Performance-M  1
-Performance-L  1
+type               total
+─────────────────  ─────
+Eco                1
+Basic              1
+Standard-1X        1
+Standard-2X        1
+Performance-M      1
+Performance-L      1
+Performance-L-RAM  1
+Performance-XL     1
+Performance-2XL    1
 
 $5 (flat monthly fee, shared across all Eco dynos)
 `)
@@ -58,8 +77,37 @@ $5 (flat monthly fee, shared across all Eco dynos)
       .then(() => api.done())
   })
 
+  it('switches to performance-l-ram dyno when feature flag is enabled', function () {
+    let api = nock('https://api.heroku.com')
+      .get('/account/features/frontend-larger-dynos')
+      .reply(200, featureFlagPayload(true))
+      .get('/apps/myapp')
+      .reply(200, app())
+      .get('/apps/myapp/formation')
+      .reply(200, [{type: 'web', quantity: 1, size: 'Eco'}])
+      .patch('/apps/myapp/formation', {updates: [{type: 'web', size: 'performance-l-ram'}]})
+      .reply(200, [{type: 'web', quantity: 1, size: 'Performance-L-RAM'}])
+      .get('/apps/myapp/formation')
+      .reply(200, [{type: 'web', quantity: 1, size: 'Performance-L-RAM'}])
+
+    return cmd.run({app: 'myapp', args: ['web=performance-l-ram']})
+      .then(() => expect(cli.stdout).to.eq(`=== Dyno Types
+type  size               qty  cost/hour  max cost/month
+────  ─────────────────  ───  ─────────  ──────────────
+web   Performance-L-RAM  1    ~$0.694    $500
+=== Dyno Totals
+type               total
+─────────────────  ─────
+Performance-L-RAM  1
+`))
+      .then(() => expect(cli.stderr).to.eq('Scaling dynos on myapp... done\n'))
+      .then(() => api.done())
+  })
+
   it('switches to hobby dynos', function () {
     let api = nock('https://api.heroku.com')
+      .get('/account/features/frontend-larger-dynos')
+      .reply(200, featureFlagPayload())
       .get('/apps/myapp')
       .reply(200, app())
       .get('/apps/myapp/formation')
@@ -86,6 +134,8 @@ Basic  3
 
   it('switches to standard-1x and standard-2x dynos', function () {
     let api = nock('https://api.heroku.com')
+      .get('/account/features/frontend-larger-dynos')
+      .reply(200, featureFlagPayload())
       .get('/apps/myapp')
       .reply(200, app())
       .get('/apps/myapp/formation')
@@ -113,6 +163,8 @@ Standard-2X  2
 
   it('displays Shield dynos for apps in shielded spaces', function () {
     let api = nock('https://api.heroku.com')
+      .get('/account/features/frontend-larger-dynos')
+      .reply(200, featureFlagPayload())
       .get('/apps/myapp')
       .reply(200, app({space: {shield: true}}))
       .get('/apps/myapp/formation')
@@ -132,4 +184,14 @@ Shield-L  0
 `))
       .then(() => api.done())
   })
+})
+
+it('errors when user requests larger dynos and feature flag is NOT enabled', function () {
+  let api = nock('https://api.heroku.com')
+    .get('/account/features/frontend-larger-dynos')
+    .reply(200, featureFlagPayload())
+
+  return cmd.run({app: 'myapp', args: ['web=performance-l-ram']})
+    .catch(error => expect(error.message).to.eq('No such size as performance-l-ram. Use eco, basic, standard-1x, standard-2x, performance-m, performance-l, private-s, private-m, private-l, shield-s, shield-m, shield-l.'))
+    .then(() => api.done())
 })
