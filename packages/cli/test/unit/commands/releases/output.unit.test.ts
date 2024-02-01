bcef266cdbc4f18c4b4be861132f52f29c880f30
RyanDagg <rdagg@salesforce.com>
2024-02-01 14:11:51 -0700

    wip

--- packages/cli/test/unit/commands/releases/output.unit.test.ts
+++ packages/cli/test/unit/commands/releases/output.unit.test.ts
@@ -7,23 +7,22 @@ import {unwrap} from '../../../helpers/utils/unwrap'
 const stdMocks = require('std-mocks')
 
 describe('releases:output', function () {
-  it('warns if there is no output available', function () {
+  it('warns if there is no output available', async function () {
     const api = nock('https://api.heroku.com:443')
       .get('/apps/myapp/releases/10')
       .reply(200, {version: 40})
-    runCommand(Cmd, [
+    await runCommand(Cmd, [
       '--app',
       'myapp',
       'v10',
     ])
-      .then(() => expect(stdout.output).to.equal(''))
-      .then(() => expect(unwrap(stderr.output)).to.contain('Release v40 has no release output available.\n'))
-      .then(() => api.done())
+
+    expect(stdout.output).to.equal('')
+    expect(unwrap(stderr.output)).to.contain('Release v40 has no release output available.\n')
+    api.done()
   })
 
   it('shows the output from a specific release', async function () {
-    stdMocks.use()
-
     const busl = nock('https://busl.test:443')
       .get('/streams/release.log')
       .reply(200, 'Release Output Content')
@@ -31,20 +30,17 @@ describe('releases:output', function () {
       .get('/apps/myapp/releases/10')
       .reply(200, {version: 40, output_stream_url: 'https://busl.test/streams/release.log'})
 
-    try {
-      await runCommand(Cmd, [
-        '--app',
-        'myapp',
-        'v10',
-      ])
-      busl.done()
-      api.done()
-      expect(stdMocks.flush().stdout.join('')).to.equal('Release Output Content')
-      expect(stderr.output).to.equal('')
-      stdMocks.restore()
-    } catch {
-      stdMocks.restore()
-    }
+    await runCommand(Cmd, [
+      '--app',
+      'myapp',
+      'v10',
+    ])
+
+    busl.done()
+    api.done()
+    expect(stdout.output).to.equal('Release Output Content')
+    expect(stderr.output).to.equal('')
+    stdMocks.restore()
   })
 
   it('shows the output from the latest release', async function () {
@@ -57,20 +53,16 @@ describe('releases:output', function () {
       .get('/apps/myapp/releases')
       .reply(200, [{version: 40, output_stream_url: 'https://busl.test/streams/release.log'}])
 
-    try {
-      await runCommand(Cmd, [
-        '--app',
-        'myapp',
-      ])
+    await runCommand(Cmd, [
+      '--app',
+      'myapp',
+    ])
 
-      expect(stdMocks.flush().stdout.join('')).to.equal('Release Output Content')
-      expect(stderr.output).to.equal('')
-      busl.done()
-      api.done()
-      stdMocks.restore()
-    } catch {
-      stdMocks.restore()
-    }
+    busl.done()
+    api.done()
+    expect(stdMocks.flush().stdout.join('')).to.equal('Release Output Content')
+    expect(stderr.output).to.equal('')
+    stdMocks.restore()
   })
 
   it('has a missing output', async function () {
@@ -86,9 +78,9 @@ describe('releases:output', function () {
       'myapp',
     ])
 
-    expect(stdout.output).to.equal('')
-    expect(stderr.output).to.contain('Release command not started yet. Please try again in a few seconds.\n')
     api.done()
     busl.done()
+    expect(stdout.output).to.equal('')
+    expect(stderr.output).to.contain('Release command not started yet. Please try again in a few seconds.\n')
   })
 })
