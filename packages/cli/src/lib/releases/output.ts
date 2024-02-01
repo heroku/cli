36d8cfbbd62c08897ae6c2c9af15431b9a3ad0cc
RyanDagg <rdagg@salesforce.com>
2024-02-01 15:19:28 -0700

    chore: better comment

--- packages/cli/src/lib/releases/output.ts
+++ packages/cli/src/lib/releases/output.ts
@@ -2,7 +2,7 @@ const cli = require('heroku-cli-util')
 
 export const stream = function (url: string) {
   return new Promise(function (resolve, reject) {
-    // todo: remove this heroku-cli-util usage. The time to do so appears significant.
+    // todo: remove this heroku-cli-util usage. Replacing with require('got).stream works for commands but fails in tests
     const stream = cli.got.stream(url)
     stream.on('error', reject)
     stream.on('end', resolve)
