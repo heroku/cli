df3314d13c2baca8067f8d6a2863367cde7377cc
RyanDagg <rdagg@salesforce.com>
2024-02-02 12:18:19 -0700

    chore: comment explaining this is an issue with nock v10

--- packages/cli/src/lib/releases/output.ts
+++ packages/cli/src/lib/releases/output.ts
@@ -2,7 +2,7 @@ const cli = require('heroku-cli-util')
 
 export const stream = function (url: string) {
   return new Promise(function (resolve, reject) {
-    // todo: remove this heroku-cli-util usage. Replacing with require('got).stream works for commands but fails in tests
+    // todo: remove this heroku-cli-util usage after upgrading to nock 11+. Replacing with require('got).stream works for commands but fails in tests
     const stream = cli.got.stream(url)
     stream.on('error', reject)
     stream.on('end', resolve)
