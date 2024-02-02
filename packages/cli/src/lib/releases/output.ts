2d0d635c96145759c3f2ad47b00065933e4aeba8
RyanDagg <rdagg@salesforce.com>
2024-02-02 13:37:51 -0700

    fix: remove heroku-cli-util usages after nock upgrade

--- packages/cli/src/lib/releases/output.ts
+++ packages/cli/src/lib/releases/output.ts
@@ -1,9 +1,8 @@
-const cli = require('heroku-cli-util')
+import got = require('got')
 
 export const stream = function (url: string) {
   return new Promise(function (resolve, reject) {
-    // todo: remove this heroku-cli-util usage after upgrading to nock 11+. Replacing with require('got).stream works for commands but fails in tests
-    const stream = cli.got.stream(url)
+    const stream = got.stream(url)
     stream.on('error', reject)
     stream.on('end', resolve)
     const piped = stream.pipe(process.stdout)
