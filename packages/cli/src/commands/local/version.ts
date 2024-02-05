98f8507211e3bddf90eacf0c6bcc7dafcb686959
Katy Bowman <katy.bowman@salesforce.com>
2024-02-05 11:35:34 -0500

    fix: fix version import of run-foreman and unit tests

--- packages/cli/src/commands/local/version.ts
+++ packages/cli/src/commands/local/version.ts
@@ -1,6 +1,6 @@
 import {Command} from '@oclif/core'
 
-import {nf} from '../../lib/local/run-foreman'
+import {fork as foreman} from '../../lib/local/fork-foreman'
 
 export default class Version extends Command {
   static description = 'display node-foreman version'
@@ -9,6 +9,6 @@ export default class Version extends Command {
     await this.parse(Version)
 
     const execArgv = ['--version']
-    await nf(execArgv)
+    await foreman(execArgv)
   }
 }
