18bde87a8df1399df81d19bfa634b8ba59583db7
Katy Bowman <katy.bowman@salesforce.com>
2024-02-07 08:48:20 -0500

    refactor: initial refactor for releases:info command

---
+++ packages/cli/src/commands/releases/info.ts
@@ -0,0 +1,52 @@
+import color from '@heroku-cli/color'
+import {Command, flags} from '@heroku-cli/command'
+import {Args, ux} from '@oclif/core'
+const shellescape = require('shell-escape')
+const {forEach} = require('lodash')
+import * as Heroku from '@heroku-cli/schema'
+
+import {findByLatestOrId} from '../../lib/releases/releases'
+import {description, color as getStatusColor} from '../../lib/releases/status_helper'
+
+export default class Info extends Command {
+    static topic = 'releases';
+    static description = 'view detailed information for a release';
+    static flags = {
+      json: flags.boolean({description: 'output in json format'}),
+      shell: flags.boolean({char: 's', description: 'output in shell format'}),
+      app: flags.app({required: true}),
+    };
+
+    static args = {
+      release: Args.string(),
+    };
+
+    public async run(): Promise<void> {
+      const {flags, args} = await this.parse(Info)
+      const {json, shell, app} = flags
+      const release = await findByLatestOrId(this.heroku, app, args.release)
+      const config = await this.heroku.get(`/apps/${app}/releases/${release.version}/config-vars`)
+      if (json) {
+        ux.styledJSON(release)
+      } else {
+        let releaseChange = release.description
+        const status = description(release)
+        const statusColor = getStatusColor(release.status)
+        if (status !== undefined) {
+          releaseChange += ' (' + color[statusColor](status) + ')'
+        }
+
+        ux.styledHeader(`Release ${color.cyan('v' + release.version)}`)
+        ux.styledObject({
+          'Add-ons': release.addon_plan_names, Change: releaseChange, By: release.user.email, When: release.created_at,
+        })
+        ux.log()
+        ux.styledHeader(`${color.cyan('v' + release.version)} Config vars`)
+        if (shell) {
+          forEach(config, (v, k) => ux.log(`${k}=${shellescape([v])}`))
+        } else {
+          ux.styledObject(config)
+        }
+      }
+    }
+}
