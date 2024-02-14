1eb160a93e95f324612333617da86cb760f24cd7
Eric Black <eblack@salesforce.com>
2024-02-14 10:27:56 -0800

    Fix test for plans

--- packages/cli/test/unit/commands/addons/plans.unit.test.ts
+++ packages/cli/test/unit/commands/addons/plans.unit.test.ts
@@ -13,18 +13,17 @@ describe('addons:plans', function () {
       fixtures.plans['heroku-postgresql:premium-3'],
       fixtures.plans['heroku-postgresql:private-4'],
     ]
-    nock('https://api.heroku.com', {reqheaders: {'Accept-Expansion': 'addon_service,plan'}})
+    nock('https://api.heroku.com')
       .get('/addon-services/daservice/plans')
       .reply(200, plans)
   })
   it('shows add-on plans', async function () {
     await runCommand(Cmd, ['daservice'])
-    expectOutput(stdout.output, `
-         slug                           name         price          max price
-───────  ─────────────────────────────  ──────────   ─────────────  ──────────
-default  heroku-postgresql:mini         Mini         ~$0.007/hour   $5/month
-         heroku-postgresql:standard-2   Standard 2   ~$0.278/hour   $200/month
-         heroku-postgresql:premium-3    Premium 3    ~$1.042/hour   $750/month
-         heroku-postgresql:private-4    Private 4    ~$2.083/hour   $1500/month`)
+    expectOutput(stdout.output, `sslug                         name       price        max price
+ ─────── ──────────────────────────── ────────── ──────────── ───────────
+ default heroku-postgresql:mini       Mini       ~$0.007/hour $5/month
+         heroku-postgresql:standard-2 Standard 2 ~$0.278/hour $200/month
+         heroku-postgresql:premium-3  Premium 3  ~$1.042/hour $750/month
+         heroku-postgresql:private-4  Private 4  ~$2.083/hour $1500/month`)
   })
 })
