42bb9b13b927bd688c5c59d0669c73d563304188
Santiago Bosio <santiago.bosio@gmail.com>
2024-02-19 12:40:52 -0300

    Move command 'container' (index) to CLI and OClif

---
+++ packages/cli/test/unit/commands/container/index.unit.test.ts
@@ -0,0 +1,13 @@
+import {stdout} from 'stdout-stderr'
+import Cmd  from '../../../../src/commands/container/index'
+import runCommand from '../../../helpers/runCommand'
+import {expect} from 'chai'
+const {version} = require('../../../../package.json')
+
+describe('container', () => {
+  it('shows package version', async function () {
+    await runCommand(Cmd)
+
+    expect(stdout.output).to.equal(`${version}\n`)
+  })
+})
