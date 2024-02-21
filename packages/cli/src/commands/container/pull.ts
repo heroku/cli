53706ecf31be80a284661f02e330edb2a3ac81d8
Eric Black <eblack@salesforce.com>
2024-02-21 11:17:05 -0800

    Update help info

--- packages/cli/src/commands/container/pull.ts
+++ packages/cli/src/commands/container/pull.ts
@@ -7,10 +7,11 @@ import color from '@heroku-cli/color'
 export default class Pull extends Command {
   static topic = 'container'
   static description = 'pulls an image from an app\'s process type'
-  static usage = `
-  ${color.cmd('heroku container:pull web')}        # Pulls the web image from the app
-  ${color.cmd('heroku container:pull web worker')} # Pulls both the web and worker images from the app
-  ${color.cmd('heroku container:pull web:latest')} # Pulls the latest tag from the web image`
+  static usage = '$ heroku container:pull -a APP [-v] PROCESS_TYPE...'
+  static example = `
+  ${color.cmd('$ heroku container:pull web')}        # Pulls the web image from the app
+  ${color.cmd('$ heroku container:pull web worker')} # Pulls both the web and worker images from the app
+  ${color.cmd('$ heroku container:pull web:latest')} # Pulls the latest tag from the web image`
 
   static strict = false
 
@@ -24,7 +25,7 @@ export default class Pull extends Command {
     const {verbose, app} = flags
 
     if (argv.length === 0) {
-      this.error(`Error: Requires one or more process types\n ${Pull.usage}`)
+      this.error(`Error: Requires one or more process types\n${Pull.example}`)
     }
 
     const herokuHost = process.env.HEROKU_HOST || 'heroku.com'
