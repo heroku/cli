08548f806a31b84ffa021940d5249877df527420
Zane Whitfield <zwhitfield@salesforce.com>
2024-02-21 12:18:32 -0800

    WIP discovery

--- packages/cli/src/hooks/prerun/analytics.ts
+++ packages/cli/src/hooks/prerun/analytics.ts
@@ -1,11 +1,24 @@
-import {Hook} from '@oclif/core'
+import {Hook, ux} from '@oclif/core'
 
 import Analytics from '../../analytics'
 import * as telemetry from '../../global_telemetry'
+import * as Heroku from '@heroku-cli/schema'
+import {APIClient} from '@heroku-cli/command'
+import {Config} from '@oclif/core'
+const path = require('path')
 
 declare const global: telemetry.TelemetryGlobal
 
 const analytics: Hook<'prerun'> = async function (options) {
+  ux.log('WE ARE HERE')
+
+  const root = path.resolve(__dirname, '../package.json')
+  const config = new Config({root})
+  const heroku = new APIClient(config)
+
+  const {body: accountInfo} = await heroku.get<Heroku.Account>('/account')
+
+  console.log('accountInfo', accountInfo)
   global.cliTelemetry = telemetry.setupTelemetry(this.config, options)
   const analytics = new Analytics(this.config)
   await analytics.record(options)
