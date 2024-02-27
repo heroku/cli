6b88ea1d2189cc642bab06cb36ff8404f237868f
Katy Bowman <katy.bowman@salesforce.com>
2024-02-27 11:02:43 -0500

    refactor: typescript and eslint fixes

--- packages/cli/src/commands/container/release.ts
+++ packages/cli/src/commands/container/release.ts
@@ -1,9 +1,20 @@
 import color from '@heroku-cli/color'
 import {Command, flags} from '@heroku-cli/command'
-import {Args, ux} from '@oclif/core'
+import {ux} from '@oclif/core'
 import * as Heroku from '@heroku-cli/schema'
 import {debug} from '../../lib/container/debug'
-import streamer from '../../lib/container/streamer'
+import {streamer} from '../../lib/container/streamer'
+
+type ImageResponse = {
+  schemaVersion: number,
+  history: [{
+    v1Compatibility: string,
+    }
+  ],
+  config: {
+    digest: string
+  }
+}
 
 const usage = `\n    ${color.bold.underline.magenta('Usage:')}\n    ${color.cyan.bold('heroku container:release web')}                       # Releases the previously pushed web process type\n    ${color.cyan.bold('heroku container:release web worker')}                # Releases the previously pushed web and worker process types`
 
@@ -15,31 +26,37 @@ export default class Release extends Command {
     verbose: flags.boolean({char: 'v'}),
   }
 
-  async run(): Promise<void> {
+  async run() {
     const {flags, argv, args} = await this.parse(Release)
     const {app, verbose} = flags
-    if (verbose)
+
+    if (argv.length === 0) {
+      this.error(`Error: Requires one or more process types\n ${usage}`)
+    }
+
+    if (verbose) {
       debug.enabled = true
-    if (args.length === 0) {
-      ux.error(`Error: Requires one or more process types\n ${usage}`, {exit: 1})
-      return
     }
 
     await this.heroku.get(`/apps/${app}`)
-    const herokuHost = process.env.HEROKU_HOST || 'heroku.com'
-    const updateData = []
-    for (const process of args) {
+
+    const herokuHost: string = process.env.HEROKU_HOST || 'heroku.com'
+    const updateData: any[] = []
+    for (const process of argv) {
       const image = `${app}/${process}`
       const tag = 'latest'
-      const imageResp = await this.heroku.request({
-        host: `registry.${herokuHost}`, path: `/v2/${image}/manifests/${tag}`, headers: {
+      const {body: imageResp} = await this.heroku.request<ImageResponse>(
+        `/v2/${image}/manifests/${tag}`,
+        {headers: {
           Accept: 'application/vnd.docker.distribution.manifest.v2+json',
-        },
-      })
+          host: `registry.${herokuHost}`,
+        }},
+      )
       let imageID
+      let v1Comp
       switch (imageResp.schemaVersion) {
       case 1:
-        const v1Comp = JSON.parse(imageResp.history[0].v1Compatibility)
+        v1Comp = JSON.parse(imageResp.history[0].v1Compatibility)
         imageID = v1Comp.id
         break
       case 2:
@@ -52,20 +69,24 @@ export default class Release extends Command {
       })
     }
 
-    const req = this.heroku.patch(`/apps/${app}/formation`, {
+    ux.action.start(`Releasing images ${argv.join(',')} to ${app}`)
+    await this.heroku.patch(`/apps/${app}/formation`, {
       body: {updates: updateData}, headers: {
         Accept: 'application/vnd.heroku+json; version=3.docker-releases',
       },
     })
-    const oldRelease = await this.heroku.request({
-      path: `/apps/${app}/releases`, partial: true, headers: {Range: 'version ..; max=2, order=desc'},
+    ux.action.stop()
+
+    const {body: oldReleases} = await this.heroku.get<Heroku.Release[]>(`/apps/${app}/releases`, {
+      partial: true, headers: {Range: 'version ..; max=2, order=desc'},
     })
-      .then(releases => releases[0])
-    await ux.action(`Releasing images ${args.join(',')} to ${app}`, req)
-    const release = await this.heroku.request({
-      path: `/apps/${app}/releases`, partial: true, headers: {Range: 'version ..; max=2, order=desc'},
+    const oldRelease = oldReleases[0]
+
+    const {body: updatedReleases} = await this.heroku.get<Heroku.Release[]>(`/apps/${app}/releases`, {
+      partial: true, headers: {Range: 'version ..; max=1, order=desc'},
     })
-      .then(releases => releases[0])
+    const release = updatedReleases[0]
+
     if ((!oldRelease && !release) || (oldRelease && (oldRelease.id === release.id))) {
       return
     }
@@ -75,9 +96,7 @@ export default class Release extends Command {
     } else if ((release.status === 'pending') && release.output_stream_url) {
       ux.log('Running release command...')
       await streamer(release.output_stream_url, process.stdout)
-      const finishedRelease = await this.heroku.request({
-        path: `/apps/${app}/releases/${release.id}`,
-      })
+      const {body: finishedRelease} = await this.heroku.request<Heroku.Release>(`/apps/${app}/releases/${release.id}`)
       if (finishedRelease.status === 'failed') {
         ux.error('Error: release command failed', {exit: 1})
       }
