8a5b05c98c32fbf232cd0cc33b98ca942bc8e169
Katy Bowman <katy.bowman@salesforce.com>
2024-02-29 10:46:16 -0500

    refactor: migrate members:index tests and utils function fix

--- packages/cli/src/lib/members/utils.ts
+++ packages/cli/src/lib/members/utils.ts
@@ -5,7 +5,6 @@ import {ParserOutput} from '@oclif/core/lib/interfaces/parser'
 import Create from '../../commands/apps/create'
 
 export const getTeamInfo = async function (team: string | undefined, heroku: APIClient) {
-  const teamName = team || ''
-  if (!teamName) exit(1, 'No team or org specified.\nRun this command with --team')
-  return heroku.get<Heroku.Team>(`/teams/${teamName}`)
+  if (!team) exit(1, 'No team or org specified.\nRun this command with --team')
+  return heroku.get<Heroku.Team>(`/teams/${team}`)
 }
---
+++ packages/cli/test/unit/commands/members/index.unit.test.ts
@@ -0,0 +1,120 @@
+import {stdout, stderr} from 'stdout-stderr'
+import {expect} from 'chai'
+import * as nock from 'nock'
+import Cmd  from '../../../../src/commands/members'
+import runCommand from '../../../helpers/runCommand'
+import {
+  teamInfo,
+  teamInvites,
+  teamFeatures,
+  teamMembers,
+} from '../../../helpers/stubs/get'
+
+describe('heroku members', () => {
+  afterEach(() => nock.cleanAll())
+  let apiGetOrgMembers: nock.Scope
+  const adminTeamMember = {email: 'admin@heroku.com', role: 'admin', user: {email: 'admin@heroku.com'}}
+  const collaboratorTeamMember = {email: 'collab@heroku.com', role: 'collaborator', user: {email: 'collab@heroku.com'}}
+  const memberTeamMember = {email: 'member@heroku.com', role: 'member', user: {email: 'member@heroku.com'}}
+  context('when it is an Enterprise team', () => {
+    beforeEach(() => {
+      teamInfo('enterprise')
+    })
+    it('shows there are not team members if it is an orphan team', () => {
+      apiGetOrgMembers = teamMembers([])
+      return runCommand(Cmd, [
+        '--team',
+        'myteam',
+      ])
+        .then(() => expect('No members in myteam\n').to.eq(stdout.output))
+        .then(() => expect('').to.eq(stderr.output))
+        .then(() => apiGetOrgMembers.done())
+    })
+    it('shows all the team members', () => {
+      apiGetOrgMembers = teamMembers([adminTeamMember, collaboratorTeamMember])
+      return runCommand(Cmd, [
+        '--team',
+        'myteam',
+      ])
+        .then(() => expect(stdout.output).to.contain('admin@heroku.com  admin               \n collab@heroku.com collaborator'))
+        .then(() => expect('').to.eq(stderr.output))
+        .then(() => apiGetOrgMembers.done())
+    })
+    it('filters members by role', () => {
+      apiGetOrgMembers = teamMembers([adminTeamMember, memberTeamMember])
+      return runCommand(Cmd, [
+        '--team',
+        'myteam',
+        '--role',
+        'member',
+      ])
+        .then(() => expect(stdout.output).to.contain('member@heroku.com member'))
+        .then(() => expect('').to.eq(stderr.output))
+        .then(() => apiGetOrgMembers.done())
+    })
+    it("shows the right message when filter doesn't return results", () => {
+      apiGetOrgMembers = teamMembers([adminTeamMember, memberTeamMember])
+      return runCommand(Cmd, [
+        '--team',
+        'myteam',
+        '--role',
+        'collaborator',
+      ])
+        .then(() => expect('No members in myteam with role collaborator\n').to.eq(stdout.output))
+        .then(() => expect('').to.eq(stderr.output))
+        .then(() => apiGetOrgMembers.done())
+    })
+    it('filters members by role', () => {
+      apiGetOrgMembers = teamMembers([adminTeamMember, memberTeamMember])
+      return runCommand(Cmd, [
+        '--team',
+        'myteam',
+        '--role',
+        'member',
+      ])
+        .then(() => expect(stdout.output).to.contain('member@heroku.com member'))
+        .then(() => expect('').to.eq(stderr.output))
+        .then(() => apiGetOrgMembers.done())
+    })
+  })
+  context('when it is a team', () => {
+    beforeEach(() => {
+      teamInfo('team')
+    })
+    context('without the feature flag team-invite-acceptance', () => {
+      beforeEach(() => {
+        teamFeatures([])
+      })
+    })
+    context('with the feature flag team-invite-acceptance', () => {
+      beforeEach(() => {
+        teamFeatures([{name: 'team-invite-acceptance', enabled: true}])
+      })
+      it('shows all members including those with pending invites', () => {
+        const apiGetTeamInvites = teamInvites()
+        apiGetOrgMembers = teamMembers([adminTeamMember, collaboratorTeamMember])
+        return runCommand(Cmd, [
+          '--team',
+          'myteam',
+        ])
+          .then(() => expect(stdout.output).to.contain('admin@heroku.com      admin                \n collab@heroku.com     collaborator         \n invited-user@mail.com admin        pending'))
+          .then(() => expect('').to.eq(stderr.output))
+          .then(() => apiGetTeamInvites.done())
+          .then(() => apiGetOrgMembers.done())
+      })
+      it('filters members by pending invites', () => {
+        const apiGetTeamInvites = teamInvites()
+        apiGetOrgMembers = teamMembers([adminTeamMember, collaboratorTeamMember])
+        return runCommand(Cmd, [
+          '--team',
+          'myteam',
+          '--pending',
+        ])
+          .then(() => expect(stdout.output).to.contain('invited-user@mail.com admin pending'))
+          .then(() => expect('').to.eq(stderr.output))
+          .then(() => apiGetTeamInvites.done())
+          .then(() => apiGetOrgMembers.done())
+      })
+    })
+  })
+})
