c062c590956afe38c60ea57982ccc28b51b2c5b5
Jon Daniel <binarycleric@users.noreply.github.com>
2024-03-01 17:52:53 -0500

    feat(pg:upgrade): support essential dbs (#2637)
    * Proof-of-concept on changes to support essential pg:upgrade
    
    This code is still a work-in-progress but I am just trying to get an
    idea of what changes would be required in the CLI for us to support
    pg:upgrade on the essential tier.
    
    * WIP update tests
    
    * WIP update tests waiting on feedback
    
    Waiting for feedback before finalizaing updated tests
    
    * Update tests
    
    * Update copy
    
    * Update smoke tests
    
    * WIP fix smoke tests
    
    * Update api call from PR feedback
    
    * Add comment for custom stripAnsi function
    
    ---------
    
    Co-authored-by: Zane Whitfield <zwhitfield@salesforce.com>

--- packages/cli/test/acceptance/smoke.acceptance.test.ts
+++ packages/cli/test/acceptance/smoke.acceptance.test.ts
@@ -7,6 +7,15 @@ import * as qq from 'qqjs'
 
 import commandsOutput from './commands-output'
 
+// this is a custom function that strips both ansi characters and several additional characters
+const stripAnsi = (input: string) => {
+  // eslint-disable-next-line no-control-regex, unicorn/escape-case
+  const ansiRegex = /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]|\s|─/g
+  const cleanedString = input.replace(ansiRegex, '')
+
+  return cleanedString
+}
+
 const globby = require('globby')
 
 const app = 'heroku-cli-ci-smoke-test-app'
@@ -193,7 +202,7 @@ describe('@acceptance smoke tests', () => {
     // this test will fail when run locally depending on which plugins you have installed
     it('heroku commands', async () => {
       const {stdout} = await run('commands')
-      expect(stdout).to.equal(commandsOutput)
+      expect(stripAnsi(stdout)).to.equal(stripAnsi(commandsOutput))
     })
 
     it('asserts monorepo plugins are in core', async () => {
