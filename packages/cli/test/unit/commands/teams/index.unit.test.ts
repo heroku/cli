b7e6c536c386b7a19b4151a09f0b5c0bb8eaff89
Eric Black <eblack@salesforce.com>
2024-03-01 10:46:16 -0800

    Use heredoc to make indentation nicer

--- packages/cli/test/unit/commands/teams/index.unit.test.ts
+++ packages/cli/test/unit/commands/teams/index.unit.test.ts
@@ -4,6 +4,7 @@ import runCommand from '../../../helpers/runCommand'
 import {teams} from '../../../helpers/stubs/get'
 import * as nock from 'nock'
 import expectOutput from '../../../helpers/utils/expectOutput'
+import heredoc from 'tsheredoc'
 
 describe('heroku teams', () => {
   afterEach(() => nock.cleanAll())
@@ -11,14 +12,14 @@ describe('heroku teams', () => {
   it('shows the teams you are a member of', async () => {
     teams()
     await runCommand(Cmd, [])
-    expectOutput(stdout.output, `
-Team         Role
- ──────────── ────────────
- enterprise a collaborator
- enterprise b admin
- team a       collaborator
- team b       admin
-`)
+    expectOutput(heredoc(stdout.output), heredoc(`
+    Team         Role
+    ──────────── ────────────
+    enterprise a collaborator
+    enterprise b admin
+    team a       collaborator
+    team b       admin
+    `))
     expectOutput(stderr.output, '')
   })
 })
