d975edf8f4e845b0285cdb1359e1881b9f6308fe
Katy Bowman <katy.bowman@salesforce.com>
2024-03-02 20:37:07 -0500

    refactor: add opening message and move open action to separate function

--- packages/cli/src/commands/orgs/open.ts
+++ packages/cli/src/commands/orgs/open.ts
@@ -1,20 +1,25 @@
 import {Command, flags} from '@heroku-cli/command'
 import * as Heroku from '@heroku-cli/schema'
 import * as open from 'open'
+import {ux} from '@oclif/core'
+import color from '@heroku-cli/color'
 
 export default class Open extends Command {
     static topic = 'orgs';
     static description = 'open the team interface in a browser window';
     static flags = {
-      team: flags.team(),
+      team: flags.team({required: true}),
     };
+
+    public static async openUrl(url: string): Promise<void> {
+      ux.log(`Opening ${color.cyan(url)}...`)
+      await open(url)
+    }
 
     public async run(): Promise<void> {
       const {flags, argv, args} = await this.parse(Open)
       const team = flags.team
-      if (!team)
-        throw new Error('No team specified')
       const {body: org} = await this.heroku.get<Heroku.Team>(`/teams/${team}`)
-      await open(`https://dashboard.heroku.com/teams/${org.name}`)
+      await Open.openUrl(`https://dashboard.heroku.com/teams/${org.name}`)
     }
 }
