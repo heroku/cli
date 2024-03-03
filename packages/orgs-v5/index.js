a1db54daf62cbf52b7e6f1aef0c60e1a1f457905
Katy Bowman <katy.bowman@salesforce.com>
2024-03-03 12:03:41 -0500

    refactor: remove orgs-v5 access:remove files

--- packages/orgs-v5/index.js
+++ packages/orgs-v5/index.js
@@ -16,7 +16,6 @@ exports.topics = [
 
 exports.commands = flatten([
   require('./commands/access/add'),
-  require('./commands/access/remove'),
   require('./commands/apps/join'),
   require('./commands/apps/leave'),
   require('./commands/apps/lock'),
