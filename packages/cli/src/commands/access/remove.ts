257f3e3f77bdff2942b1e84d4a2a09bcba13c1ac
Katy Bowman <katy.bowman@salesforce.com>
2024-03-03 11:59:09 -0500

    refactor: test fix and update class name

--- packages/cli/src/commands/access/remove.ts
+++ packages/cli/src/commands/access/remove.ts
@@ -3,7 +3,7 @@ import {Command, flags} from '@heroku-cli/command'
 import {ux} from '@oclif/core'
 import * as Heroku from '@heroku-cli/schema'
 
-export default class Remove extends Command {
+export default class AccessRemove extends Command {
   static description = 'remove users from a team app'
   static example = '$ heroku access:remove user@email.com --app APP'
   static topic = 'access'
@@ -15,7 +15,7 @@ export default class Remove extends Command {
   static strict = false
 
   public async run(): Promise<void> {
-    const {flags, argv} = await this.parse(Remove)
+    const {flags, argv} = await this.parse(AccessRemove)
     const {app} = flags
     const email = argv[0] as string
     const appName = app
