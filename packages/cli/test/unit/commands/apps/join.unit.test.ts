d4909d74c28a40735d453200186a876775832966
Katy Bowman <katy.bowman@salesforce.com>
2024-03-06 14:33:18 -0500

    refactor: migrate stubs/post and update apps:join tests

--- packages/cli/test/unit/commands/apps/join.unit.test.ts
+++ packages/cli/test/unit/commands/apps/join.unit.test.ts
@@ -1,34 +1,34 @@
 import {stdout, stderr} from 'stdout-stderr'
+import * as nock from 'nock'
+import {expect} from 'chai'
 import Cmd  from '../../../../src/commands/apps/join'
 import runCommand from '../../../helpers/runCommand'
 import {userAccount} from '../../../helpers/stubs/get'
+import {teamAppCollaborators} from '../../../helpers/stubs/post'
 
-
-let stubPost = require('../../stub/post')
 describe('heroku apps:join', () => {
-  let apiGetUserAccount
-  let apiPostCollaborators
+  let apiGetUserAccount: nock.Scope
+  let apiPostCollaborators: nock.Scope
   beforeEach(() => {
-    cli.mockConsole()
     apiGetUserAccount = userAccount('raulb@heroku.com')
   })
   afterEach(() => nock.cleanAll())
   it('joins the app', () => {
-    apiPostCollaborators = stubPost.teamAppCollaborators('raulb@heroku.com')
+    apiPostCollaborators = teamAppCollaborators('raulb@heroku.com')
     return runCommand(Cmd, [
       '--app',
       'myapp',
     ])
       .then(() => expect('').to.eq(stdout.output))
-      .then(() => expect('Joining myapp... done\n').to.eq(stderr.output))
+      .then(() => expect('Joining myapp...\nJoining myapp... done\n').to.eq(stderr.output))
       .then(() => apiGetUserAccount.done())
       .then(() => apiPostCollaborators.done())
   })
   it('is forbidden from joining the app', () => {
-    let response = {
+    const response = {
       code: 403, description: {id: 'forbidden', error: 'You do not have access to the team heroku-tools.'},
     }
-    apiPostCollaborators = stubPost.teamAppCollaborators('raulb@heroku.com', [], response)
+    apiPostCollaborators = teamAppCollaborators('raulb@heroku.com', [], response)
     let thrown = false
     return runCommand(Cmd, [
       '--app',
