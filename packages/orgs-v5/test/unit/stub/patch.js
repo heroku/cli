bcd449b7b18fefc5d14ac2761b3ddb9801bd670e
Justin Wilaby <jwilaby@salesforce.com>
2024-03-08 11:57:35 -0800

    migrate(W-14162583): Migrate orgs:apps:leave

--- packages/orgs-v5/test/unit/stub/patch.js
+++ packages/orgs-v5/test/unit/stub/patch.js
@@ -2,13 +2,6 @@
 
 const nock = require('nock')
 
-function appCollaboratorWithPermissions(args) {
-  return nock('https://api.heroku.com:443')
-    .patch(`/teams/apps/myapp/collaborators/${args.email}`, {
-      permissions: args.permissions,
-    }).reply(200)
-}
-
 function teamAppTransfer() {
   return nock('https://api.heroku.com:443')
     .patch('/teams/apps/myapp', {owner: 'team'})
@@ -21,15 +14,7 @@ function personalToPersonal() {
     .reply(200, {name: 'myapp', owner: {email: 'raulb@heroku.com'}})
 }
 
-function updateMemberRole(email = 'raulb@heroku.com', role = 'admin') {
-  return nock('https://api.heroku.com:443')
-    .patch('/teams/myteam/members', {email, role})
-    .reply(200)
-}
-
 module.exports = {
-  appCollaboratorWithPermissions,
   teamAppTransfer,
   personalToPersonal,
-  updateMemberRole,
 }
