fc84e2e3e580cf044b635eacfb329dc43359be84
Katy Bowman <katy.bowman@salesforce.com>
2024-03-11 17:22:12 -0400

    refactor: add lock alias

--- packages/cli/src/commands/apps/lock.ts
+++ packages/cli/src/commands/apps/lock.ts
@@ -6,6 +6,7 @@ import * as Heroku from '@heroku-cli/schema'
 export default class AppsLock extends Command {
     static topic = 'apps'
     static description = 'prevent team members from joining an app';
+    static aliases = ['lock']
     static flags = {
       app: flags.app({required: true}),
       remote: flags.remote(),
