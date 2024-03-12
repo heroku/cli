1f1b6b137ca8c31f798910cf9fa690c56a6a647a
Katy Bowman <katy.bowman@salesforce.com>
2024-03-12 09:58:52 -0400

    refactor: replace param in test helper function

--- packages/cli/test/helpers/stubs/post.ts
+++ packages/cli/test/helpers/stubs/post.ts
@@ -7,7 +7,7 @@ export function collaborators() {
     }).reply(200)
 }
 
-export function teamAppCollaborators(email = 'raulb@heroku.com', permissions: string[] = []) {
+export function teamAppCollaborators(email = 'raulb@heroku.com', permissions: string[] = [], response: {code?: number, description?: Record<string, unknown>} = {}) {
   const body: {user: string, permissions?: string[]} = {user: email}
   if (permissions.length > 0) {
     body.permissions = permissions
@@ -16,7 +16,7 @@ export function teamAppCollaborators(email = 'raulb@heroku.com', permissions: st
   return nock('https://api.heroku.com:443', {
     reqheaders: {Accept: 'application/vnd.heroku+json; version=3'},
   })
-    .post('/teams/apps/myapp/collaborators', body).reply(200)
+    .post('/teams/apps/myapp/collaborators', body).reply(response.code || 200, response.description)
 }
 
 export function personalToPersonal() {
