2a9a0c7b11972c8bb80660f3df2216ca518b7bda
Katy Bowman <katy.bowman@salesforce.com>
2024-03-14 16:46:08 -0400

    fix: add space to fix linting error

--- packages/cli/test/unit/commands/apps/unlock.unit.test.ts
+++ packages/cli/test/unit/commands/apps/unlock.unit.test.ts
@@ -20,7 +20,7 @@ describe('heroku apps:unlock', () => {
     expect('Unlocking myapp...\nUnlocking myapp... done\n').to.eq(stderr.output)
     api.done()
   })
-  it('returns an error if the app is already unlocked', async() => {
+  it('returns an error if the app is already unlocked', async () => {
     const api = nock('https://api.heroku.com:443')
       .get('/teams/apps/myapp')
       .reply(200, {name: 'myapp', locked: false})
