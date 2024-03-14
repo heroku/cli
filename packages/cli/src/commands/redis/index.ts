1da77e1bfefdb260ca7f58218e3faa48aabbb148
Eric Black <eblack@salesforce.com>
2024-03-14 10:20:59 -0700

    Go back to duplicating code

--- packages/cli/src/commands/redis/index.ts
+++ packages/cli/src/commands/redis/index.ts
@@ -2,7 +2,7 @@ import {Command, flags} from '@heroku-cli/command'
 import {Args} from '@oclif/core'
 import redisApi from '../../lib/redis/info'
 
-export default class Info extends Command {
+export default class Index extends Command {
   static topic = 'redis'
   static description = 'gets information about redis'
   static flags = {
@@ -16,7 +16,7 @@ export default class Info extends Command {
   }
 
   public async run(): Promise<void> {
-    const {flags, args} = await this.parse(Info)
+    const {flags, args} = await this.parse(Index)
     const {app, json} = flags
     const {database} = args
     return redisApi(app, database, json, this.heroku).info()
