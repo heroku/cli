607ed085f89c2a7eccae78edf1c7e9ce7dd664f0
Eric Black <eblack@salesforce.com>
2024-03-15 15:41:19 -0700

    Convert redis upgrade to oclif

---
+++ packages/cli/src/commands/redis/upgrade.ts
@@ -0,0 +1,43 @@
+import {Command, flags} from '@heroku-cli/command'
+import {Args, ux} from '@oclif/core'
+import redisApi from '../../lib/redis/api'
+import confirmApp from '../../lib/apps/confirm-app'
+import heredoc from 'tsheredoc'
+import color from '@heroku-cli/color'
+
+type RedisUpgradeResponse = {
+  message: string
+}
+
+export default class Upgrade extends Command {
+  static topic = 'redis'
+  static description = 'perform in-place version upgrade'
+  static flags = {
+    app: flags.app({required: true}),
+    remote: flags.remote(),
+    version: flags.string({char: 'v', required: true}),
+    confirm: flags.string({char: 'c'}),
+  }
+
+  static args = {
+    database: Args.string(),
+  }
+
+  public async run(): Promise<void> {
+    const {flags, args} = await this.parse(Upgrade)
+    const {app, version, confirm} = flags
+    const {database} = args
+    const addon = await redisApi(app, database, false, this.heroku).getRedisAddon()
+    const warning = heredoc(`
+      WARNING: Irreversible action.
+      Redis database will be upgraded to ${color.configVar(version)}. This cannot be undone.
+    `)
+    await confirmApp(app, confirm, warning)
+    ux.action.start(`Requesting upgrade of ${color.addon(addon.name || '')} to ${version}`)
+    // const {body: response} = await this.heroku.post<RedisUpgradeResponse>(`/redis/v0/databases/${addon.name}/upgrade`, {body: {version: version}})
+    const {body: response} = await redisApi(app, database, false, this.heroku)
+      .request<RedisUpgradeResponse>(`/redis/v0/databases/${addon.name}/upgrade`, 'POST', {version: version})
+    console.log('hi:', response.message)
+    ux.action.stop(response.message)
+  }
+}
---
+++ packages/cli/test/unit/commands/upgrade.unit.test.ts
@@ -0,0 +1,52 @@
+import {stdout, stderr} from 'stdout-stderr'
+import Cmd from '../../../src/commands/redis/upgrade'
+import runCommand from '../../helpers/runCommand'
+import * as nock from 'nock'
+import {expect} from 'chai'
+import expectOutput from '../../helpers/utils/expectOutput'
+import heredoc from 'tsheredoc'
+import stripAnsi = require('strip-ansi')
+
+describe('heroku redis:upgrade', () => {
+  beforeEach(() => {
+    nock.cleanAll()
+  })
+  it('# upgrades the redis version', async () => {
+    nock('https://api.heroku.com:443')
+      .get('/apps/example/addons')
+      .reply(200, [
+        {name: 'redis-haiku', addon_service: {name: 'heroku-redis'}, config_vars: ['REDIS_URL']},
+      ])
+    nock('https://api.data.heroku.com:443')
+      .post('/redis/v0/databases/redis-haiku/upgrade', {version: '6.2'})
+      .reply(200, {
+        message: 'Upgrading version now!',
+      })
+    await runCommand(Cmd, [
+      '--app',
+      'example',
+      '--confirm',
+      'example',
+      '--version',
+      '6.2',
+    ])
+    expectOutput(stderr.output, 'Requesting upgrade of redis-haiku to 6.2... Upgrading version now!')
+  })
+
+  it('# errors on missing version', async function () {
+    await runCommand(Cmd, [
+      '--app',
+      'example',
+    ])
+      .catch((error: Error) => {
+        expect(stdout.output).to.equal('')
+        expect(stripAnsi(error.message)).to.equal(heredoc(`
+          The following error occurred:
+            Missing required flag version
+          See more help with --help`))
+      })
+    // .to.be.rejectedWith(exit.ErrorExit)
+      // .then(() => expect(stdout.output).to.equal(''))
+      // .then(() => expect(unwrap(stderr.output)).to.equal('Please specify a valid version.\n'))
+  })
+})
