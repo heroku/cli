f44ee1b6d1ebe48645ed877c7e4e6ade09634d01
Justin Wilaby <jwilaby@gmail.com>
2024-03-18 15:18:05 -0700

    chore(W-15263489): improve unit test performance (#2716)

--- packages/cli/src/hooks/prerun/analytics.ts
+++ packages/cli/src/hooks/prerun/analytics.ts
@@ -6,6 +6,10 @@ import * as telemetry from '../../global_telemetry'
 declare const global: telemetry.TelemetryGlobal
 
 const analytics: Hook<'prerun'> = async function (options) {
+  if (process.env.IS_HEROKU_TEST_ENV === 'true') {
+    return
+  }
+
   global.cliTelemetry = telemetry.setupTelemetry(this.config, options)
   const analytics = new Analytics(this.config)
   await analytics.record(options)
