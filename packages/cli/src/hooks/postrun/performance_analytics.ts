f8df90d2521be5af85d316bfe4bfc671427de585
Justin Wilaby <jwilaby@gmail.com>
2024-03-19 06:51:02 -0700

    chore(W-15263489): improve unit test performance (#2716) (#2721)

--- packages/cli/src/hooks/postrun/performance_analytics.ts
+++ packages/cli/src/hooks/postrun/performance_analytics.ts
@@ -5,11 +5,13 @@ import * as telemetry from '../../global_telemetry'
 declare const global: telemetry.TelemetryGlobal
 
 const performance_analytics: Hook<'postrun'> = async function () {
-  if (global.cliTelemetry) {
-    const cmdStartTime = global.cliTelemetry.commandRunDuration
-    global.cliTelemetry.commandRunDuration = telemetry.computeDuration(cmdStartTime)
-    global.cliTelemetry.lifecycleHookCompletion.postrun = true
+  if (process.env.IS_HEROKU_TEST_ENV === 'true' || !global.cliTelemetry) {
+    return
   }
+
+  const cmdStartTime = global.cliTelemetry.commandRunDuration
+  global.cliTelemetry.commandRunDuration = telemetry.computeDuration(cmdStartTime)
+  global.cliTelemetry.lifecycleHookCompletion.postrun = true
 }
 
 export default performance_analytics
--- packages/cli/test/helpers/init.js
+++ packages/cli/test/helpers/init.js
@@ -1,5 +1,15 @@
 const path = require('path')
+
+globalThis.setInterval = () => ({unref: () => {}})
+const tm = globalThis.setTimeout
+globalThis.setTimeout = cb => {
+  return tm(cb)
+}
+
 process.env.TS_NODE_PROJECT = path.resolve('test/tsconfig.json')
+
+// Env var used to prevent some expensive
+// prerun and postrun hooks from initializing
 process.env.IS_HEROKU_TEST_ENV = 'true'
 
 let nock = require('nock')
