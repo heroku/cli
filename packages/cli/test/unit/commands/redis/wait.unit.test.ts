bb29d60f59a977f10a1d63303f5387db77ffad1e
Santiago Bosio <santiago.bosio@gmail.com>
2024-03-25 14:35:20 -0300

    refactor(redis-v5): Move `redis:wait` command to CLI (#2728)
    * Migrated 'wait' command
    
    * Migrated unit tests for command 'redis:wait'
    
    * Removing old command files

---
+++ packages/cli/test/unit/commands/redis/wait.unit.test.ts
@@ -0,0 +1,62 @@
+import {stdout, stderr} from 'stdout-stderr'
+import Cmd  from '../../../../src/commands/redis/wait'
+import runCommand from '../../../helpers/runCommand'
+import {expect} from 'chai'
+import * as nock from 'nock'
+import {shouldHandleArgs} from '../../lib/redis/shared.unit.test'
+
+describe('heroku redis:credentials', function () {
+  shouldHandleArgs(Cmd)
+})
+
+describe('heroku redis:wait ', () => {
+  afterEach(() => {
+    nock.cleanAll()
+  })
+
+  it('# returns when waiting? is false', async () => {
+    const api = nock('https://api.heroku.com')
+      .get('/apps/example/addons')
+      .reply(200, [
+        {name: 'redis-haiku', addon_service: {name: 'heroku-redis'}, config_vars: ['REDIS_URL']},
+      ])
+    const redis = nock('https://api.data.heroku.com')
+      .get('/redis/v0/databases/redis-haiku/wait')
+      .reply(200, {'waiting?': false})
+
+    await runCommand(Cmd, [
+      '--app',
+      'example',
+    ])
+
+    api.done()
+    redis.done()
+
+    expect(stdout.output).to.equal('')
+    expect(stderr.output).to.equal('')
+  })
+
+  it('# waits for version upgrade', async () => {
+    const api = nock('https://api.heroku.com')
+      .get('/apps/example/addons')
+      .reply(200, [
+        {name: 'redis-haiku', addon_service: {name: 'heroku-redis'}, config_vars: ['REDIS_URL']},
+      ])
+    const redis = nock('https://api.data.heroku.com')
+      .get('/redis/v0/databases/redis-haiku/wait')
+      .reply(200, {'waiting?': true, message: 'upgrading version'})
+      .get('/redis/v0/databases/redis-haiku/wait')
+      .reply(200, {'waiting?': false, message: 'available'})
+
+    await runCommand(Cmd, [
+      '--app',
+      'example',
+    ])
+
+    api.done()
+    redis.done()
+
+    expect(stdout.output).to.equal('')
+    expect(stderr.output).to.equal('Waiting for database redis-haiku... upgrading version\nWaiting for database redis-haiku... available\n')
+  })
+})
