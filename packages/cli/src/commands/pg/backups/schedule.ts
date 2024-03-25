3a5f5a7c5307afec25aee4f26d301cc0e9114edd
Eric Black <eblack@salesforce.com>
2024-03-25 12:08:35 -0700

    Convert pg backup schedule to oclif

---
+++ packages/cli/src/commands/pg/backups/schedule.ts
@@ -0,0 +1,105 @@
+import color from '@heroku-cli/color'
+import {Command, flags} from '@heroku-cli/command'
+import {Args, ux} from '@oclif/core'
+import pgHost from '../../../lib/pg/host'
+import {getAttachment} from '../../../lib/pg/fetcher'
+
+type Timezone = {
+  [key: string]: string;
+  PST: string;
+  PDT: string;
+  MST: string;
+  MDT: string;
+  CST: string;
+  CDT: string;
+  EST: string;
+  EDT: string;
+  Z: string;
+  GMT: string;
+  BST: string;
+  CET: string;
+  CEST: string;
+}
+
+const TZ:Timezone = {
+  PST: 'America/Los_Angeles',
+  PDT: 'America/Los_Angeles',
+  MST: 'America/Boise',
+  MDT: 'America/Boise',
+  CST: 'America/Chicago',
+  CDT: 'America/Chicago',
+  EST: 'America/New_York',
+  EDT: 'America/New_York',
+  Z: 'UTC',
+  GMT: 'Europe/London',
+  BST: 'Europe/London',
+  CET: 'Europe/Paris',
+  CEST: 'Europe/Paris',
+}
+
+type PostgresDatabase = {
+  id: string;
+  name: string;
+  info: {name: string; values: string[]}[];
+}
+
+type BackupSchedule = {
+  hour: string;
+  timezone: string;
+  schedule_name?: string;
+}
+
+export default class Schedule extends Command {
+  static topic = 'pg';
+  static description = 'schedule daily backups for given database';
+  static flags = {
+    at: flags.string({required: true, description: "at a specific (24h) hour in the given timezone. Defaults to UTC. --at '[HOUR]:00 [TIMEZONE]'"}),
+    app: flags.app({required: true}),
+  };
+
+  // static TZ:Timezone = {
+  //   PST: 'America/Los_Angeles', PDT: 'America/Los_Angeles', MST: 'America/Boise', MDT: 'America/Boise', CST: 'America/Chicago', CDT: 'America/Chicago', EST: 'America/New_York', EDT: 'America/New_York', Z: 'UTC', GMT: 'Europe/London', BST: 'Europe/London', CET: 'Europe/Paris', CEST: 'Europe/Paris',
+  // };
+
+  static args = {
+    database: Args.string(),
+  };
+
+  parseDate = function (at: string): BackupSchedule {
+    const m = at.match(/^([0-2]?[0-9]):00 ?(\S*)$/)
+    if (!m)
+      throw new Error("Invalid schedule format: expected --at '[HOUR]:00 [TIMEZONE]'")
+    const [, hour, timezone] = m
+    return {hour, timezone: TZ[timezone.toUpperCase()] || timezone || 'UTC'}
+  };
+
+  public async run(): Promise<void> {
+    const {flags, args} = await this.parse(Schedule)
+    const {app} = flags
+    const {database} = args
+    const schedule = this.parseDate(flags.at)
+    const attachment = await getAttachment(this.heroku, app, database)
+    const db = attachment.addon
+    const at = color.cyan(`${schedule.hour}:00 ${schedule.timezone}`)
+    const {body: dbInfo} = await this.heroku.get<PostgresDatabase>(`/client/v11/databases/${db.id}`, {hostname: pgHost()})
+      .catch(error => {
+        if (error.statusCode !== 404)
+          throw error
+        ux.error(`${color.yellow(db.name)} is not yet provisioned.\nRun ${color.cyan.bold('heroku addons:wait')} to wait until the db is provisioned.`, {exit: 1})
+      })
+    if (dbInfo) {
+      const dbProtected = /On/.test(dbInfo.info.find(attribute => attribute.name === 'Continuous Protection')?.values[0] || '')
+      if (dbProtected) {
+        ux.warn('Continuous protection is already enabled for this database. Logical backups of large databases are likely to fail.')
+        ux.warn('See https://devcenter.heroku.com/articles/heroku-postgres-data-safety-and-continuous-protection#physical-backups-on-heroku-postgres.')
+      }
+    }
+
+    ux.action.start(`Scheduling automatic daily backups of ${color.yellow(db.name)} at ${at}`)
+    schedule.schedule_name = attachment.name + '_URL'
+    await this.heroku.post(`/client/v11/databases/${db.id}/transfer-schedules`, {
+      body: schedule, hostname: pgHost(),
+    })
+    ux.action.stop()
+  }
+}
--- packages/cli/src/lib/pg/fetcher.ts
+++ packages/cli/src/lib/pg/fetcher.ts
@@ -1,6 +1,9 @@
 import {APIClient} from '@heroku-cli/command'
 import * as Heroku from '@heroku-cli/schema'
+import {appAttachment} from '../../lib/addons/resolve'
 import debug from 'debug'
+import {color} from '@heroku-cli/color'
+import {getConfigVarName} from './util'
 
 const pgDebug = debug('pg')
 
@@ -8,6 +11,21 @@ export type TransferSchedule = {
   hour: number,
   name: string,
   timezone: string,
+}
+
+type AddOnAttachment = {
+  addon: {
+    id: string;
+    name: string;
+    plan: {
+      name: string;
+    };
+  };
+  app: {
+    id: string;
+    name: string;
+  };
+  config_vars: string[];
 }
 
 export async function arbitraryAppDB(heroku: APIClient, app: string) {
@@ -20,4 +38,85 @@ export async function arbitraryAppDB(heroku: APIClient, app: string) {
   const addon = addons.find(a => a.app?.name === app && a.plan?.name?.startsWith('heroku-postgresql'))
   if (!addon) throw new Error(`No heroku-postgresql databases on ${app}`)
   return addon
+}
+
+async function allAttachments(heroku: APIClient, app: string) {
+  const {body: attachments} = await heroku.get<AddOnAttachment[]>(`/apps/${app}/addon-attachments`, {
+    headers: {'Accept-Inclusion': 'addon:plan,config_vars'},
+  })
+  return attachments.filter((a: AddOnAttachment) => a.addon?.plan.name.startsWith('heroku-postgresql'))
+}
+
+export async function getAttachment(heroku: APIClient, app: string, passedDb?: string, namespace?: string) {
+  let db = passedDb || 'DATABASE_URL'
+
+  function matchesHelper(app: string, db: string) {
+    pgDebug(`fetching ${db} on ${app}`)
+
+    const addonService = process.env.HEROKU_POSTGRESQL_ADDON_NAME || 'heroku-postgresql'
+    pgDebug(`addon service: ${addonService}`)
+    return appAttachment(heroku, app, db, {addon_service: addonService, namespace: namespace})
+      .then(attached => ({matches: [attached]}))
+      .catch(function (error) {
+        if (error.statusCode === 422 && error.body && error.body.id === 'multiple_matches' && error.matches) {
+          return {matches: error.matches, err: error}
+        }
+
+        if (error.statusCode === 404 && error.body && error.body.id === 'not_found') {
+          return {matches: null, err: error}
+        }
+
+        throw error
+      })
+  }
+
+  let {matches} = await matchesHelper(app, db)
+
+  // happy path where the resolver matches just one
+  if (matches && matches.length === 1) {
+    return matches[0]
+  }
+
+  // case for 404 where there are implicit attachments
+  if (!matches) {
+    // eslint-disable-next-line prefer-regex-literals
+    const appConfigMatch = new RegExp('^(.+?)::(.+)').exec(db)
+    if (appConfigMatch) {
+      app = appConfigMatch[1]
+      db = appConfigMatch[2]
+    }
+
+    if (!db.endsWith('_URL')) {
+      db += '_URL'
+    }
+
+    const [{body: config}, attachments] = await Promise.all([
+      heroku.get<Heroku.ConfigVars>(`/apps/${app}/config-vars`),
+      // getConfig(heroku, app),
+      allAttachments(heroku, app),
+    ])
+
+    if (attachments.length === 0) {
+      throw new Error(`${color.app(app)} has no databases`)
+    }
+
+    matches = attachments.filter(attachment => config[db] && config[db] === config[getConfigVarName(attachment.config_vars)])
+
+    if (matches.length === 0) {
+      const validOptions = attachments.map(attachment => getConfigVarName(attachment.config_vars))
+      throw new Error(`Unknown database: ${passedDb}. Valid options are: ${validOptions.join(', ')}`)
+    }
+  }
+
+  // case for multiple attachments with passedDb
+  const first = matches[0]
+
+  // case for 422 where there are ambiguous attachments that are equivalent
+  if (matches.every((match: AddOnAttachment) => first.addon.id === match.addon.id && first.app.id === match.app.id)) {
+    const {body: config} = await heroku.get<Heroku.ConfigVars>(`/apps/${app}/config-vars`)
+
+    if (matches.every((match: AddOnAttachment)  => config[getConfigVarName(first.config_vars)] === config[getConfigVarName(match.config_vars)])) {
+      return first
+    }
+  }
 }
