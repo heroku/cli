0efc20824a353b226855225ff15e01e35015b500
Justin Wilaby <jwilaby@salesforce.com>
2024-03-26 07:18:48 -0700

    migrate(W-14170470): Upgrade pg:credentials:create

--- packages/cli/src/lib/pg/util.ts
+++ packages/cli/src/lib/pg/util.ts
@@ -1,5 +1,14 @@
-export function getConfigVarName(configVars: string[]) {
-  const connstringVars = configVars.filter(cv => (cv.endsWith('_URL')))
-  if (connstringVars.length === 0) throw new Error('Database URL not found for this addon')
-  return connstringVars[0]
+import type {AddOnAttachmentWithConfigVarsAndPlan} from './types'
+
+export function getConfigVarName(configVars: string[]): string {
+  const connStringVars = configVars.filter(cv => (cv.endsWith('_URL')))
+  if (connStringVars.length === 0) throw new Error('Database URL not found for this addon')
+  return connStringVars[0]
+}
+
+export const essentialNumPlan = (addon: AddOnAttachmentWithConfigVarsAndPlan) => Boolean(addon?.plan?.name?.split(':')[1].match(/^essential/))
+export const legacyEssentialPlan = (addon: AddOnAttachmentWithConfigVarsAndPlan) => Boolean(addon?.plan?.name?.split(':')[1].match(/(dev|basic|mini)$/))
+
+export function essentialPlan(addon:AddOnAttachmentWithConfigVarsAndPlan) {
+  return essentialNumPlan(addon) || legacyEssentialPlan(addon)
 }
---
+++ packages/cli/test/unit/commands/pg/credentials/create.unit.test.ts
@@ -0,0 +1,81 @@
+import {stdout, stderr} from 'stdout-stderr'
+import runCommand from '../../../../helpers/runCommand'
+import {expect} from 'chai'
+import nock = require('nock')
+import Cmd from '../../../../../src/commands/pg/credentials/create'
+
+describe('pg:credentials:create', () => {
+  let api: nock.Scope
+  let pg: nock.Scope
+  beforeEach(() => {
+    api = nock('https://api.heroku.com')
+    pg = nock('https://api.data.heroku.com')
+  })
+
+  afterEach(() => {
+    nock.cleanAll()
+    api.done()
+  })
+
+  it('creates the credential', async () => {
+    const addon = {
+      name: 'postgres-1', plan: {name: 'heroku-postgresql:standard-0'},
+    }
+    api.post('/actions/addon-attachments/resolve', {
+      app: 'myapp',
+      addon_attachment: 'DATABASE_URL',
+      addon_service: 'heroku-postgresql',
+    }).reply(200, [{addon}])
+
+    pg.post('/postgres/v0/databases/postgres-1/credentials')
+      .reply(200)
+
+    await runCommand(Cmd, [
+      '--app',
+      'myapp',
+      '--name',
+      'credname',
+    ])
+    expect(stdout.output).to.equal('\nPlease attach the credential to the apps you want to use it in by running heroku addons:attach postgres-1 --credential credname -a myapp.\nPlease define the new grants for the credential within Postgres: heroku pg:psql postgres-1 -a myapp.\n')
+    return expect(stderr.output).to.equal('Creating credential credname...\nCreating credential credname... done\n')
+  })
+
+  it('throws an error when the db is numbered essential plan', () => {
+    const essentialAddon = {
+      name: 'postgres-1', plan: {name: 'heroku-postgresql:essential-0'},
+    }
+
+    api.post('/actions/addon-attachments/resolve', {
+      app: 'myapp',
+      addon_attachment: 'DATABASE_URL',
+      addon_service: 'heroku-postgresql',
+    }).reply(200, [{addon: essentialAddon}])
+
+    const err = "You can't create a custom credential on Essential-tier databases."
+    return expect(runCommand(Cmd, [
+      '--app',
+      'myapp',
+      '--name',
+      'jeff',
+    ])).to.be.rejectedWith(Error, err)
+  })
+
+  it('throws an error when the db is essential plan', () => {
+    const hobbyAddon = {
+      name: 'postgres-1', plan: {name: 'heroku-postgresql:mini'},
+    }
+    api.post('/actions/addon-attachments/resolve', {
+      app: 'myapp',
+      addon_attachment: 'DATABASE_URL',
+      addon_service: 'heroku-postgresql',
+    }).reply(200, [{addon: hobbyAddon}])
+
+    const err = "You can't create a custom credential on Essential-tier databases."
+    return expect(runCommand(Cmd, [
+      '--app',
+      'myapp',
+      '--name',
+      'jeff',
+    ])).to.be.rejectedWith(Error, err)
+  })
+})
