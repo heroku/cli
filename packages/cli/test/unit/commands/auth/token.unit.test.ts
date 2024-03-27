0069d5404ecf18fb71aac996da1deb54ea990296
Zane Whitfield <zwhitfield@salesforce.com>
2024-03-27 11:19:19 -0700

    Add & update tests

--- packages/cli/test/unit/commands/auth/token.unit.test.ts
+++ packages/cli/test/unit/commands/auth/token.unit.test.ts
@@ -7,7 +7,7 @@ describe('auth:token', () => {
       .get('/oauth/authorizations')
       .reply(200, [
         {access_token: {token: 'somethingelse'}},
-        {access_token: {token: 'foobar', expires_in: 60}},
+        {access_token: {token: 'foobar', expires_in: 60}, user: {email: 'foo@bar.com'}},
         {},
       ]),
     )
@@ -17,5 +17,43 @@ describe('auth:token', () => {
     .it('shows auth token', ctx => {
       expect(ctx.stdout).to.equal('foobar\n')
       expect(ctx.stderr).to.contain('Warning: token will expire today')
+    })
+
+  test
+    .env({HEROKU_API_KEY: 'foobar'})
+    .nock('https://api.heroku.com', api => api
+      .get('/oauth/authorizations')
+      .reply(200, [
+        {access_token: {token: 'somethingelse'}},
+        {access_token: {token: 'foobar', expires_in: 60}, user: {email: 'foo@bar.com'}},
+        {},
+      ]),
+    )
+    .stdout()
+    .stderr()
+    .command(['auth:token'])
+    .it('shows "long-term" token generation warning for non-internal users', ctx => {
+      expect(ctx.stdout).to.equal('foobar\n')
+      expect(ctx.stderr).to.contain('Use heroku authorizations:create to generate a long-term token')
+      expect(ctx.stderr).to.not.contain('All tokens will expire one year after last generation')
+    })
+
+  test
+    .env({HEROKU_API_KEY: 'foobar'})
+    .nock('https://api.heroku.com', api => api
+      .get('/oauth/authorizations')
+      .reply(200, [
+        {access_token: {token: 'somethingelse'}},
+        {access_token: {token: 'foobar', expires_in: 60}, user: {email: 'foo@heroku.com'}},
+        {},
+      ]),
+    )
+    .stdout()
+    .stderr()
+    .command(['auth:token'])
+    .it('shows AT2 token generation warning for internal users', ctx => {
+      expect(ctx.stdout).to.equal('foobar\n')
+      expect(ctx.stderr).to.contain('All tokens will expire one year after last generation')
+      expect(ctx.stderr).to.not.contain('Use heroku authorizations:create to generate a long-term token')
     })
 })
