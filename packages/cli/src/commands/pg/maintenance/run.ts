20505c08d721f63336a57417c65e202e2d68a94a
Eric Black <eblack@salesforce.com>
2024-03-29 16:43:37 -0700

    Convert pg:maintenance:run to oclif

---
+++ packages/cli/src/commands/pg/maintenance/run.ts
@@ -0,0 +1,39 @@
+import color from '@heroku-cli/color'
+import {Command, flags} from '@heroku-cli/command'
+import {Args, ux} from '@oclif/core'
+import {getAddon} from '../../../lib/pg/fetcher'
+import {essentialPlan} from '../../../lib/pg/util'
+import pgHost from '../../../lib/pg/host'
+import {MaintenanceApiResponse} from '../../../lib/pg/types'
+import * as Heroku from '@heroku-cli/schema'
+
+export default class Run extends Command {
+  static topic = 'pg';
+  static description = 'start maintenance';
+  static flags = {
+    force: flags.boolean({char: 'f'}),
+    app: flags.app({required: true}),
+  };
+
+  static args = {
+    database: Args.string(),
+  };
+
+  public async run(): Promise<void> {
+    const {flags, args} = await this.parse(Run)
+    const {app, force} = flags
+    const {database} = args
+    const db = await getAddon(this.heroku, app, database)
+    if (essentialPlan(db))
+      throw new Error("pg:maintenance isn't available for Essential-tier databases.")
+    ux.action.start(`Starting maintenance for ${color.yellow(db.name)}`)
+    if (!force) {
+      const {body: appInfo} = await this.heroku.get<Heroku.App>(`/apps/${app}`)
+      if (!appInfo.maintenance)
+        throw new Error('Application must be in maintenance mode or run with --force')
+    }
+
+    const {body: response} = await this.heroku.post<MaintenanceApiResponse>(`/client/v11/databases/${db.id}/maintenance`, {hostname: pgHost()})
+    ux.action.stop(response.message || 'done')
+  }
+}
