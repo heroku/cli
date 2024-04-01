318bd258bd4dc14c3032bb6898dc6b02728aea23
Eric <eblack@salesforce.com>
2024-04-01 11:26:25 -0700

    refactor(pg-v5): Move command pg:maintenance:run to oclif (#2760)
    * Convert pg:maintenance:run to oclif
    
    * Use ux.error
    
    * Update packages/cli/src/commands/pg/maintenance/run.ts
    
    Co-authored-by: Santiago Bosio <santiago.bosio@gmail.com>
    
    ---------
    
    Co-authored-by: Santiago Bosio <santiago.bosio@gmail.com>

---
+++ packages/cli/test/unit/commands/pg/maintenance/run.unit.test.ts
@@ -0,0 +1,36 @@
+import {stdout, stderr} from 'stdout-stderr'
+import Cmd from '../../../../../src/commands/pg/maintenance/run'
+import runCommand from '../../../../helpers/runCommand'
+import * as nock from 'nock'
+import expectOutput from '../../../../helpers/utils/expectOutput'
+import * as fixtures from '../../../../fixtures/addons/fixtures'
+import heredoc from 'tsheredoc'
+
+describe('pg:maintenance:run', () => {
+  const addon = fixtures.addons['dwh-db']
+  afterEach(() => {
+    nock.cleanAll()
+  })
+
+  it('runs maintenance', async () => {
+    nock('https://api.heroku.com')
+      .post('/actions/addon-attachments/resolve')
+      .reply(200, [{addon}])
+    nock('https://api.heroku.com')
+      .get('/apps/myapp')
+      .reply(200, {maintenance: true})
+    nock('https://api.data.heroku.com')
+      .post(`/client/v11/databases/${addon.id}/maintenance`)
+      .reply(200, {message: 'foo'})
+
+    await runCommand(Cmd, [
+      '--app',
+      'myapp',
+    ])
+    expectOutput(stderr.output, heredoc(`
+      Starting maintenance for ${addon.name}...
+      Starting maintenance for ${addon.name}... foo
+    `))
+    expectOutput(stdout.output, '')
+  })
+})
