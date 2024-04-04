05297da50e6eed1442d9be0742269ae2b35437dc
Eric Black <eblack@salesforce.com>
2024-04-04 14:34:38 -0700

    Update log-lock-waits command to extend pg settings command

--- packages/cli/src/commands/pg/settings/log-lock-waits.ts
+++ packages/cli/src/commands/pg/settings/log-lock-waits.ts
@@ -1,12 +1,8 @@
-import {Command, flags} from '@heroku-cli/command'
-import {Args, ux} from '@oclif/core'
-import {getAddon} from '../../../lib/pg/fetcher'
-import {essentialPlan} from '../../../lib/pg/util'
-import pgHost from '../../../lib/pg/host'
-import {PgDatabaseConfig} from '../../../lib/pg/types'
+import {flags} from '@heroku-cli/command'
+import {Args} from '@oclif/core'
 import heredoc from 'tsheredoc'
-
-export default class LogLockWaits extends Command {
+import {PGSettingsCommand, type Setting} from '../../../lib/pg/setter'
+export default class LogLockWaits extends PGSettingsCommand {
   static topic = 'pg'
   static description = heredoc(`
     Controls whether a log message is produced when a session waits longer than the deadlock_timeout to acquire a lock. deadlock_timeout is set to 1 second
@@ -20,41 +16,21 @@ export default class LogLockWaits extends Command {
   }
 
   static args = {
-    value: Args.boolean(),
     database: Args.string(),
+    value: Args.boolean(),
   }
 
-  name = 'log_lock_waits'
+  protected settingsName = 'log_lock_waits'
 
-  explain(setting: PgDatabaseConfig) {
+  protected convertValue(val: boolean): boolean {
+    return val
+  }
+
+  protected explain(setting: Setting) {
     if (setting.value) {
       return "When a deadlock is detected, a log message will be emitted in your application's logs."
     }
 
     return "When a deadlock is detected, no log message will be emitted in your application's logs."
-  }
-
-  public async run(): Promise<void> {
-    const {flags, args} = await this.parse(LogLockWaits)
-    const {app} = flags
-    const {value, database} = args
-    const db = await getAddon(this.heroku, app, database)
-
-    if (essentialPlan(db)) ux.error("You can't perform this operation on Essential-tier databases.")
-
-    if (value) {
-      const {body: settings} = await this.heroku.patch<PgDatabaseConfig>(`/postgres/v0/databases/${db.id}/config`, {
-        hostname: pgHost(),
-        body: {[this.name]: value},
-      })
-      const setting = settings[this.name]
-      ux.log(`${this.name.replace(/_/g, '-')} has been set to ${setting.value} for ${db.name}.`)
-      ux.log(this.explain(setting))
-    } else {
-      const {body: settings} = await this.heroku.get<PgDatabaseConfig>(`/postgres/v0/databases/${db.id}/config`, {hostname: pgHost()})
-      const setting = settings[this.name]
-      ux.log(`${this.name.replace(/_/g, '-')} is set to ${setting.value} for ${db.name}.`)
-      ux.log(this.explain(setting))
-    }
   }
 }
