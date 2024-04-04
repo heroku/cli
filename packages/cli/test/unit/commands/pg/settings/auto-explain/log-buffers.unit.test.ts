3fb3807c90b72fdc0f99926ce91b3a8b46d56d09
Eric Black <eblack@salesforce.com>
2024-04-04 16:20:11 -0700

    Fix whitespace

--- packages/cli/test/unit/commands/pg/settings/auto-explain/log-buffers.unit.test.ts
+++ packages/cli/test/unit/commands/pg/settings/auto-explain/log-buffers.unit.test.ts
@@ -41,29 +41,3 @@ describe('pg:settings:auto-explain:log-buffers', () => {
     `))
   })
 })
-
-
-
-// it('shows settings for auto_explain_log_buffers with value', () => {
-//   setupSettingsMockData('auto_explain.log_buffers')
-//   cmd = proxyquire('../../../../commands/settings/auto_explain_log_buffers', {
-//     settings: proxyquire.noCallThru().load('../../../../lib/setter', {
-//       './fetcher': fetcher,
-//     }),
-//   })
-//   pg.get('/postgres/v0/databases/1/config').reply(200, settingsResult)
-//   return cmd.run({args: {database: 'test-database', value: ''}, flags: {}})
-//     .then(() => expect(cli.stdout).to.equal('auto-explain.log-buffers is set to test_value for postgres-1.\nBuffer statistics have been enabled for auto_explain.\n'))
-// })
-
-// it('shows settings for auto_explain_log_buffers with no value', () => {
-//   setupSettingsMockData('auto_explain.log_buffers', '')
-//   cmd = proxyquire('../../../../commands/settings/auto_explain_log_buffers', {
-//     settings: proxyquire.noCallThru().load('../../../../lib/setter', {
-//       './fetcher': fetcher,
-//     }),
-//   })
-//   pg.get('/postgres/v0/databases/1/config').reply(200, settingsResult)
-//   return cmd.run({args: {database: 'test-database', value: ''}, flags: {}})
-//     .then(() => expect(cli.stdout).to.equal('auto-explain.log-buffers is set to  for postgres-1.\nBuffer statistics have been disabled for auto_explain.\n'))
-// })
