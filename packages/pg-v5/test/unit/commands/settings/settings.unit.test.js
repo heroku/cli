749a91f2d407a2ff7c8167fe8e94192b373bf1a5
Justin Wilaby <jwilaby@salesforce.com>
2024-04-04 10:18:33 -0700

    migrate(W-14179828): pg-v5: Upgrade pg:settings:auto-explain

--- packages/pg-v5/test/unit/commands/settings/settings.unit.test.js
+++ packages/pg-v5/test/unit/commands/settings/settings.unit.test.js
@@ -96,30 +96,6 @@ describe('pg:settings', () => {
       .then(() => expect(cli.stdout).to.equal(`${settingsResultName} is set to ${settingResult.value} for postgres-1.\n${settingResult.values[settingResult.value]}\n`))
   })
 
-  it('shows settings for auto_explain with value', () => {
-    setupSettingsMockData('auto_explain')
-    cmd = proxyquire('../../../../commands/settings/auto_explain', {
-      settings: proxyquire.noCallThru().load('../../../../lib/setter', {
-        './fetcher': fetcher,
-      }),
-    })
-    pg.get('/postgres/v0/databases/1/config').reply(200, settingsResult)
-    return cmd.run({args: {database: 'test-database', value: ''}, flags: {}})
-      .then(() => expect(cli.stdout).to.equal('auto-explain is set to test_value for postgres-1.\nExecution plans of queries will be logged for future connections.\n'))
-  })
-
-  it('shows settings for auto_explain with no value', () => {
-    setupSettingsMockData('auto_explain', '')
-    cmd = proxyquire('../../../../commands/settings/auto_explain', {
-      settings: proxyquire.noCallThru().load('../../../../lib/setter', {
-        './fetcher': fetcher,
-      }),
-    })
-    pg.get('/postgres/v0/databases/1/config').reply(200, settingsResult)
-    return cmd.run({args: {database: 'test-database', value: ''}, flags: {}})
-      .then(() => expect(cli.stdout).to.equal('auto-explain is set to  for postgres-1.\nExecution plans of queries will not be logged for future connections.\n'))
-  })
-
   it('shows settings for auto_explain_log_analyze with value', () => {
     setupSettingsMockData('auto_explain.log_analyze')
     cmd = proxyquire('../../../../commands/settings/auto_explain_log_analyze', {
