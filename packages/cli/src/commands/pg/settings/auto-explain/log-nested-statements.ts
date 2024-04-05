a6fbf0d5bc773975916430f517b38752ecaac709
Eric Black <eblack@salesforce.com>
2024-04-05 10:38:55 -0700

    Fix name of command

--- packages/cli/src/commands/pg/settings/auto-explain/log-nested-statements.ts
+++ packages/cli/src/commands/pg/settings/auto-explain/log-nested-statements.ts
@@ -1,7 +1,7 @@
 import {Args} from '@oclif/core'
 import {PGSettingsCommand, type Setting, type SettingKey, booleanConverter} from '../../../../lib/pg/setter'
 
-export default class LogBuffersWaits extends PGSettingsCommand {
+export default class LogNestedStatements extends PGSettingsCommand {
   static description = "Nested statements are included in the execution plan's log."
 
   static args = {
