f69c5b74e06c82bc9640f06f9b6cfaa1c2982a57
Katy Bowman <katy.bowman@salesforce.com>
2024-04-05 14:14:49 -0400

    test: test summary instead of description

--- packages/cli/src/commands/auth/2fa/disable.ts
+++ packages/cli/src/commands/auth/2fa/disable.ts
@@ -2,7 +2,7 @@ import {Command} from '@heroku-cli/command'
 import {ux} from '@oclif/core'
 
 export default class Auth2faGenerate extends Command {
-  static description = 'disables 2fa on account'
+  static summary = 'disables 2fa on account'
 
   static example = '$ heroku auth:2fa:disable'
 
