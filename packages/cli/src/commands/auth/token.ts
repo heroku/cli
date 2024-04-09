4ddead43285c4c20354f1392db06217e17fd8207
Zane Whitfield <zwhitfield@salesforce.com>
2024-04-09 13:13:09 -0700

    chore(auth): update warning message for auth:token (#2750)
    * Update warning based on userpass
    
    * Add & update tests
    
    * Update copy

--- packages/cli/src/commands/auth/token.ts
+++ packages/cli/src/commands/auth/token.ts
@@ -18,10 +18,11 @@ By default, the CLI auth token is only valid for 1 year. To generate a long-live
     try {
       const {body: tokens} = await this.heroku.get<Heroku.OAuthAuthorization>('/oauth/authorizations', {retryAuth: false})
       const token = tokens.find((t: any) => t.access_token && t.access_token.token === this.heroku.auth)
+      const isInternal = token ? token.user.email.includes('@heroku.com') : false
       if (token && token.access_token.expires_in) {
         const d = new Date()
         d.setSeconds(d.getSeconds() + token.access_token.expires_in)
-        this.warn(`token will expire ${formatRelative(d, new Date())}\nUse ${color.cmd('heroku authorizations:create')} to generate a long-term token`)
+        this.warn(`token will expire ${formatRelative(d, new Date())}\n${isInternal ? 'All tokens expire one year after we generate it.' : `To generate a long-lived token, use ${color.cmd('heroku authorizations:create')}.`}`)
       }
     } catch (error: any) {
       this.warn(error)
