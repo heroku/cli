c9385d81087c4ebeeceb1ecf01afc53f2ebf8fd9
Katy Bowman <katy.bowman@salesforce.com>
2024-04-17 12:52:10 -0400

    refactor: update types

--- packages/cli/src/lib/pg/types.ts
+++ packages/cli/src/lib/pg/types.ts
@@ -82,6 +82,8 @@ type ServiceInfo =
   | 'Maintenance window'
   | 'Infrastructure'
   | 'Warning'
+  | 'Billing App'
+  | 'Add-on'
 
 export type PgDatabaseService = {
   addon_id: string
@@ -123,15 +125,22 @@ export type PgDatabaseService = {
     name: ServiceInfo
     values: string[]
   }>
+  configVars: string[]
+}
+
+export type TenantInfoNames = 'Plan' | 'Status' | 'Connections' | 'PG Version' | 'Created' | 'Data Size' | 'Tables' | 'Fork/Follow'
+  | 'Rollback' | 'Continuous Protection' | 'Billing App' | 'Add-on'
+
+export type TenantInfo = {
+  name: TenantInfoNames
+  values: string[]
+  resolve_db_name?: boolean
 }
 
 export type PgStatus = {
   'waiting?': boolean
   message: string
 }
-
-type TenantInfo = 'Plan' | 'Status' | 'Connections' | 'PG Version' | 'Created' | 'Data Size' | 'Tables' | 'Fork/Follow'
-  | 'Rollback' | 'Continuous Protection'
 
 export type PgDatabaseTenant = {
   addon_id: string
@@ -144,10 +153,7 @@ export type PgDatabaseTenant = {
   resource_url: string
   'waiting?': boolean
   num_bytes: number
-  info: Array<{
-    name: TenantInfo
-    values: string[]
-  }>
+  info: Array<TenantInfo>
 }
 
 export type PgDatabase = PgDatabaseService & PgDatabaseTenant
@@ -214,81 +220,3 @@ export type Setting<T> = {
   default: T
 }
 export type SettingsResponse = Record<SettingKey, Setting<unknown>>
-
-export type PGDiagnoseResponse = {
-  id: string,
-  app: string,
-  database: string,
-  created_at: string,
-  checks: [
-    PGDiagnoseCheck<ConnCountResult>,
-    PGDiagnoseCheck<QueriesResult>,
-    PGDiagnoseCheck<QueriesResult>,
-    PGDiagnoseCheck<UnusedIndexesResult>,
-    PGDiagnoseCheck<BloatResult>,
-    PGDiagnoseCheck<HitRateResult>,
-    PGDiagnoseCheck<BlockingResult>,
-  ],
-}
-
-export type PGDiagnoseCheck<T extends PGDiagnoseResult = PGDiagnoseResult> = {
-  name: string,
-  status: 'red' | 'yellow' | 'green',
-  results: T[]
-}
-export type PGDiagnoseResult =
-  ConnCountResult
-  | QueriesResult
-  | UnusedIndexesResult
-  | BloatResult
-  | HitRateResult
-  | BlockingResult
-export type ConnCountResult = {
-  count: number
-}
-
-export type QueriesResult = {
-  pid: number,
-  duration: string,
-  query: string,
-}
-
-export type UnusedIndexesResult = {
-  reason: string,
-  index: string,
-  index_scan_pct: string,
-  scans_per_write: string,
-  index_size: string,
-  table_size: string,
-}
-
-export type BloatResult = {
-  type: string,
-  object: string,
-  bloat: number,
-  waste: string,
-}
-
-export type HitRateResult = {
-  name: string,
-  ratio: number,
-}
-
-export type BlockingResult = {
-  blocked_pid: number,
-  blocking_statement: string,
-  blocking_duration: string,
-  blocking_pid: number,
-  blocked_statement: string,
-  blocked_duration: string,
-}
-
-export type PGDiagnoseRequest = {
-  url: string,
-  plan: string,
-  app: string,
-  database: string,
-  metrics?: unknown[],
-  burst_data_present?: boolean,
-  burst_status?: string,
-}
