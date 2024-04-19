af9603450437cbeb5443231901d004e40ba3c8a3
Eric Black <eblack@salesforce.com>
2024-04-18 17:47:30 -0700

    Convert pg promote test, clean up

--- packages/cli/src/commands/pg/promote.ts
+++ packages/cli/src/commands/pg/promote.ts
@@ -4,12 +4,10 @@ import {Command, flags} from '@heroku-cli/command'
 import {Args, ux} from '@oclif/core'
 import * as Heroku from '@heroku-cli/schema'
 import heredoc from 'tsheredoc'
-import {attachment, getRelease} from '../../lib/pg/fetcher'
+import {getAttachment, getRelease} from '../../lib/pg/fetcher'
 import pgHost from '../../lib/pg/host'
 import {PgStatus, PgDatabase} from '../../lib/pg/types'
 
-// const cli = require('heroku-cli-util')
-// const host = require('../lib/host')
 export default class Promote extends Command {
   static topic = 'pg';
   static description = 'sets DATABASE as your DATABASE_URL';
@@ -20,30 +18,25 @@ export default class Promote extends Command {
   };
 
   static args = {
-    database: Args.string({required: true}),
+    database: Args.string(),
   };
 
   public async run(): Promise<void> {
     const {flags, args} = await this.parse(Promote)
-    // const fetcher = require('../lib/fetcher')(heroku)
-    // const {app, args, flags} = context
     const {force, app} = flags
     const {database} = args
-    const dbAttachment = await attachment(this.heroku, app, database)
-    // let current
-    // let attachments
+    const attachment = await getAttachment(this.heroku, app, database)
     ux.action.start(`Ensuring an alternate alias for existing ${color.green('DATABASE_URL')}`)
-    // await ux.action(`Ensuring an alternate alias for existing ${color.green('DATABASE_URL')}`, (async () => {
     const {body: attachments} = await this.heroku.get<Heroku.AddOnAttachment[]>(`/apps/${app}/addon-attachments`)
-    // attachments = addonAttachments
     const current = attachments.find(a => a.name === 'DATABASE')
     if (!current)
       return
-    if (current.addon?.name === dbAttachment.addon.name && current.namespace === dbAttachment.namespace) {
-      if (dbAttachment.namespace) {
-        throw new Error(`${color.cyan(dbAttachment.name)} is already promoted on ${color.magenta(app)}`)
+    // eslint-disable-next-line eqeqeq
+    if (current.addon?.name === attachment.addon.name && current.namespace == attachment.namespace) {
+      if (attachment.namespace) {
+        ux.error(`${color.cyan(attachment.name)} is already promoted on ${color.app(app)}`)
       } else {
-        throw new Error(`${color.yellow(dbAttachment.addon.name)} is already promoted on ${color.magenta(app)}`)
+        ux.error(`${color.addon(attachment.addon.name)} is already promoted on ${color.app(app)}`)
       }
     }
 
@@ -57,14 +50,17 @@ export default class Promote extends Command {
       // error, we can create a secondary attachment, just-in-time.
       const {body: backup} = await this.heroku.post<Heroku.AddOnAttachment>('/addon-attachments', {
         body: {
-          app: {name: app}, addon: {name: current.addon?.name}, namespace: current.namespace, confirm: app,
+          app: {name: app},
+          addon: {name: current.addon?.name},
+          namespace: current.namespace,
+          confirm: app,
         },
       })
       ux.action.stop(color.green(backup.name + '_URL'))
     }
 
     if (!force) {
-      const {body: status} = await this.heroku.get<PgStatus>(`/client/v11/databases/${dbAttachment.addon.id}/wait_status`, {
+      const {body: status} = await this.heroku.get<PgStatus>(`/client/v11/databases/${attachment.addon.id}/wait_status`, {
         hostname: pgHost(),
       })
       if (status['waiting?']) {
@@ -75,85 +71,74 @@ export default class Promote extends Command {
           
           To ignore this error, you can pass the --force flag to promote the database and risk application issues.
         `))
-        // ux.error(`Database cannot be promoted while in state: ${status.message}\n\nPromoting this database can lead to application errors and outage. Please run pg:wait to wait for database to become available.\n\nTo ignore this error, you can pass the --force flag to promote the database and risk application issues.`)
       }
     }
 
     let promotionMessage
-    if (dbAttachment.namespace) {
-      promotionMessage = `Promoting ${color.cyan(dbAttachment.name)} to ${color.green('DATABASE_URL')} on ${color.magenta(app)}`
+    if (attachment.namespace) {
+      promotionMessage = `Promoting ${color.cyan(attachment.name)} to ${color.green('DATABASE_URL')} on ${color.app(app)}`
     } else {
-      promotionMessage = `Promoting ${color.addon(dbAttachment.addon.name)} to ${color.green('DATABASE_URL')} on ${color.magenta(app)}`
+      promotionMessage = `Promoting ${color.addon(attachment.addon.name)} to ${color.green('DATABASE_URL')} on ${color.app(app)}`
     }
 
     ux.action.start(promotionMessage)
-    // await ux.action(promotionMessage, (async function () {
     await this.heroku.post('/addon-attachments', {
       body: {
         name: 'DATABASE',
         app: {name: app},
-        addon: {name: dbAttachment.addon.name},
-        namespace: dbAttachment.namespace,
+        addon: {name: attachment.addon.name},
+        namespace: attachment.namespace || null,
         confirm: app,
       },
     })
     ux.action.stop()
-    // })())
     const currentPooler = attachments.find(a => a.namespace === 'connection-pooling:default' && a.addon?.id === current.addon?.id && a.name === 'DATABASE_CONNECTION_POOL')
     if (currentPooler) {
       ux.action.start('Reattaching pooler to new leader')
-      // await ux.action('Reattaching pooler to new leader', (async function () {
       await this.heroku.post('/addon-attachments', {
         body: {
           name: currentPooler.name,
           app: {name: app},
-          addon: {name: dbAttachment.addon.name},
+          addon: {name: attachment.addon.name},
           namespace: 'connection-pooling:default',
           confirm: app,
         },
       })
-      // })())
       ux.action.stop()
     }
 
-    const {body: promotedDatabaseDetails} = await this.heroku.get<PgDatabase>(`/client/v11/databases/${dbAttachment.addon.id}`, {
+    const {body: promotedDatabaseDetails} = await this.heroku.get<PgDatabase>(`/client/v11/databases/${attachment.addon.id}`, {
       hostname: pgHost(),
     })
     if (promotedDatabaseDetails.following) {
-      const unfollowLeaderCmd = `heroku pg:unfollow ${dbAttachment.addon.name}`
+      const unfollowLeaderCmd = `heroku pg:unfollow ${attachment.addon.name}`
       ux.warn(heredoc(`
-        WARNING: Your database has been promoted but it is currently a follower database in read-only mode.
+        Your database has been promoted but it is currently a follower database in read-only mode.
         
         Promoting a database with ${color.cmd('heroku pg:promote')} doesn't automatically unfollow its leader.
         
         Use ${color.cyan(unfollowLeaderCmd)} to stop this follower from replicating from its leader (${color.yellow(promotedDatabaseDetails.leader as string)}) and convert it into a writable database.
       `))
-      // ux.warn(`WARNING: Your database has been promoted but it is currently a follower database in read-only mode.\n      \n Promoting a database with ${color.cyan.bold('heroku pg:promote')} doesn't automatically unfollow its leader.\n      \n Use ${color.cyan.bold(unfollowLeaderCmd)} to stop this follower from replicating from its leader (${color.yellow(promotedDatabaseDetails.leader.name)}) and convert it into a writable database.`)
     }
 
     const {body: formation} = await this.heroku.get<Heroku.Formation[]>(`/apps/${app}/formation`)
     const releasePhase = formation.find(process => process.type === 'release')
     if (releasePhase) {
       ux.action.start('Checking release phase')
-      // await ux.action('Checking release phase', (async function () {
       const {body: releases} = await this.heroku.get<Heroku.Release[]>(`/apps/${app}/releases`, {
         partial: true,
         headers: {
           Range: 'version ..; max=5, order=desc',
         },
       })
-      // let releases = await this.heroku.request({
-      //   path: `/apps/${app}/releases`, partial: true, headers: {
-      //     Range: 'version ..; max=5, order=desc',
-      //   },
-      // })
+
       const attach = releases.find(release => release.description?.includes('Attach DATABASE'))
       const detach = releases.find(release => release.description?.includes('Detach DATABASE'))
       if (!attach || !detach) {
         ux.error('Unable to check release phase. Check your Attach DATABASE release for failures.')
       }
 
-      const endTime = Date.now() + 900000
+      const endTime = Date.now() + 900000 // 15 minutes from now
       const [attachId, detachId] = [attach?.id as string, detach?.id as string]
       while (true) {
         const attach = await getRelease(this.heroku, app, attachId)
@@ -164,8 +149,7 @@ export default class Promote extends Command {
             msg += ` It is safe to ignore the failed ${detach.description} release.`
           }
 
-          ux.action.stop(msg)
-          // return ux.action.done(msg)
+          return ux.action.stop(msg)
         }
 
         if (attach && attach.status === 'failed') {
@@ -178,18 +162,15 @@ export default class Promote extends Command {
           }
 
           msg += ' Check your release phase logs for failure causes.'
-          ux.action.stop(msg)
-          // return ux.action.done(msg)
+          return ux.action.stop(msg)
         }
 
         if (Date.now() > endTime) {
-          ux.action.stop('timeout. Check your Attach DATABASE release for failures.')
-          // return ux.action.done('timeout. Check your Attach DATABASE release for failures.')
+          return ux.action.stop('timeout. Check your Attach DATABASE release for failures.')
         }
 
         await new Promise(resolve => setTimeout(resolve, 5000))
       }
-      // })())
     }
   }
 }
