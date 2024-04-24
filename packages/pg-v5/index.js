f4f1c41934b2a4ee951b498402a6eaeb495c0e13
Justin Wilaby <jwilaby@gmail.com>
2024-04-24 12:21:40 -0700

    migrate(W-14180163): pg-v5: Upgrade pg:ps (#2829)

--- packages/pg-v5/index.js
+++ packages/pg-v5/index.js
@@ -9,7 +9,6 @@ exports.topics = [
 exports.commands = flatten([
   require('./commands/info'),
   require('./commands/locks'),
-  require('./commands/ps'),
   require('./commands/psql'),
   require('./commands/pull'),
   require('./commands/repoint'),
