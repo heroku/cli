3ad0593dd334a1b745346dbe111bb1de17791c24
Benjamin Leong <benri@users.noreply.github.com>
2024-04-26 10:51:30 -0700

    fix pull unit test

--- packages/container-registry-v5/test/unit/commands/pull.unit.test.js
+++ packages/container-registry-v5/test/unit/commands/pull.unit.test.js
@@ -7,31 +7,52 @@ const {expect} = require('chai')
 const sinon = require('sinon')
 
 const Sanbashi = require('../../../lib/sanbashi')
+const nock = require('nock')
+const helpers = require('../../helpers')
 let sandbox
 
 describe('container pull', () => {
   beforeEach(() => {
     cli.mockConsole()
     sandbox = sinon.createSandbox()
+    cli.exit.mock()
   })
-  afterEach(() => sandbox.restore())
+  afterEach(() => {
+    sandbox.restore()
+    nock.cleanAll()
+  })
 
   it('requires a process type', async () => {
-    sandbox.stub(process, 'exit')
+    await helpers.assertExit(1, cmd.run({app: 'testapp', args: [], flags: {}}))
+      .then(error => {
+        expect(error.message).to.contain('Requires one or more process types')
+      })
+  })
 
-    await cmd.run({app: 'testapp', args: [], flags: {}})
-      .then(() => expect(cli.stderr).to.contain('Requires one or more process types'))
-      .then(() => expect(cli.stdout, 'to be empty'))
-      .then(() => expect(process.exit.calledWith(1)).to.equal(true))
+  it('exits when the app stack is not "container"', () => {
+    let api = nock('https://api.heroku.com:443')
+      .get('/apps/testapp')
+      .reply(200, {name: 'testapp', stack: {name: 'heroku-22'}})
+
+    return helpers.assertExit(1, cmd.run({app: 'testapp', args: ['web'], flags: {}}))
+      .then(error => {
+        expect(error.message).to.equal('This command is only supported for the container stack. The stack for app testapp is heroku-22.')
+        api.done()
+      })
   })
 
   it('pulls from the docker registry', () => {
+    let api = nock('https://api.heroku.com:443')
+      .get('/apps/testapp')
+      .reply(200, {name: 'testapp', stack: {name: 'container'}})
+
     let pull = sandbox.stub(Sanbashi, 'pullImage')
       .withArgs('registry.heroku.com/testapp/web')
 
     return cmd.run({app: 'testapp', args: ['web'], flags: {}})
-      .then(() => expect(cli.stderr, 'to be empty'))
+      .then(() => expect(cli.stderr).to.equal(''))
       .then(() => expect(cli.stdout).to.contain('Pulling web as registry.heroku.com/testapp/web'))
       .then(() => sandbox.assert.calledOnce(pull))
+      .then(() => api.done())
   })
 })
