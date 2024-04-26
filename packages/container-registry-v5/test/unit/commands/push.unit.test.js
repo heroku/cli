cf22d189605cdfbd7271855a645fb31309eb298e
Benjamin Leong <benri@users.noreply.github.com>
2024-04-26 10:54:21 -0700

    fix push unit test

--- packages/container-registry-v5/test/unit/commands/push.unit.test.js
+++ packages/container-registry-v5/test/unit/commands/push.unit.test.js
@@ -6,199 +6,175 @@ const cmd = require('../../..').commands.find(c => c.topic === 'container' && c.
 const {expect} = require('chai')
 const sinon = require('sinon')
 const nock = require('nock')
+const helpers = require('../../helpers')
 
 const Sanbashi = require('../../../lib/sanbashi')
 let sandbox
 
 describe('container push', () => {
+  let api
   beforeEach(() => {
     cli.mockConsole()
     sandbox = sinon.createSandbox()
+    cli.exit.mock()
   })
-  afterEach(() => sandbox.restore())
-
-  it('exits when the app stack is not "container"', () => {
-    sandbox.stub(process, 'exit')
-
-    let api = nock('https://api.heroku.com:443')
-      .get('/apps/testapp')
-      .reply(200, {name: 'testapp', stack: {name: 'heroku-22'}})
-
-    let dockerfiles = sandbox.stub(Sanbashi, 'getDockerfiles')
-      .returns(['/path/to/Dockerfile'])
-    let build = sandbox.stub(Sanbashi, 'buildImage')
-
-    return cmd.run({app: 'testapp', args: ['web'], flags: {}})
-      .then(() => expect(cli.stderr).to.contain('This command is only supported for the container stack'))
-      .then(() => expect(process.exit.calledWith(1)).to.equal(true))
-      .then(() => sandbox.assert.notCalled(dockerfiles))
-      .then(() => sandbox.assert.notCalled(build))
-      .then(() => api.done())
+  afterEach(() => {
+    sandbox.restore()
+    nock.cleanAll()
   })
 
-  it('gets a build error', () => {
-    sandbox.stub(process, 'exit')
+  context('when the app stack is not "container"', () => {
+    beforeEach(() => {
+      api = nock('https://api.heroku.com:443')
+        .get('/apps/testapp')
+        .reply(200, {name: 'testapp', stack: {name: 'heroku-22'}})
+    })
+    afterEach(() => api.done())
 
-    let api = nock('https://api.heroku.com:443')
-      .get('/apps/testapp')
-      .reply(200, {name: 'testapp'})
-
-    let dockerfiles = sandbox.stub(Sanbashi, 'getDockerfiles')
-      .returns(['/path/to/Dockerfile'])
-    let build = sandbox.stub(Sanbashi, 'buildImage').throws()
-
-    return cmd.run({app: 'testapp', args: ['web'], flags: {}})
-      .then(() => expect(cli.stderr).to.contain('docker build exited with Error: Error'))
-      .then(() => expect(cli.stdout).to.contain('Building web (/path/to/Dockerfile)'))
-      .then(() => expect(process.exit.calledWith(1)).to.equal(true))
-      .then(() => sandbox.assert.calledOnce(dockerfiles))
-      .then(() => sandbox.assert.calledOnce(build))
-      .then(() => api.done())
+    it('exits', () => {
+      return helpers.assertExit(1, cmd.run({app: 'testapp', args: ['web'], flags: {}}))
+        .then(error => {
+          expect(error.message).to.equal('This command is only supported for the container stack. The stack for app testapp is heroku-22.')
+        })
+    })
   })
 
-  it('gets a push error', () => {
-    sandbox.stub(process, 'exit')
+  context('when the app is a container app', () => {
+    beforeEach(() => {
+      api = nock('https://api.heroku.com:443')
+        .get('/apps/testapp')
+        .reply(200, {name: 'testapp', stack: {name: 'container'}})
+    })
+    afterEach(() => api.done())
 
-    let api = nock('https://api.heroku.com:443')
-      .get('/apps/testapp')
-      .reply(200, {name: 'testapp'})
+    it('gets a build error', () => {
+      let dockerfiles = sandbox.stub(Sanbashi, 'getDockerfiles')
+        .returns(['/path/to/Dockerfile'])
+      let build = sandbox.stub(Sanbashi, 'buildImage').throws()
 
-    let dockerfiles = sandbox.stub(Sanbashi, 'getDockerfiles')
-      .returns(['/path/to/Dockerfile'])
-    let build = sandbox.stub(Sanbashi, 'buildImage')
-    let push = sandbox.stub(Sanbashi, 'pushImage').throws()
+      return helpers.assertExit(1, cmd.run({app: 'testapp', args: ['web'], flags: {}}))
+        .then(error => {
+          expect(error.message).to.equal('Error: docker build exited with Error: Error')
+          expect(cli.stdout).to.contain('Building web (/path/to/Dockerfile)')
+          sandbox.assert.calledOnce(dockerfiles)
+          sandbox.assert.calledOnce(build)
+        })
+    })
 
-    return cmd.run({app: 'testapp', args: ['web'], flags: {}})
-      .then(() => expect(cli.stderr).to.contain('docker push exited with Error: Error'))
-      .then(() => expect(cli.stdout).to.contain('Building web (/path/to/Dockerfile)'))
-      .then(() => expect(process.exit.calledWith(1)).to.equal(true))
-      .then(() => sandbox.assert.calledOnce(dockerfiles))
-      .then(() => sandbox.assert.calledOnce(build))
-      .then(() => sandbox.assert.calledOnce(push))
-      .then(() => api.done())
-  })
+    it('gets a push error', () => {
+      let dockerfiles = sandbox.stub(Sanbashi, 'getDockerfiles')
+        .returns(['/path/to/Dockerfile'])
+      let build = sandbox.stub(Sanbashi, 'buildImage')
+      let push = sandbox.stub(Sanbashi, 'pushImage').throws()
 
-  it('pushes to the docker registry', () => {
-    let api = nock('https://api.heroku.com:443')
-      .get('/apps/testapp')
-      .reply(200, {name: 'testapp'})
+      return helpers.assertExit(1, cmd.run({app: 'testapp', args: ['web'], flags: {}}))
+        .then(error => {
+          expect(error.message).to.contain('docker push exited with Error: Error')
+          expect(cli.stderr).to.contain('docker push exited with Error: Error')
+          expect(cli.stdout).to.contain('Building web (/path/to/Dockerfile)')
+          sandbox.assert.calledOnce(dockerfiles)
+          sandbox.assert.calledOnce(build)
+          sandbox.assert.calledOnce(push)
+        })
+    })
 
-    let dockerfiles = sandbox.stub(Sanbashi, 'getDockerfiles')
-      .returns(['/path/to/Dockerfile'])
-    let build = sandbox.stub(Sanbashi, 'buildImage')
-      .withArgs('/path/to/Dockerfile', 'registry.heroku.com/testapp/web', [])
-    let push = sandbox.stub(Sanbashi, 'pushImage')
-      .withArgs('registry.heroku.com/testapp/web')
+    it('pushes to the docker registry', () => {
+      let dockerfiles = sandbox.stub(Sanbashi, 'getDockerfiles')
+        .returns(['/path/to/Dockerfile'])
+      let build = sandbox.stub(Sanbashi, 'buildImage')
+        .withArgs('/path/to/Dockerfile', 'registry.heroku.com/testapp/web', [])
+      let push = sandbox.stub(Sanbashi, 'pushImage')
+        .withArgs('registry.heroku.com/testapp/web')
 
-    return cmd.run({app: 'testapp', args: ['web'], flags: {}})
-      .then(() => expect(cli.stdout).to.contain('Building web (/path/to/Dockerfile)'))
-      .then(() => expect(cli.stdout).to.contain('Pushing web (/path/to/Dockerfile)'))
-      .then(() => sandbox.assert.calledOnce(dockerfiles))
-      .then(() => sandbox.assert.calledOnce(build))
-      .then(() => sandbox.assert.calledOnce(push))
-      .then(() => api.done())
-  })
+      return cmd.run({app: 'testapp', args: ['web'], flags: {}})
+        .then(() => expect(cli.stdout).to.contain('Building web (/path/to/Dockerfile)'))
+        .then(() => expect(cli.stdout).to.contain('Pushing web (/path/to/Dockerfile)'))
+        .then(() => sandbox.assert.calledOnce(dockerfiles))
+        .then(() => sandbox.assert.calledOnce(build))
+        .then(() => sandbox.assert.calledOnce(push))
+    })
 
-  it('pushes the standard dockerfile to non-web', () => {
-    let api = nock('https://api.heroku.com:443')
-      .get('/apps/testapp')
-      .reply(200, {name: 'testapp'})
+    it('pushes the standard dockerfile to non-web', () => {
+      let dockerfiles = sandbox.stub(Sanbashi, 'getDockerfiles')
+        .returns(['/path/to/Dockerfile'])
+      let build = sandbox.stub(Sanbashi, 'buildImage')
+        .withArgs('/path/to/Dockerfile', 'registry.heroku.com/testapp/worker', [])
+      let push = sandbox.stub(Sanbashi, 'pushImage')
+        .withArgs('registry.heroku.com/testapp/worker')
 
-    let dockerfiles = sandbox.stub(Sanbashi, 'getDockerfiles')
-      .returns(['/path/to/Dockerfile'])
-    let build = sandbox.stub(Sanbashi, 'buildImage')
-      .withArgs('/path/to/Dockerfile', 'registry.heroku.com/testapp/worker', [])
-    let push = sandbox.stub(Sanbashi, 'pushImage')
-      .withArgs('registry.heroku.com/testapp/worker')
+      return cmd.run({app: 'testapp', args: ['worker'], flags: {}})
+        .then(() => expect(cli.stdout).to.contain('Building worker (/path/to/Dockerfile)'))
+        .then(() => expect(cli.stdout).to.contain('Pushing worker (/path/to/Dockerfile)'))
+        .then(() => sandbox.assert.calledOnce(dockerfiles))
+        .then(() => sandbox.assert.calledOnce(build))
+        .then(() => sandbox.assert.calledOnce(push))
+    })
 
-    return cmd.run({app: 'testapp', args: ['worker'], flags: {}})
-      .then(() => expect(cli.stdout).to.contain('Building worker (/path/to/Dockerfile)'))
-      .then(() => expect(cli.stdout).to.contain('Pushing worker (/path/to/Dockerfile)'))
-      .then(() => sandbox.assert.calledOnce(dockerfiles))
-      .then(() => sandbox.assert.calledOnce(build))
-      .then(() => sandbox.assert.calledOnce(push))
-      .then(() => api.done())
-  })
+    it('pushes several dockerfiles recursively', () => {
+      let dockerfiles = sandbox.stub(Sanbashi, 'getDockerfiles')
+        .returns(['/path/to/Dockerfile.web', '/path/to/Dockerfile.worker'])
+      let build = sandbox.stub(Sanbashi, 'buildImage')
+      build.withArgs('/path/to/Dockerfile.web', 'registry.heroku.com/testapp/web', [])
+      build.withArgs('/path/to/Dockerfile.worker', 'registry.heroku.com/testapp/worker', [])
+      let push = sandbox.stub(Sanbashi, 'pushImage')
+      push.withArgs('registry.heroku.com/testapp/web')
+      push.withArgs('registry.heroku.com/testapp/worker')
 
-  it('pushes several dockerfiles recursively', () => {
-    let api = nock('https://api.heroku.com:443')
-      .get('/apps/testapp')
-      .reply(200, {name: 'testapp'})
+      return cmd.run({app: 'testapp', args: ['web', 'worker'], flags: {recursive: true}})
+        .then(() => expect(cli.stdout).to.contain('Building web (/path/to/Dockerfile.web)'))
+        .then(() => expect(cli.stdout).to.contain('Building worker (/path/to/Dockerfile.worker)'))
+        .then(() => expect(cli.stdout).to.contain('Pushing web (/path/to/Dockerfile.web)'))
+        .then(() => expect(cli.stdout).to.contain('Pushing worker (/path/to/Dockerfile.worker)'))
+        .then(() => sandbox.assert.calledOnce(dockerfiles))
+        .then(() => sandbox.assert.calledTwice(build))
+        .then(() => sandbox.assert.calledTwice(push))
+    })
 
-    let dockerfiles = sandbox.stub(Sanbashi, 'getDockerfiles')
-      .returns(['/path/to/Dockerfile.web', '/path/to/Dockerfile.worker'])
-    let build = sandbox.stub(Sanbashi, 'buildImage')
-    build.withArgs('/path/to/Dockerfile.web', 'registry.heroku.com/testapp/web', [])
-    build.withArgs('/path/to/Dockerfile.worker', 'registry.heroku.com/testapp/worker', [])
-    let push = sandbox.stub(Sanbashi, 'pushImage')
-    push.withArgs('registry.heroku.com/testapp/web')
-    push.withArgs('registry.heroku.com/testapp/worker')
+    it('builds with custom context path and pushes to the docker registry', () => {
+      let dockerfiles = sandbox.stub(Sanbashi, 'getDockerfiles')
+        .returns(['/path/to/Dockerfile'])
+      let build = sandbox.stub(Sanbashi, 'buildImage')
+        .withArgs('/path/to/Dockerfile', 'registry.heroku.com/testapp/web', [], '/custom/context/path')
+      let push = sandbox.stub(Sanbashi, 'pushImage')
+        .withArgs('registry.heroku.com/testapp/web')
 
-    return cmd.run({app: 'testapp', args: ['web', 'worker'], flags: {recursive: true}})
-      .then(() => expect(cli.stdout).to.contain('Building web (/path/to/Dockerfile.web)'))
-      .then(() => expect(cli.stdout).to.contain('Building worker (/path/to/Dockerfile.worker)'))
-      .then(() => expect(cli.stdout).to.contain('Pushing web (/path/to/Dockerfile.web)'))
-      .then(() => expect(cli.stdout).to.contain('Pushing worker (/path/to/Dockerfile.worker)'))
-      .then(() => sandbox.assert.calledOnce(dockerfiles))
-      .then(() => sandbox.assert.calledTwice(build))
-      .then(() => sandbox.assert.calledTwice(push))
-      .then(() => api.done())
-  })
+      return cmd.run({app: 'testapp', args: ['web'], flags: {'context-path': '/custom/context/path'}})
+        .then(() => expect(cli.stdout).to.contain('Building web (/path/to/Dockerfile)'))
+        .then(() => expect(cli.stdout).to.contain('Pushing web (/path/to/Dockerfile)'))
+        .then(() => sandbox.assert.calledOnce(dockerfiles))
+        .then(() => sandbox.assert.calledOnce(build))
+        .then(() => sandbox.assert.calledOnce(push))
+    })
 
-  it('builds with custom context path and pushes to the docker registry', () => {
-    let api = nock('https://api.heroku.com:443')
-      .get('/apps/testapp')
-      .reply(200, {name: 'testapp'})
+    it('does not find an image to push', () => {
+      let dockerfiles = sandbox.stub(Sanbashi, 'getDockerfiles')
+        .returns([])
 
-    let dockerfiles = sandbox.stub(Sanbashi, 'getDockerfiles')
-      .returns(['/path/to/Dockerfile'])
-    let build = sandbox.stub(Sanbashi, 'buildImage')
-      .withArgs('/path/to/Dockerfile', 'registry.heroku.com/testapp/web', [], '/custom/context/path')
-    let push = sandbox.stub(Sanbashi, 'pushImage')
-      .withArgs('registry.heroku.com/testapp/web')
-
-    return cmd.run({app: 'testapp', args: ['web'], flags: {'context-path': '/custom/context/path'}})
-      .then(() => expect(cli.stdout).to.contain('Building web (/path/to/Dockerfile)'))
-      .then(() => expect(cli.stdout).to.contain('Pushing web (/path/to/Dockerfile)'))
-      .then(() => sandbox.assert.calledOnce(dockerfiles))
-      .then(() => sandbox.assert.calledOnce(build))
-      .then(() => sandbox.assert.calledOnce(push))
-      .then(() => api.done())
-  })
-
-  it('does not find an image to push', () => {
-    sandbox.stub(process, 'exit')
-
-    let api = nock('https://api.heroku.com:443')
-      .get('/apps/testapp')
-      .reply(200, {name: 'testapp'})
-
-    let dockerfiles = sandbox.stub(Sanbashi, 'getDockerfiles')
-      .returns([])
-
-    return cmd.run({app: 'testapp', args: ['web'], flags: {}})
-      .then(() => expect(cli.stderr).to.contain('No images to push'))
-      .then(() => expect(cli.stdout, 'to be empty'))
-      .then(() => expect(process.exit.calledWith(1)).to.equal(true))
-      .then(() => sandbox.assert.calledOnce(dockerfiles))
-      .then(() => api.done())
+      return helpers.assertExit(1, cmd.run({app: 'testapp', args: ['web'], flags: {}}))
+        .then(error => {
+          expect(error.message).to.contain('No images to push')
+          expect(cli.stderr).to.contain('No images to push')
+          expect(cli.stdout).to.equal('')
+          sandbox.assert.calledOnce(dockerfiles)
+        })
+    })
   })
 
   it('requires a process type if we are not recursive', () => {
-    sandbox.stub(process, 'exit')
-
-    return cmd.run({app: 'testapp', args: [], flags: {}})
-      .then(() => expect(cli.stderr).to.contain('Requires either --recursive or one or more process types'))
-      .then(() => expect(cli.stdout, 'to be empty'))
-      .then(() => expect(process.exit.calledWith(1)).to.equal(true))
+    return helpers.assertExit(1, cmd.run({app: 'testapp', args: [], flags: {}}))
+      .then(error => {
+        expect(error.message).to.contain('Requires either --recursive or one or more process types')
+        expect(cli.stderr).to.contain('Requires either --recursive or one or more process types')
+        expect(cli.stdout).to.equal('')
+      })
   })
 
   it('rejects multiple process types if we are not recursive', () => {
-    sandbox.stub(process, 'exit')
-
-    return cmd.run({app: 'testapp', args: ['web', 'worker'], flags: {}})
-      .then(() => expect(cli.stderr).to.contain('Requires exactly one target process type, or --recursive option'))
-      .then(() => expect(cli.stdout, 'to be empty'))
-      .then(() => expect(process.exit.calledWith(1)).to.equal(true))
+    return helpers.assertExit(1, cmd.run({app: 'testapp', args: ['web', 'worker'], flags: {}}))
+      .then(error => {
+        expect(error.message).to.contain('Requires exactly one target process type, or --recursive option')
+        expect(cli.stderr).to.contain('Requires exactly one target process type, or --recursive option')
+        expect(cli.stdout).to.equal('')
+      })
   })
 })
