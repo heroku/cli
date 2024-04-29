5f62176556898131821d7f07d70f54856be9ea46
Justin Wilaby <jwilaby@salesforce.com>
2024-04-29 11:57:28 -0700

    Added module scoped functions as class members

--- packages/cli/src/commands/spaces/ps.ts
+++ packages/cli/src/commands/spaces/ps.ts
@@ -6,7 +6,7 @@ import {ago} from '../../lib/time'
 
 const getProcessNum = (s: string) => Number.parseInt(s.split('.', 2)[1], 10)
 
-type SpaceDyno = {
+type SpaceDynosInfo = {
   app_name: string,
   dynos: Required<Heroku.Dyno>[]
 }
@@ -30,7 +30,7 @@ export default class Ps extends Command {
     }
 
     const [{body: spaceDynos}, {body: space}] = await Promise.all([
-      this.heroku.get<SpaceDyno[]>(`/spaces/${spaceName}/dynos`),
+      this.heroku.get<SpaceDynosInfo[]>(`/spaces/${spaceName}/dynos`),
       this.heroku.get<Heroku.Space>(`/spaces/${spaceName}`),
     ])
 
@@ -47,47 +47,47 @@ export default class Ps extends Command {
     if (flags.json) {
       ux.styledJSON(spaceDynos)
     } else {
-      render(spaceDynos)
+      this.render(spaceDynos)
     }
   }
-}
 
-function render(spaceDynos?: SpaceDyno[]) {
-  spaceDynos?.forEach(spaceDyno => {
-    printDynos(spaceDyno.app_name, spaceDyno.dynos)
-  })
-}
-
-function printDynos(appName: string, dynos: Required<Heroku.Dyno>[]) {
-  const dynosByCommand = new Map<string, string[]>()
-  for (const dyno of dynos) {
-    const since = ago(new Date(dyno.updated_at))
-    const size = dyno.size ?? '1X'
-    let key = ''
-    let item = ''
-    if (dyno.type === 'run') {
-      key = 'run: one-off processes'
-      item = `${dyno.name} (${size}): ${dyno.state} ${since}: ${dyno.command}`
-    } else {
-      key = `${color.green(dyno.type)} (${color.cyan(size)}): ${dyno.command}`
-      const state = dyno.state === 'up' ? color.green(dyno.state) : color.yellow(dyno.state)
-      item = `${dyno.name}: ${color.green(state)} ${color.dim(since)}`
-    }
-
-    if (!dynosByCommand.has(key)) {
-      dynosByCommand.set(key, [])
-    }
-
-    dynosByCommand.get(key)?.push(item)
+  private render(spaceDynos?: SpaceDynosInfo[]) {
+    spaceDynos?.forEach(spaceDyno => {
+      this.printDynos(spaceDyno.app_name, spaceDyno.dynos)
+    })
   }
 
-  for (const [key, dynos] of dynosByCommand) {
-    ux.styledHeader(`${appName} ${key} (${color.yellow(dynos.length)})`)
-    dynos.sort((a, b) => getProcessNum(a) - getProcessNum(b))
+  private printDynos(appName: string, dynos: Required<Heroku.Dyno>[]) {
+    const dynosByCommand = new Map<string, string[]>()
     for (const dyno of dynos) {
-      ux.log(dyno)
+      const since = ago(new Date(dyno.updated_at))
+      const size = dyno.size ?? '1X'
+      let key = ''
+      let item = ''
+      if (dyno.type === 'run') {
+        key = 'run: one-off processes'
+        item = `${dyno.name} (${size}): ${dyno.state} ${since}: ${dyno.command}`
+      } else {
+        key = `${color.green(dyno.type)} (${color.cyan(size)}): ${dyno.command}`
+        const state = dyno.state === 'up' ? color.green(dyno.state) : color.yellow(dyno.state)
+        item = `${dyno.name}: ${color.green(state)} ${color.dim(since)}`
+      }
+
+      if (!dynosByCommand.has(key)) {
+        dynosByCommand.set(key, [])
+      }
+
+      dynosByCommand.get(key)?.push(item)
     }
 
-    ux.log()
+    for (const [key, dynos] of dynosByCommand) {
+      ux.styledHeader(`${appName} ${key} (${color.yellow(dynos.length)})`)
+      dynos.sort((a, b) => getProcessNum(a) - getProcessNum(b))
+      for (const dyno of dynos) {
+        ux.log(dyno)
+      }
+
+      ux.log()
+    }
   }
 }
