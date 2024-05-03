7eeb63c8921a00a061d2716c508d875af8282d44
Eric <eblack@salesforce.com>
2024-05-03 10:21:49 -0700

    refactor(spaces): Move command spaces:hosts to oclif (#2845)
    * Convert spaces:hosts to oclif
    
    * Fix output
    
    * Fix test data legibility.
    
    * Update packages/cli/src/commands/spaces/hosts.ts
    
    Co-authored-by: Santiago Bosio <santiago.bosio@gmail.com>
    
    * Add variant to hosts command
    
    ---------
    
    Co-authored-by: Santiago Bosio <santiago.bosio@gmail.com>

--- packages/cli/src/lib/spaces/format.ts
+++ packages/cli/src/lib/spaces/format.ts
@@ -1,3 +1,21 @@
+import color from '@heroku-cli/color'
+
 export function displayCIDR(cidr: string[] | undefined) {
   return cidr?.join(', ') ?? ''
+}
+
+export function hostStatus(s: string) {
+  switch (s) {
+  case 'available':
+    return `${color.green(s)}`
+  case 'under-assessment':
+    return `${color.yellow(s)}`
+  case 'permanent-failure':
+  case 'released-permanent-failure':
+    return `${color.red(s)}`
+  case 'released':
+    return `${color.gray(s)}`
+  default:
+    return s
+  }
 }
