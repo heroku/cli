b10f6bd56d7d0d93ae38524b19ab24fcca4df152
Eric <eblack@salesforce.com>
2024-05-07 08:54:50 -0700

    refactor(spaces): Move command spaces:vpn:info to oclif (#2869)
    * Convert vpn:info to oclif
    
    * Remove name as a flag field

--- packages/spaces/index.js
+++ packages/spaces/index.js
@@ -8,7 +8,6 @@ exports.topics = [
 exports.commands = [
   require('./commands/vpn/connect'),
   require('./commands/vpn/index'),
-  require('./commands/vpn/info'),
   require('./commands/vpn/wait'),
   require('./commands/vpn/destroy'),
   require('./commands/vpn/update'),
