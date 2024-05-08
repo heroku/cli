675f4bcff7246885a6e4eda8c7a8927ba74ed500
Katy Bowman <katy.bowman@salesforce.com>
2024-05-08 16:23:40 -0400

    refactor: complete migration of vpn:wait command

--- packages/cli/src/commands/spaces/vpn/wait.ts
+++ packages/cli/src/commands/spaces/vpn/wait.ts
@@ -1,65 +1,55 @@
 import color from '@heroku-cli/color'
 import {Command, flags} from '@heroku-cli/command'
-import {Args, ux} from '@oclif/core'
+import {ux} from '@oclif/core'
+import Spinner from '@oclif/core/lib/cli-ux/action/spinner'
 import * as Heroku from '@heroku-cli/schema'
+import {displayVPNConfigInfo} from '../../../lib/spaces/vpn-connections'
 
-const cli = require('@heroku/heroku-cli-util')
 const wait = (ms: number) => new Promise(resolve => {
   setTimeout(resolve, ms)
 })
-function check(val: string | undefined, message:string) {
-  if (!val)
-    throw new Error(`${message}.\nUSAGE: heroku spaces:vpn:wait --space my-space vpn-connection-name`)
-}
 
 export default class Wait extends Command {
     static topic = 'spaces';
     static description = 'wait for VPN Connection to be created';
     static flags = {
-      space: flags.string({char: 's', description: 'space the vpn connection belongs to'}),
-      name: flags.string({char: 'n', description: 'name or id of the vpn connection to wait for'}),
+      space: flags.string({char: 's', description: 'space the vpn connection belongs to', required: true}),
+      name: flags.string({char: 'n', description: 'name or id of the vpn connection to wait for', required: true}),
       json: flags.boolean({description: 'output in json format'}),
       interval: flags.string({char: 'i', description: 'seconds to wait between poll intervals'}),
       timeout: flags.string({char: 't', description: 'maximum number of seconds to wait'}),
     };
 
-    static args = {
-      name: Args.string({hidden: true}),
-    };
-
     public async run(): Promise<void> {
-      const {flags, argv, args} = await this.parse(Wait)
-      const space = flags.space
-      check(space, 'Space name required')
-      const name = flags.name || args.name
-      check(name, 'VPN connection name required')
+      const {flags} = await this.parse(Wait)
+      const {name, space} = flags
       const interval = (flags.interval ? Number.parseInt(flags.interval, 10) : 10) * 1000
       const timeout = (flags.timeout ? Number.parseInt(flags.timeout, 10) : 20 * 60) * 1000
       const deadline = new Date(Date.now() + timeout)
-      let lib = require('../../lib/vpn-connections')(heroku)
-      let info = await lib.getVPNConnection(space, name)
-      if (info.status === 'active') {
+      let {body: vpnConnection} = await this.heroku.get<Heroku.PrivateSpacesVpn>(`/spaces/${space}/vpn-connections/${name}`)
+      if (vpnConnection.status === 'active') {
         ux.log('VPN has been allocated.')
         return
       }
 
-      const spinner = new cli.Spinner({text: `Waiting for VPN Connection ${color.green(name)} to allocate...`})
-      spinner.start()
-      do {
-        if (Date.now() >= deadline) {
+      const action = new Spinner()
+      action.start(`Waiting for VPN Connection ${color.green(name)} to allocate...`)
+      while (vpnConnection.status !== 'active') {
+        if (new Date() > deadline) {
           throw new Error('Timeout waiting for VPN to become allocated.')
         }
 
-        if (info.status === 'failed') {
-          throw new Error(info.status_message)
+        if (vpnConnection.status === 'failed') {
+          throw new Error(vpnConnection.status_message)
         }
 
         await wait(interval)
-        info = await lib.getVPNConnection(space, name)
-      } while (info.status !== 'active')
+        const {body: updatedVpnConnection} = await this.heroku.get<Heroku.PrivateSpacesVpn>(`/spaces/${space}/vpn-connections/${name}`)
+        vpnConnection = updatedVpnConnection
+      }
 
-      spinner.stop('done\n')
-      var config = await lib.getVPNConnection(space, name)
-      lib.displayVPNConfigInfo(space, name, config)
+      action.stop('done\n')
+      const config = await this.heroku.get<Heroku.PrivateSpacesVpn>(`/spaces/${space}/vpn-connections/${name}`)
+      displayVPNConfigInfo(space, name, config)
     }
 }
