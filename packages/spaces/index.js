c3353c09bb661e63980099bed3c1ba2f0b2fa023
Santiago Bosio <santiago.bosio@gmail.com>
2024-05-08 15:44:11 -0300

    Move command 'spaces:vpn:connections' to CLI

--- packages/spaces/index.js
+++ packages/spaces/index.js
@@ -7,7 +7,6 @@ exports.topics = [
 
 exports.commands = [
   require('./commands/vpn/connect'),
-  require('./commands/vpn/index'),
   require('./commands/vpn/wait'),
   require('./commands/vpn/destroy'),
   require('./commands/vpn/update'),
