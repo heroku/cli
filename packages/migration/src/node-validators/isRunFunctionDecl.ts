c92abc27028e33602f996cb5f91a93690c7c5b7c
Katy Bowman <katy.bowman@salesforce.com>
2024-05-15 09:54:11 -0400

    chore: remove the migration package

--- packages/migration/src/node-validators/isRunFunctionDecl.ts
+++
@@ -1,5 +0,0 @@
-import ts from 'typescript'
-
-export function isRunFunctionDecl(node: ts.Node): node is ts.FunctionDeclaration & {name: {text: 'run'}} {
-  return ts.isFunctionDeclaration(node) && ts.isIdentifier(node.name) && node.name.text === 'run'
-}
