0159b08d614de554fd23d64a213f15ad20c39d86
Katy Bowman <katy.bowman@salesforce.com>
2024-05-16 13:43:35 -0400

    chore: remove the migration package (#2884)
    * chore: remove the migration package
    
    * chore: remove migration scripts from package.json
    
    * fix package.json

--- packages/migration/src/node-validators/isCommandMigrationCandidate.ts
+++
@@ -1,7 +0,0 @@
-import {isRunFunctionDecl} from './isRunFunctionDecl.js'
-import ts from 'typescript'
-
-export function isCommandMigrationCandidate(sourceFile: ts.SourceFile): boolean {
-  // todo: handle run in module.exports case. example: packages/redis-v5/commands/info.js
-  return sourceFile.statements.some(stmt => isRunFunctionDecl(stmt))
-}
