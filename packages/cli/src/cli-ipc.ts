ae49bfbc344bb75bf6a5ec98ba461430443a3a81
Zane Whitfield <zwhitfield@salesforce.com>
2024-06-10 09:56:31 -0700

    Add environment variable to enable IPC connections

--- packages/cli/src/cli-ipc.ts
+++ packages/cli/src/cli-ipc.ts
@@ -1,5 +1,18 @@
+import {ux} from '@oclif/core'
+import color from '@heroku-cli/color'
 const net = require('node:net')
+const ipcEnabled = process.env.ALLOW_IPC_CONNECT === 'true'
 
 export function setupIPC(config: any, opts: any) {
+  // cancel IPC connection setup if ALLOW_IPC_CONNECT is not enabled
+  if (!ipcEnabled) {
+    return
+  }
+
+  ux.log(color.heroku(`Allow IPC Connection: ${color.green('true')}`))
+
   // setup IPC connection
+
+  // check for specific arg values like --interactive
+  //   console.log('argv', opts.argv)
 }
