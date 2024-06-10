bab4c0dae3192a4bd071842013e1a4c2d7d09ea7
Zane Whitfield <zwhitfield@salesforce.com>
2024-06-10 14:23:18 -0700

    Add console logging and functions for socket events

--- packages/cli/src/cli-ipc.ts
+++ packages/cli/src/cli-ipc.ts
@@ -1,5 +1,6 @@
 import {ux} from '@oclif/core'
 import color from '@heroku-cli/color'
+const {Console} = require('node:console')
 const net = require('node:net')
 const ipcEnabled = process.env.ALLOW_IPC_CONNECT === 'true'
 
@@ -11,7 +12,37 @@ export function setupIPC(config: any, opts: any) {
 
   ux.log(color.heroku(`Allow IPC Connection: ${color.green('true')}`))
 
-  // setup IPC connection
+  const vsCodeLogger = new Console({stdout: process.stdout, stderr: process.stderr})
+  const vsCodeIpcPath = 'example/path/here'
+
+  const connectListener = () => {
+    ux.log('Connected to IPC server')
+    socketConnection.end()
+  }
+
+  // establish IPC connection
+  const socketConnection = net.createConnection(vsCodeIpcPath, connectListener)
+
+  // establish connection capabilities
+
+  // listen for 'data' events
+  socketConnection.on('data', (data: Buffer | string) => {
+    // read data and log output
+    vsCodeLogger.log('Received from server: ', data.toString())
+  })
+
+  // listen for 'end' events
+  socketConnection.on('end', () => {
+    // close IPC connection
+    socketConnection.end()
+  })
+
+  // listen for 'error' events
+  socketConnection.on('error', () => {
+    console.log('An error occurred')
+    // close IPC connection
+    socketConnection.end()
+  })
 
   // check for specific arg values like --interactive
   //   console.log('argv', opts.argv)
