0d504ec18685f9cef95f291fba08c9af8b349748
Katy Bowman <katy.bowman@salesforce.com>
2024-07-09 09:21:45 -0400

    chore: reapply bump filesize from 4.0.0 to 10.1.2

--- packages/cli/src/commands/apps/info.ts
+++ packages/cli/src/commands/apps/info.ts
@@ -3,7 +3,7 @@ import {Command, flags} from '@heroku-cli/command'
 import * as Heroku from '@heroku-cli/schema'
 import * as util from 'util'
 import * as _ from 'lodash'
-const filesize = require('filesize')
+import {filesize} from 'filesize'
 const {countBy, snakeCase} = _
 
 function formatDate(date: Date) {
@@ -61,7 +61,7 @@ function print(info: Heroku.App, addons: Heroku.AddOn[], collaborators: Heroku.C
   if (info.app.archived_at) data['Archived At'] = formatDate(new Date(info.app.archived_at))
   if (info.app.cron_finished_at) data['Cron Finished At'] = formatDate(new Date(info.app.cron_finished_at))
   if (info.app.cron_next_run) data['Cron Next Run'] = formatDate(new Date(info.app.cron_next_run))
-  if (info.app.database_size) data['Database Size'] = filesize(info.app.database_size, {round: 0})
+  if (info.app.database_size) data['Database Size'] = filesize(info.app.database_size, {standard: 'jedec', round: 0})
   if (info.app.create_status !== 'complete') data['Create Status'] = info.app.create_status
   if (info.app.space) data.Space = info.app.space.name
   if (info.app.space && info.app.internal_routing) data['Internal Routing'] = info.app.internal_routing
@@ -70,8 +70,8 @@ function print(info: Heroku.App, addons: Heroku.AddOn[], collaborators: Heroku.C
   data['Auto Cert Mgmt'] = info.app.acm
   data['Git URL'] = info.app.git_url
   data['Web URL'] = info.app.web_url
-  data['Repo Size'] = filesize(info.app.repo_size, {round: 0})
-  data['Slug Size'] = filesize(info.app.slug_size, {round: 0})
+  data['Repo Size'] = filesize(info.app.repo_size, {standard: 'jedec', round: 0})
+  data['Slug Size'] = filesize(info.app.slug_size, {standard: 'jedec', round: 0})
   data.Owner = info.app.owner.email
   data.Region = info.app.region.name
   data.Dynos = countBy(info.dynos, 'type')
@@ -152,14 +152,14 @@ repo_size=5000000
       if (info.app.archived_at) print('archived_at', formatDate(new Date(info.app.archived_at)))
       if (info.app.cron_finished_at) print('cron_finished_at', formatDate(new Date(info.app.cron_finished_at)))
       if (info.app.cron_next_run) print('cron_next_run', formatDate(new Date(info.app.cron_next_run)))
-      if (info.app.database_size) print('database_size', filesize(info.app.database_size, {round: 0}))
+      if (info.app.database_size) print('database_size', filesize(info.app.database_size, {standard: 'jedec', round: 0}))
       if (info.app.create_status !== 'complete') print('create_status', info.app.create_status)
       if (info.pipeline_coupling) print('pipeline', `${info.pipeline_coupling.pipeline.name}:${info.pipeline_coupling.stage}`)
 
       print('git_url', info.app.git_url)
       print('web_url', info.app.web_url)
-      print('repo_size', filesize(info.app.repo_size, {round: 0}))
-      print('slug_size', filesize(info.app.slug_size, {round: 0}))
+      print('repo_size', filesize(info.app.repo_size, {standard: 'jedec', round: 0}))
+      print('slug_size', filesize(info.app.slug_size, {standard: 'jedec', round: 0}))
       print('owner', info.app.owner.email)
       print('region', info.app.region.name)
       print('dynos', util.inspect(countBy(info.dynos, 'type')))
