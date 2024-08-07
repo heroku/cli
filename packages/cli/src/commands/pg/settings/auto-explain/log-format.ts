74be27afb11e128251c6c4393020cd6aba1740a2
Daniel Bright <daniel.bright@salesforce.com>
2024-08-07 16:00:07 -0700

    created pg setting auto explain format

---
+++ packages/cli/src/commands/pg/settings/auto-explain/log-format.ts
@@ -0,0 +1,27 @@
+import {Args} from '@oclif/core'
+import {PGSettingsCommand} from '../../../../lib/pg/setter'
+import {Setting, SettingKey} from '../../../../lib/pg/types'
+import heredoc from 'tsheredoc'
+
+export default class LogFormat extends PGSettingsCommand {
+  static topic = 'pg'
+  static description = heredoc(`
+    Selects the EXPLAIN output format to be used.
+    The allowed values are text, xml, json, and yaml. The default is text.
+  `)
+
+  static args = {
+    database: Args.string(),
+    value: Args.string({options: ['text', 'json', 'yaml', 'xml']}),
+  }
+
+  protected settingKey: SettingKey = 'auto_explain.log_format'
+
+  protected explain(setting: Setting<string>) {
+    return `Auto explain log output will log in ${setting.value} format.`
+  }
+
+  protected convertValue(val: string): string {
+    return val
+  }
+}
