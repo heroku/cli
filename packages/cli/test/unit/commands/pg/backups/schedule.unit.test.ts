bfac6538617085d82e351227c68853d324b6b4a0
Santiago Bosio <santiago.bosio@gmail.com>
2024-08-19 12:19:04 -0300

    fix(pg): Restoring behavior for backup scheduling (#2981)
    * Restoring behavior for backup scheduling
    
    * Adding unit tests

--- packages/cli/test/unit/commands/pg/backups/schedule.unit.test.ts
+++ packages/cli/test/unit/commands/pg/backups/schedule.unit.test.ts
@@ -5,12 +5,127 @@ import * as nock from 'nock'
 import heredoc from 'tsheredoc'
 import {expect} from 'chai'
 import stripAnsi = require('strip-ansi')
+import {CLIError} from '@oclif/core/lib/errors'
+import {HTTPError} from 'http-call'
 
-const shouldSchedule = function (cmdRun: (args: string[]) => Promise<any>) {
-  const continuousProtectionWarning = heredoc('Logical backups of large databases are likely to fail.')
+describe('pg:backups:schedule', function () {
+  let api: nock.Scope
+  let data: nock.Scope
 
-  beforeEach(function () {
-    nock('https://api.heroku.com')
+  context('with correct arguments', function () {
+    const continuousProtectionWarning = 'Logical backups of large databases are likely to fail.'
+
+    beforeEach(function () {
+      api = nock('https://api.heroku.com')
+        .post('/actions/addon-attachments/resolve', {
+          app: 'myapp',
+          addon_attachment: 'DATABASE_URL',
+          addon_service: 'heroku-postgresql',
+        })
+        .reply(200, [
+          {
+            addon: {
+              id: 1,
+              name: 'postgres-1',
+              plan: {name: 'heroku-postgresql:standard-0'},
+            },
+            config_vars: [
+              'DATABASE_URL',
+            ],
+            name: 'DATABASE',
+          },
+        ])
+      data = nock('https://api.data.heroku.com')
+        .post('/client/v11/databases/1/transfer-schedules', {
+          hour: '06',
+          timezone: 'America/New_York',
+          schedule_name: 'DATABASE_URL',
+        })
+        .reply(201)
+    })
+
+    afterEach(function () {
+      nock.cleanAll()
+      api.done()
+      data.done()
+    })
+
+    it('schedules a backup', async function () {
+      const dbA = {info: [
+        {name: 'Continuous Protection', values: ['On']},
+      ]}
+      nock('https://api.data.heroku.com')
+        .get('/client/v11/databases/1')
+        .reply(200, dbA)
+
+      await runCommand(Cmd, ['--at', '06:00 EDT', '--app', 'myapp'])
+
+      expect(stdout.output).to.equal('')
+      expect(stderr.output).to.include(heredoc(`
+        Scheduling automatic daily backups of postgres-1 at 06:00 America/New_York...
+        Scheduling automatic daily backups of postgres-1 at 06:00 America/New_York... done
+      `))
+    })
+
+    it('warns user that logical backups are error prone if continuous protection is on', async function () {
+      const dbA = {info: [
+        {name: 'Continuous Protection', values: ['On']},
+      ]}
+      nock('https://api.data.heroku.com')
+        .get('/client/v11/databases/1')
+        .reply(200, dbA)
+
+      await runCommand(Cmd, ['--at', '06:00 EDT', '--app', 'myapp'])
+
+      expect(stripAnsi(stderr.output)).to.include(continuousProtectionWarning)
+    })
+
+    it('does not warn user that logical backups are error prone if continuous protection is off', async function () {
+      const dbA = {info: [
+        {name: 'Continuous Protection', values: ['Off']},
+      ]}
+      nock('https://api.data.heroku.com')
+        .get('/client/v11/databases/1')
+        .reply(200, dbA)
+
+      await runCommand(Cmd, ['--at', '06:00 EDT', '--app', 'myapp'])
+
+      expect(stripAnsi(stderr.output)).not.to.include(continuousProtectionWarning)
+    })
+  })
+
+  it('errors when the scheduled time has an invalid hour value', async function () {
+    try {
+      await runCommand(Cmd, ['--at', '24:00', '--app', 'myapp'])
+    } catch (error: unknown) {
+      const {message, oclif} = error as CLIError
+      expect(message).to.eq("Invalid schedule format: expected --at '[HOUR]:00 [TIMEZONE]'")
+      expect(oclif.exit).to.equal(1)
+    }
+  })
+
+  it('errors when the scheduled time has an invalid time zone value', async function () {
+    try {
+      await runCommand(Cmd, ['--at', '01:00 New York', '--app', 'myapp'])
+    } catch (error: unknown) {
+      const {message, oclif} = error as CLIError
+      expect(message).to.eq("Invalid schedule format: expected --at '[HOUR]:00 [TIMEZONE]'")
+      expect(oclif.exit).to.equal(1)
+    }
+  })
+
+  it('errors when the scheduled time specifies minutes', async function () {
+    try {
+      await runCommand(Cmd, ['--at', '06:15 EDT', '--app', 'myapp'])
+    } catch (error: unknown) {
+      const {message, oclif} = error as CLIError
+      expect(message).to.eq("Invalid schedule format: expected --at '[HOUR]:00 [TIMEZONE]'")
+      expect(oclif.exit).to.equal(1)
+    }
+  })
+
+  it('accepts a correctly formatted time string even if the time zone might not be correct', async function () {
+    api = nock('https://api.heroku.com')
       .post('/actions/addon-attachments/resolve', {
         app: 'myapp',
         addon_attachment: 'DATABASE_URL',
@@ -29,56 +144,25 @@ const shouldSchedule = function (cmdRun: (args: string[]) => Promise<any>) {
           name: 'DATABASE',
         },
       ])
-    nock('https://api.data.heroku.com')
+    data = nock('https://api.data.heroku.com')
+      .get('/client/v11/databases/1')
+      .reply(200, {info: []})
       .post('/client/v11/databases/1/transfer-schedules', {
         hour: '06',
-        timezone: 'America/New_York',
+        timezone: 'New_York',
         schedule_name: 'DATABASE_URL',
       })
-      .reply(201)
-  })
-  afterEach(function () {
+      .reply(400, {id: 'bad_request', message: 'Bad request.'})
+
+    try {
+      await runCommand(Cmd, ['--at', '06:00 New_York', '--app', 'myapp'])
+    } catch (error: unknown) {
+      const {message} = error as CLIError
+      expect(message).to.contain('Bad request.')
+    }
+
+    api.done()
+    data.done()
     nock.cleanAll()
   })
-
-  it('schedules a backup', async function () {
-    const dbA = {info: [
-      {name: 'Continuous Protection', values: ['On']},
-    ]}
-    nock('https://api.data.heroku.com')
-      .get('/client/v11/databases/1')
-      .reply(200, dbA)
-    await cmdRun(['--at', '06:00 EDT', '--app', 'myapp'])
-    expect(stdout.output).to.equal('')
-    expect(stderr.output).to.include(heredoc(`
-      Scheduling automatic daily backups of postgres-1 at 06:00 America/New_York...
-      Scheduling automatic daily backups of postgres-1 at 06:00 America/New_York... done
-    `))
-  })
-
-  it('warns user that logical backups are error prone if continuous protection is on', async function () {
-    const dbA = {info: [
-      {name: 'Continuous Protection', values: ['On']},
-    ]}
-    nock('https://api.data.heroku.com')
-      .get('/client/v11/databases/1')
-      .reply(200, dbA)
-    await cmdRun(['--at', '06:00 EDT', '--app', 'myapp'])
-    expect(stripAnsi(stderr.output)).to.include(continuousProtectionWarning)
-  })
-
-  it('does not warn user that logical backups are error prone if continuous protection is off', async function () {
-    const dbA = {info: [
-      {name: 'Continuous Protection', values: ['Off']},
-    ]}
-    nock('https://api.data.heroku.com')
-      .get('/client/v11/databases/1')
-      .reply(200, dbA)
-    await cmdRun(['--at', '06:00 EDT', '--app', 'myapp'])
-    expect(stripAnsi(stderr.output)).not.to.include(continuousProtectionWarning)
-  })
-}
-
-describe('pg:backups:schedule', function () {
-  shouldSchedule((args: string[]) => runCommand(Cmd, args))
 })
