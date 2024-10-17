ca0eef5b6ad63b40c8ae0f73c8b03941bfa3ce32
Katy Bowman <katy.bowman@salesforce.com>
2024-10-17 14:48:13 -0400

    chore: update telemetry/index tests to use fixtures

--- packages/cli/test/fixtures/telemetry/fixtures.ts
+++ packages/cli/test/fixtures/telemetry/fixtures.ts
@@ -1,4 +1,6 @@
-export const spaceTelemetryDrain1 = {
+import {TelemetryDrain} from '../../../src/lib/types/telemetry'
+
+export const spaceTelemetryDrain1: TelemetryDrain = {
   id: '44444321-5717-4562-b3fc-2c963f66afa6',
   owner: {id: '12345678-5717-4562-b3fc-2c963f66afa6', type: 'space', name: 'myspace'},
   capabilities: ['traces', 'metrics', 'logs'],
@@ -12,7 +14,7 @@ export const spaceTelemetryDrain1 = {
   },
 }
 
-export const appTelemetryDrain1 = {
+export const appTelemetryDrain1: TelemetryDrain = {
   id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
   owner: {id: '87654321-5717-4562-b3fc-2c963f66afa6', type: 'app', name: 'myapp'},
   capabilities: ['traces', 'metrics'],
@@ -26,7 +28,7 @@ export const appTelemetryDrain1 = {
   },
 }
 
-export const appTelemetryDrain2 = {
+export const appTelemetryDrain2: TelemetryDrain = {
   id: '55555f64-5717-4562-b3fc-2c963f66afa6',
   owner: {id: '87654321-5717-4562-b3fc-2c963f66afa6', type: 'app', name: 'myapp'},
   capabilities: ['logs'],
