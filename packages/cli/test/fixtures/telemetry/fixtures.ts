2005912516d69e59d9469a7578f7ab5b55f44090
Katy Bowman <katy.bowman@salesforce.com>
2024-10-17 14:42:59 -0400

    feat: separate calls for app and space drains and update tests

--- packages/cli/test/fixtures/telemetry/fixtures.ts
+++ packages/cli/test/fixtures/telemetry/fixtures.ts
@@ -1,7 +1,41 @@
-export const addAllDrainsConfig = {
-  signals: ['traces', 'metrics', 'logs'],
+export const spaceTelemetryDrain1 = {
+  id: '44444321-5717-4562-b3fc-2c963f66afa6',
+  owner: {id: '12345678-5717-4562-b3fc-2c963f66afa6', type: 'space', name: 'myspace'},
+  capabilities: ['traces', 'metrics', 'logs'],
   exporter: {
-    endpoint: 'https://api.testendpoint.com',
-    transport: 'otlphttp',
+    type: 'otlphttp',
+    endpoint: 'https://api.honeycomb.io/',
+    headers: {
+      'x-honeycomb-team': 'your-api-key',
+      'x-honeycomb-dataset': 'your-dataset',
+    },
+  },
+}
+
+export const appTelemetryDrain1 = {
+  id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+  owner: {id: '87654321-5717-4562-b3fc-2c963f66afa6', type: 'app', name: 'myapp'},
+  capabilities: ['traces', 'metrics'],
+  exporter: {
+    type: 'otlphttp',
+    endpoint: 'https://api.honeycomb.io/',
+    headers: {
+      'x-honeycomb-team': 'your-api-key',
+      'x-honeycomb-dataset': 'your-dataset',
+    },
+  },
+}
+
+export const appTelemetryDrain2 = {
+  id: '55555f64-5717-4562-b3fc-2c963f66afa6',
+  owner: {id: '87654321-5717-4562-b3fc-2c963f66afa6', type: 'app', name: 'myapp'},
+  capabilities: ['logs'],
+  exporter: {
+    type: 'otlphttp',
+    endpoint: 'https://api.papertrail.com/',
+    headers: {
+      'x-papertrail-team': 'your-api-key',
+      'x-papertrail-dataset': 'your-dataset',
+    },
   },
 }
--- packages/cli/test/unit/commands/telemetry/add.unit.test.ts
+++ packages/cli/test/unit/commands/telemetry/add.unit.test.ts
@@ -1,76 +1,25 @@
-import {stdout, stderr} from 'stdout-stderr'
+import {stdout} from 'stdout-stderr'
 import Cmd  from '../../../../src/commands/telemetry/add'
 import runCommand from '../../../helpers/runCommand'
-import * as chai from 'chai'
-import * as chaiNock from 'chai-nock'
+import {expect} from 'chai'
 import * as nock from 'nock'
 import expectOutput from '../../../helpers/utils/expectOutput'
-import heredoc from 'tsheredoc'
-import {TelemetryDrains} from '../../../../src/lib/types/telemetry'
-import {addAllDrainsConfig} from '../../../fixtures/telemetry/fixtures'
+import {spaceTelemetryDrain1, appTelemetryDrain1} from '../../../fixtures/telemetry/fixtures'
 
-chai.use(chaiNock)
-const expect = chai.expect
+const appId = appTelemetryDrain1.owner.id
+const spaceId = spaceTelemetryDrain1.owner.id
+const testEndpoint = appTelemetryDrain1.exporter.endpoint
 
 describe('telemetry:index', function () {
-  const appId = '87654321-5717-4562-b3fc-2c963f66afa6'
-  const spaceId = '12345678-5717-4562-b3fc-2c963f66afa6'
-  let appTelemetryDrains: TelemetryDrains
-  let spaceTelemetryDrains: TelemetryDrains
-
-  beforeEach(function () {
-    spaceTelemetryDrains = [
-      {
-        id: '44444321-5717-4562-b3fc-2c963f66afa6',
-        owner: {id: spaceId, type: 'space', name: 'myspace'},
-        capabilities: ['traces', 'metrics', 'logs'],
-        exporter: {
-          type: 'otlphttp',
-          endpoint: 'https://api.honeycomb.io/',
-          headers: {
-            'x-honeycomb-team': 'your-api-key',
-            'x-honeycomb-dataset': 'your-dataset',
-          },
-        },
-      },
-    ]
-    appTelemetryDrains = [
-      {
-        id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-        owner: {id: appId, type: 'app', name: 'myapp'},
-        capabilities: ['traces', 'metrics'],
-        exporter: {
-          type: 'otlphttp',
-          endpoint: 'https://api.honeycomb.io/',
-          headers: {
-            'x-honeycomb-team': 'your-api-key',
-            'x-honeycomb-dataset': 'your-dataset',
-          },
-        },
-      },
-      {
-        id: '55555f64-5717-4562-b3fc-2c963f66afa6',
-        owner: {id: appId, type: 'app', name: 'myapp'},
-        capabilities: ['logs'],
-        exporter: {
-          type: 'otlphttp',
-          endpoint: 'https://api.papertrail.com/',
-          headers: {
-            'x-papertrail-team': 'your-api-key',
-            'x-papertrail-dataset': 'your-dataset',
-          },
-        },
-      },
-    ]
-  })
-
   afterEach(function () {
     return nock.cleanAll()
   })
 
   it('returns an error if an app, remote, or space is not set', async function () {
     try {
-      await runCommand(Cmd)
+      await runCommand(Cmd, [
+        '{"x-honeycomb-team": "your-api-key", "x-honeycomb-dataset": "your-dataset"}',
+      ])
     } catch (error) {
       const {message} = error as { message: string }
       expect(message).to.contain('Exactly one of the following must be provided: --app, --remote, --space')
@@ -80,6 +29,7 @@ describe('telemetry:index', function () {
   it('returns an error if values are provided for both the app and the space flags', async function () {
     try {
       await runCommand(Cmd, [
+        '{"x-honeycomb-team": "your-api-key", "x-honeycomb-dataset": "your-dataset"}',
         '--app',
         'myapp',
         '--space',
@@ -91,71 +41,56 @@ describe('telemetry:index', function () {
     }
   })
 
-  it('correctly assembles telemetry configuration when all flags are correctly set', async function () {
-    const telemetryNock = nock('https://api.heroku.com', {reqheaders: {Accept: 'application/vnd.heroku+json; version=3.fir'}})
+  it('successfully creates a telemetry drain for an app', async function () {
+    nock('https://api.heroku.com', {reqheaders: {Accept: 'application/vnd.heroku+json; version=3.sdk'}})
+      .post(`/apps/${appId}/telemetry-drains`)
+      .reply(200, spaceTelemetryDrain1)
+
+    await runCommand(Cmd, [
+      '{"x-honeycomb-team": "your-api-key", "x-honeycomb-dataset": "your-dataset"}',
+      '--app',
+      appId,
+      '--signal',
+      'logs',
+      '--endpoint',
+      testEndpoint,
+      '--transport',
+      'http',
+    ])
+
+    expectOutput(stdout.output, `successfully added drain ${testEndpoint}`)
+  })
+
+  it('successfully creates a telemetry drain for a space', async function () {
+    nock('https://api.heroku.com', {reqheaders: {Accept: 'application/vnd.heroku+json; version=3.sdk'}})
       .post(`/spaces/${spaceId}/telemetry-drains`)
-      .reply(200, spaceTelemetryDrains)
+      .reply(200, spaceTelemetryDrain1)
 
     await runCommand(Cmd, [
-      '--space',
-      spaceId,
-      '--endpoint',
-      'https://api.testendpoint.com',
-      '--transport',
-      'http',
-    ])
-
-    expect(telemetryNock).to.have.been.requestedWith(addAllDrainsConfig)
-  })
-
-  it('sets ["logs", "metrics", "traces"] as the default value for the --signal flag', async function () {
-    const telemetryNock = nock('https://api.heroku.com', {reqheaders: {Accept: 'application/vnd.heroku+json; version=3.fir'}})
-      .put(`/spaces/${spaceId}/telemetry-drains`)
-      .reply(200, spaceTelemetryDrains)
-
-    await runCommand(Cmd, [
-      '--space',
-      spaceId,
-      '--endpoint',
-      'https://api.testendpoint.com',
-      '--transport',
-      'http',
-    ])
-
-    expect(telemetryNock).to.have.been.requestedWith(addAllDrainsConfig)
-  })
-  it('does not accept options other than logs, metrics, traces, or all for the --signal flag', async function () {
-    const telemetryNock = nock('https://api.heroku.com', {reqheaders: {Accept: 'application/vnd.heroku+json; version=3.fir'}})
-      .put(`/spaces/${spaceId}/telemetry-drains`)
-      .reply(200, spaceTelemetryDrains)
-
-    await runCommand(Cmd, [
+      '{"x-honeycomb-team": "your-api-key", "x-honeycomb-dataset": "your-dataset"}',
       '--space',
       spaceId,
       '--signal',
-      'all',
+      'logs',
       '--endpoint',
-      'https://api.testendpoint.com',
+      testEndpoint,
       '--transport',
       'http',
     ])
 
-    expect(telemetryNock).to.have.been.requestedWith(addAllDrainsConfig)
+    expectOutput(stdout.output, `successfully added drain ${testEndpoint}`)
   })
 
-  it('sets the signals to logs, metrics, and traces if "all" is specified for the --signal flag', async function () {
-    nock('https://api.heroku.com', {reqheaders: {Accept: 'application/vnd.heroku+json; version=3.fir'}})
-      .put(`/spaces/${spaceId}/telemetry-drains`)
-      .reply(200, spaceTelemetryDrains)
-
+  it('does not accept options other than logs, metrics, traces, or all for the --signal flag', async function () {
     try {
       await runCommand(Cmd, [
+        '{"x-honeycomb-team": "your-api-key", "x-honeycomb-dataset": "your-dataset"}',
         '--space',
         spaceId,
         '--signal',
         'logs,foo',
         '--endpoint',
-        'https://api.testendpoint.com',
+        testEndpoint,
         '--transport',
         'http',
       ])
@@ -166,18 +101,15 @@ describe('telemetry:index', function () {
   })
 
   it('returns an error when the --signal flag is set to "all" in combination with other options', async function () {
-    nock('https://api.heroku.com', {reqheaders: {Accept: 'application/vnd.heroku+json; version=3.fir'}})
-      .put(`/spaces/${spaceId}/telemetry-drains`)
-      .reply(200, spaceTelemetryDrains)
-
     try {
       await runCommand(Cmd, [
+        '{"x-honeycomb-team": "your-api-key", "x-honeycomb-dataset": "your-dataset"}',
         '--space',
         spaceId,
         '--signal',
         'logs,all',
         '--endpoint',
-        'https://api.testendpoint.com',
+        testEndpoint,
         '--transport',
         'http',
       ])
