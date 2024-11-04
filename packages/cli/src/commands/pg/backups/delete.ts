5ea67e2a730d9e238586d71e622b3f945b6dcd9e
Justin Wilaby <jwilaby@gmail.com>
2024-11-04 11:26:02 -0800

    Update packages/cli/src/commands/pg/backups/delete.ts
    Co-authored-by: Helen Cheng <48834224+heicheng18@users.noreply.github.com>

--- packages/cli/src/commands/pg/backups/delete.ts
+++ packages/cli/src/commands/pg/backups/delete.ts
@@ -15,7 +15,7 @@ export default class Delete extends Command {
   }
 
   static args = {
-    backup_id: Args.string({required: true, description: 'ID of the backup to delete'}),
+    backup_id: Args.string({required: true, description: 'ID of the backup'}),
   }
 
   static examples = [
