bf2f68f1d6917048b19960d4b10631094a20482c
Justin Wilaby <jwilaby@gmail.com>
2024-11-04 11:15:21 -0800

    Update packages/cli/src/commands/pg/backups/cancel.ts
    Co-authored-by: Helen Cheng <48834224+heicheng18@users.noreply.github.com>

--- packages/cli/src/commands/pg/backups/cancel.ts
+++ packages/cli/src/commands/pg/backups/cancel.ts
@@ -13,7 +13,7 @@ export default class Cancel extends Command {
   }
 
   static args = {
-    backup_id: Args.string({description: 'backup ID to cancel. If omitted, we use the last unfinished backup ID.'}),
+    backup_id: Args.string({description: 'ID of the backup. If omitted, we use the last unfinished backup ID.'}),
   }
 
   public async run(): Promise<void> {
