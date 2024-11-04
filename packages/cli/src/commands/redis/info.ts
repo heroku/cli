03456f3f3adf3b7975ddd8557e63c3282d9fbd86
Justin Wilaby <jwilaby@gmail.com>
2024-11-04 11:33:52 -0800

    Update packages/cli/src/commands/redis/info.ts
    Co-authored-by: Helen Cheng <48834224+heicheng18@users.noreply.github.com>

--- packages/cli/src/commands/redis/info.ts
+++ packages/cli/src/commands/redis/info.ts
@@ -13,7 +13,7 @@ export default class Info extends Command {
   }
 
   static args = {
-    database: Args.string({description: 'name of the Key-Value Store database. If omitted, it defaults to the primary instance associated with the app.'}),
+    database: Args.string({description: 'name of the Key-Value Store database. If omitted, it defaults to the primary database associated with the app.'}),
   }
 
   public async run(): Promise<void> {
