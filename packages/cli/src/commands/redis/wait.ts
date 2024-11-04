1dac4833d40d474c296cec3465a55e8733475d15
Justin Wilaby <jwilaby@gmail.com>
2024-11-04 11:37:08 -0800

    Update packages/cli/src/commands/redis/wait.ts
    Co-authored-by: Helen Cheng <48834224+heicheng18@users.noreply.github.com>

--- packages/cli/src/commands/redis/wait.ts
+++ packages/cli/src/commands/redis/wait.ts
@@ -18,7 +18,7 @@ export default class Wait extends Command {
   };
 
   static args = {
-    database: Args.string({required: false, description: 'name of the Key-Value Store database. If omitted, it defaults to the primary instance associated with the app.'}),
+    database: Args.string({required: false, description: 'name of the Key-Value Store database. If omitted, it defaults to the primary database associated with the app.'}),
   };
 
   public async run(): Promise<void> {
