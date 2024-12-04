c1966a246019595522101ff9f5d64eeb0c7dd1c8
Katy Bowman <katy.bowman@salesforce.com>
2024-12-04 15:43:05 -0500

    chore: update legal message for addons:create and test

--- packages/cli/src/commands/addons/create.ts
+++ packages/cli/src/commands/addons/create.ts
@@ -84,21 +84,11 @@ export default class Create extends Command {
     }
 
     if (isInferenceAddon) {
-      ux.warn(
-        '\n\nThis pilot feature is a Beta Service. You may opt to try such Beta Service in your sole discretion. ' +
-    'Any use of the Beta Service is subject to the applicable Beta Services Terms provided at ' +
-    'https://www.salesforce.com/company/legal/customer-agreements/. While use of the pilot feature itself is free, ' +
-    'to the extent such use consumes a generally available Service, you may be charged for that consumption as set ' +
-    'forth in the Documentation. Your continued use of this pilot feature constitutes your acceptance of the foregoing.\n\n' +
-    'For clarity and without limitation, the various third-party machine learning and generative artificial intelligence ' +
-    '(AI) models and applications (each a “Platform”) integrated with the Beta Service are Non-SFDC Applications, ' +
-    'as that term is defined in the Beta Services Terms. Note that these third-party Platforms include features that use ' +
-    'generative AI technology. Due to the nature of generative AI, the output that a Platform generates may be ' +
-    'unpredictable, and may include inaccurate or harmful responses. Before using any generative AI output, Customer is ' +
-    'solely responsible for reviewing the output for accuracy, safety, and compliance with applicable laws and third-party ' +
-    'acceptable use policies. In addition, Customer’s use of each Platform may be subject to the Platform’s own terms and ' +
-    'conditions, compliance with which Customer is solely responsible.\n',
-      )
+      ux.warn(heredoc`
+      Heroku Managed Inference and Agent is a pilot or beta service that is subject to the Beta Services Terms at https://www.salesforce.com/company/legal/customer-agreements/ or a written Unified Pilot Agreement if executed by Customer, and the Non-GA Gen AI and Non-GA Credit Consumption terms in the Product Terms Directory at https://ptd.salesforce.com/?_ga=2.247987783.1372150065.1709219475-629000709.1639001992. While use of this pilot or beta service is itself free, such use may consume GA Heroku credits and/or resources for which the Customer may have paid or be charged. Use of this pilot or beta is at the Customer's sole discretion.
+
+      For clarity and without limitation, the various third-party machine learning and generative artificial intelligence (AI) models and applications (each a “Platform”) integrated with the Beta Service are Non-SFDC Applications, as that term is defined in the Beta Services Terms. Note that these third-party Platforms include features that use generative AI technology. Due to the nature of generative AI, the output that a Platform generates may be unpredictable, and may include inaccurate or harmful responses. Before using any generative AI output, Customer is solely responsible for reviewing the output for accuracy, safety, and compliance with applicable laws and third-party acceptable use policies. In addition, Customer’s use of each Platform may be subject to the Platform’s own terms and conditions, compliance with which Customer is solely responsible.
+    `)
     }
 
     const config = parseConfig(argv)
