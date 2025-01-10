ff20d8e6a30f2156065cabec740d79be5746e4fa
Justin Wilaby <jwilaby@salesforce.com>
2025-01-10 11:14:21 -0800

    Updated to differentiate between fir and non-fir spaces

--- packages/cli/src/commands/spaces/destroy.ts
+++ packages/cli/src/commands/spaces/destroy.ts
@@ -5,8 +5,9 @@ import heredoc from 'tsheredoc'
 import confirmCommand from '../../lib/confirmCommand'
 import {displayNat} from '../../lib/spaces/spaces'
 import color from '@heroku-cli/color'
+import {Space} from '../../lib/types/fir'
 
-type RequiredSpaceWithNat = Required<Heroku.Space> & {outbound_ips?: Required<Heroku.SpaceNetworkAddressTranslation>}
+type RequiredSpaceWithNat = Required<Space> & {outbound_ips?: Required<Heroku.SpaceNetworkAddressTranslation>}
 
 export default class Destroy extends Command {
   static topic = 'spaces'
@@ -40,22 +41,31 @@ export default class Destroy extends Command {
 
     let natWarning = ''
     const {body: space} = await this.heroku.get<RequiredSpaceWithNat>(`/spaces/${spaceName}`)
+
     if (space.state === 'allocated') {
       ({body: space.outbound_ips} = await this.heroku.get<Required<Heroku.SpaceNetworkAddressTranslation>>(`/spaces/${spaceName}/nat`))
       if (space.outbound_ips && space.outbound_ips.state === 'enabled') {
-        natWarning = heredoc`
+        if (space.generation?.name === 'fir') {
+          natWarning = heredoc`
           ${color.dim('===')} ${color.bold('WARNING: Outbound IPs Will Be Reused')}
-          ${color.yellow('⚠️ Deleting this space frees up the following outbound IPv4 and IPv6 IPs for reuse:')}
+          ${color.yellow('⚠️ The following outbound IPs (IPv4 and IPv6) will become available for reuse:')}
           ${color.bold(displayNat(space.outbound_ips) ?? '')}
 
-          ${color.dim('Update the following configurations:')}
+          ${color.dim('Please update the following configurations:')}
           ${color.dim('=')} IP allowlists
           ${color.dim('=')} Firewall rules
           ${color.dim('=')} Security group configurations
           ${color.dim('=')} Network ACLs
 
-          ${color.yellow('Ensure that you remove the listed IPv4 and IPv6 addresses from your security configurations.')}
+          ${color.yellow('Ensure all IPv4 and IPv6 addresses are removed from your security configurations.')}
         `
+        } else {
+          natWarning = heredoc`
+          ${color.dim('===')} ${color.bold('WARNING: Outbound IPs Will Be Reused')}
+          ${color.yellow('⚠️ The following outbound IPs will become available for reuse:')}
+          ${color.bold(displayNat(space.outbound_ips) ?? '')}
+        `
+        }
       }
     }
 
--- packages/cli/test/unit/commands/spaces/destroy.unit.test.ts
+++ packages/cli/test/unit/commands/spaces/destroy.unit.test.ts
@@ -6,6 +6,7 @@ import {expect} from 'chai'
 import heredoc from 'tsheredoc'
 import {ux} from '@oclif/core'
 import * as sinon from 'sinon'
+
 describe('spaces:destroy', function () {
   const now = new Date()
 
@@ -18,10 +19,17 @@ describe('spaces:destroy', function () {
     sinon.restore()
   })
 
-  it('destroys a space', async function () {
+  it('shows extended NAT warning for fir generation space', async function () {
     const api = nock('https://api.heroku.com')
       .get('/spaces/my-space')
-      .reply(200, {name: 'my-space', team: {name: 'my-team'}, region: {name: 'my-region'}, state: 'allocated', created_at: now})
+      .reply(200, {
+        name: 'my-space',
+        team: {name: 'my-team'},
+        region: {name: 'my-region'},
+        state: 'allocated',
+        created_at: now,
+        generation: {name: 'fir'},
+      })
       .get('/spaces/my-space/nat')
       .reply(200, {state: 'enabled', sources: ['1.1.1.1', '2.2.2.2']})
       .delete('/spaces/my-space')
@@ -45,6 +53,38 @@ describe('spaces:destroy', function () {
      ›
      ›   Ensure all IPv4 and IPv6 addresses are removed from your security 
      ›   configurations.
+     ›
+     ›
+    
+    Destroying space my-space...
+    Destroying space my-space... done
+    `)
+  })
+
+  it('shows simple NAT warning for non-fir generation space', async function () {
+    const api = nock('https://api.heroku.com')
+      .get('/spaces/my-space')
+      .reply(200, {
+        name: 'my-space',
+        team: {name: 'my-team'},
+        region: {name: 'my-region'},
+        state: 'allocated',
+        created_at: now,
+        generation: {name: 'cedar'},
+      })
+      .get('/spaces/my-space/nat')
+      .reply(200, {state: 'enabled', sources: ['1.1.1.1', '2.2.2.2']})
+      .delete('/spaces/my-space')
+      .reply(200)
+
+    await runCommand(Cmd, ['--space', 'my-space'])
+    api.done()
+
+    expect(stderr.output).to.eq(heredoc`     ›   Warning: Destructive Action
+     ›   This command will destroy the space my-space
+     ›   === WARNING: Outbound IPs Will Be Reused
+     ›   ⚠️ The following outbound IPs will become available for reuse:
+     ›   1.1.1.1, 2.2.2.2
      ›
      ›
     
