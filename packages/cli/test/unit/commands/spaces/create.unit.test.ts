68d11c81f3aa2aaaf36a990efea555b8599d057e
Eric Black <eblack@salesforce.com>
2025-01-17 16:08:45 -0800

    Strip ansi to try to fix test

--- packages/cli/test/unit/commands/spaces/create.unit.test.ts
+++ packages/cli/test/unit/commands/spaces/create.unit.test.ts
@@ -4,6 +4,7 @@ import runCommand from '../../../helpers/runCommand'
 import * as nock from 'nock'
 import {expect} from 'chai'
 import heredoc from 'tsheredoc'
+import stripAnsi = require('strip-ansi')
 
 describe('spaces:create', function () {
   const now = new Date()
@@ -88,10 +89,7 @@ describe('spaces:create', function () {
     ])
 
     api.done()
-
-    expect(stderr.output).to.include('Warning: Spend Alert. Each Heroku Standard')
-    expect(stderr.output).to.include('Private Space costs ~$1.39/hour (max $1000/month), pro-rated to the')
-    expect(stderr.output).to.include('second.')
+    expect(stripAnsi(stderr.output)).to.include('Warning: Spend Alert. Each Heroku Standard Private Space costs ~$1.39/hour (max $1000/month), pro-rated to the second.')
   })
 
   it('creates a Shield space', async function () {
@@ -174,9 +172,7 @@ describe('spaces:create', function () {
 
     api.done()
 
-    expect(stderr.output).to.include('Warning: Spend Alert. Each Heroku Shield')
-    expect(stderr.output).to.include('Private Space costs ~$4.17/hour (max $3000/month), pro-rated to the')
-    expect(stderr.output).to.include('second.')
+    expect(stripAnsi(stderr.output)).to.include('Warning: Spend Alert. Each Heroku Shield Private Space costs ~$4.17/hour (max $3000/month), pro-rated to the second.')
   })
 
   it('creates a space with custom cidr and data cidr', async function () {
