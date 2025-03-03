6888527af31d05cd4cf871f55ade3badc4f1d20a
Shirley Xiaolin Xu <xiaolin.xu@salesforce.com>
2025-03-03 09:42:15 -0500

    change all heroku data services to only use HEROKU_DATA_HOST custom env var

--- packages/cli/src/lib/pg/host.ts
+++ packages/cli/src/lib/pg/host.ts
@@ -1,4 +1,4 @@
 export default function () {
-  const host = process.env.HEROKU_DATA_HOST || process.env.HEROKU_POSTGRESQL_HOST
+  const host = process.env.HEROKU_DATA_HOST
   return host ? host : 'api.data.heroku.com'
 }
