fe55ea19bbbd984b1202dbe256f1b72d8c8042fe
Zane Whitfield <zwhitfield@salesforce.com>
2025-03-12 17:38:47 -0700

    Update command & add tests

--- packages/cli/src/commands/releases/retry.ts
+++ packages/cli/src/commands/releases/retry.ts
@@ -2,50 +2,49 @@ import {Command, flags} from '@heroku-cli/command'
 import {ux} from '@oclif/core'
 import color from '@heroku-cli/color'
 import * as Heroku from '@heroku-cli/schema'
-import {streamer} from '../../lib/container/streamer'
+import {stream} from '../../lib/releases/output'
 import {findByLatestOrId} from '../../lib/releases/releases'
 
 export default class Retry extends Command {
-    static topic = 'releases';
-    static description = 'retry the latest release-phase command';
-    static examples = ['heroku releases:retry --app happy-samurai-42']
-    static help = 'Copies the latest release into a new one, retrying the latest release-phase command.'
-    static flags = {
-      app: flags.app({required: true}),
-    };
+  static topic = 'releases';
+  static description = 'retry the latest release-phase command';
+  static examples = ['heroku releases:retry --app happy-samurai-42']
+  static help = 'Copies the latest release into a new one, retrying the latest release-phase command.'
+  static flags = {
+    app: flags.app({required: true}),
+  };
 
-    public async run(): Promise<void> {
-      const {flags} = await this.parse(Retry)
-      const {app} = flags
-      const release = await findByLatestOrId(this.heroku, app)
+  public async run(): Promise<void> {
+    const {flags} = await this.parse(Retry)
+    const {app} = flags
+    const release = await findByLatestOrId(this.heroku, app)
 
-      if (!release) {
-        return ux.error('No release found for this app')
+    if (!release) {
+      return ux.error('No release found for this app')
+    }
+
+    ux.action.start(`Retrying ${color.green('v' + release.version)} on ${color.app(app)}`)
+
+    const {body: retry} = await this.heroku.post<Heroku.Release>(`/apps/${app}/releases`, {
+      body: {
+        slug: release?.slug?.id,
+        description: `Retry of v${release.version}: ${release.description}`
       }
+    })
 
-      ux.action.start(`Retrying ${color.green('v' + release.version)} on ${color.app(app)}`)
+    ux.action.stop(`done, ${color.green('v' + retry.version)}`)
 
-      const {body: retry} = await this.heroku.post<Heroku.Release>(`/apps/${app}/releases`, {
-        body: {
-          slug: release?.slug?.id,
-          description: `Retry of v${release.version}: ${release.description}`
-        }
-      })
-
-      ux.action.stop(`done, ${color.green('v' + retry.version)}`)
-
-      if (retry.output_stream_url) {
-        ux.log('Running release command...')
-        try {
-          await streamer(retry.output_stream_url, process.stdout)
-        } catch (error: any) {
-          if (error.statusCode === 404) {
+    if (retry.output_stream_url) {
+      ux.log('Running release command...')
+      await stream(retry.output_stream_url)
+        .catch(error => {
+          if (error.statusCode === 404 || error.response?.statusCode === 404) {
             ux.warn('Release command starting. Use `heroku releases:output` to view the log.')
             return
           }
 
           throw error
-        }
-      }
+        })
     }
+  }
 }
--- packages/cli/test/unit/commands/releases/retry.unit.test.ts
+++ packages/cli/test/unit/commands/releases/retry.unit.test.ts
@@ -1,15 +1,33 @@
 import {stdout, stderr} from 'stdout-stderr'
-import Cmd  from '../../../../src/commands/releases/retry'
+import Cmd from '../../../../src/commands/releases/retry'
 import runCommand from '../../../helpers/runCommand'
 import * as nock from 'nock'
 import {expect} from 'chai'
-import expectOutput from '../../../helpers/utils/expectOutput'
-import heredoc from 'tsheredoc'
+
+const isTTY = process.stderr.isTTY
 
 describe('releases:retry', async function () {
+  beforeEach(function () {
+    process.stderr.isTTY = true
+    process.stderr.columns = 1000
+    console.log("isTTY", isTTY)
+    console.log("columns", process.stderr.columns)
+  })
   afterEach(function () {
+    process.stderr.isTTY = isTTY
     return nock.cleanAll()
   })
+
+  const release = [{
+    slug: { id: 'slug_uuid' },
+    version: 40,
+    description: 'A release'
+  }]
+
+  const releaseRetry = {
+    slug: 'slug_uuid',
+    description: 'Retry of v40: A release'
+  }
 
   it('errors when there are no releases yet', async function () {
     nock('https://api.heroku.com')
@@ -22,5 +40,62 @@ describe('releases:retry', async function () {
     ]).catch((error: any) => {
       expect(error.message).to.eq('No release found for this app')
     })
+  })
+
+  it('retries the release', async function () {
+    nock('https://api.heroku.com')
+      .get('/apps/myapp/releases')
+      .reply(200, release)
+      .post('/apps/myapp/releases', releaseRetry)
+      .reply(200, {})
+
+    await runCommand(Cmd, [
+      '--app',
+      'myapp',
+    ])
+  })
+
+  it('shows the output from the latest release', async function () {
+    const busl = nock('https://busl.test')
+      .get('/streams/release.log')
+      .reply(200, 'Release Output Content')
+
+    const api = nock('https://api.heroku.com')
+      .get('/apps/myapp/releases')
+      .reply(200, release)
+      .post('/apps/myapp/releases', releaseRetry)
+      .reply(200, {output_stream_url: 'https://busl.test/streams/release.log'})
+
+    await runCommand(Cmd, [
+      '--app',
+      'myapp',
+    ])
+
+    api.done()
+    busl.done()
+    expect(stderr.output).to.contains('Retrying v40 on ⬢ myapp...')
+    expect(stdout.output).to.contains('Release Output Content')
+  })
+
+  it('has a missing output', async function () {
+    const busl = nock('https://busl.test')
+      .get('/streams/release.log')
+      .reply(404, '')
+
+    const api = nock('https://api.heroku.com')
+      .get('/apps/myapp/releases')
+      .reply(200, release)
+      .post('/apps/myapp/releases', releaseRetry)
+      .reply(200, {output_stream_url: 'https://busl.test/streams/release.log'})
+
+    await runCommand(Cmd, [
+      '--app',
+      'myapp',
+    ])
+
+    api.done()
+    busl.done()
+    expect(stdout.output).to.equal('Running release command...\n')
+    expect(stderr.output).to.contains('Release command starting. Use `heroku releases:output` to view the log.')
   })
 })
