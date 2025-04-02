813b15e95e14f288a3e1dc3f4a55d1e82dae85ce
Nila Muthu <nmuthu@salesforce.com>
2025-04-02 13:23:23 -0700

    format response

--- packages/cli/src/commands/pg/upgrade/prepare.ts
+++ packages/cli/src/commands/pg/upgrade/prepare.ts
@@ -4,8 +4,8 @@ import {Args, ux} from '@oclif/core'
 import heredoc from 'tsheredoc'
 import {getAddon} from '../../../lib/pg/fetcher'
 import pgHost from '../../../lib/pg/host'
-import {legacyEssentialPlan, essentialNumPlan} from '../../../lib/pg/util'
-import {PgDatabase} from '../../../lib/pg/types'
+import {legacyEssentialPlan, essentialNumPlan, formatResponseWithCommands} from '../../../lib/pg/util'
+import {PgDatabase, PgUpgradeError, PgUpgradeResponse} from '../../../lib/pg/types'
 import confirmCommand from '../../../lib/confirmCommand'
 import {nls} from '../../../nls'
 
@@ -52,9 +52,14 @@ export default class Upgrade extends Command {
         This command prepares the upgrade for ${color.addon(db.name)} to the latest supported PostgreSQL version and schedules to upgrade it during the next available maintenance window.
       `))
 
-    const data = {version}
-    ux.action.start(`Preparing upgrade on ${color.addon(db.name)}`)
-    await this.heroku.post(`/client/v11/databases/${db.id}/upgrade/prepare`, {hostname: pgHost(), body: data})
-    ux.action.stop(`done\nYour database is scheduled for upgrade during your next available maintenance window.\nRun ${color.cmd('heroku pg:upgrade:wait')} to track its status.\nYou can also run this upgrade manually before the maintenance window with ${color.cmd('heroku pg:upgrade:run')}. You can only run the upgrade after it's fully prepared, which can take up to a day.`)
+    try {
+      const data = {version}
+      ux.action.start(`Preparing upgrade on ${color.addon(db.name)}`)
+      const response = await this.heroku.post<PgUpgradeResponse>(`/client/v11/databases/${db.id}/upgrade/prepare`, {hostname: pgHost(), body: data})
+      ux.action.stop('done\n' + formatResponseWithCommands(response.body.message))
+    } catch (error) {
+      const response = error as PgUpgradeError
+      ux.error(formatResponseWithCommands(response.body.message) + `\n\nError ID: ${response.body.id}`)
+    }
   }
 }
