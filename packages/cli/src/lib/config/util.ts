260a64a0b34dc6f5b4a8522aa2efc322c1249c7d
Eric Black <eblack@salesforce.com>
2025-05-28 11:54:40 -0700

    Rewrite Editor to stop using ancient library

--- packages/cli/src/lib/config/util.ts
+++ packages/cli/src/lib/config/util.ts
@@ -1,9 +1,35 @@
-const edit = require('edit-string')
+import * as tmp from 'tmp'
+import * as fs from 'fs-extra'
+import {spawn} from 'child_process'
 
-// This indirection exists to give tests
-// a way to stub calls to `edit(...)`
 export class Editor {
-  edit(input: string, options = {}) {
-    return edit(input, options)
+  async edit(input: string, options = {}) {
+    // Create a temporary file
+    const tmpFile = tmp.fileSync(options)
+
+    // Write the input to the temporary file
+    await fs.writeFile(tmpFile.name, input)
+
+    // Get the editor from environment variables
+    const editor = process.env.VISUAL || process.env.EDITOR || 'vim'
+
+    // Spawn the editor process
+    const child = spawn(editor, [tmpFile.name], {stdio: 'inherit'})
+
+    // Wait for the editor to close
+    await new Promise((resolve, reject) => {
+      child.on('exit', code => {
+        if (code === 0) resolve(null)
+        else reject(new Error(`Editor exited with code ${code}`))
+      })
+    })
+
+    // Read the edited file
+    const result = await fs.readFile(tmpFile.name, 'utf8')
+
+    // Clean up the temporary file
+    tmpFile.removeCallback()
+
+    return result
   }
 }
