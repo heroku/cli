32eb4093a8657a767e5d16c7cb5778cd931786b3
Katy Bowman <katy.bowman@salesforce.com>
2025-08-26 09:12:46 -0400

    feat(W-19005486): add support for SF Trust API to status command (#3357)
    * feat: add status types
    
    * feat: retrieve and format data from SF Trust API
    
    * refactor: update json return
    
    * refactor: print system status from either Heroku API or Trust API
    
    * feat: format incident details from Trust API
    
    * refactor: update calculation for incident title padding
    
    * refactor: update status types
    
    * feat: add localization and alternative service names for status api response
    
    * test: first test updates
    
    * test: add tests for SF Trust API
    
    * feat: add startTime param to maintenances endpoint request
    
    * test: add test for API failures
    
    * fix: update logic for classifying incidents by system

---
+++ packages/cli/test/fixtures/status/fixtures.ts
@@ -0,0 +1,546 @@
+import {
+  TrustInstance,
+  TrustIncident,
+  TrustMaintenance,
+  Localization,
+} from '../../../src/lib/types/status'
+
+export const fixtureNow = Date.now()
+export const fixtureNowISO = new Date(fixtureNow).toISOString()
+
+export const instancesResponse: TrustInstance[] = [
+  {
+    key: 'Heroku London',
+    location: 'NA',
+    environment: 'production',
+    releaseVersion: '',
+    releaseNumber: '',
+    status: 'OK',
+    isActive: true,
+    city: '',
+    stateName: '',
+    stateCode: '',
+    countryName: '',
+    countryCode: '',
+    maintenanceWindow: '',
+    Services: [
+      {
+        key: 'HerokuApps',
+        order: 1,
+        isCore: false,
+      },
+      {
+        key: 'HerokuData',
+        order: 5,
+        isCore: false,
+      },
+    ],
+    Products: [
+      {
+        key: 'Heroku',
+        order: 60,
+        isActive: true,
+        name: 'Heroku',
+        altDisplayName: 'Heroku',
+        url: '/products/Heroku',
+      },
+    ],
+    Incidents: [],
+    Tags: [],
+  },
+  {
+    key: 'Heroku Tokyo',
+    location: 'APAC',
+    environment: 'production',
+    releaseVersion: '',
+    releaseNumber: '',
+    status: 'OK',
+    isActive: true,
+    city: '',
+    stateName: '',
+    stateCode: '',
+    countryName: '',
+    countryCode: '',
+    maintenanceWindow: '',
+    Services: [
+      {
+        key: 'HerokuApps',
+        order: 1,
+        isCore: false,
+      },
+      {
+        key: 'HerokuData',
+        order: 5,
+        isCore: false,
+      },
+    ],
+    Products: [
+      {
+        key: 'Heroku',
+        order: 60,
+        isActive: true,
+        name: 'Heroku',
+        altDisplayName: 'Heroku',
+        url: '/products/Heroku',
+      },
+    ],
+    Incidents: [],
+    Tags: [],
+  },
+  {
+    key: 'Heroku Europe',
+    location: 'EMEA',
+    environment: 'production',
+    releaseVersion: '',
+    releaseNumber: '',
+    status: 'OK',
+    isActive: true,
+    city: '',
+    stateName: '',
+    stateCode: '',
+    countryName: '',
+    countryCode: '',
+    maintenanceWindow: '',
+    Services: [
+      {
+        key: 'HerokuApps',
+        order: 1,
+        isCore: false,
+      },
+      {
+        key: 'HerokuData',
+        order: 5,
+        isCore: false,
+      },
+    ],
+    Products: [
+      {
+        key: 'Heroku',
+        order: 60,
+        isActive: true,
+        name: 'Heroku',
+        altDisplayName: 'Heroku',
+        url: '/products/Heroku',
+      },
+    ],
+    Incidents: [],
+    Tags: [],
+  },
+  {
+    key: 'Heroku Asia',
+    location: 'EMEA',
+    environment: 'production',
+    releaseVersion: '',
+    releaseNumber: '',
+    status: 'OK',
+    isActive: true,
+    city: '',
+    stateName: '',
+    stateCode: '',
+    countryName: '',
+    countryCode: '',
+    maintenanceWindow: '',
+    Services: [
+      {
+        key: 'HerokuApps',
+        order: 1,
+        isCore: false,
+      },
+      {
+        key: 'HerokuData',
+        order: 5,
+        isCore: false,
+      },
+    ],
+    Products: [
+      {
+        key: 'Heroku',
+        order: 60,
+        isActive: true,
+        name: 'Heroku',
+        altDisplayName: 'Heroku',
+        url: '/products/Heroku',
+      },
+    ],
+    Incidents: [],
+    Tags: [],
+  },
+  {
+    key: 'Heroku North America',
+    location: 'NA',
+    environment: 'production',
+    releaseVersion: '',
+    releaseNumber: '',
+    status: 'OK',
+    isActive: true,
+    city: '',
+    stateName: '',
+    stateCode: '',
+    countryName: '',
+    countryCode: '',
+    maintenanceWindow: '',
+    Services: [
+      {
+        key: 'CLI',
+        order: 20,
+        isCore: false,
+      },
+      {
+        key: 'Dashboard',
+        order: 10,
+        isCore: false,
+      },
+      {
+        key: 'PlatformAPI',
+        order: 1,
+        isCore: false,
+      },
+    ],
+    Products: [
+      {
+        key: 'Heroku',
+        order: 60,
+        isActive: true,
+        name: 'Heroku',
+        altDisplayName: 'Heroku',
+        url: '/products/Heroku',
+      },
+    ],
+    Incidents: [],
+    Tags: [],
+  },
+]
+
+export const herokuMaintenanceResponse: TrustMaintenance[] = [
+  {
+    id: 20745576,
+    message: {
+      maintenanceType: 'scheduledMaintenance',
+      eventStatus: 'resolved',
+      availability: 'available',
+    },
+    externalId: '12345',
+    name: 'test',
+    externalMaintenanceType: null,
+    substrate: null,
+    releaseType: null,
+    plannedStartTime: '2025-08-01T16:00:00.000Z',
+    plannedEndTime: '2025-08-01T16:05:00.000Z',
+    additionalInformation: '',
+    isCore: false,
+    affectsAll: false,
+    createdAt: '2025-08-07T16:00:22.214Z',
+    updatedAt: '2025-08-07T16:00:22.226Z',
+    instanceKeys: [
+      'Heroku North America',
+    ],
+    serviceKeys: [
+      'HerokuApps',
+      'HerokuData',
+    ],
+    MaintenanceImpacts: [
+      {
+        id: 20034313,
+        startTime: '2025-08-01T16:00:00.000Z',
+        endTime: '2025-08-01T16:05:00.000Z',
+        type: 'deployingRelease',
+        severity: 'maintenance',
+        createdAt: '2025-08-07T16:00:22.273Z',
+        updatedAt: '2025-08-07T16:00:22.281Z',
+        startTimeCreatedAt: '2025-08-07T16:00:22.273Z',
+        startTimeModifiedAt: null,
+        endTimeCreatedAt: '2025-08-07T16:00:22.275Z',
+        endTimeModifiedAt: null,
+      },
+    ],
+    MaintenanceEvents: [
+      {
+        id: 21634955,
+        type: 'scheduled',
+        message: 'This maintenance has been scheduled.',
+        createdAt: '2025-08-07T16:00:22.266Z',
+        updatedAt: '2025-08-07T16:00:22.270Z',
+      },
+    ],
+  },
+]
+
+export const trustLocalizationsResponse: Localization[] = [
+  {
+    id: 123,
+    modelName: 'incidentEventType',
+    modelKey: 'herokuIncident',
+    modelAttribute: 'label',
+    text: 'Heroku - Incident',
+    locale: 'en',
+  },
+  {
+    id: 123,
+    modelName: 'incidentEventType',
+    modelKey: 'herokuIncidentInvestigating',
+    modelAttribute: 'label',
+    text: 'Heroku Update - Investigating',
+    locale: 'en',
+  },
+  {
+    id: 123,
+    modelName: 'incidentEventType',
+    modelKey: 'herokuIncidentMonitoring',
+    modelAttribute: 'label',
+    text: 'Heroku Incident - Monitoring',
+    locale: 'en',
+  },
+]
+
+/*
+SF Trust API incident responses
+ */
+const nonHerokuIncident: TrustIncident = {
+  id: 12345,
+  externalId: '12345',
+  message: {
+    pathToResolution: null,
+    actionPlan: null,
+    rootCause: null,
+  },
+  additionalInformation: 'TEST',
+  isCore: false,
+  affectsAll: false,
+  createdAt: `${fixtureNowISO}`,
+  updatedAt: '2025-08-21T08:53:30.069Z',
+  instanceKeys: [
+    'KEY1',
+    'KEY2',
+  ],
+  serviceKeys: [
+    'Service1',
+    'Service2',
+  ],
+  IncidentImpacts: [
+    {
+      id: 20011917,
+      startTime: '2025-08-21T08:52:00.000Z',
+      endTime: null,
+      type: 'featurePerfDegradation',
+      severity: 'minor',
+      createdAt: '2025-08-21T08:53:01.799Z',
+      updatedAt: '2025-08-21T08:53:01.808Z',
+      startTimeCreatedAt: '2025-08-21T08:53:01.800Z',
+      startTimeModifiedAt: null,
+      endTimeCreatedAt: null,
+      endTimeModifiedAt: null,
+    },
+  ],
+  IncidentEvents: [
+    {
+      id: 20008740,
+      type: 'issueIsolatedDatabaseTier',
+      message: 'Incident update 1',
+      createdAt: '2025-08-21T08:54:00.000Z',
+      updatedAt: '2025-08-21T08:57:47.859Z',
+    },
+    {
+      id: 20008739,
+      type: 'issueIsolatedDatabaseTier',
+      message: 'Incident update 2',
+      createdAt: '2025-08-21T08:53:00.000Z',
+      updatedAt: '2025-08-21T08:53:53.787Z',
+    },
+    {
+      id: 20008738,
+      type: 'investigatingCauseOfIssue',
+      message: 'Incident update 3',
+      createdAt: '2025-08-21T08:53:00.000Z',
+      updatedAt: '2025-08-21T08:53:30.112Z',
+    },
+  ],
+}
+
+const herokuDataIncident: TrustIncident = {
+  id: 12345,
+  externalId: '12345',
+  message: {
+    pathToResolution: null,
+    actionPlan: null,
+    rootCause: null,
+  },
+  additionalInformation: 'TEST',
+  isCore: false,
+  affectsAll: false,
+  createdAt: `${fixtureNowISO}`,
+  updatedAt: '2025-08-21T08:53:30.069Z',
+  instanceKeys: [
+    'Heroku North America',
+  ],
+  serviceKeys: [
+    'Data',
+  ],
+  IncidentImpacts: [
+    {
+      id: 20011917,
+      startTime: '2025-08-21T08:52:00.000Z',
+      endTime: null,
+      type: 'herokuDataFeatureDisruption',
+      severity: 'major',
+      createdAt: '2025-08-21T08:53:01.799Z',
+      updatedAt: '2025-08-21T08:53:01.808Z',
+      startTimeCreatedAt: '2025-08-21T08:53:01.800Z',
+      startTimeModifiedAt: null,
+      endTimeCreatedAt: null,
+      endTimeModifiedAt: null,
+    },
+  ],
+  IncidentEvents: [
+    {
+      id: 20008740,
+      type: 'herokuIncident',
+      message: 'Incident update 1',
+      createdAt: `${fixtureNowISO}`,
+      updatedAt: '2025-08-21T08:57:47.859Z',
+    },
+    {
+      id: 20008739,
+      type: 'herokuIncidentMonitoring',
+      message: 'Incident update 2',
+      createdAt: `${fixtureNowISO}`,
+      updatedAt: '2025-08-21T08:53:53.787Z',
+    },
+    {
+      id: 20008738,
+      type: 'herokuIncidentInvestigating',
+      message: 'Incident update 3',
+      createdAt: `${fixtureNowISO}`,
+      updatedAt: '2025-08-21T08:53:30.112Z',
+    },
+  ],
+}
+
+const herokuAppsIncident: TrustIncident = {
+  id: 12345,
+  externalId: '12345',
+  message: {
+    pathToResolution: null,
+    actionPlan: null,
+    rootCause: null,
+  },
+  additionalInformation: 'TEST',
+  isCore: false,
+  affectsAll: false,
+  createdAt: `${fixtureNowISO}`,
+  updatedAt: '2025-08-21T08:53:30.069Z',
+  instanceKeys: [
+    'Heroku Europe',
+  ],
+  serviceKeys: [
+    'Apps',
+  ],
+  IncidentImpacts: [
+    {
+      id: 20011917,
+      startTime: '2025-08-21T08:52:00.000Z',
+      endTime: null,
+      type: 'herokuAppsServiceDisruption',
+      severity: 'minor',
+      createdAt: '2025-08-21T08:53:01.799Z',
+      updatedAt: '2025-08-21T08:53:01.808Z',
+      startTimeCreatedAt: '2025-08-21T08:53:01.800Z',
+      startTimeModifiedAt: null,
+      endTimeCreatedAt: null,
+      endTimeModifiedAt: null,
+    },
+  ],
+  IncidentEvents: [
+    {
+      id: 20008740,
+      type: 'herokuIncident',
+      message: 'Incident update 1',
+      createdAt: `${fixtureNowISO}`,
+      updatedAt: '2025-08-21T08:57:47.859Z',
+    },
+    {
+      id: 20008739,
+      type: 'herokuIncidentMonitoring',
+      message: 'Incident update 2',
+      createdAt: `${fixtureNowISO}`,
+      updatedAt: '2025-08-21T08:53:53.787Z',
+    },
+    {
+      id: 20008738,
+      type: 'herokuIncidentInvestigating',
+      message: 'Incident update 3',
+      createdAt: `${fixtureNowISO}`,
+      updatedAt: '2025-08-21T08:53:30.112Z',
+    },
+  ],
+}
+
+const herokuToolsIncident: TrustIncident = {
+  id: 12345,
+  externalId: '12345',
+  message: {
+    pathToResolution: null,
+    actionPlan: null,
+    rootCause: null,
+  },
+  additionalInformation: 'TEST',
+  isCore: false,
+  affectsAll: false,
+  createdAt: `${fixtureNowISO}`,
+  updatedAt: '2025-08-21T08:53:30.069Z',
+  instanceKeys: [
+    'Heroku Asia',
+  ],
+  serviceKeys: [
+    'Dashboard',
+  ],
+  IncidentImpacts: [
+    {
+      id: 20011917,
+      startTime: '2025-08-21T08:52:00.000Z',
+      endTime: null,
+      type: 'herokuToolsFeatureDisruption',
+      severity: 'minor',
+      createdAt: '2025-08-21T08:53:01.799Z',
+      updatedAt: '2025-08-21T08:53:01.808Z',
+      startTimeCreatedAt: '2025-08-21T08:53:01.800Z',
+      startTimeModifiedAt: null,
+      endTimeCreatedAt: null,
+      endTimeModifiedAt: null,
+    },
+  ],
+  IncidentEvents: [
+    {
+      id: 20008740,
+      type: 'herokuIncident',
+      message: 'Incident update 1',
+      createdAt: `${fixtureNowISO}`,
+      updatedAt: '2025-08-21T08:57:47.859Z',
+    },
+    {
+      id: 20008739,
+      type: 'herokuIncidentMonitoring',
+      message: 'Incident update 2',
+      createdAt: `${fixtureNowISO}`,
+      updatedAt: '2025-08-21T08:53:53.787Z',
+    },
+    {
+      id: 20008738,
+      type: 'herokuIncidentInvestigating',
+      message: 'Incident update 3',
+      createdAt: `${fixtureNowISO}`,
+      updatedAt: '2025-08-21T08:53:30.112Z',
+    },
+  ],
+}
+
+export const nonHerokuIncidentResponse: TrustIncident[] = [
+  nonHerokuIncident,
+  nonHerokuIncident,
+]
+
+export const herokuDataAppsToolsIncidentResponse: TrustIncident[] = [
+  nonHerokuIncident,
+  herokuDataIncident,
+  herokuAppsIncident,
+  herokuToolsIncident,
+]
--- packages/cli/test/unit/commands/status.unit.test.ts
+++ packages/cli/test/unit/commands/status.unit.test.ts
@@ -1,93 +1,95 @@
 import * as nock from 'nock'
 import * as sinon from 'sinon'
-import {expect, test as base} from '@oclif/test'
+import {expect, test} from '@oclif/test'
+import {
+  herokuDataAppsToolsIncidentResponse,
+  herokuMaintenanceResponse,
+  instancesResponse,
+  nonHerokuIncidentResponse,
+  trustLocalizationsResponse,
+  fixtureNowISO,
+  fixtureNow,
+} from '../../fixtures/status/fixtures'
 
-const test = base
+const herokuStatusApi = 'https://status.heroku.com:443'
+const salesforceTrustApi = 'https://api.status.salesforce.com/v1'
 
-const api = nock('https://status.heroku.com:443')
-
-beforeEach(function () {
-  return nock.cleanAll()
-})
-afterEach(function () {
-  return api.done()
-})
-
-describe('when heroku is green', function () {
-  test
-    .stdout()
-    .nock('https://status.heroku.com', api => {
-      api.get('/api/v4/current-status').reply(200, {
-        status: [
-          {system: 'Apps', status: 'green'},
-          {system: 'Data', status: 'green'},
-          {system: 'Tools', status: 'green'},
-        ],
-        incidents: [],
-        scheduled: [],
+describe('status - Heroku Status API', function () {
+  describe('when heroku is green', function () {
+    test
+      .stdout()
+      .nock(herokuStatusApi, api => {
+        api.get('/api/v4/current-status').reply(200, {
+          status: [
+            {system: 'Apps', status: 'green'},
+            {system: 'Data', status: 'green'},
+            {system: 'Tools', status: 'green'},
+          ],
+          incidents: [],
+          scheduled: [],
+        })
       })
-    })
-    .command(['status'])
-    .it('shows success message', ctx => {
-      expect(ctx.stdout).to.equal(`Apps:      No known issues at this time.
+      .command(['status'])
+      .it('shows success message', ctx => {
+        expect(ctx.stdout).to.equal(`Apps:      No known issues at this time.
 Data:      No known issues at this time.
 Tools:     No known issues at this time.\n`)
-    })
-
-  test
-    .stdout()
-    .nock('https://status.heroku.com', api => {
-      api.get('/api/v4/current-status').reply(200, {
-        status: [
-          {system: 'Apps', status: 'green'},
-          {system: 'Data', status: 'green'},
-          {system: 'Tools', status: 'green'},
-        ],
-        incidents: [],
-        scheduled: [],
       })
-    })
-    .command(['status', '--json'])
-    .it('--json', ctx => {
-      expect(JSON.parse(ctx.stdout).status[0]).to.deep.include({status: 'green'})
-    })
-})
 
-describe('when heroku has issues', function () {
-  const now = Date.now()
-  const timeISO = new Date(now).toISOString()
-
-  before(function () {
-    sinon.stub(Date, 'now').returns(now)
+    test
+      .stdout()
+      .nock(herokuStatusApi, api => {
+        api.get('/api/v4/current-status').reply(200, {
+          status: [
+            {system: 'Apps', status: 'green'},
+            {system: 'Data', status: 'green'},
+            {system: 'Tools', status: 'green'},
+          ],
+          incidents: [],
+          scheduled: [],
+        })
+      })
+      .command(['status', '--json'])
+      .it('--json', ctx => {
+        expect(JSON.parse(ctx.stdout).status[0]).to.deep.include({status: 'green'})
+      })
   })
 
-  after(function () {
-    sinon.restore()
-  })
+  describe('when heroku has issues', function () {
+    const now = Date.now()
+    const timeISO = new Date(now).toISOString()
 
-  test
-    .stdout()
-    .nock('https://status.heroku.com', api => {
-      api.get('/api/v4/current-status').reply(200, {
-        status: [
-          {system: 'Apps', status: 'red'},
-          {system: 'Data', status: 'green'},
-          {system: 'Tools', status: 'green'},
-        ],
-        incidents: [
-          {
-            title: 'incident title',
-            created_at: timeISO,
-            full_url: 'https://status.heroku.com',
-            updates: [{update_type: 'update type', updated_at: timeISO, contents: 'update contents'}],
-          },
-        ],
-        scheduled: [],
-      })
+    before(function () {
+      sinon.stub(Date, 'now').returns(now)
     })
-    .command(['status'])
-    .it('shows the issues', ctx => {
-      expect(ctx.stdout).to.equal(`Apps:      Red
+
+    after(function () {
+      sinon.restore()
+    })
+
+    test
+      .stdout()
+      .nock(herokuStatusApi, api => {
+        api.get('/api/v4/current-status').reply(200, {
+          status: [
+            {system: 'Apps', status: 'red'},
+            {system: 'Data', status: 'green'},
+            {system: 'Tools', status: 'green'},
+          ],
+          incidents: [
+            {
+              title: 'incident title',
+              created_at: timeISO,
+              full_url: 'https://status.heroku.com',
+              updates: [{update_type: 'update type', updated_at: timeISO, contents: 'update contents'}],
+            },
+          ],
+          scheduled: [],
+        })
+      })
+      .command(['status'])
+      .it('shows the issues', ctx => {
+        expect(ctx.stdout).to.equal(`Apps:      Red
 Data:      No known issues at this time.
 Tools:     No known issues at this time.
 
@@ -97,5 +99,164 @@ update type ${timeISO} (less than a minute ago)
 update contents
 
 `)
+      })
+  })
+})
+
+describe('status - SF Trust API', function () {
+  describe('when there are no Heroku incidents', function () {
+    test
+      .stdout()
+      .nock(herokuStatusApi, api => {
+        api.get('/api/v4/current-status').reply(404)
+      })
+      .nock(salesforceTrustApi, api => {
+        api.get('/instances?products=Heroku').reply(200, instancesResponse)
+        api.get('/incidents/active').reply(200, nonHerokuIncidentResponse)
+        api.get('/maintenances')
+          .query(params => {
+            return params.limit === '10' && params.offset === '0' && params.product === 'Heroku' && params.locale === 'en'
+          })
+          .reply(200)
+        api.get('/localizations?locale=en').reply(200, trustLocalizationsResponse)
+      })
+      .command(['status'])
+      .it('shows success message', ctx => {
+        expect(ctx.stdout).to.equal(`Apps:      No known issues at this time.
+Data:      No known issues at this time.
+Tools:     No known issues at this time.\n`)
+      })
+
+    test
+      .stdout()
+      .nock(herokuStatusApi, api => {
+        api.get('/api/v4/current-status').reply(404)
+      })
+      .nock(salesforceTrustApi, api => {
+        api.get('/instances?products=Heroku').reply(200, instancesResponse)
+        api.get('/incidents/active').reply(200, nonHerokuIncidentResponse)
+        api.get('/maintenances')
+          .query(params => {
+            return params.limit === '10' && params.offset === '0' && params.product === 'Heroku' && params.locale === 'en'
+          })
+          .reply(200)
+        api.get('/localizations?locale=en').reply(200, trustLocalizationsResponse)
+      })
+      .command(['status', '--json'])
+      .it('returns json response with --json flag', ctx => {
+        expect(JSON.parse(ctx.stdout).status[0]).to.deep.include({status: 'green'})
+      })
+
+    test
+      .stdout()
+      .nock(herokuStatusApi, api => {
+        api.get('/api/v4/current-status').reply(404)
+      })
+      .nock(salesforceTrustApi, api => {
+        api.get('/instances?products=Heroku').reply(200, instancesResponse)
+        api.get('/incidents/active').reply(200, nonHerokuIncidentResponse)
+        api.get('/maintenances')
+          .query(params => {
+            return params.limit === '10' && params.offset === '0' && params.product === 'Heroku' && params.locale === 'en'
+          })
+          .reply(200, herokuMaintenanceResponse)
+        api.get('/localizations?locale=en').reply(200, trustLocalizationsResponse)
+      })
+      .command(['status', '--json'])
+      .it('includes planned maintenances in the json response with --json flag', ctx => {
+        expect(JSON.parse(ctx.stdout).scheduled).to.deep.equal(herokuMaintenanceResponse)
+      })
+  })
+
+  describe('when there are active Heroku incidents', function () {
+    before(function () {
+      sinon.stub(Date, 'now').returns(fixtureNow)
     })
+
+    after(function () {
+      sinon.restore()
+    })
+
+    test
+      .stdout()
+      .nock(herokuStatusApi, api => {
+        api.get('/api/v4/current-status').reply(404)
+      })
+      .nock(salesforceTrustApi, api => {
+        api.get('/instances?products=Heroku').reply(200, instancesResponse)
+        api.get('/incidents/active').reply(200, herokuDataAppsToolsIncidentResponse)
+        api.get('/maintenances')
+          .query(params => {
+            return params.limit === '10' && params.offset === '0' && params.product === 'Heroku' && params.locale === 'en'
+          })
+          .reply(200)
+        api.get('/localizations?locale=en').reply(200, trustLocalizationsResponse)
+      })
+      .command(['status'])
+      .it('shows the issues', ctx => {
+        expect(ctx.stdout).to.equal(`Apps:      Yellow
+Data:      Red
+Tools:     Yellow
+
+=== 12345 ${fixtureNowISO} https://status.salesforce.com/incidents/12345
+
+Heroku - Incident             ${fixtureNowISO} (less than a minute ago)
+Incident update 1
+
+Heroku Incident - Monitoring  ${fixtureNowISO} (less than a minute ago)
+Incident update 2
+
+Heroku Update - Investigating ${fixtureNowISO} (less than a minute ago)
+Incident update 3
+
+
+=== 12345 ${fixtureNowISO} https://status.salesforce.com/incidents/12345
+
+Heroku - Incident             ${fixtureNowISO} (less than a minute ago)
+Incident update 1
+
+Heroku Incident - Monitoring  ${fixtureNowISO} (less than a minute ago)
+Incident update 2
+
+Heroku Update - Investigating ${fixtureNowISO} (less than a minute ago)
+Incident update 3
+
+
+=== 12345 ${fixtureNowISO} https://status.salesforce.com/incidents/12345
+
+Heroku - Incident             ${fixtureNowISO} (less than a minute ago)
+Incident update 1
+
+Heroku Incident - Monitoring  ${fixtureNowISO} (less than a minute ago)
+Incident update 2
+
+Heroku Update - Investigating ${fixtureNowISO} (less than a minute ago)
+Incident update 3
+
+`)
+      })
+  })
+
+  describe('when calls to both the Heroku Status API and the SF Trust API fail', function () {
+    test
+      .stdout()
+      .nock(herokuStatusApi, api => {
+        api.get('/api/v4/current-status').reply(404)
+      })
+      .nock(salesforceTrustApi, api => {
+        api.get('/instances?products=Heroku').reply(404, instancesResponse)
+        api.get('/incidents/active').reply(200, herokuDataAppsToolsIncidentResponse)
+        api.get('/maintenances')
+          .query(params => {
+            return params.limit === '10' && params.offset === '0' && params.product === 'Heroku' && params.locale === 'en'
+          })
+          .reply(200)
+        api.get('/localizations?locale=en').reply(200, trustLocalizationsResponse)
+      })
+      .command(['status'])
+      .catch((error: any) => {
+        expect(error.message).to.include('Heroku platform status is unavailable at this time. Refer to https://status.salesforce.com/products/Heroku or try again later.')
+      })
+      .it('displays an error message')
+  })
 })
