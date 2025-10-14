a9fe396f3dfa67c623e86ced59dd5dde19f46feb
zwhitfield3 <zwhitfield@salesforce.com>
2025-10-14 14:31:04 -0700

    fix: Reinstate login flow

--- packages/cli/src/commands/accounts/add.ts
+++ packages/cli/src/commands/accounts/add.ts
@@ -1,7 +1,8 @@
-import {Command} from '@heroku-cli/command'
+import {Command, flags} from '@heroku-cli/command'
 import {Args, ux} from '@oclif/core'
 import * as Heroku from '@heroku-cli/schema'
 import {add, list} from '../../lib/accounts/accounts'
+import {FlagInput} from '@oclif/core/lib/interfaces/parser'
 
 export default class Add extends Command {
   static description = 'add a Heroku account to your cache'
@@ -10,17 +11,23 @@ export default class Add extends Command {
     name: Args.string({description: 'name of Heroku account to add', required: true}),
   }
 
+  static flags: FlagInput  = {
+    sso: flags.boolean({char: 's', description: 'login for enterprise users under SSO'}),
+  }
+
   static example = 'heroku accounts:add my-account'
 
   async run() {
-    const {args} = await this.parse(Add)
+    const {args, flags} = await this.parse(Add)
     const {name} = args
+    const {sso} = flags
     const logInMessage = 'You must be logged in to run this command.'
 
     if (list().some(a => a.name === name)) {
       ux.error(`${name} already exists`)
     }
 
+    await this.heroku.login({method: sso ? 'sso' : 'interactive'})
     const {body: account} = await this.heroku.get<Heroku.Account>('/account')
     const email = account.email || ''
 
