e10a34b8b10f6d0a8384683e1de4e0406855ab1c
Timothy Lowrimore <tlowrimore@heroku.com>
2025-12-16 15:32:45 -0700

    trying different characters for table divider

--- packages/cli/test/helpers/utils/normalizeTableOutput.ts
+++ packages/cli/test/helpers/utils/normalizeTableOutput.ts
@@ -2,10 +2,14 @@ import stripAnsi from 'strip-ansi'
 
 export default function normalizeTableOutput(output: string): string {
   return stripAnsi(output)
-    .normalize('NFC')
+    .normalize('NFKC')
     .toLowerCase()
     .split('\n')
     .map(line => line.replaceAll(/\s+/g, ' ').trim())
-    .filter(line => line && !line.match(/^\s*(?:[─]+\s*)+$/))
+
+    // Note, there are 2 types of dashes in this regex: one from
+    // the US keyboard of a Mac, and the other from the oclif/table
+    // repo for the table layout we're using.
+    .filter(line => line && !line.match(/^\s*(?:[-─]+\s*)+$/))
     .join('\n')
 }
