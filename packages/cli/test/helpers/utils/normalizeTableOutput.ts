d6e5e039d8f8b537afb6f2032f09ee1c04cd7984
Timothy Lowrimore <tlowrimore@heroku.com>
2025-12-16 15:10:37 -0700

    using string unicode normalization in table output normalization function

--- packages/cli/test/helpers/utils/normalizeTableOutput.ts
+++ packages/cli/test/helpers/utils/normalizeTableOutput.ts
@@ -2,6 +2,7 @@ import stripAnsi from 'strip-ansi'
 
 export default function normalizeTableOutput(output: string): string {
   return stripAnsi(output)
+    .normalize('NFC')
     .toLowerCase()
     .split('\n')
     .map(line => line.replaceAll(/\s+/g, ' ').trim())
