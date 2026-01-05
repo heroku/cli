966fc5c1980199fcc2a90c24953032b885df704a
Eric Black <eblack@salesforce.com>
2026-01-05 15:47:38 -0800

    test: migrate index.unit.test.ts to @oclif/test v4
    - Convert from v2 chaining API to v4 runCommand API
    - All 3 tests passing

---
+++ packages/cli/test/unit/commands/sessions/index.unit.test.ts
@@ -0,0 +1,59 @@
+import {runCommand} from '@oclif/test'
+import {expect} from 'chai'
+import nock from 'nock'
+import removeAllWhitespace from '../../../helpers/utils/remove-whitespaces.js'
+
+describe('sessions:index', function () {
+  const exampleSession1 = {
+    description: 'B Session @ 166.176.184.223',
+    id: 'aBcD1234-129f-42d2-854b-dEf123abc123',
+  }
+  const exampleSession2 = {
+    description: 'A Session @ 166.176.184.223',
+    id: 'f6e8d969-129f-42d2-854b-c2eca9d5a42e',
+  }
+
+  afterEach(() => nock.cleanAll())
+
+  it('lists the sessions alphabetically by description', async () => {
+    nock('https://api.heroku.com:443')
+      .get('/oauth/sessions')
+      .reply(200, [exampleSession1, exampleSession2])
+
+    const {stdout} = await runCommand(['sessions'])
+
+    const actual = removeAllWhitespace(stdout)
+    const expected = removeAllWhitespace(
+      ' A Session @ 166.176.184.223 f6e8d969-129f-42d2-854b-c2eca9d5a42e \n'
+      + ' B Session @ 166.176.184.223 aBcD1234-129f-42d2-854b-dEf123abc123 \n',
+    )
+    expect(actual).to.include(expected)
+  })
+
+  context('with json flag', function () {
+    it('lists the sessions alphabetically as json', async () => {
+      nock('https://api.heroku.com:443')
+        .get('/oauth/sessions')
+        .reply(200, [exampleSession1, exampleSession2])
+
+      const {stdout} = await runCommand(['sessions', '--json'])
+
+      const sessionJSON = JSON.parse(stdout)
+
+      expect(sessionJSON[0]).to.eql(exampleSession2)
+      expect(sessionJSON[1]).to.eql(exampleSession1)
+    })
+  })
+
+  context('without sessions', function () {
+    it('shows no sessions message', async () => {
+      nock('https://api.heroku.com:443')
+        .get('/oauth/sessions')
+        .reply(200, [])
+
+      const {stdout} = await runCommand(['sessions'])
+
+      expect(stdout).to.equal('No OAuth sessions.\n')
+    })
+  })
+})
