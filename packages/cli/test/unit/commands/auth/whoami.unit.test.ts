fe1f7a5757fc528c6d66f000cc3ffc62430dafb1
Eric Black <eblack@salesforce.com>
2026-01-05 15:32:37 -0800

    test: migrate auth/whoami to @oclif/test v4
    - Convert from v2 chaining API to v4 runCommand API
    - Handle error exit code with error?.oclif?.exit
    - All 2 tests passing

---
+++ packages/cli/test/unit/commands/auth/whoami.unit.test.ts
@@ -0,0 +1,35 @@
+import {runCommand} from '@oclif/test'
+import {expect} from 'chai'
+import nock from 'nock'
+
+describe('auth:whoami', function () {
+  afterEach(function () {
+    delete process.env.HEROKU_API_KEY
+    nock.cleanAll()
+  })
+  it('shows user email when logged in', async () => {
+    process.env.HEROKU_API_KEY = 'foobar'
+
+    nock('https://api.heroku.com')
+      .get('/account')
+      .reply(200, {email: 'gandalf@example.com'})
+
+    const {stdout, stderr} = await runCommand(['auth:whoami'])
+
+    expect(stdout).to.equal('gandalf@example.com\n')
+    expect(stderr).to.contain('Warning: HEROKU_API_KEY is set')
+  })
+
+  it('exits with status 100 when not logged in', async () => {
+    process.env.HEROKU_API_KEY = 'foobar'
+
+    nock('https://api.heroku.com')
+      .get('/account')
+      .reply(401)
+
+    const {error, stderr} = await runCommand(['auth:whoami'])
+
+    expect(error?.oclif?.exit).to.equal(100)
+    expect(stderr).to.contain('Warning: HEROKU_API_KEY is set')
+  })
+})
