8f526fa280f7e1881d235147db2079cf38e24976
Eric Black <eblack@salesforce.com>
2026-01-06 11:00:41 -0800

    test: migrate members/remove to @oclif/test v4

---
+++ packages/cli/test/unit/commands/members/remove.unit.test.ts
@@ -0,0 +1,80 @@
+import {runCommand} from '@oclif/test'
+import {expect} from 'chai'
+import nock from 'nock'
+
+describe('heroku members:remove', function () {
+  afterEach(() => nock.cleanAll())
+
+  context('from an org', function () {
+    it('removes a member from an org', async () => {
+      nock('https://api.heroku.com')
+        .get('/teams/myteam')
+        .reply(200, {name: 'myteam', role: 'admin', type: 'enterprise'})
+        .delete('/teams/myteam/members/foo%40foo.com')
+        .reply(200)
+
+      const {stderr} = await runCommand(['members:remove', '--team', 'myteam', 'foo@foo.com'])
+
+      expect(stderr).to.contain('Removing foo@foo.com from myteam')
+    })
+  })
+  context('from a team', function () {
+    context('without the feature flag team-invite-acceptance', function () {
+      it('removes a member from an org', async () => {
+        nock('https://api.heroku.com')
+          .get('/teams/myteam')
+          .reply(200, {name: 'myteam', role: 'admin', type: 'team'})
+          .get('/teams/myteam/features')
+          .reply(200, [])
+          .delete('/teams/myteam/members/foo%40foo.com')
+          .reply(200)
+
+        const {stderr} = await runCommand(['members:remove', '--team', 'myteam', 'foo@foo.com'])
+
+        expect(stderr).to.contain('Removing foo@foo.com from myteam')
+      })
+    })
+
+    context('with the feature flag team-invite-acceptance', function () {
+      context('with no pending invites', function () {
+        it('removes a member', async () => {
+          nock('https://api.heroku.com')
+            .get('/teams/myteam')
+            .reply(200, {name: 'myteam', role: 'admin', type: 'team'})
+            .get('/teams/myteam/features')
+            .reply(200, [{enabled: true, name: 'team-invite-acceptance'}])
+            .get('/teams/myteam/invitations')
+            .reply(200, [])
+            .delete('/teams/myteam/members/foo%40foo.com')
+            .reply(200)
+
+          const {stderr} = await runCommand(['members:remove', '--team', 'myteam', 'foo@foo.com'])
+
+          expect(stderr).to.contain('Removing foo@foo.com from myteam')
+        })
+      })
+
+      context('with pending invites', function () {
+        it('revokes the invite', async () => {
+          nock('https://api.heroku.com')
+            .get('/teams/myteam')
+            .reply(200, {name: 'myteam', role: 'admin', type: 'team'})
+            .get('/teams/myteam/features')
+            .reply(200, [{enabled: true, name: 'team-invite-acceptance'}])
+            .get('/teams/myteam/invitations')
+            .reply(200, [{
+              invited_by: {email: 'invite@foo.com'},
+              role: 'member',
+              user: {email: 'foo@foo.com'},
+            }])
+            .delete('/teams/myteam/invitations/foo@foo.com')
+            .reply(200, {})
+
+          const {stderr} = await runCommand(['members:remove', '--team', 'myteam', 'foo@foo.com'])
+
+          expect(stderr).to.contain('Revoking invite for foo@foo.com in myteam')
+        })
+      })
+    })
+  })
+})
