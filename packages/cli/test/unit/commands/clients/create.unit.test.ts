8dce2f0741c7e86540083a1300aa3609af849d14
Eric Black <eblack@salesforce.com>
2026-01-07 12:32:19 -0800

    test: migrate clients/create to @oclif/test v4

---
+++ packages/cli/test/unit/commands/clients/create.unit.test.ts
@@ -0,0 +1,46 @@
+import {expect} from 'chai'
+import {runCommand} from '@oclif/test'
+import nock from 'nock'
+
+describe('clients:create', function () {
+  const createResponse = {
+    name: 'awesome',
+    id: 'f6e8d969-129f-42d2-854b-c2eca9d5a42e',
+    redirect_uri: 'https://myapp.com',
+    secret: 'clientsecret',
+  }
+
+  afterEach(function () {
+    nock.cleanAll()
+  })
+
+  it('creates the client and outputs id and secret', async function () {
+    nock('https://api.heroku.com:443')
+      .post('/oauth/clients', {
+        name: 'awesome',
+        redirect_uri: 'https://myapp.com',
+      })
+      .reply(201, createResponse)
+
+    const {stdout, stderr} = await runCommand(['clients:create', 'awesome', 'https://myapp.com'])
+
+    expect(stdout).to.equal(
+      'HEROKU_OAUTH_ID=f6e8d969-129f-42d2-854b-c2eca9d5a42e\nHEROKU_OAUTH_SECRET=clientsecret\n',
+    )
+    expect(stderr).to.contain('Creating awesome... done\n')
+  })
+
+  it('creates the client and outputs json', async function () {
+    nock('https://api.heroku.com:443')
+      .post('/oauth/clients', {
+        name: 'awesome',
+        redirect_uri: 'https://myapp.com',
+      })
+      .reply(201, createResponse)
+
+    const {stdout, stderr} = await runCommand(['clients:create', 'awesome', 'https://myapp.com', '--json'])
+
+    expect(JSON.parse(stdout)).to.contain(createResponse)
+    expect(stderr).to.contain('Creating awesome... done\n')
+  })
+})
