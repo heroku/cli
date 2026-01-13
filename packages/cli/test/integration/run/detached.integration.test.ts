a996f4056e81b2d65fa39260738552b0f4980079
Eric Black <eblack@salesforce.com>
2026-01-13 11:02:33 -0800

    convert and skip commented out old oclf/test tests

--- packages/cli/test/integration/run/detached.integration.test.ts
+++ packages/cli/test/integration/run/detached.integration.test.ts
@@ -1,13 +1,9 @@
+import {runCommand} from '@oclif/test'
 import {expect} from 'chai'
 
-/*
 describe('run:detached', function () {
-  test
-    .stdout()
-    .command(['run:detached', '--app=heroku-cli-ci-smoke-test-app', 'echo 1 2 3'])
-    .it('runs a command', ctx => {
-      expect(ctx.stdout).to.include('Run heroku logs --app heroku-cli-ci-smoke-test-app --dyno')
-    })
+  it.skip('runs a command', async function () {
+    const {stdout} = await runCommand(['run:detached', '--app=heroku-cli-ci-smoke-test-app', 'echo', '1', '2', '3'])
+    expect(stdout).to.include('Run heroku logs --app heroku-cli-ci-smoke-test-app --dyno')
+  })
 })
-
-*/
