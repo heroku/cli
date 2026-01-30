ce4b52094c157f7f995faa5ea0d0e5a8a138da4a
Eric Black <eblack@salesforce.com>
2026-01-29 17:21:45 -0800

    Update s3bucket script to look for s3 link in oclif config file

--- scripts/utils/getHerokuS3Bucket.js
+++ scripts/utils/getHerokuS3Bucket.js
@@ -1,12 +1,13 @@
 const path = require('path')
 
-const getHerokuS3Bucket = () => {
-  const pjsonPath = path.join(__dirname, '..', '..', 'packages', 'cli', 'package.json')
-  const pjson = require(pjsonPath)
-  const bucket = pjson.oclif?.update?.s3?.bucket
+const getHerokuS3Bucket = async () => {
+  const configPath = path.join(__dirname, '..', '..', 'packages', 'cli', 'oclif.config.mjs')
+  // Use dynamic import to load ESM module from CommonJS
+  const config = await import('file://' + configPath)
+  const bucket = config.default?.update?.s3?.bucket
 
   if (!bucket) {
-    throw new Error('S3 bucket missing from packages/cli/package.json')
+    throw new Error('S3 bucket missing from packages/cli/oclif.config.mjs')
   }
 
   return bucket
