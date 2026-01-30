bbb1949df756539753bfb7d28563aa56e8d9a6bf
Eric <eblack@salesforce.com>
2026-01-30 15:34:48 -0800

    build: switch to commit-and-tag-version and update test config (#3496)
    * switch to commit-and-tag-version in create-release-pr
    
    * update test config to resolve color and update how we register ts-node/esm
    
    * add script to npm install and run oclif readme
    
    * fix yarn -> npm issue
    
    * explicitly build before packing.
    
    * add minimal oclif config to package.json
    
    * try changing directory so oclif can find oclif.mjs. undo direct oclif settings in package.json
    
    * Update s3bucket script to look for s3 link in oclif config file
    
    * undo probably unnecessary change to pack-upload. explicitly declare conventional commit types.
    
    * remove other instance of unnecessary pack-and-upload change

--- scripts/postrelease/install_scripts.js
+++ scripts/postrelease/install_scripts.js
@@ -17,7 +17,7 @@ qq.run(async () => {
   const {GITHUB_REF_TYPE, GITHUB_REF_NAME} = process.env
 
   if (isStableRelease(GITHUB_REF_TYPE, GITHUB_REF_NAME)) {
-    const HEROKU_S3_BUCKET = getHerokuS3Bucket()
+    const HEROKU_S3_BUCKET = await getHerokuS3Bucket()
     await execa.command(`aws s3 cp --content-type text/plain --cache-control max-age=604800 ./install-standalone.sh s3://${HEROKU_S3_BUCKET}/install-standalone.sh`, opts)
     await execa.command(`aws s3 cp --content-type text/plain --cache-control max-age=604800 ./install-standalone.sh s3://${HEROKU_S3_BUCKET}/install.sh`, opts)
     await execa.command(`aws s3 cp --content-type text/plain --cache-control max-age=604800 ./install-ubuntu.sh s3://${HEROKU_S3_BUCKET}/install-ubuntu.sh`, opts)
