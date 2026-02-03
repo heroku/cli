5909845d41364370cd7e7388a1e40ed1c0a3398b
Santiago Bosio <sbosio@salesforce.com>
2026-02-03 16:22:07 -0300

    Migrating command 'data:pg:docs'

---
+++ src/commands/data/pg/docs.ts
@@ -0,0 +1,20 @@
+import {hux} from '@heroku/heroku-cli-util'
+import {flags} from '@heroku-cli/command'
+
+import BaseCommand from '../../../lib/data/baseCommand.js'
+
+export default class DataPgDocs extends BaseCommand {
+  static defaultUrl = 'https://devcenter.heroku.com/categories/heroku-postgres'
+  static description = 'open documentation for Heroku Postgres in your web browser'
+  static flags = {
+    browser: flags.string({description: 'browser to open docs with (example: "firefox", "safari")'}),
+  }
+
+  public async run(): Promise<void> {
+    const {flags} = await this.parse(DataPgDocs)
+    const {browser} = flags
+    const url = DataPgDocs.defaultUrl
+
+    await hux.openUrl(url, browser, 'view the documentation')
+  }
+}
