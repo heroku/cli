f1e47709ad19069756e1a07ce2b6c9102ff023ea
Eric Black <eblack@salesforce.com>
2026-02-04 16:18:54 -0800

    move config back into package.json

--- scripts/utils/getHerokuS3Bucket.js
+++ scripts/utils/getHerokuS3Bucket.js
@@ -1,17 +1,17 @@
 import path from 'path'
 import { fileURLToPath } from 'url'
+import fs from 'fs'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const getHerokuS3Bucket = async () => {
-  const configPath = path.join(__dirname, '..', '..', 'oclif.config.mjs')
-  // Use dynamic import to load ESM module
-  const config = await import('file://' + configPath)
-  const bucket = config.default?.update?.s3?.bucket
+  const packageJsonPath = path.join(__dirname, '..', '..', 'package.json')
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'))
+  const bucket = packageJson.oclif?.update?.s3?.bucket
 
   if (!bucket) {
-    throw new Error('S3 bucket missing from oclif.config.mjs')
+    throw new Error('S3 bucket missing from package.json oclif.update.s3.bucket')
   }
 
   return bucket
