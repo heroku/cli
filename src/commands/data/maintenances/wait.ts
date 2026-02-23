1d15938a4caf8a503d329760a01d6ba57508a390
Katy Bowman <katy.bowman@salesforce.com>
2026-02-23 13:35:14 -0500

    feat: add data:maintenances:wait

---
+++ src/commands/data/maintenances/wait.ts
@@ -0,0 +1,49 @@
+import {color, utils} from '@heroku/heroku-cli-util'
+import {flags as Flags} from '@heroku-cli/command'
+import {Args, ux} from '@oclif/core'
+
+import BaseCommand from '../../../lib/data/baseCommand.js'
+import {Maintenance, MaintenanceStatus} from '../../../lib/data/types.js'
+import {waitUntilMaintenanceComplete} from '../../../lib/data/utils.js'
+
+export default class DataMaintenancesWait extends BaseCommand {
+  static args = {
+    addon: Args.string({description: 'data addon', required: true}),
+  }
+
+  static description = 'blocks until the maintenance process has completed'
+
+  static examples = [
+    '$ heroku data:maintenances:wait postgresql-sinuous-83720',
+    '$ heroku data:maintenances:wait DATABASE --app production-app',
+  ]
+
+  static flags = {
+    app: Flags.app(),
+    remote: Flags.remote(),
+  }
+
+  async run() {
+    const {args, flags} = await this.parse(DataMaintenancesWait)
+    const addonResolver = new utils.AddonResolver(this.heroku)
+    const addon = await addonResolver.resolve(args.addon, flags.app, utils.pg.addonService())
+
+    const isEssentialTier = utils.pg.isEssentialDatabase(addon) || utils.pg.isLegacyEssentialDatabase(addon)
+    if (isEssentialTier) {
+      this.error('maintenance is only available for production databases')
+    }
+
+    const {body: maintenance} = await this.dataApi.get<Maintenance>(
+      `/data/maintenances/v1/${addon.id}`,
+      this.dataApi.defaults,
+    )
+
+    if (maintenance.status !== MaintenanceStatus.running) {
+      this.error(`There currently isn't any maintenance in progress for ${color.addon(addon.name!)}`)
+    }
+
+    ux.action.start(`Waiting for maintenance on ${color.addon(addon.name!)} to complete`)
+    await waitUntilMaintenanceComplete(addon.id!, this.dataApi)
+    ux.action.stop('maintenance completed')
+  }
+}
---
+++ test/unit/commands/data/maintenances/wait.unit.test.ts
@@ -0,0 +1,99 @@
+import {runCommand} from '@oclif/test'
+import {expect} from 'chai'
+import nock from 'nock'
+
+import {Maintenance, MaintenanceStatus} from '../../../../../src/lib/data/types.js'
+import {maintenance} from '../../../../fixtures/data/maintenances/fixtures.js'
+import {addon} from '../../../../fixtures/data/pg/fixtures.js'
+
+const completedMaintenance: Maintenance = {
+  ...maintenance,
+  status: MaintenanceStatus.completed,
+}
+
+const runningMaintenance: Maintenance = {
+  ...maintenance,
+  status: MaintenanceStatus.running,
+}
+
+describe('data:maintenances:wait', function () {
+  let herokuApi: nock.Scope
+  let dataApi: nock.Scope
+
+  beforeEach(function () {
+    herokuApi = nock('https://api.heroku.com')
+    dataApi = nock('https://api.data.heroku.com')
+  })
+
+  afterEach(function () {
+    herokuApi.done()
+    dataApi.done()
+    nock.cleanAll()
+  })
+
+  it('waits until maintenance is complete', async function () {
+    herokuApi
+      .post('/actions/addons/resolve')
+      .reply(200, [addon])
+
+    let pollingCalls = 0
+    dataApi
+      .get(`/data/maintenances/v1/${addon.id}`)
+      .reply(200, runningMaintenance)
+    dataApi
+      .get(`/data/maintenances/v1/${addon.id}`)
+      .thrice()
+      .reply(() => {
+        pollingCalls++
+
+        return pollingCalls === 3
+          ? [200, completedMaintenance]
+          : [200, runningMaintenance]
+      })
+
+    const {stderr} = await runCommand(['data:maintenances:wait', addon.name])
+
+    expect(stderr).to.contain(`Waiting for maintenance on ${addon.name} to complete`)
+    expect(stderr).to.contain('maintenance completed')
+  })
+
+  it('waits until maintenance is complete scoped by optional app flag', async function () {
+    herokuApi
+      .post('/actions/addons/resolve')
+      .reply(200, [addon])
+
+    let pollingCalls = 0
+    dataApi
+      .get(`/data/maintenances/v1/${addon.id}`)
+      .reply(200, runningMaintenance)
+    dataApi
+      .get(`/data/maintenances/v1/${addon.id}`)
+      .thrice()
+      .reply(() => {
+        pollingCalls++
+
+        return pollingCalls === 3
+          ? [200, completedMaintenance]
+          : [200, runningMaintenance]
+      })
+
+    const {stderr} = await runCommand(['data:maintenances:wait', addon.name, `--app=${addon.app.name}`])
+
+    expect(stderr).to.contain(`Waiting for maintenance on ${addon.name} to complete`)
+    expect(stderr).to.contain('maintenance completed')
+  })
+
+  it('shows error if initial maintenance state is not running', async function () {
+    herokuApi
+      .post('/actions/addons/resolve')
+      .reply(200, [addon])
+
+    dataApi
+      .get(`/data/maintenances/v1/${addon.id}`)
+      .reply(200, completedMaintenance)
+
+    const {error} = await runCommand(['data:maintenances:wait', addon.name])
+
+    expect(error?.message).to.equal(`There currently isn't any maintenance in progress for ${addon.name}`)
+  })
+})
