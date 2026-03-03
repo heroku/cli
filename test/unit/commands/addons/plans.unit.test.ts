1dd6bfbbdb8cf60a0f116c90647c14d4cca2e5e3
Timothy Lowrimore <tlowrimore@salesforce.com>
2026-03-03 16:51:19 -0700

    removeds 'default' header from addon plans table

--- test/unit/commands/addons/plans.unit.test.ts
+++ test/unit/commands/addons/plans.unit.test.ts
@@ -4,7 +4,7 @@ import Cmd from '../../../../src/commands/addons/plans.js'
 import runCommand from '../../../helpers/runCommand.js'
 import nock from 'nock'
 import * as fixtures from '../../../fixtures/addons/fixtures.js'
-import removeAllWhitespace from '../../../helpers/utils/remove-whitespaces.js'
+import normalizeTableOutput from '../../../helpers/utils/normalizeTableOutput.js'
 
 describe('addons:plans', function () {
   context('with non-metered plans', function () {
@@ -22,16 +22,20 @@ describe('addons:plans', function () {
 
     it('shows add-on plans', async function () {
       await runCommand(Cmd, ['daservice'])
-      const actual = removeAllWhitespace(stdout.output)
-      const expectedHeader = removeAllWhitespace(`
+      const [header, body] = stdout.output.split(/\s─+\s/gm)
+      const actualHeader  = normalizeTableOutput(header)
+      const actualBody    = normalizeTableOutput(body)
+
+      const expectedHeader = normalizeTableOutput(`
                 Slug                         Name       Price        Max Price`)
-      expect(actual).to.include(expectedHeader)
-      const expected = removeAllWhitespace(`
+      expect(actualHeader).to.eq(expectedHeader)
+
+      const expectedBody = normalizeTableOutput(`
         default heroku-postgresql:mini       Mini       ~$0.007/hour $5/month
                 heroku-postgresql:standard-2 Standard 2 ~$0.278/hour $200/month
                 heroku-postgresql:premium-3  Premium 3  ~$1.042/hour $750/month
                 heroku-postgresql:private-4  Private 4  ~$2.083/hour $1500/month`)
-      expect(actual).to.include(expected)
+      expect(actualBody).to.eq(expectedBody)
     })
   })
 
@@ -49,11 +53,11 @@ describe('addons:plans', function () {
 
     it('formats price for metered usage plans', async function () {
       await runCommand(Cmd, ['metered-service'])
-      const actual = removeAllWhitespace(stdout.output)
-      const expectedHeader = removeAllWhitespace(`
+      const actual = normalizeTableOutput(stdout.output)
+      const expectedHeader = normalizeTableOutput(`
                 Slug                    Name   Price   Max Price`)
       expect(actual).to.include(expectedHeader)
-      const expected = removeAllWhitespace(`
+      const expected = normalizeTableOutput(`
         default heroku-inference:plan-1 Plan 1 metered https://elements.heroku.com/addons/metered-service#pricing
                 heroku-inference:plan-2 Plan 2 metered https://elements.heroku.com/addons/metered-service#pricing
                 heroku-inference:plan-3 Plan 3 metered https://elements.heroku.com/addons/metered-service#pricing`)
