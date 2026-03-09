a14e82b42a3a96edb983d4e3671409a6eb34660f
Michael Malave <michael.malave@salesforce.com>
2026-03-09 15:01:45 -0700

    updating the unit test for dev center article

--- test/unit/commands/pipelines/info.unit.test.ts
+++ test/unit/commands/pipelines/info.unit.test.ts
@@ -103,12 +103,11 @@ describe('pipelines:info', function () {
       `Warning: Some apps in this pipeline do not belong to ${owner}.`,
       'All apps in a pipeline must have the same owner as the pipeline owner.',
       'Transfer these apps or change the pipeline owner in pipeline settings.',
-      'See https://devcenter.heroku.com/articles/pipelines#pipelines-ownership-and-transfer',
-      'for more info.',
+      'See https://devcenter.heroku.com/articles/pipelines#pipelines-ownership-and-transfer for more info.',
     ]
-
+    const normalizedStderr = stderr.replace(/\s+/g, ' ')
     warningMessage.forEach(message => {
-      expect(stderr).to.contain(message)
+      expect(normalizedStderr).to.contain(message)
     })
   }
 
