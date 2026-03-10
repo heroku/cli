73d6b4bc4ec475359dac4fbfa214ead19f09c919
Santiago Bosio <sbosio@salesforce.com>
2026-03-10 18:00:26 -0300

    Adds command 'data:pg:levels'

---
+++ src/commands/data/pg/levels.ts
@@ -0,0 +1,40 @@
+import {hux} from '@heroku/heroku-cli-util'
+import {Command} from '@heroku-cli/command'
+import {ux} from '@oclif/core'
+import {printTable, TableOptions} from '@oclif/table'
+
+import BaseCommand from '../../../lib/data/baseCommand.js'
+import {PostgresLevelInfo, PostgresLevelsResponse} from '../../../lib/data/types.js'
+
+export default class DataPgLevels extends BaseCommand {
+  static baseFlags = Command.baseFlagsWithoutPrompt()
+  static description = 'show available levels for Heroku Postgres Advanced databases'
+  static promptFlagActive = false
+
+  async run(): Promise<void> {
+    const {body: {items: levels}} = await this.dataApi.get<PostgresLevelsResponse>('/data/postgres/v1/levels/advanced')
+
+    ux.stdout()
+    hux.styledHeader('Available levels for Heroku Postgres Advanced databases')
+
+    const defaultStyle = {
+      borderColor: 'whiteBright',
+      borderStyle: 'headers-only-with-underline',
+      headerOptions: {
+        bold: true,
+        color: 'white',
+      },
+    } as Partial<TableOptions<PostgresLevelInfo>>
+
+    printTable<PostgresLevelInfo>({
+      columns: [
+        {key: 'name', name: 'Name'},
+        {key: 'vcpu', name: 'vCPU', horizontalAlignment: 'right'},
+        {key: 'memory_in_gb', name: 'Memory (GB)', horizontalAlignment: 'right'},
+        {key: 'connection_limit', name: 'Max Connections', horizontalAlignment: 'right'},
+      ],
+      data: levels,
+      ...defaultStyle,
+    })
+  }
+}
