102cb5d2736d8a5176e5a2fe585fe98383cff3bf
Katy Bowman <katy.bowman@salesforce.com>
2026-03-11 11:19:56 -0400

    feat: add ps:socks command

---
+++ src/commands/ps/socks.ts
@@ -0,0 +1,61 @@
+import {color} from '@heroku/heroku-cli-util'
+import {Command, flags} from '@heroku-cli/command'
+import * as Heroku from '@heroku-cli/schema'
+import {ux} from '@oclif/core'
+import tsheredoc from 'tsheredoc'
+
+import {HerokuExec} from '../../lib/ps-exec/exec.js'
+
+const heredoc = tsheredoc.default
+
+export default class Socks extends Command {
+  static description = 'Launch a SOCKS proxy into a dyno'
+
+  static examples = [heredoc`
+    $ ${color.command('heroku ps:socks')} --app murmuring-headland-14719
+    Establishing credentials... done
+    SOCKSv5 proxy server started on port 1080
+    Use CTRL+C to stop the proxy
+  `]
+
+  static flags = {
+    app: flags.app({required: true}),
+    dyno: flags.string({
+      char: 'd',
+      description: 'specify the dyno to connect to',
+    }),
+    remote: flags.remote(),
+  }
+
+  static topic = 'ps'
+
+  public async run(): Promise<void> {
+    const {flags} = await this.parse(Socks)
+    const {app, dyno} = flags
+
+    const context = {
+      app,
+      auth: {password: this.heroku.auth},
+      flags: {dyno},
+    }
+
+    const exec = new HerokuExec()
+
+    await exec.initFeature(context, this.heroku, async (configVars: Heroku.ConfigVars) => {
+      exec.createSocksProxy(context, this.heroku, configVars)
+    }, 'socks')
+
+    // Keep the process running until interrupted
+    await new Promise<void>(resolve => {
+      process.on('SIGINT', () => {
+        ux.stdout('Stopping SOCKS proxy...')
+        resolve()
+      })
+
+      process.on('SIGTERM', () => {
+        ux.stdout('Stopping SOCKS proxy...')
+        resolve()
+      })
+    })
+  }
+}
