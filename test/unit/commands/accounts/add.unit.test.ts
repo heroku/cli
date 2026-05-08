49e3938f44ec52451870cf24c692d440a7a69701
Erika Wallace <erika.wallace@salesforce.com>
2026-05-08 19:37:45 -0400

    feat: update accounts:add to use the credential manager (#3699)
    * update accounts:add to use the credential manager
    
    * check for useNetrc
    
    * revert

--- test/unit/commands/accounts/add.unit.test.ts
+++ test/unit/commands/accounts/add.unit.test.ts
@@ -1,22 +1,24 @@
 import {expect} from 'chai'
+import {APIClient} from '@heroku-cli/command'
 import runCommand from '../../../helpers/runCommand.js'
 import nock from 'nock'
 import sinon from 'sinon'
 import Cmd from '../../../../src/commands/accounts/add.js'
 import AccountsModule from '../../../../src/lib/accounts/accounts.js'
+import {stubCredentialManager} from '../../../helpers/credential-manager-stub.js'
 
 describe('accounts:add', function () {
   let api: nock.Scope
   let addStub: sinon.SinonStub
+  let listStub: sinon.SinonStub
 
   beforeEach(function () {
-    sinon.stub(AccountsModule, 'list').resolves([])
+    listStub = sinon.stub(AccountsModule, 'list').resolves([])
     addStub = sinon.stub(AccountsModule, 'add')
     api = nock('https://api.heroku.com')
   })
 
   afterEach(function () {
-    delete process.env.HEROKU_API_KEY
     sinon.restore()
     api.done()
     nock.cleanAll()
@@ -24,7 +26,10 @@ describe('accounts:add', function () {
 
   describe('when the user is logged in', function () {
     it('should call the accounts.add function with the account name, user email, and auth token', async function () {
-      process.env.HEROKU_API_KEY = 'testHerokuAPIKey'
+      stubCredentialManager({
+        getAuth: async () => ({account: 'testEmail', token: 'testHerokuAPIKey'}),
+      })
+
       api.get('/account')
         .reply(200, {email: 'testEmail'})
 
@@ -35,26 +40,37 @@ describe('accounts:add', function () {
       expect(addStub.args[0][2]).to.equal('testHerokuAPIKey')
     })
 
-    it('should prompt the user to log in if the user does not have an auth token', async function () {
-      process.env.HEROKU_API_KEY = ''
+    it('should prompt the user to log in if the user is not logged in', async function () {
+      stubCredentialManager({
+        getAuth: async () => ({account: undefined, token: undefined}),
+      })
+
+      const APIClientStub = sinon.stub(APIClient.prototype, 'login')
+      APIClientStub.resolves()
+
+      api.get('/account')
+        .reply(401, {id: 'unauthorized', message: 'Unauthorized'})
+
       api.get('/account')
         .reply(200, {email: 'testEmail'})
 
       await runCommand(Cmd, ['testAccountName'])
-        .catch(error =>  {
-          expect(error.message).to.equal('You must be logged in to run this command.')
-        })
+
+      expect(APIClientStub.calledOnce).to.equal(true)
     })
 
-    it('should prompt the user to log in if the user does not have an email', async function () {
-      process.env.HEROKU_API_KEY = 'testHerokuAPIKey'
-      api.get('/account')
-        .reply(200, {})
+    it('should error if the account name already exists', async function () {
+      stubCredentialManager({
+        getAuth: async () => ({account: 'testEmail', token: 'testHerokuAPIKey'}),
+      })
 
-      await runCommand(Cmd, ['testAccountName'])
-        .catch(error =>  {
-          expect(error.message).to.equal('You must be logged in to run this command.')
-        })
+      listStub.resolves([{name: 'testAccountName', username: 'testEmail'}])
+
+      try {
+        await runCommand(Cmd, ['testAccountName'])
+      } catch (error: any) {
+        expect((error as Error).message).to.contain('testAccountName already exists')
+      }
     })
   })
 })
