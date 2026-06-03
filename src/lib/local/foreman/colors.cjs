07d06941a6f8224c9c2a0cf2caead4aa7787f01c
Richard Schneeman <rschneeman@salesforce.com>
2026-06-03 10:37:28 -0500

    chore: vendor node-foreman library files into CLI
    Copy the 9 library files from node-foreman v3.0.1 (MIT license) into
    src/lib/local/foreman/ with .cjs extensions and updated internal
    require paths for the .cjs extension. No other changes to the
    upstream source.
    
    Refs #3736

---
+++ src/lib/local/foreman/colors.cjs
@@ -0,0 +1,45 @@
+// Copyright IBM Corp. 2012,2016. All Rights Reserved.
+// Node module: foreman
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+var reset = '\x1B[0m';
+var colors = {
+  magenta: '\x1B[35m',
+  blue: '\x1B[34m',
+  cyan: '\x1B[36m',
+  green: '\x1B[32m',
+  yellow: '\x1B[33m',
+  red: '\x1B[31m',
+  bright_magenta: '\x1B[35m',
+  bright_cyan: '\x1B[36m',
+  bright_blue: '\x1B[34m',
+  bright_green: '\x1B[32m',
+  bright_yellow: '\x1B[33m',
+  bright_red: '\x1B[31m',
+};
+
+function identity(self) {
+  return self;
+}
+
+function colorizer(color) {
+  if (process.stdout.isTTY) {
+    return function (str) {
+      return colors[color] + str + reset;
+    };
+  } else {
+    return identity;
+  }
+}
+
+module.exports.colors = [];
+
+var colorKeys = Object.keys(colors);
+colorKeys.forEach(function(name) {
+  var colorFn = colorizer(name);
+  module.exports[name] = colorFn;
+  module.exports.colors.push(colorFn);
+});
+
+module.exports.colors_max = module.exports.colors.length;
