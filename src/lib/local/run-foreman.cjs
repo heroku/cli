6ad250e677885487a198a53f0f84a56441dce304
Richard Schneeman <rschneeman@salesforce.com>
2026-06-04 16:40:41 -0500

    Remove vendored exporter
    The exporter.cjs depends on mustache, causing an error in CI:
    
    ```
    
    node:internal/modules/cjs/loader:1210
      throw err;
      ^
    
    Error: Cannot find module 'mustache'
    Require stack:
    - /home/runner/work/cli/cli/dist/lib/local/foreman/exporters.cjs
    - /home/runner/work/cli/cli/dist/lib/local/run-foreman.cjs
        at Module._resolveFilename (node:internal/modules/cjs/loader:1207:15)
        at Module._load (node:internal/modules/cjs/loader:1038:27)
        at Module.require (node:internal/modules/cjs/loader:1289:19)
        at require (node:internal/modules/helpers:182:18)
        at Object.<anonymous> (/home/runner/work/cli/cli/dist/lib/local/foreman/exporters.cjs:11:10)
        at Module._compile (node:internal/modules/cjs/loader:1521:14)
        at Module._extensions..js (node:internal/modules/cjs/loader:1623:10)
        at Module.load (node:internal/modules/cjs/loader:1266:32)
        at Module._load (node:internal/modules/cjs/loader:1091:12)
        at Module.require (node:internal/modules/cjs/loader:1289:19) {
      code: 'MODULE_NOT_FOUND',
      requireStack: [
        '/home/runner/work/cli/cli/dist/lib/local/foreman/exporters.cjs',
        '/home/runner/work/cli/cli/dist/lib/local/run-foreman.cjs'
      ]
    }
    ```
    
    The `heroku local` command does not use `export` therefore it doesn't need this module, so we can remove it and the need to have mustache.

--- src/lib/local/run-foreman.cjs
+++ src/lib/local/run-foreman.cjs
@@ -21,12 +21,9 @@
 const program = require('commander')
 const colors = require('./foreman/colors.cjs')
 const events = require('node:events')
-const fs = require('node:fs')
-const path = require('node:path')
 const {quote} = require('shell-quote')
 const display = require('./foreman/console.cjs').Console
 const _envs    = require('./foreman/envs.cjs')
-const exporters = require('./foreman/exporters.cjs')
 const {startForward} = require('./foreman/forward.cjs')
 const _proc = require('./foreman/proc.cjs')
 const _procfile = require('./foreman/procfile.cjs')
@@ -144,145 +141,6 @@ program
     display.trimline = process.stdout.columns - 5
 
     once(input, envs, callback)
-  })
-
-program
-  .command('export [PROCS]')
-  .option('-a, --app  <NAME>', 'export upstart application as NAME', 'foreman')
-  .option('-u, --user <NAME>', 'export upstart user as NAME', 'root')
-  .option('-o, --out  <DIR>', 'export upstart files to DIR', '.')
-  .option('-c, --cwd  <DIR>', 'change current working directory to DIR')
-  .option('-g, --gid  <GID>', 'set gid of upstart config to GID')
-  .option('-l, --log  <DIR>', 'specify upstart log directory', '/var/log')
-  .option('-t, --type <TYPE>', 'export file to TYPE (default upstart)', 'upstart')
-  .option('-m, --template <DIR>', 'use template folder')
-  .description('Export to an upstart job independent of foreman')
-  .action(function (procArgs) {
-    const envs = loadEnvs(program.env)
-
-    const procs = loadProc(program.procfile)
-
-    if (!procs) {
-      return
-    }
-
-    const req  = getreqs(procArgs, procs)
-
-    // Variables for Upstart Template
-    const config = {
-      application: this.app,
-      cwd: path.resolve(process.cwd(), this.cwd || ''),
-      envs,
-      group: this.gid || this.user,
-      logs: this.log,
-      template: this.template,
-      user: this.user,
-    }
-
-    config.envfile = path.resolve(program.env)
-
-    let writeout
-    if (exporters[this.type]) {
-      writeout = exporters[this.type]
-    } else {
-      new display.Error('Unknown Export Format', this.type)
-      process.exit(1)
-    }
-
-    // Check for Upstart User
-    // friendly warning - does not stop export
-    let userExists = false
-    for (const line of fs.readFileSync('/etc/passwd')
-      .toString().split(/\n/)) {
-      if (line.match(/^[^:]*/)[0] === config.user) {
-        userExists = true
-      }
-    }
-
-    if (!userExists) {
-      display.Warn(display.fmt('User %s Does Not Exist on System', config.user))
-    }
-
-    // using port 5006 because it is not known to be used by other common software
-    const baseport = Number.parseInt(program.port || envs.PORT || process.env.PORT || 5006)
-    let baseport_i = 0
-    let baseport_j = 0
-    let envl = []
-
-    config.processes = []
-
-    // This is ugly because of shitty support for array copying
-    // Cleanup is definitely required
-    for (let key in req) {
-      const c = {}
-      const cmd = procs[key]
-
-      if (!cmd) {
-        display.Warn("Required Key '%s' Does Not Exist in Procfile Definition", key)
-        continue
-      }
-
-      const n = req[key]
-
-      config.processes.push({n, process: key})
-      c.process = key
-      c.command = cmd
-
-      for (const _ in config) {
-        c[_] = config[_]
-      }
-
-      c.numbers = []
-      for (let i = 1; i <= n; i++) {
-        const port = (baseport + baseport_i + baseport_j) * 100
-
-        const envl = []
-        for (key in envs) {
-          envl.push({
-            key,
-            value: envs[key],
-          })
-        }
-
-        envl.push({key: 'PORT', value: conf.port})
-        // eslint-disable-next-line unicorn/no-array-push-push
-        envl.push({key: 'FOREMAN_WORKER_NAME', value: conf.process + '.' + conf.number})
-
-        conf.envs = envl
-
-        const conf = {
-          ...c,
-          envs: envl,
-          number: i,
-          port,
-        }
-
-        // Write the APP-PROCESS-N.conf File
-        writeout.foreman_app_n(conf, this.out)
-
-        baseport_i++
-        c.numbers.push({number: i})
-      }
-
-      envl = []
-      for (key in envs) {
-        envl.push({
-          key,
-          value: envs[key],
-        })
-      }
-
-      c.envs = envl
-
-      // Write the APP-Process.conf File
-      writeout.foreman_app(c, this.out)
-
-      baseport_i = 0
-      baseport_j++
-    }
-
-    // Write the APP.conf File
-    writeout.foreman(config, this.out)
   })
 
 program.parse(process.argv)
