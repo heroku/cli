75fad2ab80c9a5d31434fff737d40165c7816ad1
Erika Wallace <erika.wallace@salesforce.com>
2026-07-10 15:23:06 -0400

    preserve error status code

--- src/lib/pipelines/setup/get-repo.ts
+++ src/lib/pipelines/setup/get-repo.ts
@@ -1,5 +1,7 @@
 export default function getRepo(github: any, name: any) {
-  return github.getRepo(name).catch(() => {
-    throw new Error('Couldn\'t access that repo')
+  return github.getRepo(name).catch((err: any) => {
+    const error: any = new Error('Couldn\'t access that repo')
+    error.statusCode = err.statusCode || err.http?.statusCode
+    throw error
   })
 }
