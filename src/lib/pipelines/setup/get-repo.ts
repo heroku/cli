d94dedd2e5b83bab401bf47cd90b9d223d571de8
Erika Wallace <erika.wallace@salesforce.com>
2026-07-10 15:37:39 -0400

    fix linting error

--- src/lib/pipelines/setup/get-repo.ts
+++ src/lib/pipelines/setup/get-repo.ts
@@ -1,7 +1,7 @@
 export default function getRepo(github: any, name: any) {
-  return github.getRepo(name).catch((err: any) => {
-    const error: any = new Error('Couldn\'t access that repo')
-    error.statusCode = err.statusCode || err.http?.statusCode
-    throw error
+  return github.getRepo(name).catch((error: any) => {
+    const err: any = new Error('Couldn\'t access that repo')
+    err.statusCode = error.statusCode || error.http?.statusCode
+    throw err
   })
 }
