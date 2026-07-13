0ba72abf5e79cc96a8773c1f913dad1b8df55df1
Erika Wallace <erika.wallace@salesforce.com>
2026-07-13 17:33:10 -0400

    update clear with sdk extension

--- src/commands/domains/clear.ts
+++ src/commands/domains/clear.ts
@@ -1,6 +1,7 @@
 import {Command, flags} from '@heroku-cli/command'
-import * as Heroku from '@heroku-cli/schema'
 import * as color from '@heroku/heroku-cli-util/color'
+import {HerokuSDK} from '@heroku/sdk'
+import {domainExtensions} from '@heroku/sdk/extensions/platform'
 import {ux} from '@oclif/core/ux'
 
 export default class DomainsClear extends Command {
@@ -15,12 +16,8 @@ export default class DomainsClear extends Command {
     const {flags} = await this.parse(DomainsClear)
 
     ux.action.start(`Removing all domains from ${color.app(flags.app)}`)
-    let {body: domains} = await this.heroku.get<Array<Heroku.Domain>>(`/apps/${flags.app}/domains`)
-    domains = domains.filter((d: Heroku.Domain) => d.kind === 'custom')
-    for (const domain of domains) {
-      await this.heroku.delete(`/apps/${flags.app}/domains/${domain.hostname}`)
-    }
-
+    const {platform} = new HerokuSDK({extensions: [domainExtensions]})
+    await platform.domain.clear(flags.app)
     ux.action.stop()
   }
 }
