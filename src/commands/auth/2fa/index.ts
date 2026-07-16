a92936e47d510b16af50600d7093963359f9003f
Erika Wallace <erika.wallace@salesforce.com>
2026-07-16 16:58:09 -0400

    refactor: use @heroku/sdk for auth/2fa command

--- src/commands/auth/2fa/index.ts
+++ src/commands/auth/2fa/index.ts
@@ -1,18 +1,23 @@
+import type {Account} from '@heroku/types/3.sdk'
+
 import {Command} from '@heroku-cli/command'
-import * as Heroku from '@heroku-cli/schema'
 import * as color from '@heroku/heroku-cli-util/color'
+import {HerokuSDK} from '@heroku/sdk'
 import {ux} from '@oclif/core/ux'
 
 export default class TwoFactor extends Command {
   static aliases = ['2fa', 'twofactor']
   static description = 'check 2fa status'
 
-  async run() {
-    const {body: account} = await this.heroku.get<Heroku.Account>('/account')
+  async run(): Promise<Account> {
+    const {platform} = new HerokuSDK()
+    const account = await platform.account.info()
     if (account.two_factor_authentication) {
       ux.stdout(`Two-factor authentication is ${color.success('enabled')}`)
     } else {
       ux.stdout(`Two-factor authentication is ${color.failure('not enabled')}`)
     }
+
+    return account
   }
 }
