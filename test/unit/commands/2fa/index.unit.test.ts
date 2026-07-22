a341ed340c8659e075027fdf0faa20e59e15c8d9
Erika Wallace <erika.wallace@salesforce.com>
2026-07-22 15:05:51 -0400

    test(auth/2fa): stub @heroku/sdk directly, drop nock (#3822)

--- test/unit/commands/2fa/index.unit.test.ts
+++ test/unit/commands/2fa/index.unit.test.ts
@@ -1,25 +1,34 @@
 import {runCommand} from '@heroku-cli/test-utils'
+import {HerokuSDK} from '@heroku/sdk'
 import {expect} from 'chai'
-import nock from 'nock'
+import {restore, SinonStub, stub} from 'sinon'
 
 import TwoFactorAuth from '../../../../src/commands/auth/2fa/index.js'
 
+type FakePlatform = {
+  account: {info: SinonStub}
+}
+
+function buildFakePlatform(): FakePlatform {
+  return {
+    account: {info: stub()},
+  }
+}
+
 describe('2fa', function () {
-  let api: nock.Scope
+  let fakePlatform: FakePlatform
 
   beforeEach(function () {
-    api = nock('https://api.heroku.com')
+    fakePlatform = buildFakePlatform()
+    stub(HerokuSDK.prototype, 'platform').get(() => fakePlatform)
   })
 
   afterEach(function () {
-    api.done()
-    nock.cleanAll()
+    restore()
   })
 
   it('shows 2fa is enabled', async function () {
-    api
-      .get('/account')
-      .reply(200, {two_factor_authentication: true})
+    fakePlatform.account.info.resolves({two_factor_authentication: true})
 
     const {stdout} = await runCommand(TwoFactorAuth, [])
 
@@ -27,9 +36,7 @@ describe('2fa', function () {
   })
 
   it('shows 2fa is not enabled', async function () {
-    api
-      .get('/account')
-      .reply(200, {two_factor_authentication: false})
+    fakePlatform.account.info.resolves({two_factor_authentication: false})
 
     const {stdout} = await runCommand(TwoFactorAuth, [])
 
