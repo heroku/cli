0069cea0cb350bd8d6b87d591085cb8387f2a6c7
Erika Wallace <erika.wallace@salesforce.com>
2026-08-27 15:56:36 -0400

    refactor(features): use @heroku/sdk (#3890)
    * refactor(features): use @heroku/sdk
    
    * test(features): stub @heroku/sdk directly, drop nock

--- test/unit/commands/features/enable.unit.test.ts
+++ test/unit/commands/features/enable.unit.test.ts
@@ -1,28 +1,42 @@
 import {runCommand} from '@heroku-cli/test-utils'
+import {HerokuSDK} from '@heroku/sdk'
 import ansis from 'ansis'
 import {expect} from 'chai'
-import nock from 'nock'
+import * as sinon from 'sinon'
 
 import FeaturesEnable from '../../../../src/commands/features/enable.js'
 
+type FakePlatform = {
+  appFeature: {
+    info: sinon.SinonStub,
+    update: sinon.SinonStub,
+  },
+}
+
+function buildFakePlatform(): FakePlatform {
+  return {
+    appFeature: {
+      info: sinon.stub(),
+      update: sinon.stub(),
+    },
+  }
+}
+
 describe('features:enable', function () {
-  let api: nock.Scope
+  let fakePlatform: FakePlatform
 
   beforeEach(function () {
-    api = nock('https://api.heroku.com')
+    fakePlatform = buildFakePlatform()
+    sinon.stub(HerokuSDK.prototype, 'platform').get(() => fakePlatform)
   })
 
   afterEach(function () {
-    api.done()
-    nock.cleanAll()
+    sinon.restore()
   })
 
   it('enables an app feature', async function () {
-    api
-      .get('/apps/myapp/features/feature-a')
-      .reply(200, {enabled: false})
-      .patch('/apps/myapp/features/feature-a', {enabled: true})
-      .reply(200)
+    fakePlatform.appFeature.info.resolves({enabled: false})
+    fakePlatform.appFeature.update.resolves({enabled: true})
 
     const {stderr, stdout} = await runCommand(FeaturesEnable, ['feature-a', '--app', 'myapp'])
 
@@ -30,15 +44,16 @@ describe('features:enable', function () {
     expect(stderr).to.contain('Enabling feature-a for')
     expect(stderr).to.contain('myapp')
     expect(stderr).to.contain('done')
+    expect(fakePlatform.appFeature.info.calledOnceWithExactly('myapp', 'feature-a')).to.equal(true)
+    expect(fakePlatform.appFeature.update.calledOnceWithExactly('myapp', 'feature-a', {enabled: true})).to.equal(true)
   })
 
   it('errors if feature is already enabled', async function () {
-    api
-      .get('/apps/myapp/features/feature-a')
-      .reply(200, {enabled: true})
+    fakePlatform.appFeature.info.resolves({enabled: true})
 
     const {error} = await runCommand(FeaturesEnable, ['-a', 'myapp', 'feature-a'])
 
     expect(ansis.strip(error?.message || '')).to.equal('feature-a is already enabled.')
+    expect(fakePlatform.appFeature.update.called).to.equal(false)
   })
 })
