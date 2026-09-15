0b79872def17d9ec3d4c38eb30275399f1400078
Michael Malave <michael.malave@salesforce.com>
2026-09-15 09:37:56 -0700

    fix: send version=3.sdk Accept header on --team authorizations list
    The team-owned authorizations API route (/teams/:team/oauth/authorizations)
    is gated behind the 3.sdk API variant; without the Accept header the request
    404s. Send SDK_HEADER on the --team path only and assert it in the team tests.

--- src/commands/authorizations/index.ts
+++ src/commands/authorizations/index.ts
@@ -3,6 +3,8 @@ import * as Heroku from '@heroku-cli/schema'
 import {color, hux} from '@heroku/heroku-cli-util'
 import {ux} from '@oclif/core/ux'
 
+import {SDK_HEADER} from '../../lib/api.js'
+
 export default class AuthorizationsIndex extends Command {
   static description = 'list OAuth authorizations'
   static examples = [
@@ -19,7 +21,10 @@ export default class AuthorizationsIndex extends Command {
     const endpoint = flags.team
       ? `/teams/${encodeURIComponent(flags.team)}/oauth/authorizations`
       : '/oauth/authorizations'
-    const {body: authorizations} = await this.heroku.get<Array<Heroku.OAuthAuthorization>>(endpoint)
+    const {body: authorizations} = await this.heroku.get<Array<Heroku.OAuthAuthorization>>(
+      endpoint,
+      flags.team ? {headers: {Accept: SDK_HEADER}} : {},
+    )
 
     if (flags.json) {
       hux.styledJSON(authorizations.sort((a, b) => a.description.localeCompare(b.description)))
